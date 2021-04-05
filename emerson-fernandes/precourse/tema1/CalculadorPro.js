737f581ae5f2d4d6463e28450a6c544967c23a33
Emerson Fernandes <emerson_feral@hotmail.com>
2021-04-05 23:37:15 +0200

    Calculdadora add

---
+++ emerson-fernandes/precourse/tema1/CalculadorPro.js
@@ -0,0 +1,15 @@
+function suma(valor1,valor2){
+    
+    var valor = 0
+    
+    for(let num = 0; num < valor1.length;num++){
+        console.log(valor += valor1[num])
+    }
+
+    
+    
+     
+}
+
+suma([5,7,10,56])
+
