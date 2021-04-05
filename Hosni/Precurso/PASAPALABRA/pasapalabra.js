08c875f1828f77f8d79912d144d9c8155d5b5960
Hosni Marco Ruano <hosni.marco@gmail.com>
2021-04-05 19:00:41 +0200

    Add pasapalabra file

---
+++ Hosni/Precurso/PASAPALABRA/pasapalabra.js
@@ -0,0 +1,399 @@
+let questions = [
+    {   
+    letter: "a", 
+    status: 0, 
+    chooseQuestion:[
+        {answer: "abducir",  question: "CON LA A. Dicho de una supuesta criatura extraterrestre: Apoderarse de alguien"},
+        {answer: "anillas", question: "CON LA A. Aparato que se mueve en todas las direcciones."},
+        {answer: "adn", question: "CON LA A. Molécula portadora de la información genética."}
+        ] 
+    },
+    {
+    letter: "b", 
+    status: 0, 
+    chooseQuestion:[
+        {answer: "bingo", question: "CON LA B. Juego que ha sacado de quicio a todos los 'Skylabers' en las sesiones de precurso"},
+        {answer: "biblioteca", question: "CON LA B. Lugar en el que se lleva a cabo el préstamo de libros."},
+        {answer: "barómetro", question: "CON LA B. Aparato para medir la presión atmosférica."}
+    ]
+    },
+    {
+    letter: "c",
+    status: 0,
+    chooseQuestion:[
+        {answer: "churumbel", question: "CON LA C. Niño, crío, bebé"},
+        {answer: "caries", question: "CON LA C. Destrucción del diente por falta de higiene."},
+        {answer: "calleras", question: "CON LA C. Implemento que protege las manos."}
+    ]
+    },
+    {
+    letter: "d",
+    status: 0,
+    chooseQuestion:[
+        {answer: "diarrea", question: "CON LA D. Anormalidad en la función del aparato digestivo caracterizada por frecuentes evacuaciones y su consistencia líquida"},
+        {answer: "dificultad", question: "CON LA D. Valor de los elementos que hace un gimnasta en una competición."},
+        {answer: "densidad", question: "CON LA D. Masa partido por volumen."}
+    ]
+    },
+    {
+    letter: "e",
+    status: 0,
+    chooseQuestion:[
+        {answer: "ectoplasma", question: "CON LA E. Gelatinoso y se encuentra debajo de la membrana plasmática. Los cazafantasmas medían su radiación"},
+        {answer: "enfermedad" , question: "CON LA E. Falta de salud."},
+        {answer: "esguince", question: "CON LA E. Lesión habitual en los gimnastas."}
+    ]
+    },
+    {
+    letter: "f",
+    status: 0,
+    chooseQuestion:[
+        {answer: "facil", question: "CON LA F. Que no requiere gran esfuerzo, capacidad o dificultad"},
+        {answer: "fumar", question: "CON LA F. Hábito perjudicial para el aparato respiratorio."},
+        {answer: "frutero", question: "CON LA F. Recipiente en el que se coloca la fruta."}
+    ]
+    },
+    {
+    letter: "g",
+    status: 0,
+    chooseQuestion:[
+        {answer: "galaxia", question: "CON LA G. Conjunto enorme de estrellas, polvo interestelar, gases y partículas"},
+        {answer: "galletas", question: "CON LA G. En nuestras reuniones no pueden faltar para el momento de la merienda compartida."},
+        {answer: "guitarra", question: "CON LA G. Instrumento musical de seis cuerdas."}
+    ]
+    },
+    {
+    letter: "h",
+    status: 0,
+    chooseQuestion:[
+        {answer: "harakiri", question: "CON LA H. Suicidio ritual japonés por desentrañamiento"},
+        {answer: "hidrógeno", question: "CON LA H. Elemento qiímico de número atómico 1."},
+        {answer: "hospitales", question: "CON LA H.  Lugares importantes, que tienen que ver con el derecho a la sanidad."}
+    ]
+    },
+    {
+    letter: "i",
+    status: 0,
+    chooseQuestion:[
+        {answer: "iglesia", question: "CON LA I. Templo cristiano"},
+        {answer: "infancia", question: "CON LA I. La etapa de la vida en la que estamos los niños y las niñas."},
+        {answer: "invisible" , question: "CON LA I. Añade un prefijo a la palabra visible para obtener lo contrario."}
+    ]
+    },
+    {
+    letter: "j",
+    status: 0,
+    chooseQuestion:[
+        {answer: "jabalí", question: "CON LA J. Variedad salvaje del cerdo que sale en la película 'El Rey León', de nombre Pumba"},
+        {answer: "jugar", question: "CON LA J. Es uno de nuestros derechos favoritos, es importante que todos los niños y niñas se diviertan haciéndolo."},
+        {answer: "jueves", question: "CON LA J. Dia de la semana."}
+    ]
+    },
+    {
+    letter: "k",
+    status: 0,
+    chooseQuestion:[
+        {answer: "kamikaze", question: "CON LA K. Persona que se juega la vida realizando una acción temeraria"},
+        {answer: "koala", question: "CON LA K. Animal parecido a un oso pequeño y que podemos encontrar en Australia."},
+        {answer: "kilogramo", question: "CON LA K. Unidad de medida de masa."}
+    ]
+    },
+    {
+    letter: "l",
+    status: 0,
+    chooseQuestion:[
+        {answer: "licantropo", question: "CON LA L. Hombre lobo"},
+        {answer: "libreria", question: "CON LA L. Lugar en el que se compran libros."},
+        {answer: "lepra", question: "CON LA L. Enfermedad cutánea degenerativa."}
+    ]
+    },
+    {
+    letter: "m",
+    status: 0,
+    chooseQuestion:[
+        {answer: "misantropo", question: "CON LA M. Persona que huye del trato con otras personas o siente gran aversión hacia ellas"},
+        {answer: "medio ambiente", question: "CON LA M. Forma parte de la vida, en el Consejo municipal nos preocupamos de cuidarlo haciendo acciones como el reciclaje."},
+        {answer: "comba", question: "CONTIENE LA M. Juego que consiste en saltar por encima de una cuerda que se hace pasar por debajo de los pies y por encima de la cabeza de quien salta."}
+    ]
+    },
+    {
+    letter: "n",
+    status: 0,
+    chooseQuestion:[
+        {answer: "necedad", question: "CON LA N. Demostración de poca inteligencia"},
+        {answer: "naranja", question: "CON LA N. Del campo semántico de los colores y el nombre de una fruta con la que podemos hacer zumo."},
+        {answer: "núcleo", question: "CON LA N. Compartimento celular donde se encuentra almacenada la información genética."}
+    ]
+    },
+    {
+    letter: "ñ",
+    status: 0,
+    chooseQuestion:[
+        {answer: "señal", question: "CONTIENE LA Ñ. Indicio que permite deducir algo de lo que no se tiene un conocimiento directo."},
+        {answer: "niñez", question: "CONTIENE LA Ñ. Etapa en la que estamos en un momento de la vida anterior a la adolescencia."},
+        {answer: "bañera", question: "CONTIENE LA Ñ. Lugar donde Aquímedes descubrió la ley que lleva su nombre."}
+    ]
+    },
+    {
+    letter: "o",
+    status: 0,
+    chooseQuestion:[
+        {answer: "orco", question: "CON LA O. Humanoide fantástico de apariencia terrible y bestial, piel de color verde creada por el escritor Tolkien"},
+        {answer: "orejón", question: "CON LA O. Persona o animal con el pavellón auditivo muy desarollado."},
+        {answer: "oveja", question: "CON LA O. Animal con abrigo de lana."}
+    ]
+    },
+    {
+    letter: "p",
+    status: 0,
+    chooseQuestion:[
+        {answer: "protoss", question: "CON LA P. Raza ancestral tecnológicamente avanzada que se caracteriza por sus grandes poderes psíonicos del videojuego StarCraft"},
+        {answer: "platino", question: "CON LA P. Elemento químico cuyo símbolo es Pt."},
+        {answer: "poro", question: "CON LA P. Agujero de la piel."}
+    ]
+    },
+    {
+    letter: "q",
+    status: 0,
+    chooseQuestion:[
+        {answer: "queso", question: "CON LA Q. Producto obtenido por la maduración de la cuajada de la leche"},
+        {answer: "queratina", question: "CON LA Q. Proteína de las uñas"},
+        {answer: "quemar", question: "CON LA Q. Hacer llamas. Combustionar."}
+    ]
+    },
+    {
+    letter: "r",
+    status: 0,
+    chooseQuestion:[
+        {answer: "raton", question: "CON LA R. Pequeño roedor"},
+        {answer: "repulsiva", question: "CON LA R. Tipo de fuerza que aparece al aproximarse dos cargas del mismo tipo"},
+        {answer: "regla", question: "CON LA R. Instrumento para medir la longitud."}
+    ]
+    },
+    {
+    letter: "s",
+    status: 0,
+    chooseQuestion:[
+        {answer: "stackoverflow", question: "CON LA S. Comunidad salvadora de todo desarrollador informático"},
+        {answer: "sal", question: "CON LA S. Cloruro sódico."},
+        {answer: "sangre", question: "CON LA S. Liquido rojo del cuerpo."}
+    ]
+    },
+    {
+    letter: "t",
+    status: 0,
+    chooseQuestion:[
+        {answer: "terminator", question: "CON LA T. Película del director James Cameron que consolidó a Arnold Schwarzenegger como actor en 1984."},
+        {answer: "terremoto", question: "CON LA T. Movimiento brusco del terreno."},
+        {answer: "tormenta", question: "CON LA T. Fenómeno atmosférico y mujer mutante que tiene poderes climáticos."}
+    ]
+    },
+    {
+    letter: "u",
+    status: 0,
+    chooseQuestion:[
+        {answer: "unamuno", question: "CON LA U. Escritor y filósofo español de la generación del 98 autor del libro 'Niebla' en 1914"},
+        {answer: "uretra", question: "CON LA U. Conducto por donde la orina sale al exterior."},
+        {answer: "universidad", question: "CON LA U. Lugar donde continuar los estudios tras terminar el Bachillerato."}
+    ]
+    },
+    {
+    letter: "v",
+    status: 0,
+    chooseQuestion:[
+        {answer: "vikingos", question: "CON LA V. Nombre dado a los miembros de los pueblos nórdicos originarios de Escandinavia, famosos por sus incursiones y pillajes en Europa"},
+        {answer: "velocidad", question: "CON LA V. Magnitud cuya unidad en el S.I. es el metro por segundo."},
+        {answer: "volumen", question: "CON LA V. Espacio que ocupa un cuerpo."}
+    ]
+    },
+    {
+    letter: "w",
+    status: 0,
+    chooseQuestion:[
+        {answer: "sandwich", question: "CONTIENE LA W. Emparedado hecho con dos rebanadas de pan entre las cuales se coloca jamón y queso"},
+        {answer: "kiwi", question: "CONTIENE LA W. Fruta cuyo interior es de color verde."},
+        {answer: "waterpolo", question: "CON LA W. Juego de pelota con dos equipos, que se practica en una piscina."}
+    ]
+    },
+    {
+    letter: "x",
+    status: 0,
+    chooseQuestion:[
+        {answer: "botox", question: "CONTIENE LA X. Toxina bacteriana utilizada en cirujía estética"},
+        {answer: "experimentar", question: "CONTIENE LA X. Repetir la observación de un fenómeno en condiciones controladas, normalmente se hace en el laboratorio."},
+        {answer: "explorar", question: "CONTIENE LA X. Examinar o reconocer a un paciente con fines diagnósticos."}
+    ]
+    },
+    {
+    letter: "y",
+    status: 0,
+    chooseQuestion:[
+        {answer: "peyote", question: "CONTIENE LA Y. Pequeño cáctus conocido por sus alcaloides psicoactivos utilizado de forma ritual y medicinal  por indígenas americanos"},
+        {answer: "inyectar", question: "CONTIENE LA Y. Introducir a presión un gas o un líquido en el interior de un cuerpo."},
+        {answer: "yacimiento", question: "CON LA Y. Lugar donde se concentra una gran cantidad de minerales."}
+    ]
+    },
+    {
+    letter: "z",
+    status: 0,
+    chooseQuestion:[
+        {answer: "zen", question: "CON LA Z. Escuela de budismo que busca la experiencia de la sabiduría más allá del discurso racional"},
+        {answer: "luz", question: "CONTIENE LA Z. Agente físico que hace visible los objetos."},
+        {answer: "zancadilla", question: "CON LA Z. Si te hacen una, podrias caer y hacerte daño."}
+    ]
+    }
+];
+
+let userRanking = [{userName: 'unknown', correctAnswers: 0, failAnswers: 0}];
+
+let userName;
+let objPlayer = {userName: "", correctAnswers: 0, failAnswers: 0};
+let randomQuestionNum;
+let itsEnd = false;
+
+askUserName();
+
+    //USERNAME
+    //      -Preguntar username.
+    //      -crear obj del user.
+
+    function askUserName() { 
+        userName = prompt('Bienvenido/a! Introduzca un nombre de usuario para poder jugar.');
+        while(userName === ''){
+            userName = prompt('No ha introducido un nombre de usuario. \n Vuelva a intentarlo.');
+        }
+        console.log(`Bienvenido/a ${userName} al juego BINGO SKYLAB!`);
+        objPlayer.userName = userName;
+        console.log(objPlayer);
+        alert('REGLAS: \n Escribe la palabra para responder. \n Escribe "pasapalabra" para dejar la pregunta a la siguiente ronda. \n Escribe "end", para finalizar el juego.')
+        askQuestion();
+    }
+
+    
+    //REALIZAR PREGUNTA.
+    //      -Prompt questions[].question para realizar pregunta.
+    //      -If correct == status = true, if incorrect == status = false, if 'pasapalabra' == status = 1,  
+    //      -IF respuesta == 'end'. itsEnd = true. (finalizar juego)
+    
+    
+    //Do if never ask or 'pasapalabra'
+    function askQuestion() { 
+        for(i in questions) {
+            if(questions[i].status === 0) {
+                statusEqualZero();
+            }else if(questions[i].status === 1) {
+                statusEqualOne();
+            }            
+        }
+        checkStatus();        
+    }
+
+
+    function statusEqualZero() {
+    
+        randomQuestionNum = Math.floor(Math.random() * 3); 
+        let respuesta = prompt(questions[i].chooseQuestion[randomQuestionNum].question);
+        respuesta = respuesta.toUpperCase();
+
+        let isTrue = questions[i].chooseQuestion[randomQuestionNum].answer;
+        isTrue = isTrue.toUpperCase();
+        
+        switch(respuesta){
+            case 'PASAPALABRA': 
+                questions[i].status = 1;
+                questions[i].numberQuestion = randomQuestionNum;//añadir una propiedad al objeto para repetir la misma pregunta
+                console.log('La guardamos para la siguiente ronda!');
+            break;
+            case isTrue:
+                questions[i].status = true;
+                objPlayer.correctAnswers += 1;
+                console.log('Correcto!');
+                break;
+            case 'END':
+                for(i in questions) {
+                    if(questions[i].status === 0) {
+                        questions[i].status = false;
+                        objPlayer.failAnswers += 1;
+                    }
+                }
+                itsEnd = true;
+            default:
+                questions[i].status = false;
+                objPlayer.failAnswers += 1;
+                console.log('Error!')
+                break;
+        }    
+    }
+
+    function statusEqualOne() {
+
+        let repeatNumQuestion = questions[i].numberQuestion;
+    
+        let respuesta = prompt(questions[i].chooseQuestion[repeatNumQuestion].question);
+        respuesta = respuesta.toUpperCase();
+
+        let isTrue = questions[i].chooseQuestion[randomQuestionNum].answer;
+        isTrue = isTrue.toUpperCase();
+
+        switch(respuesta){
+            case 'PASAPALABRA': 
+                questions[i].status = 1;
+                console.log('La guardamos para la siguiente ronda!');
+                break;
+            case isTrue:
+                questions[i].status = true;
+                objPlayer.correctAnswers += 1;
+                console.log('Correcto!');
+                break;
+            case 'END':
+                for(i in questions) {
+                    if(questions[i].status === 0 || questions[i].status === 1) {
+                        questions[i].status = false;
+                        objPlayer.failAnswers += 1;
+                    }
+                }
+                itsEnd = true;
+            default:
+                questions[i].status = false;
+                objPlayer.failAnswers += 1;
+                console.log('Error!')
+                break;
+        }
+    }   
+    
+    //CUENTA DE ACUERTOS Y FALLOS.
+    //      -Si aciertos + fallos != questions.length = repetir.
+    //      -Else = calcular ranking de jugadores.    
+    function checkStatus() {
+        
+        if((objPlayer.failAnswers + objPlayer.correctAnswers) < questions.length) {
+            askQuestion();
+        }else {
+            if(itsEnd === true) {
+                console.log('Ha abandonado la partida, con un total de aciertos de: ' + objPlayer.correctAnswers);
+            }else {
+                console.log('Felicidades! has completado las preguntas con un total de: \n Aciertos: ' + objPlayer.correctAnswers + '\n Fallos: ' + objPlayer.failAnswers);
+                console.log(objPlayer);
+                userRanking.push(objPlayer);
+                ranking();
+            }
+        }        
+    }
+
+    //RANKING DE JUGADORES
+    //      -Nombre, aciertos y fallos
+    function ranking() {   
+        let higherThan = userRanking.sort((a, b) => b.correctAnswers - a.correctAnswers);
+        console.log('El ranking de jugadores és: ' + JSON.stringify(higherThan));
+    }    
+
+    let playAgain = confirm('Cliock aceptar si quieres volver a jugar.');
+    if(playAgain === true) {
+        objPlayer = {userName: "", correctAnswers: 0, failAnswers: 0};
+        for(i in questions) {
+            questions[i].status = 0;
+        }
+        askUserName();
+    }else {
+        alert(`Gracias por jugar ${userName}. Vuelve pronto!`);
+    }
