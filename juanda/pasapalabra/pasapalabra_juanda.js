08f9b22465b566920810c432d8198fa4044523d8
JDSanchon <juanda.sanchon@gmail.com>
2021-04-06 12:34:47 +0200

    branch pasapalabra and files

---
+++ juanda/pasapalabra/pasapalabra_juanda.js
@@ -0,0 +1,214 @@
+var questions = [
+
+    { letter: "a", idquestion: 0, answer: ["abducir","amar","actuar"], status: 0, question: ["CON LA A. Dicho de una supuesta criatura extraterrestre: Apoderarse de alguien","CON LA A. Desear, querer a una persona", "CON LA A. Acción. Hacer algo."]},
+
+    { letter: "b", idquestion: 0, answer: ["bingo","berrear","barril"], status: 0, question:["CON LA B. Juego que ha sacado de quicio a todos los 'Skylabers' en las sesiones de precurso","CON LA B. Dar berridos, llorar fuerte","CON LA B. Cuba o tonel donde se almacenan líquidos o sólidos"]},
+
+    { letter: "c", idquestion: 0, answer: ["churumbel","cacao","coctel"], status: 0, question: ["CON LA C. Niño, crío, bebé","CON LA C. Polvo soluble que se elabora con la semilla del arbol que lleva el mismo nombre.","CON LA C. Bebida alcohólica que se prepara mezclando licores con jugos u otros ingredientes"]},
+    
+    { letter: "d", idquestion: 0, answer: ["diarrea","desaguisado","desfogar"], status: 0, question: ["CON LA D. Anormalidad en la función del aparato digestivo caracterizada por frecuentes evacuaciones y su consistencia líquida","CON LA D.Hecho contra la ley o la razón","CON LA D. Exteriorizar una pasión"]},
+
+    { letter: "e", idquestion: 0, answer: ["ectoplasma","escafandra","elefante"], status: 0, question: ["CON LA E. Gelatinoso y se encuentra debajo de la membrana plasmática. Los cazafantasmas medían su radiación","CON LA E. Equipo de buzos y astronautas que consiste en un traje protector de una sola pieza","CON LA E. Mamífero paquidermo de gran tamaño"]},
+
+    { letter: "f", idquestion: 0, answer: ["facil","fetén","fenecer"], status: 0, question: ["CON LA F. Que no requiere gran esfuerzo, capacidad o dificultad","CON LA F. Verdad, Verdadero, Auténtico","CON LA F. Concluir o morir"]},
+
+    { letter: "g", idquestion: 0, answer: ["galaxia","gañán","gorigori"], status: 0, question: ["CON LA G. Conjunto enorme de estrellas, polvo interestelar, gases y partículas","CON LA G. mozo de labranza","CON LA G. Ruido producido por personas que hablan a la vez"]},
+
+    { letter: "h", idquestion: 0, answer: ["harakiri","hecatombe","hoplita"], status: 0, question: ["CON LA H. Suicidio ritual japonés por desentrañamiento","CON LA H. Desastre con muchas víctimas, Sacrifico","CON LA H. Infante pesado griego"]},
+
+    { letter: "i", idquestion: 0, answer: ["iglesia","icono","ilicito"], status: 0, question: ["CON LA I. Templo cristiano","CON LA I. Pintura religiosa realizada sobre una tabla","CON LA I. Prohibido por la ley"]},
+
+    { letter: "j", idquestion: 0, answer: ["jabali","jerigonza","joyel"], status: 0, question: ["CON LA J. Variedad salvaje del cerdo que sale en la película 'El Rey León', de nombre Pumba","CON LA J. Habla especial y enrevesada","CON LA J. Joya pequeña"]},
+
+    { letter: "k", idquestion: 0, answer: ["kamikaze","kefir","kosher"], status: 0, question: ["CON LA K. Persona que se juega la vida realizando una acción temeraria","CON LA K. Alimento turco gelatinoso obtenido de la leche","CON LA K. Comida que es aceptable según la Torah"]},
+
+    { letter: "l", idquestion: 0, answer: ["licantropo","lacustre","laxo"], status: 0, question: ["CON LA L. Hombre lobo","CON LA L. Relativo a un lago","CON LA L. Flojo, Poco firme o severo"]},
+
+    { letter: "m", idquestion: 0, answer: ["misantropo","macabro","metástasis"], status: 0, question: ["CON LA M. Persona que huye del trato con otras personas o siente gran aversión hacia ellas","CON LA M. Participa de lo terrorífico y repulsivo de la muerte","CON LA M. Extensión de las lesiones por el organismo"]},
+
+    { letter: "n", idquestion: 0, answer: ["necedad","nepotismo","nimio"], status: 0, question: ["CON LA N. Demostración de poca inteligencia","CON LA N. Abuso de poder favoreciendo a parientes o amigos","CON LA N. Insignificante"]},
+
+    { letter: "ñ", idquestion: 0, answer: ["señal","ñora","ñu"], status: 0, question: ["CONTIENE LA Ñ. Indicio que permite deducir algo de lo que no se tiene un conocimiento directo.","CONTIENE LA Ñ. Pimiento pequeño, redondo y de color rojo.","CONTIENE LA Ñ. Animal mamífero d gran tamaño, con cabeza de toro."]},
+
+    { letter: "o", idquestion: 0, answer: ["orco","obsoleto","oropel"], status: 0, question: ["CON LA O. Humanoide fantástico de apariencia terrible y bestial, piel de color verde creada por el escritor Tolkien","CON LA O. Viejo, Caduco, Pasado de moda, Inútil por vejez","CON LA O. Adorno de poco valor y mucha apariencia"]},
+
+    { letter: "p", idquestion: 0, answer: ["protos","pachanga","palpebral"], status: 0, question: ["CON LA P. Raza ancestral tecnológicamente avanzada que se caracteriza por sus grandes poderes psíonicos del videojuego StarCraft","CON LA P. Jolgorio ruidoso y desordenado","CON LA P. Relativo a los párpados"]},
+
+    { letter: "q", idquestion: 0, answer: ["queso","quincalla","quorum"], status: 0, question: ["CON LA Q. Producto obtenido por la maduración de la cuajada de la leche","CON LA Q. Artículo metálico de escaso valor","CON LA Q. Número de miembros requeridos en una asamblea para que sean válidos los acuerdos en ella obtenidos"]},
+
+    { letter: "r", idquestion: 0, answer: ["raton","razzia","regodeo"], status: 0, question: ["CON LA R. Roedor","CON LA R. Incursión con el fin de obtener botín","CON LA R. Alegrarse malignamente con un daño o mala situación de otro"]},
+
+    { letter: "s", idquestion: 0, answer: ["stackoverflow","solano","supino"], status: 0, question: ["CON LA S. Comunidad salvadora de todo desarrollador informático","CON LA S. Viento del este","CON LA S. Falta absoluta de conocimiento"]},
+
+    { letter: "t", idquestion: 0, answer: ["terminator","tara","tránsfuga"], status: 0, question: ["CON LA T. Película del director James Cameron que consolidó a Arnold Schwarzenegger como actor en 1984","CON LA T. Estigma, defecto","CON LA T. Que cambia de un partido a otro"]},
+
+    { letter: "u", idquestion: 0, answer: ["unamuno","ubicuo","ubérrimo"], status: 0, question: ["CON LA U. Escritor y filósofo español de la generación del 98 autor del libro 'Niebla' en 1914","CON LA U. Que está en todas partes","CON LA U. Extremadamente fertil"]},
+
+    { letter: "v", idquestion: 0, answer: ["vikingos","vagido","venial"], status: 0, question: ["CON LA V. Nombre dado a los miembros de los pueblos nórdicos originarios de Escandinavia, famosos por sus incursiones y pillajes en Europa","CON LA V. Llanto de un recién nacido","CON LA V. Que es de fácil perdón"]},
+
+    { letter: "w", idquestion: 0, answer: ["sandwich","waterpolista","whisky"], status: 0, question: ["CONTIENE LA W. Emparedado hecho con dos rebanadas de pan entre las cuales se coloca jamón y queso","CONTIENE LA W. Jugador de waterpolo","CONTIENE LA W. Bebida alcohólica fuerte y de color marrón."]},
+
+    { letter: "x", idquestion: 0, answer: ["botox","xenofobia","xifoides"], status: 0, question: ["CONTIENE LA X. Toxina bacteriana utilizada en cirujía estética","CONTIENE LA X. Odio a los extranjeros o a las cosas de otros países","CONTIENE LA X. Parte blanda que está al final del esternón del ser humano"]},
+
+    { letter: "y", idquestion: 0, answer: ["peyote","yak","yanqui"], status: 0, question: ["CONTIENE LA Y. Pequeño cáctus conocido por sus alcaloides psicoactivos utilizado de forma ritual y medicinal  por indígenas americanos","CONTIENE LA Y. Animal parecido a un búfalo con un pelo largo similar a la lana","CONTIENE LA Y. Persona o cosa de Estados Unidos"]},
+    
+    { letter: "z", idquestion: 0, answer: ["zen","zote","zamacuco"], status: 0, question: ["CON LA Z. Escuela de budismo que busca la experiencia de la sabiduría más allá del discurso racional","CON LA Z. Persona torpe e ignorante","CON LA Z. Persona que disimulando, hace siempre su voluntad"]}, ]
+
+let numErrors = 0;
+let questionDone = 27;
+let MAXPOINTS = 27;  //constante no almacenada como tal para no generar conflictos en chrome.
+let playerName = "";
+let playerResum = [];
+
+
+function recorriendoPreguntas(){
+    let wrongAnswer;
+    let respuesta;
+    
+    do{
+        questionDone = 27;
+        for (i = 0; i < questions.length ; i++){ //con esto damos una vuelta y hacemos las preguntas
+            
+            if((questions[i].idquestion === 0)&&(questions[i].status === 0)){ //asigno una id a cada pregunta, solo cuando no cuenta con id y no ha sido respondida.
+                questions[i].idquestion = Math.floor(Math.random() * Math.floor(3));  
+            }
+            
+            if((questions[i].status === 0)||(questions[i].status === 3)){
+                
+                console.log(`${questions[i].question[questions[i].idquestion]}`);
+                do{
+                wrongAnswer = false;
+                respuesta = prompt('Tu respuesta: ');
+                console.log(respuesta);
+                    if ((respuesta === null)||(!isNaN(respuesta))){
+                        wrongAnswer = true;
+                        console.log("Escribe una respuesta valida. ");
+                    }else if(respuesta.toUpperCase()=== "END"){
+                        questionDone = 0; 
+                        return false;          
+                    }else{
+                        respuestas(respuesta, i);
+                    }
+                }while(wrongAnswer === true);
+            }
+            if(respuesta.toLowerCase()=== "END"){
+                break;            
+            }
+        }
+        
+        contadorPreguntasValidadas();
+        console.log(questionDone);
+    }while(questionDone > 0); //mientras no esten todas las preguntas contestadas...
+}
+
+function respuestas (dato, num){
+    
+    let numRespuestas = 0;
+
+        if(dato.toLowerCase() === 'pasapalabra' ){
+            console.log("¡Preparate para la próxima ronda!");
+            questions[num].status = 3;
+            numRespuestas = 1;
+            
+        }else if (dato.toLowerCase() === questions[num].answer[questions[i].idquestion]) {
+            console.log ("¡Correcto!");
+            questions[num].status = 1; //se modifica el status para saber cuando es acierto o fallo.
+            numRespuestas = 1;
+            
+        }else if(numRespuestas === 0){
+            console.log("¡Error! Fallaste!");
+            console.log(`La respuesta correcta es '${questions[num].answer[questions[i].idquestion]}'`);
+            numErrors ++;
+            questions[num].status = 2;
+            
+        }
+}
+//verifica que todas las preguntas esten respondidas, correcta o incorrectamente
+function contadorPreguntasValidadas(){
+    
+    for (i = 0 ; i < questions.length ; i++ ){
+        if(questions[i].status !== 3){
+            questionDone --;
+        } 
+    }
+}
+//ejecucion del juego
+function pasapalabra(){
+    let continuar;
+    let datoErroneo;
+    let totalPoints;
+    let finalGame;
+    do{
+        restart ()
+        totalPoints = MAXPOINTS;
+        playerName = prompt("Indicanos tu nombre: ");
+        console.log("*********************");
+        console.log(`¡Preparate ${playerName}!`);
+        finalGame = recorriendoPreguntas();
+            for (i = 0 ; i < questions.length ; i++ ){ //calcula puntuacion total del jugador
+                if ((questions[i].status === 2)||(questions[i].status === 0)){
+                    totalPoints --;
+                }
+            }
+        if(finalGame !== false){
+            playerResum.push(players(playerName,totalPoints));
+        }
+        console.log(`Jugador: ${playerName}. Ha acertado: ${totalPoints}. Ha fallado ${numErrors}`);
+        console.log("----------------------\n")
+
+        do{
+            datoErroneo = false;
+            continuar = prompt("Alguien desea volver a jugar? y/n");
+            if (continuar === null){
+                console.log("Has introducido un dato erroneo. Escribe y o n.");
+                datoErroneo = true;
+            }else if((continuar.toLowerCase() !== 'y') && (continuar.toLowerCase() !== 'n')){
+                console.log("Has introducido un dato erroneo. Escribe y o n.");
+                datoErroneo = true;
+            }
+        }while(datoErroneo === true);
+        
+    }while(continuar.toLowerCase() !== 'n');
+
+    mostrarRanking();
+}
+//mostrar ranking
+function mostrarRanking(){
+    
+    playerResum.sort(compare) //llamamos a la funcion de ordenacion
+
+    console.log ("*****RANKING******");
+    console.log(" PLAYER / POINTS ")
+    for (let i = 0 ; i <playerResum.length ; i++){
+        console.log(`${playerResum[i].name} ------ ${playerResum[i].points} .`);
+    } 
+    console.log ("******************");
+}
+//plantilla para introducir objeto
+function players (nombre,aciertos){
+    return  {
+        name : nombre,
+        points : aciertos
+    }    
+}
+//restaurar al valor original
+function restart (){
+    for(i = 0 ; i < questions.length ; i++){
+        questions[i].status = 0;
+        questions[i].idquestion = 0;
+    }
+    questionDone = 27;
+    numErrors = 0;
+}
+//funcion para ordenar 2 parametros
+function compare( a, b ) { 
+    if ( a.points > b.points ){
+      return -1;
+    }
+    if ( a.points < b.points ){
+      return 1;
+    }
+    return 0;
+  }
+
+pasapalabra();
