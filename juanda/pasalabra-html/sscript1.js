4fee9e54feeeb181094cb34b45a11f941d591aea
JDSanchon <juanda.sanchon@gmail.com>
2021-04-06 12:37:47 +0200

    branch pasapalabra-html and files(no finished)

---
+++ juanda/pasalabra-html/sscript1.js
@@ -0,0 +1,22 @@
+/*seleccionamos los 4 bloques*/
+let init = document.getElementById("header");
+let rules = document.getElementById ("rules");
+let rosco = document.getElementById ("rosco");
+let finalPage = document.getElementById("footer"); 
+/*recogemos resto de variables*/
+
+
+
+
+function arranco(){
+
+    let nombre = document.getElementById("playerName").value;
+
+    console.log(nombre);
+}
+
+
+
+/*for (let name of arrayPlayers){
+    console.log(name);
+}*/
