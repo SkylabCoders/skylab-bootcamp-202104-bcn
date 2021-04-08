1dcd6f2db9d9fcdb7887e27de47f53f76b01bd45
Santiago.Martinez <infohere52@gmail.com>
2021-04-08 15:15:05 +0200

    Change object flights view in the code

--- Santiago/precurso/airlines/airlines-pro.js
+++ Santiago/precurso/airlines/airlines-pro.js
@@ -1,81 +1,17 @@
 // Declaramos los vuelos de manera global
-let flights = [{
-        id: 0,
-        to: 'Bilbao',
-        from: 'Barcelona',
-        cost: 1600,
-        scale: false
-    },
-    {
-        id: 1,
-        to: 'New York',
-        from: 'Barcelona',
-        cost: 700,
-        scale: false
-    },
-    {
-        id: 2,
-        to: 'Los Angeles',
-        from: 'Madrid',
-        cost: 1100,
-        scale: true
-    },
-    {
-        id: 3,
-        to: 'Paris',
-        from: 'Barcelona',
-        cost: 210,
-        scale: false
-    },
-    {
-        id: 4,
-        to: 'Roma',
-        from: 'Barcelona',
-        cost: 150,
-        scale: false
-    },
-    {
-        id: 5,
-        to: 'London',
-        from: 'Madrid',
-        cost: 200,
-        scale: false
-    },
-    {
-        id: 6,
-        to: 'Madrid',
-        from: 'Barcelona',
-        cost: 90,
-        scale: false
-    },
-    {
-        id: 7,
-        to: 'Tokyo',
-        from: 'Madrid',
-        cost: 1500,
-        scale: true
-    },
-    {
-        id: 8,
-        to: 'Shangai',
-        from: 'Barcelona',
-        cost: 800,
-        scale: true
-    },
-    {
-        id: 9,
-        to: 'Sydney',
-        from: 'Barcelona',
-        cost: 150,
-        scale: true
-    },
-    {
-        id: 10,
-        to: 'Tel-Aviv',
-        from: 'Madrid',
-        cost: 150,
-        scale: false
-    }
+let flights =
+[
+    { id: 0, to: 'Bilbao', from: 'Barcelona', cost: 1600, scale: false },
+    { id: 1, to: 'New York', from: 'Barcelona', cost: 700, scale: false },
+    { id: 2, to: 'Los Angeles', from: 'Madrid', cost: 1100, scale: true },
+    { id: 3, to: 'Paris', from: 'Barcelona', cost: 210, scale: false },
+    { id: 4, to: 'Roma', from: 'Barcelona', cost: 150, scale: false },
+    { id: 5, to: 'London', from: 'Madrid', cost: 200, scale: false },
+    { id: 6, to: 'Madrid', from: 'Barcelona', cost: 90, scale: false },
+    { id: 7, to: 'Tokyo', from: 'Madrid', cost: 1500, scale: true },
+    { id: 8, to: 'Shangai', from: 'Barcelona', cost: 800, scale: true },
+    { id: 9, to: 'Sydney', from: 'Barcelona', cost: 150, scale: true },
+    { id: 10, to: 'Tel-Aviv', from: 'Madrid', cost: 150, scale: false }
 ];
 
 let addedFlights = [];
