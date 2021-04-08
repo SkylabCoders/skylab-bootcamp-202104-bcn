eb66a5187768db2b7e321c4b203fbfee3b7d2758
cristian-bars <patxi.cristian@gmail.com>
2021-04-08 09:55:35 +0200

    new branch better

---
+++ cristian-bars/challenge/precurse/Calculadora/calculator-pro.js
@@ -0,0 +1,81 @@
+function operaciones(opera){
+    let numberList = [];
+    let newNumber;
+    do {
+        newNumber = prompt('Enter a number or press cancel to stop');
+        if (newNumber == ""){
+            alert("Dejaste en blanco el primer numero");
+        }else if (isNaN(newNumber)){
+            alert("Entraste letras en vez de numeros");
+            newNumber = "";
+        }else if (newNumber !== null){
+            console.log(newNumber);
+            numberList.push(Number(newNumber))
+        }
+        
+        console.log(numberList);
+    } while(newNumber !== null)
+    
+    if (numberList.length == 1){
+        console.log("La raíz cuadrada del numero " + numberList[0] + " es " + Math.sqrt(numberList[0]).toFixed(3));
+    }else{
+        
+        sumar(numberList);
+        restar(numberList);
+        multiplicar(numberList);
+        dividir(numberList);
+        
+    }
+    
+    var repeat = prompt("New numbers? y/n");
+
+    switch (repeat) {
+        case "n": 
+            console.log("Bye!");
+            break;
+            
+        case "y": 
+            console.log("Tornem-hi!!");
+            operaciones();
+    }
+        
+}
+
+function sumar (numberList){
+    opera = 0;
+    for (let l = 0; l<numberList.length; l++){
+        opera = opera + numberList[l];
+    }
+    console.log("El resultado de la suma es " + opera);
+}
+
+function restar (numberList){
+    opera = numberList[0];
+    for (let l = 1; l<numberList.length; l++){
+        opera = opera - numberList[l];
+    }
+    console.log("El resultado de la resta es " + opera);
+}
+
+function multiplicar (numberList){
+    opera = numberList[0];
+    for (let l = 1; l<numberList.length; l++){
+        opera = opera * numberList[l];
+    }
+    console.log("El resultado de la multiplicación es " + opera);
+}
+
+function dividir (numberList){
+    opera = numberList[0];
+    for (let l = 1; l<numberList.length; l++){
+        opera = opera / numberList[l];
+    }
+    console.log("El resultado de la division es " + Math.sqrt(opera).toFixed(3));
+}
+
+let opera;
+
+operaciones(opera);
+
+
+
---
+++ cristian-bars/challenge/precurse/airlines/airlines.js
@@ -0,0 +1,52 @@
+function consulta (){
+    nombre = prompt("Como puedo llamarle para dirigirme a usted??");
+    
+    mensaje = "Bienvenido/a sr/sra " + nombre + "." +
+    " Estos son los vuelos que tenemos para el día de hoy: \n";
+
+    for (let i = 0; i<vuelos.length; i++){
+        mensaje += "El vuelo con origen " + vuelos[i].from + " y destino " + vuelos[i].to + " tiene un coste de " + vuelos[i].cost + "€";
+        if (vuelos[i].scale === false){
+            mensaje += " y no realiza ninguna escala. \n";
+        }else{
+            mensaje += " y si que realiza escala. \n";
+        }
+        coste = coste + vuelos[i].cost;
+    }
+    
+    coste = coste/vuelos.length;
+    
+    mensaje += "El coste medio de los vuelos es de " + coste + "€ \n";
+    mensaje += "Los ultimos destinos disponibles son \n"
+    
+    for (let j = vuelos.length-5; j<vuelos.length; j++){
+        mensaje += vuelos[j].to + "\n";
+    }
+    
+    
+    
+    console.log(mensaje);
+}
+
+let coste = 0;
+let mensaje;
+let nombre;
+let vuelos = new Array;
+
+vuelos = [
+    {id: 0, to: 'Bilbao', from: 'Barcelona', cost: 1600, scale: false},
+    {id: 1, to: 'Barcelona', from: 'Madrid', cost: 1400, scale: false},
+    {id: 2, to: 'Lisboa', from: 'Reus', cost: 2000, scale: false},
+    {id: 3, to: 'Milan', from: 'Oslo', cost: 2500, scale: false},
+    {id: 4, to: 'New York', from: 'Viena', cost: 750, scale: true},
+    {id: 5, to: 'Tokyo', from: 'Sao Pablo', cost: 900, scale: true},
+    {id: 6, to: 'Las Palmas de Gran Canaria', from: 'Menorca', cost: 1200, scale: false},
+    {id: 7, to: 'Tanger', from: 'París', cost: 1100, scale: false},
+    {id: 8, to: 'Los Angeles', from: 'Cuba', cost: 300, scale: false},
+    {id: 9, to: 'Sevilla', from: 'Barcelona', cost: 400, scale: false}
+];
+
+consulta();
+
+
+
