227a37e3efc4d9ccde7312c2f5a9a883992e9cfc
ulldecorb <ausias.bertran@gmail.com>
2021-04-08 15:33:02 +0200

    Add chistian corrections

--- ausias-bertran/precurso/extra/main.js
+++ ausias-bertran/precurso/extra/main.js
@@ -35,39 +35,15 @@ document.addEventListener('keydown', function (event) {
     }
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //--------------------- ANIMATED BACKGROUND ------------------//
-//------------------------------------------------------------//
 
 function startRotationBackGround( rotationSpeed ) {  //  Girar fondo calidoscopico  //  rotationSpeed = 50 default mode / 100 easy mode / 25 f@## mode
-    
     createRotationBoxes();
-    
     setRotate = setInterval(
 
-        function() {
-            
+        function() {          
             baby1.style.transform = `rotate(${ baby1DegreeCurrentValue }deg)` ;
-            baby2.style.transform = `rotate(${ baby2DegreeCurrentValue }deg)` ;
-            
+            baby2.style.transform = `rotate(${ baby2DegreeCurrentValue }deg)` ;  
             if ( pauseRotation !== true ) {
                 baby1DegreeCurrentValue ++ ;
                 baby2DegreeCurrentValue -- ;
@@ -85,11 +61,8 @@ function startRotationBackGround( rotationSpeed ) {  //  Girar fondo calidoscopi
 }
     
 function createRotationBoxes() {
-
     for ( let i = 0 ; i < 2 ; i++ ) {
-
         for ( let j = 0 ; j < 50 ; j++ ) {
-
             let babyHood = document.createElement( "div" );
             babyHood.id = `baby${ i + 1 }${ j }` ;
             babyHood.className = `baby${ i + 1 }` ;
@@ -104,118 +77,79 @@ function createRotationBoxes() {
     }
 }
 
-
-
-
 //---------------------- GAME CODE ---------------------------//
-//------------------------------------------------------------//
 
 function cathDiv() {  //  Evaluar y mostrar resultado del lance
-
     clearInterval( showResultBehaviour ) ;   
-
     if ( isHit() === true ) {
-           
         console.log("HIT") ;
         pauseRotation = true ;
-
-        //stop rotation 1 seconds
-        //change color rotationDivs to hit (darkOrange)
-        //show result
         hits.innerHTML = hitCounterOnString() ;
         showResult();
-
     } else {
         console.log("KK") ;
         pauseRotation = true ;
-        // body.style.backgroundColor = "red" ;
-        //change color rotationDivs fo fail (allRed)
-
-
-        //stop rotation 1 seconds
-        //show result  
         fails.innerHTML = failCounterOnString() ;
         showResult();
     }
 }
 
 function isHit() {
-
     if ( baby1DegreeCurrentValue === 0 || baby1DegreeCurrentValue === 90 || baby1DegreeCurrentValue === 180 || baby1DegreeCurrentValue === 270 ) {
-
         return true ;
-
     } else { 
-
         return false ; 
     }
 }
 
 function hitCounterOnString() {
-
     hitCounter += "🍻" ;  //  += "💖" ;
     return hitCounter ;
 }
 
 function failCounterOnString() {
-
     failCounter += "💩" ;
     return failCounter ;
 }
 
 function setRotationBackgroundColor( color ) {
-
     for ( let i = 0 ; i < allBaby1.length ; i++ ) {
-        
         allBaby1[ i ].style.border = `5px solid ${ color }` ;
         allBaby1[ i ].style.boxShadow = `0 0 50px ${ color }`;
         allBaby2[ i ].style.border = `5px solid ${ color }` ;
         allBaby2[ i ].style.boxShadow = `0 0 50px ${ color }`;
     }
     patchBabyHole.style.backgroundColor = `${ color }` ;
-
 }
 
 function showResult() {  
-    
     popUp.style.display = "block" ;
     popUp.style.opacity = 1 ;
-        
     let frame = 0 ; 
     let opacityStatus = 1 ;
-
     if ( isHit() === true ) {
-
         popUpResult.innerHTML = "🍻" ;
-
         setRotationBackgroundColor( "darkorange" ) ;
         body.style.backgroundColor = "red"
-
     } else {
-
         popUpResult.innerHTML = "💩" ;
         setRotationBackgroundColor( "white" ) ;
     }
-    
     showResultBehaviour = setInterval( 
-    
-        function () {
-                
-            if ( isHit() === true ) {             
 
+        function () {                
+            if ( isHit() === true ) {             
                 if ( frame === 12  ) {    
                     setRotationBackgroundColor( "red" ) ;
                     body.style.backgroundColor = "darkorange"
                 } 
                 if ( frame ===  24  ) {
-                    
                     setRotationBackgroundColor( "darkorange" ) ;
                     body.style.backgroundColor = "red"                    
                 } 
                 if ( frame === 36 ) {
                     popUp.style.display = "none" ;
                     setRotationBackgroundColor( "red" ) ;
-                    // startRotationBackGround( 70 ) ;
                     body.style.backgroundColor = "black"                
                     pauseRotation = false ;
                     return null ;
@@ -224,16 +158,12 @@ function showResult() {
                     opacityStatus -= 0.25 ;
                     popUp.style.opacity = `${ opacityStatus }` ;
                 }
-                                    
             } else {
-                
                 setRotationBackgroundColor( "white" ) ;
-
                 if ( frame > 12 && frame < 36) {
                     opacityStatus -= 0.25 ;
                     popUp.style.opacity = `${ opacityStatus }` ;
                 }
-
                 if ( frame === 36 ) {
                     popUp.style.display = "none" ;
                     setRotationBackgroundColor( "red" ) ;
