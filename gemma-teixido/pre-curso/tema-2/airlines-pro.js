76863939628edfa3d009a05c8fa9187a0a2becab
gteixi <gemmateixido25@gmail.com>
2021-04-08 10:25:11 +0200

    Removed spaces of airlines and airlines-pro commented by Fer.

--- gemma-teixido/pre-curso/tema-2/airlines-pro.js
+++ gemma-teixido/pre-curso/tema-2/airlines-pro.js
@@ -10,27 +10,18 @@
 
 let flights = [
   { id: 0, to: "Bilbao", from: "Barcelona", cost: 1600, scale: false },
-
   { id: 1, to: "New York", from: "Barcelona", cost: 700, scale: false },
-
   { id: 2, to: "Los Angeles", from: "Madrid", cost: 1100, scale: true },
-
   { id: 3, to: "Paris", from: "Barcelona", cost: 210, scale: false },
-
   { id: 4, to: "Roma", from: "Barcelona", cost: 150, scale: false },
-
   { id: 5, to: "London", from: "Madrid", cost: 200, scale: false },
-
   { id: 6, to: "Madrid", from: "Barcelona", cost: 90, scale: false },
-
   { id: 7, to: "Tokyo", from: "Madrid", cost: 1500, scale: true },
-
   { id: 8, to: "Shangai", from: "Barcelona", cost: 800, scale: true },
-
   { id: 9, to: "Sydney", from: "Barcelona", cost: 150, scale: true },
-
   { id: 10, to: "Tel-Aviv", from: "Madrid", cost: 150, scale: false },
 ];
+
 const user = "user";
 const admin = "admin";
 const maxVuelos = 15;
