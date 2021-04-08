acb59f525d5ccefa8c16341a34be976d77ac8850
cristian-bars <patxi.cristian@gmail.com>
2021-04-08 10:42:16 +0200

    delete spaces and move variables declaration

--- cristian-bars/challenge/precurse/Pasapalabra_HTML/script/pasapalabra.js
+++ cristian-bars/challenge/precurse/Pasapalabra_HTML/script/pasapalabra.js
@@ -1,3 +1,49 @@
+let answer;
+let players = [];
+let rigthWords = 0;
+let wrongWords = 0;
+let pendent = 0;
+let i = 0;
+let start = false;
+let flag = 0;
+let puntua;
+let t;
+let inter;
+
+//status == 0 pregunta en primer estado
+//status == 1 pregunta acertada
+//status == 2 pregunta incorrecta
+//status == 3 pasapalabra
+
+let questions = [
+    { letter: "A", answer: "abducir", status: 0, question: "CON LA A. Dicho de una supuesta criatura extraterrestre: Apoderarse de alguien"},
+    { letter: "B", answer: "bingo", status: 0, question: "CON LA B. Juego que ha sacado de quicio a todos los 'Skylabers' en las sesiones de precurso"},
+    { letter: "C", answer: "churumbel", status: 0, question: "CON LA C. Niño, crío, bebé"},
+    { letter: "D", answer: "diarrea", status: 0, question: "CON LA D. Anormalidad en la función del aparato digestivo caracterizada por frecuentes evacuaciones y su consistencia líquida"},
+    { letter: "E", answer: "ectoplasma", status: 0, question: "CON LA E. Gelatinoso y se encuentra debajo de la membrana plasmática. Los cazafantasmas medían su radiación"},
+    { letter: "F", answer: "facil", status: 0, question: "CON LA F. Que no requiere gran esfuerzo, capacidad o dificultad"},
+    { letter: "G", answer: "galaxia", status: 0, question: "CON LA G. Conjunto enorme de estrellas, polvo interestelar, gases y partículas"},
+    { letter: "H", answer: "harakiri", status: 0, question: "CON LA H. Suicidio ritual japonés por desentrañamiento"},
+    { letter: "I", answer: "iglesia", status: 0, question: "CON LA I. Templo cristiano"},
+    { letter: "J", answer: "jabali", status: 0, question: "CON LA J. Variedad salvaje del cerdo que sale en la película 'El Rey León', de nombre Pumba"},
+    { letter: "K", answer: "kamikaze", status: 0, question: "CON LA K. Persona que se juega la vida realizando una acción temeraria"},
+    { letter: "L", answer: "licantropo", status: 0, question: "CON LA L. Hombre lobo"},
+    { letter: "M", answer: "misantropo", status: 0, question: "CON LA M. Persona que huye del trato con otras personas o siente gran aversión hacia ellas"},
+    { letter: "N", answer: "necedad", status: 0, question: "CON LA N. Demostración de poca inteligencia"},
+    { letter: "Ñ", answer: "señal", status: 0, question: "CONTIENE LA Ñ. Indicio que permite deducir algo de lo que no se tiene un conocimiento directo."},
+    { letter: "O", answer: "orco", status: 0, question: "CON LA O. Humanoide fantástico de apariencia terrible y bestial, piel de color verde creada por el escritor Tolkien"},
+    { letter: "P", answer: "protoss", status: 0, question: "CON LA P. Raza ancestral tecnológicamente avanzada que se caracteriza por sus grandes poderes psíonicos del videojuego StarCraft"},
+    { letter: "Q", answer: "queso", status: 0, question: "CON LA Q. Producto obtenido por la maduración de la cuajada de la leche"},
+    { letter: "R", answer: "raton", status: 0, question: "CON LA R. Roedor"},
+    { letter: "S", answer: "stackoverflow", status: 0, question: "CON LA S. Comunidad salvadora de todo desarrollador informático"},
+    { letter: "T", answer: "terminator", status: 0, question: "CON LA T. Película del director James Cameron que consolidó a Arnold Schwarzenegger como actor en 1984"},
+    { letter: "U", answer: "unamuno", status: 0, question: "CON LA U. Escritor y filósofo español de la generación del 98 autor del libro 'Niebla' en 1914"},
+    { letter: "V", answer: "vikingos", status: 0, question: "CON LA V. Nombre dado a los miembros de los pueblos nórdicos originarios de Escandinavia, famosos por sus incursiones y pillajes en Europa"},
+    { letter: "W", answer: "sandwich", status: 0, question: "CONTIENE LA W. Emparedado hecho con dos rebanadas de pan entre las cuales se coloca jamón y queso"},
+    { letter: "X", answer: "botox", status: 0, question: "CONTIENE LA X. Toxina bacteriana utilizada en cirujía estética"},
+    { letter: "Y", answer: "peyote", status: 0, question: "CONTIENE LA Y. Pequeño cáctus conocido por sus alcaloides psicoactivos utilizado de forma ritual y medicinal  por indígenas americanos"},
+    { letter: "Z", answer: "zen", status: 0, question: "CON LA Z. Escuela de budismo que busca la experiencia de la sabiduría más allá del discurso racional"}
+];
 
 //Eventos para detectar los clicks de los botones
 document.addEventListener("DOMContentLoaded", function() {
@@ -7,8 +53,6 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("palabra").addEventListener("keyup", pulsar);
     
 });
-
-
 
 function empezar(){
     //Preparamos la pantalla para iniciar el juego
@@ -49,12 +93,10 @@ function empezar(){
     //activar tiempo
     interval();
     document.getElementById("start").disabled = true;
-    
 }
 
 function recibir(){
     let string = document.getElementById("palabra");
-    
     //Recogemos la respuesta escrita por el usuario
     answer = string.value.toLowerCase();
 
@@ -142,13 +184,8 @@ function recibir(){
         getName();
         debugger
         showParticipants(players)
-        
-        
-
         document.getElementById("start").textContent = "Siguiente partida";
-        
     }
-    
 }
 
 //Al pulsar la tecla enter al input de palabra hacemos un click directo al boton de responder
@@ -173,13 +210,11 @@ function getName(){
         obj["name"] = yourName;
         obj["rigth"] = rigthWords;
         obj["wrong"] = wrongWords;
-        
         players.push(obj);
 }
 
 //Iniciamos la funcion cuando pulsamos el boton de terminar el juego
 function salir(){
-    
     if(start === true){
         //Si tenemos el juego en marcha y queremos terminar la partida
         clearInterval(inter);
@@ -205,11 +240,8 @@ function salir(){
         document.getElementById("start").textContent = "Volver a empezar el juego";
         document.getElementById("resp").textContent = "";
         document.getElementById("question").textContent = "";
-        
         players = [];
-    
     }
-    
 }
 
 //Función para mostrar la clasificación de participantes
@@ -237,12 +269,9 @@ function sortParticipants(players){
       }
       return 0;
     });
-    
 }
 
 //Temporizador para controlar el tiempo de la partida
-let t;
-let inter;
 function interval(){
     //Ponemos el tiempo en segundos
     t=120;
@@ -264,52 +293,5 @@ function interval(){
             document.getElementById("salir").textContent = "Finalizar juego";
         }
     },1000);
-    
-    
 }
 
-let answer;
-let players = [];
-let rigthWords = 0;
-let wrongWords = 0;
-let pendent = 0;
-let i = 0;
-let start = false;
-let flag = 0;
-let puntua;
-
-//status == 0 pregunta en primer estado
-//status == 1 pregunta acertada
-//status == 2 pregunta incorrecta
-//status == 3 pasapalabra
-
-
-let questions = [
-    { letter: "A", answer: "abducir", status: 0, question: "CON LA A. Dicho de una supuesta criatura extraterrestre: Apoderarse de alguien"},
-    { letter: "B", answer: "bingo", status: 0, question: "CON LA B. Juego que ha sacado de quicio a todos los 'Skylabers' en las sesiones de precurso"},
-    { letter: "C", answer: "churumbel", status: 0, question: "CON LA C. Niño, crío, bebé"},
-    { letter: "D", answer: "diarrea", status: 0, question: "CON LA D. Anormalidad en la función del aparato digestivo caracterizada por frecuentes evacuaciones y su consistencia líquida"},
-    { letter: "E", answer: "ectoplasma", status: 0, question: "CON LA E. Gelatinoso y se encuentra debajo de la membrana plasmática. Los cazafantasmas medían su radiación"},
-    { letter: "F", answer: "facil", status: 0, question: "CON LA F. Que no requiere gran esfuerzo, capacidad o dificultad"},
-    { letter: "G", answer: "galaxia", status: 0, question: "CON LA G. Conjunto enorme de estrellas, polvo interestelar, gases y partículas"},
-    { letter: "H", answer: "harakiri", status: 0, question: "CON LA H. Suicidio ritual japonés por desentrañamiento"},
-    { letter: "I", answer: "iglesia", status: 0, question: "CON LA I. Templo cristiano"},
-    { letter: "J", answer: "jabali", status: 0, question: "CON LA J. Variedad salvaje del cerdo que sale en la película 'El Rey León', de nombre Pumba"},
-    { letter: "K", answer: "kamikaze", status: 0, question: "CON LA K. Persona que se juega la vida realizando una acción temeraria"},
-    { letter: "L", answer: "licantropo", status: 0, question: "CON LA L. Hombre lobo"},
-    { letter: "M", answer: "misantropo", status: 0, question: "CON LA M. Persona que huye del trato con otras personas o siente gran aversión hacia ellas"},
-    { letter: "N", answer: "necedad", status: 0, question: "CON LA N. Demostración de poca inteligencia"},
-    { letter: "Ñ", answer: "señal", status: 0, question: "CONTIENE LA Ñ. Indicio que permite deducir algo de lo que no se tiene un conocimiento directo."},
-    { letter: "O", answer: "orco", status: 0, question: "CON LA O. Humanoide fantástico de apariencia terrible y bestial, piel de color verde creada por el escritor Tolkien"},
-    { letter: "P", answer: "protoss", status: 0, question: "CON LA P. Raza ancestral tecnológicamente avanzada que se caracteriza por sus grandes poderes psíonicos del videojuego StarCraft"},
-    { letter: "Q", answer: "queso", status: 0, question: "CON LA Q. Producto obtenido por la maduración de la cuajada de la leche"},
-    { letter: "R", answer: "raton", status: 0, question: "CON LA R. Roedor"},
-    { letter: "S", answer: "stackoverflow", status: 0, question: "CON LA S. Comunidad salvadora de todo desarrollador informático"},
-    { letter: "T", answer: "terminator", status: 0, question: "CON LA T. Película del director James Cameron que consolidó a Arnold Schwarzenegger como actor en 1984"},
-    { letter: "U", answer: "unamuno", status: 0, question: "CON LA U. Escritor y filósofo español de la generación del 98 autor del libro 'Niebla' en 1914"},
-    { letter: "V", answer: "vikingos", status: 0, question: "CON LA V. Nombre dado a los miembros de los pueblos nórdicos originarios de Escandinavia, famosos por sus incursiones y pillajes en Europa"},
-    { letter: "W", answer: "sandwich", status: 0, question: "CONTIENE LA W. Emparedado hecho con dos rebanadas de pan entre las cuales se coloca jamón y queso"},
-    { letter: "X", answer: "botox", status: 0, question: "CONTIENE LA X. Toxina bacteriana utilizada en cirujía estética"},
-    { letter: "Y", answer: "peyote", status: 0, question: "CONTIENE LA Y. Pequeño cáctus conocido por sus alcaloides psicoactivos utilizado de forma ritual y medicinal  por indígenas americanos"},
-    { letter: "Z", answer: "zen", status: 0, question: "CON LA Z. Escuela de budismo que busca la experiencia de la sabiduría más allá del discurso racional"}
-];
