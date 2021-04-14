e3cb0f99f953af70ccb7c806dd4d96ab13cf70aa
Anna Sala <anna.sala.mercade@gmail.com>
2021-04-14 17:56:02 +0200

    function works

---
+++ anna-sala/challenges/strictEquals/main.js
@@ -0,0 +1,17 @@
+function strictEquals(a,b){
+    a.toString();
+    b.toString();
+
+    console.log(Object.is(a,b));
+}
+
+
+if (NaN){
+    let temp = Object.is(a,b);
+    console.log(!temp);
+}
+else{
+    
+    
+    
+    
