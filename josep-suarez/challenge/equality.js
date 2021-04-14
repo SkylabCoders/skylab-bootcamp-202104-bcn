db70735fc38b0e19ebd9a62700e3f5761c44d71b
Josep Suarez <djose.anton@gmail.com>
2021-04-14 18:06:32 +0200

    solucion OK

---
+++ josep-suarez/challenge/equality.js
@@ -0,0 +1,10 @@
+function comprobar(a, b) {
+  let result = a == b;
+  if (
+    (typeof a == "string" && typeof b != "string") ||
+    (typeof b == "string" && typeof a != "string")
+  ) {
+    result = false;
+  }
+  return result;
+}
