6966f3d9d4d74a3398d89176771647bc90a499cf
ulldecorb <ausias.bertran@gmail.com>
2021-04-15 00:59:54 +0200

    Add console.log test

--- ausias-bertran/challenge/a-equals-b/a-equals-b.js
+++ ausias-bertran/challenge/a-equals-b/a-equals-b.js
@@ -1,11 +1,7 @@
 function strictEquals(a, b) {
-    
-
-
     if (Object.is(a,NaN) && Object.is(NaN,b)) {
         return false;
     } 
-
     if (Object.is( a, -0) && Object.is( 0 , b) ) {
         return true;
     }
@@ -15,11 +11,11 @@ function strictEquals(a, b) {
     else {
         return Object.is(a, b);
     }
-  
 }
+console.log("NaN === Nan is " + strictEquals(NaN, NaN));
+console.log("0 === -0 is " + strictEquals(0, -0));
+console.log("-0 === 0 is " + strictEquals(-0, 0));
 
-
-console.log(strictEquals(NaN, NaN));
 
 
 
