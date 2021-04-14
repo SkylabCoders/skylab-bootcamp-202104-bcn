4d77ded26173b9f70517f6df6206cc8ea66bb480
Edgar Alandete <edgalandete@gmail.com>
2021-04-14 18:34:27 +0200

    Add 0 exception

--- Edgar-Alandete/challenges/strict-equality/index.js
+++ Edgar-Alandete/challenges/strict-equality/index.js
@@ -1,5 +1,9 @@
 function strictEquality(a, b) {
-  return a == b && Object.is(a, b) ? true : false;
+  return (a == b && Object.is(a, b)) ||
+    (Object.is(a, 0) && Object.is(b, -0)) ||
+    (Object.is(a, -0) && Object.is(b, 0))
+    ? true
+    : false;
 }
 
 strictEquality(1, 1);
