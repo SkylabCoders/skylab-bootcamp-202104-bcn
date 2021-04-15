1b58fd22a0dbcb5012fc560140c15f5778270e9e
Daniel Martorell <dani.martorell@gmail.com>
2021-04-15 18:53:35 +0200

    Implement every method

--- dani-martorell/challenge/array-methods/sources/every.test.js
+++ dani-martorell/challenge/array-methods/sources/every.test.js
@@ -1,3 +1,9 @@
 function myEvery(array, callback){
-    
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        if(!callback(element, index, array)){
+            return false;
+        }
+    }
+    return true;
 }
