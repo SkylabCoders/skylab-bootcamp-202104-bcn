9f7db4371741d69dc70f5bd138011fb7de93cad0
Anna Sala <anna.sala.mercade@gmail.com>
2021-04-15 20:27:20 +0200

    finished filter.test

---
+++ anna-sala/challenges/array-methods/test/filter.test.js
@@ -0,0 +1,37 @@
+
+
+function annaFilter(array, filter) {
+    let newArray=[];
+    for (let i=0; i<array.length; i++){
+        if (filter(array[i])){
+            newArray=annaPush(newArray, array[i]);
+        }
+    }
+    return newArray;
+}
+
+function annaPush(a, newValue) {
+    a[a.length]=newValue;
+    return a;
+}
+
+describe('Given a filter function', () => {
+    const scenarios = [
+        { a: ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'], b: x => x.length > 6, result: ["exuberant", "destruction", "present"]}
+    ];
+    scenarios.forEach((scenario) => {
+        describe(`When invoked with array ${scenario.a} and value to filter ${scenario.b}`, () => {
+            test(`Then return new array ${scenario.result}`,() => {
+                // Arrange
+                const a = scenario.a;
+                const b = scenario.b;
+    
+                // Act 
+                const result = annaFilter(a, b);
+    
+                // Assert
+                expect(result).toEqual(scenario.result);
+            })
+        })
+    })
+})
