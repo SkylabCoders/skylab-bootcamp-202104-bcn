1d7046ec83b7921503011221f4538d92c235c1c1
Daniel Martorell <dani.martorell@gmail.com>
2021-04-15 23:45:36 +0200

    Add testing test on filter function

--- dani-martorell/challenge/array-methods/sources/filter.test.js
+++ dani-martorell/challenge/array-methods/sources/filter.test.js
@@ -20,3 +20,20 @@ function myLength(array){
     }
     return counter;
 }
+
+describe(`Given a filter function`, () => {
+    const scenarios = [
+        { array: [true, true, false, false, false], callback: (element) => element === true, result: [ true, true ]},
+        { array: [3,2,5,7,1], callback: function (element){ return element > 4 }, result: [5, 7] },
+        { array: [{ age:24 }, { age: 32 }, { age: 67} ], callback: function (element){ return element.age === 32 }, result: [ { age:32 } ] },
+    ];
+
+    scenarios.forEach((scenario) => {
+        describe(`When invoked with values ${scenario.array} and ${scenario.callback}`, () => {
+            test(`Then returns ${scenario.result}`, () => {
+                const result = myFilter(scenario.array, scenario.callback);
+                expect(result).toEqual(scenario.result) 
+            })
+        })
+    })
+})
