d0eca27ee86d9dc9d1a09d4439545afffe4359ac
Edgar Alandete <edgalandete@gmail.com>
2021-04-15 18:06:48 +0200

    Create myMap first test

--- Edgar-Alandete/challenges/arrays/src/map.test.js
+++ Edgar-Alandete/challenges/arrays/src/map.test.js
@@ -1,5 +1,8 @@
 function myMap(array, callback) {
   const result = [];
+  array.forEach((element) => {
+    result.push(callback(element));
+  });
   return result;
 }
 
@@ -7,7 +10,7 @@ describe("Given a myMap function", () => {
   const scenarios = [
     {
       origin: [true, true, true],
-      destiny: [false, false, false],
+      destiny: [0, 0, 0],
       callback: () => 0,
     },
   ];
@@ -16,8 +19,13 @@ describe("Given a myMap function", () => {
     describe(`When invoked with values ${scenario.origin} with the callback ${scenario.callback}`, () => {
       test(`Then return ${scenario.destiny}`, () => {
         //Arrange
+        const origin = scenario.origin;
+        const destiny = scenario.destiny;
+        const callback = scenario.callback;
         //Act
+        const result = myMap(origin, callback);
         //Assert
+        expect(result).toEqual(destiny);
       });
     });
   });
