ad9eff20c26deec235e20cae4a3e21c293d710c2
Laura <laura.sabater.musach@gmail.com>
2021-04-15 19:49:08 +0200

    Filter test

---
+++ laura-sabater/challenges/arrays/filter/filter.test.js
@@ -0,0 +1,34 @@
+function filter(array, duplicate) {
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    const value = duplicate(array[i]);
+    if (value) {
+      result.push(array[i]);
+    }
+  }
+  return result;
+};
+
+describe('Given a filter function', () => {
+    const scenarios = [
+        { a: [1, 2, 3], fun: (x) => x===2, result: [2]},
+        { a: ["pa", "ma", "mapa"], fun: (x) => x.length<3, result: ["pa", "ma"]},
+        { a: [10, 20, 30], fun: (x) => x>15, result: [20, 30]}
+    ];
+    
+    scenarios.forEach((scenario) => {
+        describe(`When invoked with values ${scenario.a} and ${scenario.fun}`, () => {
+            test(`Then return ${scenario.result}`,() => {
+                // Arrange
+                const a = scenario.a;
+                const fun = scenario.fun;
+    
+                // Act 
+                const result = filter(a, fun);
+    
+                // Assert
+                expect(result).toStrictEqual(scenario.result);
+            })
+        })
+    })
+})
