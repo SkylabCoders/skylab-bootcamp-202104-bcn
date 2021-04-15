945108de7b1395df427a0e33b31624882ebb4141
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-04-15 17:55:57 +0200

    Create MAP function

---
+++ alfredo-zimperz/challenges/array-methods/test/myMap.test.js
@@ -0,0 +1,41 @@
+function myMap(array, callback) {
+	const result = []
+
+	for (let i = 0; i < array.length; i++) {
+		result[i] = callback(array[i])
+	}
+	console.log(result)
+	return result
+}
+
+describe('Given a map function', () => {
+	const scenarios = [
+		{ array: [0, 0, 1], callback: (item) => item + 2, result: [2, 2, 3] },
+		{
+			array: ['a', 'b', 'comida'],
+			callback: (item) => item.toUpperCase(),
+			result: ['A', 'B', 'COMIDA']
+		},
+		{
+			array: [{ age: 20 }, { age: 30 }, { age: 90 }],
+			callback: (item) => item.age < 50,
+			result: [true, true, false]
+		}
+	]
+	scenarios.forEach((scenario) => {
+		describe(`When invoked with values ${scenario.array} and
+        callback${scenario.callback}`, () => {
+			test(`Then return ${scenario.result}`, () => {
+				// Arrange
+				const array = scenario.array
+				const callback = scenario.callback
+
+				// Act
+				const result = myMap(array, callback)
+
+				// Assert
+				expect(result).toStrictEqual(scenario.result)
+			})
+		})
+	})
+})
