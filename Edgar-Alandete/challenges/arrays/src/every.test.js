f5f7c97ec0804d541d101c5672ad4641c260b1be
Edgar Alandete <edgalandete@gmail.com>
2021-04-15 18:56:32 +0200

    Create every function and tests

---
+++ Edgar-Alandete/challenges/arrays/src/every.test.js
@@ -0,0 +1,39 @@
+function myEvery(array, callback) {
+  let result = false;
+
+  array.forEach((element) => {
+    result = callback(element, array[0]);
+  });
+  return result;
+}
+
+describe("Given a myEvery function", () => {
+  const scenarios = [
+    {
+      origin: [true, true, true],
+      result: true,
+      callback: (element, value) => element === value,
+    },
+
+    {
+      origin: [10, 7, 11],
+      result: false,
+      callback: (element, value) => element === value,
+    },
+  ];
+
+  scenarios.forEach((scenario) => {
+    describe(`When invoked with values ${scenario.origin} with the callback ${scenario.callback}`, () => {
+      test(`Then return ${scenario.result}`, () => {
+        //Arrange
+        const origin = scenario.origin;
+        const result = scenario.result;
+        const callback = scenario.callback;
+        //Act
+        const everyResult = myEvery(origin, callback);
+        //Assert
+        expect(everyResult).toBe(result);
+      });
+    });
+  });
+});
