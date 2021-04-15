2b273fda7e510afd97bc684c82a8fa9fbc58e011
Daniel Martorell <dani.martorell@gmail.com>
2021-04-16 00:31:10 +0200

    Add testing to some method

--- dani-martorell/challenge/array-methods/sources/some.test.js
+++ dani-martorell/challenge/array-methods/sources/some.test.js
@@ -16,3 +16,20 @@ function myLength(array){
     }
     return counter;
 }
+
+describe('Given a Some function', () => {
+    const scenarios = [
+        { array: [3,2,5,7], callback: function (element){ return element > 4 }, result: true },
+        { array: [{ age:24 }, { age: 32 }, { age: 67} ], callback: function (element){ return element.age > 70 }, result: false },
+        { array: [ false, false, false, 3], callback: function (element){ return typeof element === 'number' }, result: true },
+    ];
+    
+    scenarios.forEach((scenario) => {
+        describe(`When invoked with values ${scenario.array} and ${scenario.callback}`, () => {
+            test(`Then return ${scenario.result}`,() => {
+                const result = mySome(scenario.array, scenario.callback);
+                expect(result).toBe(scenario.result);
+            })
+        })
+    })
+})
