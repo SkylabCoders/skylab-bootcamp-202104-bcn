36e9f340709e71374e4ea4763b3a591f78b2dd5e
Daniel Martorell <dani.martorell@gmail.com>
2021-04-15 18:36:00 +0200

    Implement filter function

--- dani-martorell/challenge/array-methods/filter.test.js
+++ dani-martorell/challenge/array-methods/filter.test.js
@@ -1,4 +1,13 @@
-
+function myFilter(array, callback){
+    let result = [];
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        if(callback(element, index, array)){
+            result = myPush(result, element);
+        }
+    }
+    return result;
+}
 
 function myPush(array, element){
     return [...array, element];
