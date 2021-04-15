7de720d97d55b81007a3326d2232ab42f31b8116
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-04-15 20:16:38 +0200

    Array method .some done and tested

--- david-vazquez/challenges/array-methods/src/some.test.js
+++ david-vazquez/challenges/array-methods/src/some.test.js
@@ -1,15 +1,16 @@
-function myFind(array, callback) {
+function mySome(array, callback) {
     for(let index=0; index<array.length;index++){
         if(callback(array[index])) {
-            return index;
+            return true;
         }
     }
+    return false;
 }
 
 
 describe('Given a myFilter function', () => {
     const scenarios = [
-        { array: [1, 2, 5, 6, 3], value: (x) => x > 4, result: 2},
+        { array: [1, 2, 5, 6, 3], value: (x) => x > 4, result: true},
     ];
     
     scenarios.forEach((scenarios) => {
@@ -20,7 +21,7 @@ describe('Given a myFilter function', () => {
                 const b = scenarios.value;
     
                 // Act 
-                const result = myFind(a, b);
+                const result = mySome(a, b);
     
                 // Assert
                 expect(result).toStrictEqual(scenarios.result);
