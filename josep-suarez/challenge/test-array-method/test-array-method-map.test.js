83ccd18dc2c30f37bc58be9e21d27d0305826a76
Josep Suarez <djose.anton@gmail.com>
2021-04-15 20:03:00 +0200

    quitando forEach

--- josep-suarez/challenge/test-array-method/test-array-method-map.test.js
+++ josep-suarez/challenge/test-array-method/test-array-method-map.test.js
@@ -1,7 +1,9 @@
 const { expect } = require("@jest/globals");
 
 function myMap(array, callback) {
-  array.forEach((element) => callback(element));
+  for (let i = 0; i < array.length; i++) {
+    (i) => callback(i);
+  }
   return callback;
 }
 
@@ -9,6 +11,7 @@ describe("Given a map function", () => {
   const scenarios = [
     { a: [1, 2, 3], r: [2, 4, 6], callback: (element) => element * 2 },
     { a: [10, 2, 6], r: [5, 1, 3], callback: (element) => element / 2 },
+    { a: [7, 8, 9], r: [9, 10, 11], callback: (element) => element + 2 },
   ];
   scenarios.forEach((scenarios) => {
     describe(`When invoked with values ${scenarios.a}`, () => {
