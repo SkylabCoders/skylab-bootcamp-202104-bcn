ff8be33003efdedba19df429bc7136b69f0284ca
Daniel Martorell <dani.martorell@gmail.com>
2021-04-15 18:19:13 +0200

    Implement map function

--- dani-martorell/challenge/array-methods/sources/map.test.js
+++ dani-martorell/challenge/array-methods/sources/map.test.js
@@ -1,11 +1,10 @@
 
-function myMap(array, callBack){
+function myMap(array, callback){
     const result = [];
     for (let index = 0; index < array.length; index++) {
         const element = array[index];
-        
-        
+        result.push(callback(element, index, array));
     }
-
-    return result;
+    return(result);
 }
+
