969b2a46b7e9b11c51c3e1f070136eb64ecf634d
Daniel Martorell <dani.martorell@gmail.com>
2021-04-15 17:53:39 +0200

    "Delete old package.json"

--- dani-martorell/challenge/array-methods/sources/map.test.js
+++ dani-martorell/challenge/array-methods/sources/map.test.js
@@ -1,1 +1,11 @@
-hl
+
+function myMap(array, callBack){
+    const result = [];
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+        
+    }
+
+    return result;
+}
