a22cf24873d5b17eb50e95695931de421c1aa3c8
cristian-bars <patxi.cristian@gmail.com>
2021-04-15 08:58:35 +0200

    Create equality function

---
+++ cristian-bars/challenge/strictEquality/script.js
@@ -0,0 +1,22 @@
+function strictEquals (a,b){
+  
+	if ((typeof a == typeof b) && (!(isNaN(a) || isNaN(b)))){
+		if (typeof a == typeof b && a == b){
+			console.log(true)
+		}else{
+			console.log(false)
+		}
+	}else{
+		console.log(false)
+	}
+  
+}
+
+strictEquals(1,1);
+strictEquals(NaN,NaN);
+strictEquals(0,-0);
+strictEquals(-0,0);
+strictEquals(1,"1");
+strictEquals(true,false);
+strictEquals(false,false);
+strictEquals("master","oil");
