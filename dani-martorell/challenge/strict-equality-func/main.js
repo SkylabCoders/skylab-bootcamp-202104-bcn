fbf84d93e878cce1a419d324338037f862cfd3ec
Daniel Martorell <dani.martorell@gmail.com>
2021-04-15 11:41:25 +0200

    Add Number.isNan method

--- dani-martorell/challenge/strict-equality-func/main.js
+++ dani-martorell/challenge/strict-equality-func/main.js
@@ -1,5 +1,5 @@
 function strictEquals(a,b){
-    if(isNaN(a) && isNaN(b)) return false;
+    if(Number.isNaN(a) && Number.isNaN(b)) return false;
     if((!isNaN(a) && !isNaN(b)) && isNaN(a % b))
        return true;
     return Object.is(a,b)
