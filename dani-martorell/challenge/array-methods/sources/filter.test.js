9849a34e35cd76d6d3f0132a1b0c37df1fbf0d88
Daniel Martorell <dani.martorell@gmail.com>
2021-04-15 19:29:47 +0200

    Integrate length method inside function

--- dani-martorell/challenge/array-methods/sources/filter.test.js
+++ dani-martorell/challenge/array-methods/sources/filter.test.js
@@ -1,6 +1,6 @@
 function myFilter(array, callback){
     let result = [];
-    for (let index = 0; index < array.length; index++) {
+    for (let index = 0; index < myLength(array); index++) {
         const element = array[index];
         if(callback(element, index, array)){
             result = myPush(result, element);
@@ -11,4 +11,12 @@ function myFilter(array, callback){
 
 function myPush(array, element){
     return [...array, element];
+}
+
+function myLength(array){
+    let counter = 0;
+    for (let element of array){
+        counter++;
+    }
+    return counter;
 }
