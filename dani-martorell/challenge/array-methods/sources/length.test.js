9dca32135be633298997d06cf6ad1e9a8e35cab1
Daniel Martorell <dani.martorell@gmail.com>
2021-04-15 19:25:08 +0200

    "Delete length method inside function"

--- dani-martorell/challenge/array-methods/sources/length.test.js
+++ dani-martorell/challenge/array-methods/sources/length.test.js
@@ -1,7 +1,8 @@
 function myLength(array){
     let counter = 0;
-    for (let index = 0; index < array.length; index++) {
+    for (let element of array){
         counter++;
     }
     return counter;
 }
+
