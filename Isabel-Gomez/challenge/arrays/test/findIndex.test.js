d857363769af7a3e6d0bf4b8d9cdffed792182b3
Isabel Gomez <belasi87@gmail.com>
2021-04-15 20:07:11 +0200

    Create findIndex  method, function and testing

---
+++ Isabel-Gomez/challenge/arrays/test/findIndex.test.js
@@ -0,0 +1,28 @@
+function getFindIndex(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+        const element = callback(array[index]);
+        if (element) {
+            return index;
+        }
+    }
+};
+
+describe('Given a getFindIndex function', () => {
+    const scenarios = [
+        { originArray: [1, 2, 3, 4], operation: (x) => x > 2, result: 2 },
+        { originArray: [1, 2, 3, 4], operation: (x) => x < 3, result: 0 },
+        { originArray: ['hola', 'que', 'tal', 'hambre'], operation: (x) => x[0] === 't', result: 2 },
+    ];
+
+    scenarios.forEach((scenario) => {
+        describe(`When invoked with value ${scenario.originArray} and callback function ${scenario.operation}`, () => {
+            test(`Then return a first value index ${scenario.result}`, () => {
+                // Arrange
+                // Act 
+                const result = getFindIndex(scenario.originArray, scenario.operation);
+                // Assert
+                expect(result).toStrictEqual(scenario.result);
+            })
+        })
+    })
+})
