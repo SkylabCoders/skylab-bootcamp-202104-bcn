f55866470a86fb5fb78bacbcda3d02fef4395bba
albertmasats <albertmasats@gmail.com>
2021-04-15 18:51:15 +0200

    Create function map

---
+++ albert-masats/challenge/arrays/archivos-test/map.test.js
@@ -0,0 +1,10 @@
+let informacionEntrada = [2,8,3];
+function mapMethod() {
+    for (let i = 0; i < informacionEntrada.length; i++) {
+        informacionEntrada.push(informacionEntrada[i]+10);
+    }
+}
+console.log(informacionEntrada)
+
+
+
