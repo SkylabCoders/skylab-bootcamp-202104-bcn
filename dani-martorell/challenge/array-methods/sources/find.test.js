cb2b6cdf2c543e1e28f8214bcce45de9a85c4fe5
Daniel Martorell <dani.martorell@gmail.com>
2021-04-15 19:30:10 +0200

    Integrate length method inside function

--- dani-martorell/challenge/array-methods/sources/find.test.js
+++ dani-martorell/challenge/array-methods/sources/find.test.js
@@ -1,8 +1,16 @@
 function myFind(array, callback){
-    for (let index = 0; index < array.length; index++) {
+    for (let index = 0; index < myLength(array); index++) {
         const element = array[index];
         if(callback(element, index, array)){
             return element;
         }
     }
+}
+
+function myLength(array){
+    let counter = 0;
+    for (let element of array){
+        counter++;
+    }
+    return counter;
 }
