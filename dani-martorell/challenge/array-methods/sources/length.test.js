69710ed78d6e74d6f8a55c64a2b75fbadc490cfc
Daniel Martorell <dani.martorell@gmail.com>
2021-04-16 00:21:30 +0200

    Add testing to length method

--- dani-martorell/challenge/array-methods/sources/length.test.js
+++ dani-martorell/challenge/array-methods/sources/length.test.js
@@ -6,3 +6,19 @@ function myLength(array){
     return counter;
 }
 
+describe(`Guiven a myLength function`, () => {
+    const scenarios = [
+        { array: [1, 2, 3, 4], result: 4 },
+        { array: [{}, {}, {}], result: 3 },
+        { array: [], result: 0 }
+    ];
+    scenarios.forEach((scenario) => {
+        describe(`When invoked with value ${scenarios.array}`, () => {
+            test(`Then returns ${scenario.result}`, () => {
+                const result = myLength(scenario.array);
+                expect(result).toBe(scenario.result);
+            })
+        })
+    })
+})
+
