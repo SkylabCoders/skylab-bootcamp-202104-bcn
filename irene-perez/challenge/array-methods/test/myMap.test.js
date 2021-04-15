357c23a4ae322c9ca312f6480e98e9659811d118
IrenePerezAlonso <irperal@hotmail.com>
2021-04-15 19:34:42 +0200

    Create map method

---
+++ irene-perez/challenge/array-methods/test/myMap.test.js
@@ -0,0 +1,30 @@
+function myMap(array, callback) {
+
+    for (let i = 0; i < array.length; i++) {
+        const element = callback(array[i]); 
+        array[i] = element;
+    }
+
+    return array;
+}
+
+describe("Given a funcion myMap", () =>{
+    const scenarios = [
+        { array: [1, 5, 10, 15], callback: (number) => number*2, result: [2, 10, 20, 30] }
+    ];
+
+    scenarios.forEach((scenarios) => {
+        describe(`When invoked with value ${scenarios.array}`, () => {test(`Then return ${scenarios.result}`,() => {
+
+                // Arrange
+                const array = scenarios.array; 
+                // Act 
+                const result = myMap(array, scenarios.callback);
+
+                // Assert
+                expect(result).toEqual(scenarios.result);
+            
+            });
+        });
+    });
+})
