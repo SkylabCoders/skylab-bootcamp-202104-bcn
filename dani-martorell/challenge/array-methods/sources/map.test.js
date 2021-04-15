bd9454c59306c76086097f25f8345ca4f2a8a104
Daniel Martorell <dani.martorell@gmail.com>
2021-04-16 00:27:04 +0200

    Add testing to map method

--- dani-martorell/challenge/array-methods/sources/map.test.js
+++ dani-martorell/challenge/array-methods/sources/map.test.js
@@ -8,7 +8,6 @@ function myMap(array, callback){
     return(result);
 }
 
-
 function myPush(array, element){
     return [...array, element];
 }
@@ -20,3 +19,19 @@ function myLength(array){
     }
     return counter;
 }
+
+describe('Given a map function', () => {
+    const scenarios = [
+        { array: [1, 4, 9], callback: function (element){ return element ** 2 }, result: [1, 16, 81] },
+        { array: ['12', '40', '99'], callback: function (element){ return Number(element) }, result: [12, 40, 99] },
+    ];
+    
+    scenarios.forEach((scenario) => {
+        describe(`When invoked with values ${scenario.array} and ${scenario.callback}`, () => {
+            test(`Then return ${scenario.result}`,() => {
+                const result = myMap(scenario.array, scenario.callback);
+                expect(result).toEqual(scenario.result);
+            })
+        })
+    })
+})
