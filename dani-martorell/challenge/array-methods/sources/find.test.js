3863a23ce8a4be60c691b9632c3537a01cc8d383
Daniel Martorell <dani.martorell@gmail.com>
2021-04-16 00:14:15 +0200

    Add testing to find method

--- dani-martorell/challenge/array-methods/sources/find.test.js
+++ dani-martorell/challenge/array-methods/sources/find.test.js
@@ -14,3 +14,22 @@ function myLength(array){
     }
     return counter;
 }
+
+describe(`Guiven a myFind function`, () => {
+    const scenarios = [
+        {array: ['lunes', 'jueves', 'viernes'], callback: (element) => element === 'jueves', result: 'jueves'},
+        {array: [4, 6, 2, 6, -6], callback: (element) => element > 5, result: 6},
+        {array: [{ name: 'John'}, { name: 'Paul'}, { name: 'George' }], callback: (element) => element.name === 'George', result: { name: 'George'}},
+        {array: [4, 6, 2, 6, -6], callback: (element) => element > 6, result: undefined}
+    ];
+    scenarios.forEach((scenario) => {
+        describe(`When invoked with value ${scenarios.array} and ${scenarios.callback}`, () => {
+            test(`Then returns ${scenario.result}`, () => {
+                const result = myFind(scenario.array, scenario.callback);
+                expect(result).toEqual(scenario.result);
+
+            })
+        })
+    
+    })
+})
