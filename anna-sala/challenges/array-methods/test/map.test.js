796f51bca3fa195f4e5df8440d64d38d790a9295
Anna Sala <anna.sala.mercade@gmail.com>
2021-04-15 19:47:15 +0200

    fixed git map to do callbacks

--- anna-sala/challenges/array-methods/test/map.test.js
+++ anna-sala/challenges/array-methods/test/map.test.js
@@ -1,17 +1,16 @@
-
-function annaMap(array, changer) {
-    for (let i=0; i<array.length; i++){
-        array[i]=changer;
-    }
-    return array;
-}
-
+ function annaMap(array, changer) {
+      for (let i=0; i<array.length; i++){
+          array[i] = changer(array[i]);
+      }
+     return array;
+ }
 
 describe('Given a map function', () => {
     const scenarios = [
-        { a:[1,2,3], b: 7, result: [7,7,7]},
-        { a:[1,6,"like"], b: "hola", result: ["hola","hola","hola"]},
-        { a:[1,6,"like"], b: "jike", result: ["jike","jike","jike"]}
+        { a:[1,6,10], b: (x => x*2), result: [2,12,20]},
+        { a: ['one', 'two', 'three'], b: (x => typeof(x) === 'string'), result: [true, true, true]},
+        { a: [2, 3, 4], b: (x => x > 2), result: [false, true, true]},
+        { a: [2, 3, 4], b: (x => x*2), result: [4, 6, 8]}
     ];
     scenarios.forEach((scenario) => {
         describe(`When invoked with array ${scenario.a} and value to change ${scenario.b}`, () => {
@@ -21,7 +20,7 @@ describe('Given a map function', () => {
                 const b = scenario.b;
     
                 // Act 
-                const result = annaMap(a, b);
+                const result = annaMap(a,b);
     
                 // Assert
                 expect(result).toEqual(scenario.result);
