ecfc46c08da97e3c03ae29b1f361cbb28406f015
Daniel Martorell <dani.martorell@gmail.com>
2021-04-15 19:26:34 +0200

    Integrate length method inside function

--- dani-martorell/challenge/array-methods/sources/every.test.js
+++ dani-martorell/challenge/array-methods/sources/every.test.js
@@ -1,5 +1,5 @@
 function myEvery(array, callback){
-    for (let index = 0; index < array.length; index++) {
+    for (let index = 0; index < myLength(array); index++) {
         const element = array[index];
         if(!callback(element, index, array)){
             return false;
@@ -7,3 +7,12 @@ function myEvery(array, callback){
     }
     return true;
 }
+function myLength(array){
+    let counter = 0;
+    for (let element of array){
+        counter++;
+    }
+    return counter;
+}
+
+
