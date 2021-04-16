4c73a961b0d57a9cd069e80cba07f0ff71e449db
Josep Suarez <djose.anton@gmail.com>
2021-04-16 17:35:15 +0200

    empiezp con js

---
+++ josep-suarez/challenge/Game-of-life/app.js
@@ -0,0 +1,13 @@
+const initialArray = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 1, 1, 1, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+];
+function gameOfLife(array) {
+  const returnArray = [];
+  for (let i = 0; i < array.length; i++) {
+    for (let j; j < array.length; j++) {}
+  }
+}
