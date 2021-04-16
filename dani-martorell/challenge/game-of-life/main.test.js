0deee75f1461250f75b33d24a2c509ff84758b87
Daniel Martorell <dani.martorell@gmail.com>
2021-04-16 18:30:41 +0200

    Add populateMatrix function

--- dani-martorell/challenge/game-of-life/main.test.js
+++ dani-martorell/challenge/game-of-life/main.test.js
@@ -1,4 +1,17 @@
-createMatrix = function(files, columns){
+/* RULES
+
+In each cycle: 
+    IF cell isAlive TRUE:
+        IF cellNumOfNeighbours < 1 { cell isAlive FALSE }
+        IF cellNumOfNeighbours > 4 { cell isAlive FALSE }
+        else do not do anything.
+    IF cell isAlive FALSE:
+        IF cellNumOfNeighbours === 3 { cell isAlive TRUE }
+
+*/
+
+
+const createMatrix = (files, columns) => {
     let newMatrix = new Array(files);
     for (let index = 0; index < newMatrix.length; index++) {
         newMatrix[index] = new Array(columns).fill(0); 
@@ -6,6 +19,19 @@ createMatrix = function(files, columns){
     return newMatrix;
 }
 
-
+const populateMatrix = (matrix, file, row) => {
+    if(matrix[file] && matrix[row]) {
+        matrix[file][row] = 1;
+    }
+}
 
 const matrix = createMatrix(5, 5);
+
+;
+
+
+
+matrix
+
+
+
