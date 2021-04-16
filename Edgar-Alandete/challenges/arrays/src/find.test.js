473f597ea1d5eddd8341a544538ca007e737c20a
Edgar Alandete <edgalandete@gmail.com>
2021-04-16 09:12:09 +0200

    Fix find method

--- Edgar-Alandete/challenges/arrays/src/find.test.js
+++ Edgar-Alandete/challenges/arrays/src/find.test.js
@@ -1,9 +1,9 @@
-function myFind(array, value) {
+function myFind(array, callback) {
   let result;
   let isFound = false;
   for (let i = 0; i < array.length && !isFound; i++) {
-    if (array[0] === value) {
-      result = value;
+    if (callback(array[i])) {
+      result = array[i];
       isFound = true;
     }
   }
@@ -15,26 +15,26 @@ describe("Given a myFind function", () => {
   const scenarios = [
     {
       origin: ["true", "true", "true"],
-      value: true,
+      callback: (element) => element === true,
       result: undefined,
     },
 
     {
       origin: [10, 7, 11],
-      value: 7,
+      callback: (element) => element === 7,
       result: 7,
     },
   ];
 
   scenarios.forEach((scenario) => {
-    describe(`When invoked with values ${scenario.origin} with the value ${scenario.value}`, () => {
+    describe(`When invoked with values ${scenario.origin} with the callback ${scenario.callback}`, () => {
       test(`Then return ${scenario.result}`, () => {
         //Arrange
         const origin = scenario.origin;
         const result = scenario.result;
-        const value = scenario.value;
+        const callback = scenario.callback;
         //Act
-        const filterResult = myFind(origin, value);
+        const filterResult = myFind(origin, callback);
         //Assert
         expect(filterResult).toBe(result);
       });
