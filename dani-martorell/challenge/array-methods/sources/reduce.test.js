742f391c59e70c2cb5ac747e85a119a40bf786be
Daniel Martorell <dani.martorell@gmail.com>
2021-04-16 08:47:14 +0200

    Implementation reduce method

--- dani-martorell/challenge/array-methods/sources/reduce.test.js
+++ dani-martorell/challenge/array-methods/sources/reduce.test.js
@@ -1,8 +1,13 @@
-function myReduce(array, callback){
-    const result = [];
-    for (let index = 0; index < array.length; index++) {
+function myReduce(array, callback, initialValue){
+    console.log(initialValue);
+    let accumulator = initialValue || array[0];
+    let startIndex = initialValue ? 0 : 1;
+    initialValue
+    for (let index = startIndex; index < array.length; index++) {
         const element = array[index];
-        callback(element, index, array)
-
+        accumulator = callback(accumulator, element, index, array);
     }
+    return accumulator;
 }
+
+console.log(myReduce([1,6,2,2], (a,b) => a * b), 4);
