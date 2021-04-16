704c388812478ec1d2983b5db9ef14ae466db5e6
Daniel Martorell <dani.martorell@gmail.com>
2021-04-16 09:48:28 +0200

    "Add test to reduce function"

--- dani-martorell/challenge/array-methods/sources/reduce.test.js
+++ dani-martorell/challenge/array-methods/sources/reduce.test.js
@@ -1,13 +1,33 @@
 function myReduce(array, callback, initialValue){
-    console.log(initialValue);
     let accumulator = initialValue || array[0];
     let startIndex = initialValue ? 0 : 1;
-    initialValue
-    for (let index = startIndex; index < array.length; index++) {
+    for (let index = startIndex; index < myLength(array); index++) {
         const element = array[index];
         accumulator = callback(accumulator, element, index, array);
     }
     return accumulator;
 }
 
-console.log(myReduce([1,6,2,2], (a,b) => a * b), 4);
+function myLength(array){
+    let counter = 0;
+    for (let element of array){
+        counter++;
+    }
+    return counter;
+}
+
+describe('Given a Reduce function', () => {
+    const scenarios = [
+        { array: [3,2,5,7], callback: (a,b) => a + b, initialValue: undefined, result: 17 },
+        { array: [3,2,5,7], callback: (a,b) => a + b, initialValue: 1, result: 18 },
+    ];
+    
+    scenarios.forEach((scenario) => {
+        describe(`When invoked with values ${scenario.array} and ${scenario.callback} and ${scenario.initialValue}`, () => {
+            test(`Then return ${scenario.result}`,() => {
+                const result = myReduce(scenario.array, scenario.callback, scenario.initialValue);
+                expect(result).toBe(scenario.result);
+            })
+        })
+    })
+})
