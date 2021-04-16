ec909e213440c680c995df5059cdd0bdcff90cf4
Josep Suarez <djose.anton@gmail.com>
2021-04-16 17:40:18 +0200

    creamos la array inical

--- josep-suarez/challenge/Game-of-life/app.js
+++ josep-suarez/challenge/Game-of-life/app.js
@@ -1,13 +1,14 @@
-const initialArray = [
-  [0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0],
-  [0, 1, 1, 1, 0],
-  [0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0],
-];
+const arrayWidth = 5;
+const arrayHeight = 5;
+const initialArray = [[], [], [], [], []];
 function gameOfLife(array) {
   const returnArray = [];
   for (let i = 0; i < array.length; i++) {
-    for (let j; j < array.length; j++) {}
+    for (let j; j < array.length; j++) {
+      initialArray[i][j] = 0;
+    }
   }
+  return returnArray;
 }
+function checkBeforeArray() {}
+function checkNextArray() {}
