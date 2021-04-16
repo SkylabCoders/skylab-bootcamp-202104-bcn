c5504d0daf7b152cc2961f0e7a856f59ac5dec00
Josep Suarez <djose.anton@gmail.com>
2021-04-16 18:45:43 +0200

    avances

--- josep-suarez/challenge/Game-of-life/app.js
+++ josep-suarez/challenge/Game-of-life/app.js
@@ -1,14 +1,40 @@
-const arrayWidth = 5;
-const arrayHeight = 5;
+let arrayWidth = 5;
+let arrayHeight = 5;
+
 const initialArray = [[], [], [], [], []];
+
+for (let i = 0; i < arrayWidth; i++) {
+  for (let j = 0; j < arrayHeight; j++) {
+    initialArray[i][j] = 0;
+  }
+}
+
 function gameOfLife(array) {
   const returnArray = [];
-  for (let i = 0; i < array.length; i++) {
-    for (let j; j < array.length; j++) {
-      initialArray[i][j] = 0;
-    }
-  }
+
   return returnArray;
 }
-function checkBeforeArray() {}
-function checkNextArray() {}
+function createHorizontalBlinker(x, y, array) {
+  const newArray = [...array];
+  let isSuccesInit = true;
+  isCellEmpty(x, y - 1, array) ? (isSuccesInit = true) : (isSuccesInit = false);
+  isCellEmpty(x, y + 1, array) ? (isSuccesInit = true) : (isSuccesInit = false);
+  isCellEmpty(x, y, array) ? (isSuccesInit = true) : (isSuccesInit = false);
+  if (isSuccesInit) {
+    newArray[x][y + 1] = 1;
+    newArray[x][y - 1] = 1;
+    newArray[x][y] = 1;
+    return newArray;
+  }
+}
+const isCellEmpty = (x, y, array) => {
+  if (array[x][y] !== undefined && array[x][y] === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(initialArray);
+const newArray = createHorizontalBlinker(2, 2, initialArray);
+console.log(newArray);
