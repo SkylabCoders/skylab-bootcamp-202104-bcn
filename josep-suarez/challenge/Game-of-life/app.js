f1ae92e5a1b8e6b7876d21a33519bbdd55597396
Josep Suarez <djose.anton@gmail.com>
2021-04-16 19:33:13 +0200

    function vecinos

--- josep-suarez/challenge/Game-of-life/app.js
+++ josep-suarez/challenge/Game-of-life/app.js
@@ -9,32 +9,58 @@ for (let i = 0; i < arrayWidth; i++) {
   }
 }
 
-function gameOfLife(array) {
+function gameOfLifeBlinker(array) {
   const returnArray = [];
 
   return returnArray;
 }
-function createHorizontalBlinker(x, y, array) {
+function createHorizontalBlinker(row, column, array) {
   const newArray = [...array];
   let isSuccesInit = true;
-  isCellEmpty(x, y - 1, array) ? (isSuccesInit = true) : (isSuccesInit = false);
-  isCellEmpty(x, y + 1, array) ? (isSuccesInit = true) : (isSuccesInit = false);
-  isCellEmpty(x, y, array) ? (isSuccesInit = true) : (isSuccesInit = false);
+  isCellEmpty(row, column - 1, array)
+    ? (isSuccesInit = true)
+    : (isSuccesInit = false);
+  isCellEmpty(row, column + 1, array)
+    ? (isSuccesInit = true)
+    : (isSuccesInit = false);
+  isCellEmpty(row, column, array)
+    ? (isSuccesInit = true)
+    : (isSuccesInit = false);
   if (isSuccesInit) {
-    newArray[x][y + 1] = 1;
-    newArray[x][y - 1] = 1;
-    newArray[x][y] = 1;
+    newArray[row][column + 1] = 1;
+    newArray[row][column - 1] = 1;
+    newArray[row][column] = 1;
     return newArray;
+  } else {
+    alert("Esta ubicacion no es correcta!");
   }
 }
-const isCellEmpty = (x, y, array) => {
-  if (array[x][y] !== undefined && array[x][y] === 0) {
-    return true;
-  } else {
-    return false;
-  }
+const isCellEmpty = (row, column, array) => {
+  return array[row][column] !== undefined && array[row][column] === 0;
 };
 
 console.log(initialArray);
 const newArray = createHorizontalBlinker(2, 2, initialArray);
 console.log(newArray);
+
+const searchLife = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array[i].length; j++) {
+      if (array[i][j] === 1) {
+        countNeighbours(j, i, newArray);
+      }
+    }
+  }
+};
+
+const countNeighbours = (row, column, array) => {
+  let neighbours;
+  array[column - 1][row - 1] === 1 && neighbours++;
+  array[column - 1][row] === 1 && neighbours++;
+  array[column - 1][row + 1] === 1 && neighbours++;
+  array[column][row - 1] === 1 && neighbours++;
+  array[column][row + 1] === 1 && neighbours++;
+  array[column + 1][row - 1] === 1 && neighbours++;
+  array[column + 1][row] === 1 && neighbours++;
+  array[column + 1][row + 1] === 1 && neighbours++;
+};
