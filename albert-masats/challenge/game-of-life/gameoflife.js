10d62d7e6aa034356c2d3fb4892e453f52442d76
albertmasats <albertmasats@gmail.com>
2021-04-17 13:03:07 +0200

    Add neighbours variable

--- albert-masats/challenge/game-of-life/gameoflife.js
+++ albert-masats/challenge/game-of-life/gameoflife.js
@@ -6,10 +6,10 @@ function gameOfLife(array, callback) {
 
 
 arrayGameOfLife =[[0,0,0,0,0],
-                  [0,0,0,0,0],
-                  [0,0,0,0,0],
-                  [0,0,0,0,0],
-                  [0,0,0,0,0]]
+[0,0,0,0,0],
+[0,0,0,0,0],
+[0,0,0,0,0],
+[0,0,0,0,0]];
 
 
 
@@ -21,69 +21,93 @@ arrayGameOfLife =[[0,0,0,0,0],
 
 // if (array[i]+)    let position = [i,j]
 function gof () {
-    count = 0;
+    // let count = 0;
     for (let i = 0; i<arrayGameOfLife.length; i++) {
         for(let j=0; j<arrayGameOfLife[i].length; j++) {
-            
-            if (i === undefined || j+1 ===undefined){
-                break;
-            }else if ([i,j+1] ===1) {
-                count ++
-            }
 
-            if (i=== undefined || j-1 === undefined){
-                break;
-            }else if([i,j-1] ===1){
-                count ++
-            }
-            
-            if(i-1=== undefined || j-1 === undefined){
-                break;
-            }else if ([i-1,j-1] ===1){
-                count ++
-            }
-
-            if (i-1 === undefined || j === undefined){
-                break;
-            }else if ([i-1,j] ===1){
-                count ++
-            }
-
-            if (i-1 === undefined || j+1 === undefined){
-                break;
-            }else if([i-1,j+1] ===1){
-                count ++
-            }
-
-            if (i+1 === undefined || j-1 === undefined){
-                break;
-            }else if([i+1,j-1] ===1){
-                count ++
-            }
-            
-            if (i+1 === undefined || j === undefined){
-                break;
-            }else if ([i+1,j] ===1){
-                count ++
-            }
-            
-            if (i+1 === undefined || j+1 === undefined){
-                break;
-            }else if([i+1,j+1] ===1){
-                count ++
-            }
-            
-            if ([i,j] === 1 && count ===3) {
-                cell = true;
-            }else if ([i,j] === 0 && count > 2){
-                cell = true;
+            if (arrayGameOfLife[i,j] === 1) {
+                console.log("life");
             }else{
-                cell= false;
+                console.log("dead")
             }
 
-            console.log(cell)
+            // let neighbours = 
+            // [[i,j+1],
+            // [i,j-1],
+            // [i-1,j-1],
+            // [i-1,j],
+            // [i-1,j+1],
+            // [i+1,j-1],
+            // [i+1,j],
+            // [i+1,j+1]];
 
-            count=0;
+            // for (let k = 0; k< neighbours.length; k++) {
+            //     if (neighbours[k] === 1) {
+            //         count ++
+            //     }
+            // }
+          
+            
+            // if (i === undefined || j+1 ===undefined){
+            //     break;
+            // }else if ([i,j+1] ===1) {
+            //     count ++
+            // }
+
+            // if (i=== undefined || j-1 === undefined){
+            //     break;
+            // }else if([i,j-1] ===1){
+            //     count ++
+            // }
+            
+            // if(i-1=== undefined || j-1 === undefined){
+            //     break;
+            // }else if ([i-1,j-1] ===1){
+            //     count ++
+            // }
+
+            // if (i-1 === undefined || j === undefined){
+            //     break;
+            // }else if ([i-1,j] ===1){
+            //     count ++
+            // }
+
+            // if (i-1 === undefined || j+1 === undefined){
+            //     break;
+            // }else if([i-1,j+1] ===1){
+            //     count ++
+            // }
+
+            // if (i+1 === undefined || j-1 === undefined){
+            //     break;
+            // }else if([i+1,j-1] ===1){
+            //     count ++
+            // }
+            
+            // if (i+1 === undefined || j === undefined){
+            //     break;
+            // }else if ([i+1,j] ===1){
+            //     count ++
+            // }
+            
+            // if (i+1 === undefined || j+1 === undefined){
+            //     break;
+            // }else if([i+1,j+1] ===1){
+            //     count ++
+            // }
+            
+            // if ([i,j] === 1 && count ===3) {
+            //     cell = true;
+            //     console.log(cell)
+            // }else if ([i,j] === 0 && count > 2){
+            //     cell = true;
+            //     console.log(cell)
+            // }else{
+            //     cell= false;
+            //     console.log(cell)
+            // }
+
+            // count=0;
             
             // if cell=true
             // talcual.push(1)
