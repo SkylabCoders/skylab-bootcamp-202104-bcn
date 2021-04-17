4a004c68fb996abefacacf7aabc9758e45ecc413
IrenePerezAlonso <irperal@hotmail.com>
2021-04-17 17:20:24 +0200

    Finished the checkNeighbours function

--- irene-perez/challenge/Game-of-live/src/main.js
+++ irene-perez/challenge/Game-of-live/src/main.js
@@ -1,9 +1,3 @@
-
-// const init = () => {
-    //     createGrid();
-    //     blinkerCreation();
-    // }
-    
 const createGrid = (rows, columns) => {
     const grid = new Array(rows);
     for (let i = 0; i < rows; i++) {
@@ -11,9 +5,10 @@ const createGrid = (rows, columns) => {
     }
     return grid;
 }
-    
+
 const grid = createGrid(5, 5);
 const newGrid = [...grid];
+
 const coordsCreation = (num1, num2) => {
     return newGrid[num1][num2] = 1;
 }
@@ -24,36 +19,62 @@ const blinkerCreation = () => {
     coordsCreation(3, 2);
     console.log(newGrid);
 }
+
 blinkerCreation();
 
 const checkNeighbours = () => {
     let neighboursCounter = 0;
     for(let i = 0; i < newGrid.length; i++){
-        for (let j = 0; j < newGrid.length; j++){
-            if (newGrid[i][j++] !== undefined && newGrid[i][j++] === 1){
-                neighboursCounter++;
+        for (let j = 0; j < newGrid[i].length; j++){
+
+            if (newGrid[i][j] === 1) {
+                console.log("life");
+            } else {
+                console.log("dead")
             }
-            if (newGrid[i][j--] !== undefined && newGrid[i][j--] === 1){
+
+            if (newGrid[j+1] && newGrid[i][j+1]  === 1) {
                 neighboursCounter++;
+            } 
+            if (newGrid[j-1] && newGrid[i][j-1] === 1) {
+                neighboursCounter++
             }
-            if (newGrid[i++][j] !== undefined && newGrid[i++][j] === 1){
-                neighboursCounter++;
+            if (newGrid[i-1] && newGrid[j-1] && newGrid[i-1][j-1] === 1) {
+                neighboursCounter++
             }
-            if (newGrid[i--][j] !== undefined && newGrid[i--][j] === 1){
-                neighboursCounter++;
+            if (newGrid[i-1] && newGrid[i-1][j] === 1) {
+                neighboursCounter++
             }
-            if (newGrid[i++][j++] !== undefined && newGrid[i++][j++] === 1){
-                neighboursCounter++;
+            if (newGrid[i-1] && newGrid[j+1] && newGrid[i-1][j+1] === 1) {
+                neighboursCounter++
             }
-            if (newGrid[i--][j--] !== undefined && newGrid[i--][j--] === 1){
-                neighboursCounter++;
+            if (newGrid[i+1] && newGrid[j-1] && newGrid[i+1][j-1] === 1) {
+                neighboursCounter++
             }
-            if (newGrid[i++][j--] !== undefined && newGrid[i++][j--] === 1){
-                neighboursCounter++;
+            if (newGrid[i+1] && newGrid[i+1][j] === 1) {
+                neighboursCounter++
             }
-            if (newGrid[i--][j++] !== undefined && newGrid[i--][j++] === 1){
-                neighboursCounter++;
+            if (newGrid[i+1] && newGrid[j+1] && newGrid[i+1][j+1] === 1){
+                neighboursCounter++
             }
+
+            if (newGrid[i][j] === 1 && (neighboursCounter === 2 || neighboursCounter === 3)){
+                newGrid[i][j] = 1;
+                cell = true;
+                console.log(cell)
+            } else if (newGrid[i][j] === 0 && neighboursCounter === 3){
+                cell = true;
+                console.log(cell)
+            } else {
+                cell= false;
+                console.log(cell)
+            }
+            neighboursCounter = 0;
+
         }
     }
 }
+
+
+
+
