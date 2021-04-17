eb40abab7538e3b606235205cde525c57c848353
Isabel Gomez <belasi87@gmail.com>
2021-04-17 13:44:45 +0200

    Test function to check neighbours by undefined cells

--- Isabel-Gomez/challenge/game-of-life/test/index.js
+++ Isabel-Gomez/challenge/game-of-life/test/index.js
@@ -10,8 +10,8 @@ let matrix = createNewMatrix(5, 5);
 console.log('matrix', matrix);
 let newCells = [...matrix];
 
-const replaceCells = (index1, index2) => {
-    newCells[index1][index2] = 1;
+const replaceCells = (row, column) => {
+    newCells[row][column] = 1;
 }
 
 const createOriginalBlinker = () => {
@@ -20,37 +20,43 @@ const createOriginalBlinker = () => {
     replaceCells(3, 2);
 }
 createOriginalBlinker();
+
 console.log('originalBlinker', newCells);
 
 const checkNeighbours = () => {
     let numOfNeighbours = 0;
     for (let row = 0; row < newCells.length; row++) {
         for (let column = 0; column < newCells.length; column++) {
-            if (newCells[row][column++] !== undefined && newCells[row][column++] === 1) {
+            if (newCells[row] && newCells[row][column++] !== undefined && newCells[row][column++] === 1) {
                 numOfNeighbours++;
             }
-            if (newCells[row][column--] !== undefined && newCells[row][column--] === 1) {
+            if (newCells[row] && newCells[row][column--] !== undefined && newCells[row][column--] === 1) {
                 numOfNeighbours++;
             }
-            if (newCells[row++][column] !== undefined && newCells[row++][column] === 1) {
+            if (newCells[row++] && newCells[row++][column] !== undefined && newCells[row++][column] === 1) {
                 numOfNeighbours++;
             }
-            if (newCells[row--][column] !== undefined && newCells[row--][column] === 1) {
+            if (newCells[row--] && newCells[row][column] !== undefined && newCells[row--][column] === 1) {
                 numOfNeighbours++;
             }
-            if (newCells[row++][column++] !== undefined && newCells[row++][column++] === 1) {
+            if (newCells[row++] && newCells[row++][column++] !== undefined && newCells[row++][column++] === 1) {
                 numOfNeighbours++;
             }
-            if (newCells[row++][column--] !== undefined && newCells[row++][column--] === 1) {
+            if (newCells[row++] && newCells[row][column--] !== undefined && newCells[row++][column--] === 1) {
                 numOfNeighbours++;
             }
-            if (newCells[row--][column++] !== undefined && newCells[row--][column++] === 1) {
+            if (newCells[row--] && newCells[row--][column++] !== undefined && newCells[row--][column++] === 1) {
+                numOfNeighbours++;
+            }
+            if (newCells[row--] && newCells[row--][column--] !== undefined && newCells[row--][column--] === 1) {
                 numOfNeighbours++;
             }
         }
     }
+    console.log(numOfNeighbours);
 }
 checkNeighbours();
-console.log('startPlay', newCells);
+
+
 
 
