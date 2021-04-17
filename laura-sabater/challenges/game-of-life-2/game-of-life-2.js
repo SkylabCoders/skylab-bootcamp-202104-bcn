8e6829000f52427455312dcf3b372d76b548d8cc
Laura <laura.sabater.musach@gmail.com>
2021-04-17 20:39:30 +0200

    Modify game-of-life-js and create second game-of-life-2

---
+++ laura-sabater/challenges/game-of-life-2/game-of-life-2.js
@@ -0,0 +1,224 @@
+let matrix = [];
+// const dimension = 20;
+const maxRow = 20;
+const maxColumn = 50;
+const generateMatrix = () => {
+    let matrixResult = [];
+    for(let i=0; i<maxRow; i++) {
+        matrixResult[i] = [];
+        for(let j=0; j<maxColumn; j++) {
+            matrixResult[i][j] = 0;
+        }
+    }
+    return matrixResult;
+}
+const cloneMatrix = () => {
+    return result = [...matrix];
+}
+const paintBlinker = (row, column) => {
+    matrix[row - 1][column] = 1;
+    matrix[row][column] = 1;
+    matrix[row + 1][column] = 1;
+    return matrix;
+}
+const paintGlider = (row, column) => {
+    matrix[row - 1][column] = 1;
+    matrix[row][column + 1] = 1;
+    matrix[row + 1][column -1] = 1;
+    matrix[row + 1][column] = 1;
+    matrix[row + 1][column + 1] = 1;
+    return matrix;
+}
+const startGame = () => {
+    matrix = generateMatrix();
+    generateEnvironmentTable();
+    //paintBlinker(3,3);
+    paintGlider(1,1);
+    // console.log(matrix);
+    // console.log(`2,2 ---> ${collectNeighborHood(2, 2)} with ${numberOfNeighbours(collectNeighborHood(2,2))} neighbours hasTwoOrThree ? ${hasTwoOrThreeNeighbours(numberOfNeighbours(collectNeighborHood(2,2)))} hasThree? ${hasThreeNeighbours(numberOfNeighbours(collectNeighborHood(2,2)))}`);
+    // console.log(`2,3 ---> ${collectNeighborHood(2, 3)} with ${numberOfNeighbours(collectNeighborHood(2,3))} neighbours hasTwoOrThree ? ${hasTwoOrThreeNeighbours(numberOfNeighbours(collectNeighborHood(2,3)))} hasThree? ${hasThreeNeighbours(numberOfNeighbours(collectNeighborHood(2,3)))}`);
+    // console.log(`2,4 ---> ${collectNeighborHood(2, 4)} with ${numberOfNeighbours(collectNeighborHood(2,4))} neighbours hasTwoOrThree ? ${hasTwoOrThreeNeighbours(numberOfNeighbours(collectNeighborHood(2,4)))} hasThree? ${hasThreeNeighbours(numberOfNeighbours(collectNeighborHood(2,4)))}`);
+    // console.log(`3,2 ---> ${collectNeighborHood(3, 2)} with ${numberOfNeighbours(collectNeighborHood(3,2))} neighbours hasTwoOrThree ? ${hasTwoOrThreeNeighbours(numberOfNeighbours(collectNeighborHood(3,2)))} hasThree? ${hasThreeNeighbours(numberOfNeighbours(collectNeighborHood(3,2)))}`);
+    // console.log(`3,3 ---> ${collectNeighborHood(3, 3)} with ${numberOfNeighbours(collectNeighborHood(3,3))} neighbours hasTwoOrThree ? ${hasTwoOrThreeNeighbours(numberOfNeighbours(collectNeighborHood(3,3)))} hasThree? ${hasThreeNeighbours(numberOfNeighbours(collectNeighborHood(3,3)))}`);
+    // console.log(`3,4 ---> ${collectNeighborHood(3, 4)} with ${numberOfNeighbours(collectNeighborHood(3,4))} neighbours hasTwoOrThree ? ${hasTwoOrThreeNeighbours(numberOfNeighbours(collectNeighborHood(3,4)))} hasThree? ${hasThreeNeighbours(numberOfNeighbours(collectNeighborHood(3,4)))}`);
+    // console.log(`4,2 ---> ${collectNeighborHood(4, 2)} with ${numberOfNeighbours(collectNeighborHood(4,2))} neighbours hasTwoOrThree ? ${hasTwoOrThreeNeighbours(numberOfNeighbours(collectNeighborHood(4,2)))} hasThree? ${hasThreeNeighbours(numberOfNeighbours(collectNeighborHood(4,2)))}`);
+    // console.log(`4,3 ---> ${collectNeighborHood(4, 3)} with ${numberOfNeighbours(collectNeighborHood(4,3))} neighbours hasTwoOrThree ? ${hasTwoOrThreeNeighbours(numberOfNeighbours(collectNeighborHood(4,3)))} hasThree? ${hasThreeNeighbours(numberOfNeighbours(collectNeighborHood(4,3)))}`);
+    // console.log(`4,4 ---> ${collectNeighborHood(4, 4)} with ${numberOfNeighbours(collectNeighborHood(4,4))} neighbours hasTwoOrThree ? ${hasTwoOrThreeNeighbours(numberOfNeighbours(collectNeighborHood(4,4)))} hasThree? ${hasThreeNeighbours(numberOfNeighbours(collectNeighborHood(4,4)))}`);
+    timer = setInterval(updateMatrix, 1000);
+}
+const updateMatrix = () => {
+    let arrayOfChanges = gatherChanges();
+    matrix = applyChanges(arrayOfChanges, matrix);
+    // console.log(matrix);
+    updateTable();
+}
+const gatherChanges = () => {
+    let arrayOfChanges = [];
+    for(let i=0; i<maxRow; i++) {
+        for(let j=0; j<maxColumn; j++) {
+            if(stateChanges(i, j)) {
+                arrayOfChanges.push([i,j]);
+            }
+        }
+    }
+    return arrayOfChanges;
+}
+const applyChanges = (arrayOfChanges, matrix) => {
+    //console.log(arrayOfChanges);
+    for(let position of arrayOfChanges){
+        let row = position[0];
+        let column = position[1];
+        // console.log(row);
+        // console.log(column);
+        // console.log(`change ${position[0]} and ${position[1]} in original matrix with current state ${matrix[position[0]][position[1]]}`);
+        if(matrix[position[0]][position[1]] === 1) {
+            matrix[position[0]][position[1]] = 0;
+        } else {
+            matrix[position[0]][position[1]] = 1;
+        }
+    }
+    return matrix;
+}
+const stateChanges = (row, column) => {
+    let cellAlive = isCellAlive(row, column);
+    let result = false;
+    let neighborHood = collectNeighborHood(row, column);
+    if(cellAlive && hasTwoOrThreeNeighbours(neighborHood)) {
+        result = false;
+    } else if (!cellAlive && hasThreeNeighbours(neighborHood)) {
+        result = true;
+    } else if(cellAlive && (numberOfNeighbours(neighborHood) < 2)) {
+        result = true;
+    } else if (cellAlive && (numberOfNeighbours(neighborHood) > 3)) {
+        result = true;
+    }
+    return result;
+}
+const collectNeighborHood = (row, column) => {
+    let neighborHood = [];
+    if (row !== 0 && column !== 0) {
+        neighborHood.push(matrix[row - 1][column - 1]);
+    }
+    if (row !== 0) {
+        neighborHood.push(matrix[row - 1][column]);
+    }
+    if (row !== 0 && column < (maxColumn-1)) {
+        neighborHood.push(matrix[row - 1][column + 1]);
+    }
+    if (column !== 0) {
+        neighborHood.push(matrix[row][column - 1]);
+    }
+    if (column < (maxColumn-1)) {
+        neighborHood.push(matrix[row][column + 1]);
+    }
+    if (row < (maxRow -1) && column !== 0) {
+        neighborHood.push(matrix[row + 1][column - 1]);
+    }
+    if(row < (maxRow -1)){
+        neighborHood.push(matrix[row + 1][column]);
+    }
+    if(row < (maxRow -1) && column < (maxColumn -1)) {
+        neighborHood.push(matrix[row + 1][column + 1]);
+    }
+    // neighborHood[0] = matrix[row - 1][column - 1];
+    // neighborHood[1] = matrix[row - 1][column];
+    // neighborHood[2] = matrix[row - 1][column + 1];
+    // neighborHood[3] = matrix[row][column - 1];
+    // neighborHood[4] = matrix[row][column + 1];
+    // neighborHood[5] = matrix[row + 1][column - 1];
+    // neighborHood[6] = matrix[row + 1][column];
+    // neighborHood[7] = matrix[row + 1][column + 1];
+    //neighborHood = checkNeighbordHood(neighborHood);
+    return neighborHood;
+}
+// const checkNeighbordHood = (neighborHood) => {
+//     let checkedArray = [];
+//     checkedArray = neighborHood.filter(x => x !== undefined);
+//     return checkedArray;
+// }
+const isCellAlive = (row, column) => {
+    let result = false;
+    if (matrix[row][column] === 1) {
+        result = true;
+    }
+    return result;
+}
+const numberOfNeighbours = (neighborHood) => {
+    let result = 0;
+    for (let i = 0; i<neighborHood.length; i++){
+        if (neighborHood[i] === 1) {
+            result++;
+        }
+    }
+    return result;
+}
+// const updateCellStatus = (row, column) => {
+//     let neighborHood = collectNeighborHood(row, column);
+//     let cellAlive = isCellAlive(row, column);
+//     let resultantMatrix = cloneMatrix();
+//     let neighbours = numberOfNeighbours(neighborHood);
+//     console.log(neighbours);
+//     if(cellAlive && hasTwoOrThreeNeighbours(neighborHood)) {
+//         resultantMatrix = reviveCell(row, column);
+//         console.log('Revive!');
+//     } else if (!cellAlive && neighbours === 3/*hasThreeNeighbours(row, column)*/) {
+//         resultantMatrix = reviveCell(row, column, resultantMatrix);
+//         console.log('Revive');
+//     } else {
+//         resultantMatrix = killCell(row, column, resultantMatrix);
+//     }
+//     return resultantMatrix;
+// }
+const hasTwoOrThreeNeighbours = (neighborHood) => {
+    let neighboursNumber = numberOfNeighbours(neighborHood);
+    let result = false;
+    if(neighboursNumber === 2 || neighboursNumber === 3){
+        result = true;
+    }
+    return result;
+}
+const hasThreeNeighbours = (neighborHood) => {
+    let result = false;
+    if(numberOfNeighbours(neighborHood) === 3) {
+        result = true;
+    }
+    return result;
+}
+const killCell = (row, column, matrixResult) => {
+    matrixResult[row][column] = 0;
+    return matrixResult;
+}
+const reviveCell = (row, column, matrixResult) => {
+    matrixResult[row][column] = 1;
+    return matrixResult;
+}
+//clearInterval(timer);
+window.addEventListener("load", startGame);
+// startGame();
+const environmentTable = document.getElementById("environment");
+const environmentRow = document.getElementsByClassName("environment__row");
+
+const generateEnvironmentTable = () => {
+    for (let i = 0; i < maxRow; i++) {
+        const environmentRow = document.createElement("tr");
+        environmentRow.classList = "environment__row";
+        for (let j = 0; j<maxColumn; j++) {
+            const environmentSpot = document.createElement("td");
+            environmentSpot.innerHTML = 0;
+            environmentRow.appendChild(environmentSpot);   
+        }
+        environmentTable.appendChild(environmentRow);
+    }
+}
+
+const updateTable = () => {
+    for (let i=0; i< maxRow; i++) {
+        for (let j=0; j<maxColumn; j++) {
+            if (matrix[i][j] === 1) {
+                environmentRow[i].childNodes[j].style = "background: blue";
+            } else {
+                environmentRow[i].childNodes[j].style = "background: grey";
+            }
+        }
+    }
+}
--- laura-sabater/challenges/game-of-life/game-of-life.js
+++ laura-sabater/challenges/game-of-life/game-of-life.js
@@ -1,36 +1,100 @@
-let cellMatrix = [
-  [],
-  [],
-  []
-];
+let environment = [
+  [1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1],
+  [1, 0, 0, 0, 1, 1],
+  [1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1]
+]
+
+let selectedCellPosition = environment[2][2];
+
+let neighbours = [
+  environment[1][1],
+  environment[1][2],
+  environment[1][3],
+  environment[2][1],
+  environment[2][3],
+  environment[3][1],
+  environment[3][2],
+  environment[3][3],
+]
+
+let numberAliveNeighbours = countAliveNeighbours(neighbours);
+
+function showCells(cells) {
+	for (let line of cells) {
+		console.log(line.join(" "));
+		console.log("\n");
+	}
+	console.log("\n");
+}
 
 function cellState(cell) {
-  if (cell === 1) {
+  if (cell === 0) {
     return true;
-  } else {
+  } if (cell === 1) {
     return false;
   }
 }
 
-function countLiveCells(cellMatrix){
-  for (let cellArray of cellMatrix) {
-    for (let cell of cellArray) {
-      let cellNeighbours = function;
-      let cellAlive = cellState(cell); 
-      let aliveNeighbours = cellNeighbours.filter((cell) => cell(cellState));
-      if (cellAlive) {
-        if (aliveNeighbours.length < 2) {
-          cellAlive = false;
-        } else if (aliveNeighbours > 3) {
-          cellAlive = true;
-        }
-      } else {
-        if (aliveNeighbours.length = 3) {
-          cellAlive = true;
-        } else {
-          cellAlive = false;
-        }
-      }
+// function defineNeighbours(selectedCell, matrix) {
+//   let neighbours = [];
+
+//   neighbour = matrix[1,1]
+
+
+// }
+// console.log(defineNeighbours(selectedCell, environment));
+
+function countAliveNeighbours(neighbours, environment) {
+  let aliveNeighbours = [];
+  for (let neighbour of neighbours) {
+    neighbour = cellState(cell);
+    if (neighbour) {
+      aliveNeighbours.push(neighbour);
+    }
+  }
+  return aliveNeighbours.length;
+}
+
+function nextState (cell, aliveCells) {
+  let aliveCell = cellState(cell);
+  if (aliveCell) {
+    if (aliveCells.length < 2) {
+      return false;
+    } else if (aliveCells > 3) {
+      return 0;
+    } else {
+      return 1;
+    }
+  } else {
+    if (aliveCells.length === 3) {
+      return 0;
+    } else {
+      return 1;
     }
   }
 }
+
+function nextEnvironment (cellsMatrix, aliveCells) {
+  let newEnvironment =[];
+  for (let cellsArray of cellsMatrix) {
+    let newArray = [];
+    for (let cell of cellsArray) {
+      cell = nextState(cell, aliveCells);
+      newArray.push(cell);
+    }
+    newEnvironment.push(newArray);
+  }
+  return newEnvironment;
+}
+let newEnvironment = nextEnvironment(environment, aliveNeighbours);
+
+showCells(environment);
+showCells(neighbours);
+console.log(selectedCell);
+console.log(cellState(selectedCell));
+console.log(countAliveNeighbours(neighbours));
+console.log(nextState(selectedCell, aliveNeighbours));
+showCells(newEnvironment);
