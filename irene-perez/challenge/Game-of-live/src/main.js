43e810e8ce8c34f25d5e55658c9277d9e528b0e2
IrenePerezAlonso <irperal@hotmail.com>
2021-04-17 13:33:04 +0200

    created checkNeighbours

--- irene-perez/challenge/Game-of-live/src/main.js
+++ irene-perez/challenge/Game-of-live/src/main.js
@@ -1,6 +1,9 @@
-const grid = createGrid(5, 5);
-const newGrid = [...grid];
 
+// const init = () => {
+    //     createGrid();
+    //     blinkerCreation();
+    // }
+    
 const createGrid = (rows, columns) => {
     const grid = new Array(rows);
     for (let i = 0; i < rows; i++) {
@@ -8,11 +11,11 @@ const createGrid = (rows, columns) => {
     }
     return grid;
 }
-
-console.log(grid);
-
+    
+const grid = createGrid(5, 5);
+const newGrid = [...grid];
 const coordsCreation = (num1, num2) => {
-    newGrid[num1][num2];
+    return newGrid[num1][num2] = 1;
 }
 
 const blinkerCreation = () => {
@@ -20,4 +23,37 @@ const blinkerCreation = () => {
     coordsCreation(2, 2);
     coordsCreation(3, 2);
     console.log(newGrid);
+}
+blinkerCreation();
+
+const checkNeighbours = () => {
+    let neighboursCounter = 0;
+    for(let i = 0; i < newGrid.length; i++){
+        for (let j = 0; j < newGrid.length; j++){
+            if (newGrid[i][j++] !== undefined && newGrid[i][j++] === 1){
+                neighboursCounter++;
+            }
+            if (newGrid[i][j--] !== undefined && newGrid[i][j--] === 1){
+                neighboursCounter++;
+            }
+            if (newGrid[i++][j] !== undefined && newGrid[i++][j] === 1){
+                neighboursCounter++;
+            }
+            if (newGrid[i--][j] !== undefined && newGrid[i--][j] === 1){
+                neighboursCounter++;
+            }
+            if (newGrid[i++][j++] !== undefined && newGrid[i++][j++] === 1){
+                neighboursCounter++;
+            }
+            if (newGrid[i--][j--] !== undefined && newGrid[i--][j--] === 1){
+                neighboursCounter++;
+            }
+            if (newGrid[i++][j--] !== undefined && newGrid[i++][j--] === 1){
+                neighboursCounter++;
+            }
+            if (newGrid[i--][j++] !== undefined && newGrid[i--][j++] === 1){
+                neighboursCounter++;
+            }
+        }
+    }
 }
