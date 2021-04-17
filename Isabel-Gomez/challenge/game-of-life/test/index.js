59fc4e6274ea58a8fe94d08e2ccf886f30ac99c5
Isabel Gomez <belasi87@gmail.com>
2021-04-17 11:30:15 +0200

    Create function to generate a copy of matrix

--- Isabel-Gomez/challenge/game-of-life/test/index.js
+++ Isabel-Gomez/challenge/game-of-life/test/index.js
@@ -5,60 +5,74 @@ const createNewMatrix = (row, column) => {
     }
     return newArray;
 };
+
 let matrix = createNewMatrix(5, 5);
+console.log('matrix', matrix);
+let newCells = [...matrix];
 
-const createOriginalBlinker = (index1, index2) => {
-    console.log(matrix[index1][index2]);
-    matrix[index1][index2].fill(1)
-    // matrix.splice(matrix[index1][index2], 1, 1)
-
-
-    console.log(matrix)
+const replaceCells = (index1, index2) => {
+    newCells[index1][index2] = 1;
 }
-createOriginalBlinker(1, 2);
 
-
-const originBlinker = [
-    [0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0],
-    [0, 0, 1, 0, 0],
-    [0, 0, 1, 0, 0],
-    [0, 0, 0, 0, 0]
-];
-
-const newBlinker = [
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 1, 1, 1, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0]
-]
-
-const numOfNeighbours = () => {
-    let index = 0;
-    let subIndex = 0;
-    let position = originBlinker[index][subIndex];
-    console.log('primera', position);
-
-    originBlinker.forEach
-
+const createOriginalBlinker = () => {
+    replaceCells(1, 2);
+    replaceCells(2, 2);
+    replaceCells(3, 2);
 }
-numOfNeighbours();
+createOriginalBlinker();
+console.log('originalBlinker', newCells);
 
 
 
 
-/* ------------------------------ */
 
-function playBlinker(originBlinker) {
-    originBlinker.forEach((position) => {
-        position.map((subPosition) => {
-            numOfNeighbours();
-            stateOfNeighbours();
-            return newBlinker;
-        })
-    })
-};
+
+
+
+
+
+
+
+// const originBlinker = [
+//     [0, 0, 0, 0, 0],
+//     [0, 0, 1, 0, 0],
+//     [0, 0, 1, 0, 0],
+//     [0, 0, 1, 0, 0],
+//     [0, 0, 0, 0, 0]
+// ];
+
+// const newBlinker = [
+//     [0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0],
+//     [0, 1, 1, 1, 0],
+//     [0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0]
+// ]
+
+// const numOfNeighbours = () => {
+//     let index = 0;
+//     let subIndex = 0;
+//     let position = originBlinker[index][subIndex];
+
+//     originBlinker.forEach
+
+// }
+// numOfNeighbours();
+
+
+
+
+// /* ------------------------------ */
+
+// function playBlinker(originBlinker) {
+//     originBlinker.forEach((position) => {
+//         position.map((subPosition) => {
+//             numOfNeighbours();
+//             stateOfNeighbours();
+//             return newBlinker;
+//         })
+//     })
+// };
 
 
 
