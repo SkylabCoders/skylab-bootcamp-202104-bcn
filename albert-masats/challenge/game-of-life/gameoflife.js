0657b94fbc04ff821375b8a10b38b88ed9a53540
albertmasats <albertmasats@gmail.com>
2021-04-17 17:20:15 +0200

    Create new array

--- albert-masats/challenge/game-of-life/gameoflife.js
+++ albert-masats/challenge/game-of-life/gameoflife.js
@@ -1,15 +1,37 @@
-function gameOfLife(array, callback) {
+// function gameOfLife(array, callback) {
 
-    if(array[0,0])
+//     if(array[0,0])
     
+// }
+
+const createGrid = (rows, columns) => {
+    const grid = new Array(rows);
+    for (let i = 0; i < rows; i++) {
+        grid[i] = new Array(columns).fill('0');
+    }
+    return grid;
 }
 
+const grid = createGrid(5, 5);
+const newGrid = [...grid];
 
-arrayGameOfLife =[[0,0,0,0,0],
-[0,0,0,0,0],
-[0,0,0,0,0],
-[0,0,0,0,0],
-[0,0,0,0,0]];
+const coordsCreation = (num1, num2) => {
+    return newGrid[num1][num2] = 1;
+}
+const blinkerCreation = () => {
+    coordsCreation(1, 2);
+    coordsCreation(2, 2);
+    coordsCreation(3, 2);
+    console.log(newGrid);
+}
+blinkerCreation();
+
+
+// arrayGameOfLife =[[0,0,0,0,0],
+// [0,0,0,0,0],
+// [0,0,0,0,0],
+// [0,0,0,0,0],
+// [0,0,0,0,0]];
 
 
 
@@ -20,95 +42,71 @@ arrayGameOfLife =[[0,0,0,0,0],
 // if (array[i] === 0 && array[i+5] === 0)
 
 // if (array[i]+)    let position = [i,j]
-function gof () {
-    // let count = 0;
+// let neighbours = 
+    // [arrayGameOfLife[i][j+1],
+    // arrayGameOfLife[i][j-1],
+    // arrayGameOfLife[i-1][j-1],
+    // arrayGameOfLife[i-1][j],
+    // arrayGameOfLife[i-1][j+1],
+    // arrayGameOfLife[i+1][j-1],
+    // arrayGameOfLife[i+1][j],
+    // arrayGameOfLife[i+1][j+1]];
+
+function gol () {
+    let count = 0;
     for (let i = 0; i<arrayGameOfLife.length; i++) {
         for(let j=0; j<arrayGameOfLife[i].length; j++) {
 
-            if (arrayGameOfLife[i,j] === 1) {
+            if (arrayGameOfLife[i][j] === 1) {
                 console.log("life");
             }else{
                 console.log("dead")
             }
 
-            // let neighbours = 
-            // [[i,j+1],
-            // [i,j-1],
-            // [i-1,j-1],
-            // [i-1,j],
-            // [i-1,j+1],
-            // [i+1,j-1],
-            // [i+1,j],
-            // [i+1,j+1]];
+            if (arrayGameOfLife[j+1] && arrayGameOfLife[i][j+1]  === 1) {
+                count++;
+            } 
+            if (arrayGameOfLife[j-1] && arrayGameOfLife[i][j-1] ===1) {
+                count++
+            }
+            if (arrayGameOfLife[i-1] && arrayGameOfLife[j-1] && arrayGameOfLife[i-1][j-1] ===1) {
+                count++
+            }
+            if (arrayGameOfLife[i-1] && arrayGameOfLife[i-1][j] ===1) {
+                count++
+            }
+            if (arrayGameOfLife[i-1] && arrayGameOfLife[j+1] && arrayGameOfLife[i-1][j+1] ===1) {
+                count++
+            }
+            if (arrayGameOfLife[i+1] && arrayGameOfLife[j-1] && arrayGameOfLife[i+1][j-1] ===1) {
+                count++
+            }
+            if (arrayGameOfLife[i+1] && arrayGameOfLife[i+1][j] ===1) {
+                count++
+            }
+            if (arrayGameOfLife[i+1] && arrayGameOfLife[j+1] && arrayGameOfLife[i+1][j+1] ===1){
+                count++
+            }
 
-            // for (let k = 0; k< neighbours.length; k++) {
-            //     if (neighbours[k] === 1) {
-            //         count ++
-            //     }
-            // }
-          
-            
-            // if (i === undefined || j+1 ===undefined){
-            //     break;
-            // }else if ([i,j+1] ===1) {
-            //     count ++
-            // }
+            if (arrayGameOfLife[i][j] === 1 && (count ===2 || count === 3 )) {
+                arrayGameOfLife[i][j] = 1
+                cell = true;
+                console.log(cell)
+            }else if (arrayGameOfLife[i][j] === 0 && count === 3){
+                cell = true;
+                console.log(cell)
+            }else{
+                cell= false;
+                console.log(cell)
+            }
 
-            // if (i=== undefined || j-1 === undefined){
-            //     break;
-            // }else if([i,j-1] ===1){
-            //     count ++
-            // }
-            
-            // if(i-1=== undefined || j-1 === undefined){
-            //     break;
-            // }else if ([i-1,j-1] ===1){
-            //     count ++
-            // }
+            count=0;
 
-            // if (i-1 === undefined || j === undefined){
-            //     break;
-            // }else if ([i-1,j] ===1){
-            //     count ++
-            // }
+            nuevoArray.push(arrayGameOfLife[i][j])
+            
+            
 
-            // if (i-1 === undefined || j+1 === undefined){
-            //     break;
-            // }else if([i-1,j+1] ===1){
-            //     count ++
-            // }
 
-            // if (i+1 === undefined || j-1 === undefined){
-            //     break;
-            // }else if([i+1,j-1] ===1){
-            //     count ++
-            // }
-            
-            // if (i+1 === undefined || j === undefined){
-            //     break;
-            // }else if ([i+1,j] ===1){
-            //     count ++
-            // }
-            
-            // if (i+1 === undefined || j+1 === undefined){
-            //     break;
-            // }else if([i+1,j+1] ===1){
-            //     count ++
-            // }
-            
-            // if ([i,j] === 1 && count ===3) {
-            //     cell = true;
-            //     console.log(cell)
-            // }else if ([i,j] === 0 && count > 2){
-            //     cell = true;
-            //     console.log(cell)
-            // }else{
-            //     cell= false;
-            //     console.log(cell)
-            // }
-
-            // count=0;
-            
             // if cell=true
             // talcual.push(1)
             // if cell=false
