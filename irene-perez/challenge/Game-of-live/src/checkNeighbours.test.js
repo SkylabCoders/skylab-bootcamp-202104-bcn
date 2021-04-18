51a97bc22253582d823bc440bb684cee1034c2cb
IrenePerezAlonso <irperal@hotmail.com>
2021-04-18 17:54:52 +0200

    Trying to fixed the test of the function checkNeighbours but didn't work

--- irene-perez/challenge/Game-of-live/src/checkNeighbours.test.js
+++ irene-perez/challenge/Game-of-live/src/checkNeighbours.test.js
@@ -1,48 +1,49 @@
 let newGrid = [[0, 0, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 1, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 0, 0]];
-const checkNeighbours = () => {
+let checkNeighbours = (i, j, newGrid) => {
     let neighboursCounter = 0;
 
-    if (newGrid[j+1] && newGrid[i][j+1]  === 1) {
+    if (newGrid[i + 1] && newGrid[i + 1][j - 1] === 1) {
+        neighboursCounter++;
+    }
+    if (newGrid[i + 1] && newGrid[i + 1][j] === 1) {
+        neighboursCounter++;
+    }
+    if (newGrid[i + 1] && newGrid[i + 1][j + 1] === 1) {
+        neighboursCounter++;
+    }
+    if (newGrid[i] && newGrid[i][j - 1] === 1) {
+        neighboursCounter++;
+    }
+    if (newGrid[i - 1] && newGrid[i - 1][j - 1] === 1) {
+        neighboursCounter++;
+    }
+    if (newGrid[i - 1] && newGrid[i - 1][j] === 1) {
+        neighboursCounter++;
+    }
+    if (newGrid[i - 1] && newGrid[i - 1][j + 1] === 1) {
+        neighboursCounter++;
+    }
+    if (newGrid[i] && newGrid[i][j + 1] === 1) {
         neighboursCounter++;
     } 
-    if (newGrid[j-1] && newGrid[i][j-1] === 1) {
-        neighboursCounter++
-    }
-    if (newGrid[i-1] && newGrid[j-1] && newGrid[i-1][j-1] === 1) {
-        neighboursCounter++
-    }
-    if (newGrid[i-1] && newGrid[i-1][j] === 1) {
-        neighboursCounter++
-    }
-    if (newGrid[i-1] && newGrid[j+1] && newGrid[i-1][j+1] === 1) {
-        neighboursCounter++
-    }
-    if (newGrid[i+1] && newGrid[j-1] && newGrid[i+1][j-1] === 1) {
-        neighboursCounter++
-    }
-    if (newGrid[i+1] && newGrid[i+1][j] === 1) {
-        neighboursCounter++
-    }
-    if (newGrid[i+1] && newGrid[j+1] && newGrid[i+1][j+1] === 1){
-        neighboursCounter++
-    }  
+    return neighboursCounter;
 }
 
 describe("Given a checkNeighbours function", () => {
     const scenarios = [
-        { a: newGrid[j+1], b: newGrid[i][j+1] === 1, results: neighboursCounter++,},
-        { a: newGrid[j-1], b: newGrid[i][j-1] === 1, results: neighboursCounter++,},
-        { a: newGrid[i-1], b: newGrid[j-1], c: newGrid[i-1][j-1] === 1, results: neighboursCounter++,},
-        { a: newGrid[i-1], b: newGrid[i-1][j], results: neighboursCounter++,},
-        { a: newGrid[i-1], b: newGrid[j+1], c: newGrid[i-1][j+1] === 1, results: neighboursCounter++,},
-        { a: newGrid[i+1], b: newGrid[j-1], c:newGrid[i+1][j-1] === 1, results: neighboursCounter++,},
-        { a: newGrid[i+1], b: newGrid[i+1][j] === 1, results: neighboursCounter++,},
-        { a: newGrid[i+1], b: newGrid[j+1], c: newGrid[i+1][j+1] === 1, results: neighboursCounter++,}
+        { a: newGrid[2+1], b: newGrid[1][2+1] === 1, results: 0,},
+        { a: newGrid[2-1], b: newGrid[1][2-1] === 1, results: 0,},
+        { a: newGrid[1-1], b: newGrid[2-1], c: newGrid[1-1][2-1] === 1, results: 0,},
+        { a: newGrid[1-1], b: newGrid[1-1][2], results: 1,},
+        { a: newGrid[1-1], b: newGrid[2+1], c: newGrid[1-1][2+1] === 1, results: 0,},
+        { a: newGrid[1+1], b: newGrid[2-1], c:newGrid[1+1][2-1] === 1, results: 0,},
+        { a: newGrid[1+1], b: newGrid[1+1][2] === 1, results: 0,},
+        { a: newGrid[1+1], b: newGrid[2+1], c: newGrid[1+1][2+1] === 1, results: 0,}
     ];
     scenarios.forEach(scenario => {
-        describe(`When invoked with values ${scenario.num1} and ${scenario.num2}`, () => {
+        describe(`When invoked with values ${scenario.a} and ${scenario.b}`, () => {
             test(`Then return ${scenario.results}`, () => {
-                const result = checkNeighbours(scenario.num1, scenario.num2);
+                const result = checkNeighbours(scenario.a, scenario.b);
                 expect(result).toStrictEqual(scenario.results);
             });
         });
