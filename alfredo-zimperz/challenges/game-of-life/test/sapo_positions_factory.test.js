edf88b3412160eaff835a6b964499b06ab918d94
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-04-18 12:42:42 +0200

    Test vBlinker-positions-function

--- alfredo-zimperz/challenges/game-of-life/test/sapo_positions_factory.test.js
+++ alfredo-zimperz/challenges/game-of-life/test/sapo_positions_factory.test.js
@@ -1,8 +1,11 @@
-function vBlinker_positions_factory(row, column) {
+function sapo_positions_factory(row, column) {
 	let positions = [
 		[row - 1, column],
+		[row + 1, column],
 		[row, column],
-		[row + 1, column]
+		[row, column - 1],
+		[row + 1, column - 1],
+		[row + 2, column - 1]
 	]
 	return positions
 }
@@ -14,22 +17,28 @@ describe('Given a map function', () => {
 			column: 2,
 			result: [
 				[3, 2],
+				[5, 2],
 				[4, 2],
-				[5, 2]
+				[4, 1],
+				[5, 1],
+				[6, 1]
 			]
 		},
 		{
-			row: 3,
-			column: 3,
+			row: 1,
+			column: 2,
 			result: [
-				[2, 3],
-				[3, 3],
-				[4, 3]
+				[0, 2],
+				[2, 2],
+				[1, 2],
+				[1, 1],
+				[2, 1],
+				[3, 1]
 			]
 		}
 	]
 	scenarios.forEach((scenario) => {
-		describe(`When invoked a function to create a shep with initial coordinates row:${scenario.row}, column:${scenario.column}  and
+		describe(`When invoked a function to evaluate a matrix with coordinates row:${scenario.row}, column:${scenario.column}  and
         a matrix: ${scenario.matrix}`, () => {
 			test(`Then return ${scenario.result}`, () => {
 				// Arrange
@@ -37,7 +46,7 @@ describe('Given a map function', () => {
 				const column = scenario.column
 
 				// Act
-				const result = vBlinker_positions_factory(row, column)
+				const result = sapo_positions_factory(row, column)
 				// Assert
 				expect(result).toStrictEqual(scenario.result)
 			})
