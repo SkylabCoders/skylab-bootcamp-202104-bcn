19fe4c1cbed08628f6e7a2c44da0202e414d4605
Anna Sala <anna.sala.mercade@gmail.com>
2021-04-18 17:18:04 +0200

    started testing checkAlive

--- anna-sala/challenges/game-of-life/src/gameoflife.js
+++ anna-sala/challenges/game-of-life/src/gameoflife.js
@@ -1,120 +1,156 @@
 let neighbours = 0;
-let rows= 6;
-let colums=6;
-const currentGeneration = [
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0],
-    [0, 1, 0, 0, 1, 0],
-    [0, 1, 0, 0, 1, 0],
-    [0, 0, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-]
-let nextGeneration= [];
-let body = document.body
+let rows = 6;
+let colums = 6;
+let created = false;
+let currentGeneration = [
+  [0, 0, 0, 0, 0, 0],
+  [0, 1, 1, 0, 0, 0],
+  [0, 1, 1, 0, 0, 0],
+  [0, 0, 0, 1, 1, 0],
+  [0, 0, 0, 1, 1, 0],
+  [0, 0, 0, 0, 0, 0],
+];
+let nextGeneration = [];
+let body = document.body;
 let td;
 let tr;
 let tbl;
+setInterval(runGame, 1000);
+
 const gridCopy = (currentGeneration) => {
-    for (let i = 0; i<currentGeneration.length; i++) {
-        nextGeneration.push([...currentGeneration[i]])
-    }
-    return nextGeneration;
-}
-function tableCreate(rows, colums){
-    tbl  = document.createElement('table');
-    for(let i = 0; i < rows; i++){
-        tr = tbl.insertRow();
-        for(let j = 0; j < colums; j++){
-                td = tr.insertCell();
-                td.appendChild(document.createTextNode(currentGeneration[i][j]));
-        }
+  for (let i = 0; i < currentGeneration.length; i++) {
+    nextGeneration.push([...currentGeneration[i]]);
+  }
+  return nextGeneration;
+};
+function tableCreate(rows, colums) {
+  if (!created) {
+    tbl = document.createElement("table");
+    for (let i = 0; i < rows; i++) {
+      tr = tbl.insertRow();
+      for (let j = 0; j < colums; j++) {
+        td = tr.insertCell();
+        td.appendChild(document.createTextNode(currentGeneration[i][j]));
+      }
     }
     body.appendChild(tbl);
-    console.log("current", currentGeneration);
-    setTimeout(function(){printNewGeneration(rows, colums); }, 3000);
-}
-tableCreate(rows, colums);
-
-function printNewGeneration(rows, colums){
-    var items = document.getElementsByTagName('table')[0].children;
-    console.log(items);
-    for(let i = 0; i < rows; i++){
-        let row=items[0].children[i];
-        for(let j = 0; j < colums; j++){
-            row.children[j].innerHTML=nextGeneration[i][j];
-        }
-    }
+    created = true;
+  }
 }
 
-gridCopy(currentGeneration);
-checkAlive(rows, colums, currentGeneration);
-
-function checkAlive (rows, colums, array){
-    for (let i=0; i<rows; i++){
-        for (let j=0; j<colums; j++){
-            if (array[i][j]===1){
-                checkNeighbours(array, i, j);
-                console.log(neighbours, "neighbours after function");
-                killCell(nextGeneration, i, j, neighbours);
-            }
-            else {
-                checkNeighbours(array,i,j);
-                console.log(neighbours, "(dead) neighbours after function");
-                reviveCell(nextGeneration, i, j, neighbours);
-            }
-        }
+function printNewGeneration(rows, colums) {
+  var items = document.getElementsByTagName("table")[0].children;
+  for (let i = 0; i < rows; i++) {
+    let row = items[0].children[i];
+    for (let j = 0; j < colums; j++) {
+      row.children[j].innerHTML = nextGeneration[i][j];
     }
-    return nextGeneration;
+  }
 }
 
-function checkNeighbours(array, row, column){
-    
-    neighbours = 0;
-    if (cellExists(array[row-1]) && cellExists(array[column-1]) && array[row-1][column-1]=== 1){
-        neighbours++;
+function checkAlive(rows, colums, array) {
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < colums; j++) {
+      if (array[i][j] === 1) {
+        checkNeighbours(array, i, j);
+        killCell(nextGeneration, i, j, neighbours);
+      } else {
+        checkNeighbours(array, i, j);
+        reviveCell(nextGeneration, i, j, neighbours);
+      }
     }
-    if (cellExists(array[row-1]) && cellExists(array[column]) && array[row-1][column]=== 1){
-        neighbours++;
-    }
-    if (cellExists(array[row-1]) && cellExists(array[column+1]) && array[row-1][column+1]=== 1){
-        neighbours++;
-    }
-    if (cellExists(array[row]) && cellExists(array[column-1]) && array[row][column-1]=== 1){
-        neighbours++;
-    }
-    if (cellExists(array[row]) && cellExists(array[column+1]) && array[row][column+1]=== 1){
-        neighbours++;
-    }
-    if (cellExists(array[row+1]) && cellExists(array[column-1]) && array[row+1][column-1]=== 1){
-        neighbours++;
-    }
-    if (cellExists(array[row+1]) && cellExists(array[column]) && array[row+1][column]=== 1){
-        neighbours++;
-    }
-    if (cellExists(array[row+1]) && cellExists(array[column+1]) && array[row+1][column+1]=== 1){
-        neighbours++;
-    }
-    return neighbours;
+  }
+  return nextGeneration;
 }
 
-function cellExists(cell){
-    if (cell!==undefined){
-        return true;
-    }
+function checkNeighbours(array, row, column) {
+  neighbours = 0;
+  if (
+    cellExists(array[row - 1]) &&
+    cellExists(array[column - 1]) &&
+    array[row - 1][column - 1] === 1
+  ) {
+    neighbours++;
+  }
+  if (
+    cellExists(array[row - 1]) &&
+    cellExists(array[column]) &&
+    array[row - 1][column] === 1
+  ) {
+    neighbours++;
+  }
+  if (
+    cellExists(array[row - 1]) &&
+    cellExists(array[column + 1]) &&
+    array[row - 1][column + 1] === 1
+  ) {
+    neighbours++;
+  }
+  if (
+    cellExists(array[row]) &&
+    cellExists(array[column - 1]) &&
+    array[row][column - 1] === 1
+  ) {
+    neighbours++;
+  }
+  if (
+    cellExists(array[row]) &&
+    cellExists(array[column + 1]) &&
+    array[row][column + 1] === 1
+  ) {
+    neighbours++;
+  }
+  if (
+    cellExists(array[row + 1]) &&
+    cellExists(array[column - 1]) &&
+    array[row + 1][column - 1] === 1
+  ) {
+    neighbours++;
+  }
+  if (
+    cellExists(array[row + 1]) &&
+    cellExists(array[column]) &&
+    array[row + 1][column] === 1
+  ) {
+    neighbours++;
+  }
+  if (
+    cellExists(array[row + 1]) &&
+    cellExists(array[column + 1]) &&
+    array[row + 1][column + 1] === 1
+  ) {
+    neighbours++;
+  }
+  return neighbours;
 }
 
-function killCell(array, positionRow, positionColumn, neighbours){
-    if (neighbours<2 || neighbours>3){
-        array[positionRow][positionColumn]=0;
-    }
+function cellExists(cell) {
+  if (cell !== undefined) {
+    return true;
+  }
 }
 
-function reviveCell (array, positionRow, positionColumn, neighbours){
-    if (neighbours===3){
-        array[positionRow][positionColumn]=1;
-    }
-    
-
-
+function killCell(array, positionRow, positionColumn, neighbours) {
+  if (neighbours < 2 || neighbours > 3) {
+    array[positionRow][positionColumn] = 0;
+  }
 }
-console.log(nextGeneration);
+
+function reviveCell(array, positionRow, positionColumn, neighbours) {
+  if (neighbours === 3) {
+    array[positionRow][positionColumn] = 1;
+  }
+}
+
+function runGame() {
+  gridCopy(currentGeneration);
+  tableCreate(rows, colums);
+  checkAlive(rows, colums, currentGeneration);
+  printNewGeneration(rows, colums);
+  restartGeneration();
+}
+
+function restartGeneration() {
+  currentGeneration = nextGeneration;
+  nextGeneration = [];
+}
