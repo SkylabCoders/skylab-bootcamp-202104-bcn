b56452cc29555bdca6f28983493554688b6097ab
Daniel Martorell <dani.martorell@gmail.com>
2021-04-18 12:27:33 +0200

    Create createMatrix test file

---
+++ dani-martorell/challenge/game-of-life/test/createMatrix.test.js
@@ -0,0 +1,8 @@
+const createMatrix = (numberRows, numberColumns) => {
+    let newMatrix = new Array(numberRows);
+    for (let index = 0; index < newMatrix.length; index++) {
+        newMatrix[index] = new Array(numberColumns).fill(0); 
+    }
+    return newMatrix;
+}
+
