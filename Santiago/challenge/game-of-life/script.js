d29409cd6f410d1848a9ccb1bb2f1545d3e1fd30
Santiago.Martinez <infohere52@gmail.com>
2021-04-19 15:23:35 +0200

    Add some corrections in gol script.js

--- Santiago/challenge/game-of-life/script.js
+++ Santiago/challenge/game-of-life/script.js
@@ -1,96 +1,85 @@
-let createGrid = (rows, cols) => {
-    let grid = new Array(rows);
-    for (let i = 0; i < rows; i++) {
-        grid[i] = new Array(cols).fill(0);
-    }
-    return grid;
-}
-let fillGrid = createGrid(5, 5);
-let modifiedGrid = [...fillGrid];
+const createGrid = (rows, cols) => {
+  const grid = new Array(rows);
+  for (let i = 0; i < rows; i += 1) {
+    grid[i] = new Array(cols).fill(0);
+  }
+  return grid;
+};
+const fillGrid = createGrid(5, 5);
+const modifiedGrid = [...fillGrid];
 
-let cellPosition = (a, b) => {
-    return modifiedGrid[a][b] = 1;
-}
+const cellPosition = (a, b) => modifiedGrid[a][b] = 1;
 
+const createBlinker = () => {
+  cellPosition(1, 2);
+  cellPosition(2, 2);
+  cellPosition(3, 2);
 
-let createBlinker = () => {
-    cellPosition(1, 2);
-    cellPosition(2, 2);
-    cellPosition(3, 2);
-
-    return modifiedGrid;
-}
-let blinker = createBlinker();
+  return modifiedGrid;
+};
+const blinker = createBlinker();
 console.log(blinker);
 
+const findNeighbours = (a, b) => {
+  let neighbours = 0;
 
+  if (modifiedGrid[a + 1] && modifiedGrid[a + 1][b - 1] === 1) {
+    neighbours += 1;
+  }
+  if (modifiedGrid[a + 1] && modifiedGrid[a + 1][b] === 1) {
+    neighbours += 1;
+  }
+  if (modifiedGrid[a + 1] && modifiedGrid[a + 1][b + 1] === 1) {
+    neighbours += 1;
+  }
+  if (modifiedGrid[a] && modifiedGrid[a][b - 1] === 1) {
+    neighbours += 1;
+  }
+  if (modifiedGrid[a - 1] && modifiedGrid[a - 1][b - 1] === 1) {
+    neighbours += 1;
+  }
+  if (modifiedGrid[a - 1] && modifiedGrid[a - 1][b] === 1) {
+    neighbours += 1;
+  }
+  if (modifiedGrid[a - 1] && modifiedGrid[a - 1][b + 1] === 1) {
+    neighbours += 1;
+  }
+  if (modifiedGrid[a] && modifiedGrid[a][b + 1] === 1) {
+    neighbours += 1;
+  }
 
-let findNeighbours = (a, b, modifiedGrid) => {
+  return neighbours;
+};
+const neighboursNumber = findNeighbours(2, 2, modifiedGrid);
 
-    let neighbours = 0;
+const rules = (a, b, modifiedGrid, neighboursNumber) => {
+  if (neighboursNumber < 2) {
+    modifiedGrid[a][b] = 0;
+  }
+  if (neighboursNumber === 2 || neighboursNumber === 3) {
+    modifiedGrid[a][b] = 1;
+  }
+  if (neighboursNumber > 3) {
+    modifiedGrid[a][b] = 0;
+  }
+};
 
-    if (modifiedGrid[a + 1] && modifiedGrid[a + 1][b - 1] === 1) {
-        neighbours++;
-    }
-    if (modifiedGrid[a + 1] && modifiedGrid[a + 1][b] === 1) {
-        neighbours++;
-    }
-    if (modifiedGrid[a + 1] && modifiedGrid[a + 1][b + 1] === 1) {
-        neighbours++;
-    }
-    if (modifiedGrid[a] && modifiedGrid[a][b - 1] === 1) {
-        neighbours++;
-    }
-    if (modifiedGrid[a - 1] && modifiedGrid[a - 1][b - 1] === 1) {
-        neighbours++;
-    }
-    if (modifiedGrid[a - 1] && modifiedGrid[a - 1][b] === 1) {
-        neighbours++;
-    }
-    if (modifiedGrid[a - 1] && modifiedGrid[a - 1][b + 1] === 1) {
-        neighbours++;
-    }
-    if (modifiedGrid[a] && modifiedGrid[a][b + 1] === 1) {
-        neighbours++;
-    }
+const checkEveryCell = () => {
+  const rowBlinker = createGrid(5, 5);
 
-    return neighbours;
-}
-let neighboursNumber = findNeighbours(2, 2, modifiedGrid);
+  modifiedGrid.forEach((row, indexRow) => {
+    row.forEach((column, indexColumn) => {
+      const neighboursOfEveryCell = findNeighbours(indexRow, indexColumn, modifiedGrid);
 
-
-let rules = (a, b, modifiedGrid, neighboursNumber) => {
-
-    if (neighboursNumber < 2) {
-        modifiedGrid[a][b] = 0;
-    }
-    if (neighboursNumber === 2 || neighboursNumber === 3) {
-        modifiedGrid[a][b] = 1;
-    }
-    if (neighboursNumber > 3) {
-        modifiedGrid[a][b] = 0;
-    }
-}
-
-let checkEveryCell = modifiedGrid => {
-
-    let rowBlinker = createGrid(5, 5);
-
-    modifiedGrid.forEach((row, indexRow) => {
-        row.forEach((column, indexColumn) => {
-
-            let neighboursOfEveryCell = findNeighbours(indexRow, indexColumn, modifiedGrid);
-
-            if (modifiedGrid[indexRow][indexColumn] === 1) {
-                rules(indexRow, indexColumn, rowBlinker, neighboursOfEveryCell);
-
-            } else if (modifiedGrid[indexRow][indexColumn] === 0 && neighboursOfEveryCell === 3) {
-                rowBlinker[indexRow][indexColumn] = 1;
-            }
-        });
+      if (modifiedGrid[indexRow][indexColumn] === 1) {
+        rules(indexRow, indexColumn, rowBlinker, neighboursOfEveryCell);
+      } else if (modifiedGrid[indexRow][indexColumn] === 0 && neighboursOfEveryCell === 3) {
+        rowBlinker[indexRow][indexColumn] = 1;
+      }
     });
+  });
 
-    return rowBlinker;
-}
-let checkCell = checkEveryCell(modifiedGrid);
+  return rowBlinker;
+};
+const checkCell = checkEveryCell(modifiedGrid);
 console.log(checkCell);
