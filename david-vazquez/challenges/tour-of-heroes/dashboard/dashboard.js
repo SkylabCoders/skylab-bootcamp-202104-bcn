39d742a8a96df1812fa767ff9946bddae92b5a60
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-04-19 18:35:38 +0200

    Dashboard content finished with js

--- david-vazquez/challenges/tour-of-heroes/dashboard/dashboard.js
+++ david-vazquez/challenges/tour-of-heroes/dashboard/dashboard.js
@@ -11,7 +11,59 @@ const heroes = [
     { id: 20, name: 'Tornado' }
     ]
 
+function generateTitle (){
+    const container = document.querySelector('.container');
+    const title = document.createElement('h1');
+    container.appendChild(title);
+    title.className='title';
+    title.innerText = 'Tour of Heroes';
+    generateNav(container);
+    generateDashboard(container);
 
-const container = document.querySelector('.container');
-const title = container.createElement('h1');
-title.innerText = 'Tour of Heroes';
+}
+
+function generateNav (container){
+    const nav = document.createElement('nav');
+    container.appendChild(nav);
+    nav.className='nav';
+    generateNavButton(nav,'Dashboard');
+    generateNavButton(nav,'Heroes');
+}
+
+function generateNavButton (nav, text){
+    const button = document.createElement('button');
+    button.innerText = text;
+    nav.appendChild(button);
+    button.className='nav__button';
+}
+
+function generateDashboard (container){
+    const dashboardContainer = document.createElement('div');
+    container.appendChild(dashboardContainer);
+    dashboardContainer.className='dashboard';
+    const dashboardTitle = document.createElement('h2');
+    dashboardContainer.appendChild(dashboardTitle);
+    dashboardTitle.className='dashboard__title';
+    dashboardTitle.innerText='Top Heroes';
+    generateList(dashboardContainer);
+    
+}
+
+function generateList(dashboardContainer){
+    const dashboardList = document.createElement('ul');
+    dashboardContainer.appendChild(dashboardList);
+    dashboardList.className='dashboard__list';
+    generateItems(dashboardList,'Narco');
+    generateItems(dashboardList,'Bombasto');
+    generateItems(dashboardList,'Celeritas');
+    generateItems(dashboardList,'Magneta');
+
+}
+
+function generateItems(dashboardList, text){
+    const dashboardItems = document.createElement('li');
+    dashboardList.appendChild(dashboardItems);
+    dashboardItems.className='list__items';
+    dashboardItems.innerText=text;
+}
+
