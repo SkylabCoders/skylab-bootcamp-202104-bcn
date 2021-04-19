2320dece0402ec94fe179149d3f5e68798ca17bf
blopez-dev <pjgl.frontend@gmail.com>
2021-04-19 09:02:54 +0200

    upated all files project

--- Pablo-Garcia/challenges/challenge-pablo-garcia-game-of-life/js/app.js
+++ Pablo-Garcia/challenges/challenge-pablo-garcia-game-of-life/js/app.js
@@ -1,26 +1,166 @@
-const tableGame = (rows, cell)=> {
-    let table = document.createElement('table');
-    table.setAttribute('id','gridLife');
-    for(let i=0; i<rows; i++){
-        let row = document.createElement('tr');
-        row.classList.add('row');
-        for(let j=0; j<cell; j++){
-            let col = document.createElement('td');
-            col.setAttribute('state', `id_${j}`);
-            col.setAttribute('class', `dead`);
-            col.classList.add('cell');
-            row.appendChild(col);
+
+const initSetting ={
+    rows: 38,
+    cols: 100,
+    playing: false,
+    timePlay: 100,
+
+}
+let timer;
+const grid = new Array(initSetting.rows);
+const copyGrid = new Array(initSetting.rows);
+
+const createTable = () =>{
+    const gridTable = document.querySelector('#app');
+    const table = document.createElement('table');
+    for(let i=0; i<initSetting.rows; i++){
+        const row = document.createElement('tr');
+        for(let j=0; j<initSetting.cols; j++){
+            const cols = document.createElement('td');
+            cols.setAttribute("id", i + "_" + j);
+            cols.setAttribute("class", "dead");
+            cols.onclick = cellHandleClick;
+            row.appendChild(cols);
         }
         table.appendChild(row);
     }
-    return table;
+    gridTable.appendChild(table);
+}
+const initGrid = () =>{
+    for(let i = 0; i< initSetting.rows; i++){
+        grid[i] = new Array(initSetting.cols);
+        copyGrid[i] = new Array(initSetting.cols)
+    }
+}
+const controls = () =>{
+    const play = document.querySelector('#start');
+    start.onclick = playHandleClick;
+}
+function cellHandleClick(){
+    const cellCoordinantes = this.id.split("_");
+
+    const cellCoordinateX = cellCoordinantes[0];
+    const cellCoordinateY = cellCoordinantes[1];
+
+    const getClassCell = this.getAttribute('class');
+    if(getClassCell.indexOf("live") > -1){
+        this.setAttribute("class", "dead");
+        grid[cellCoordinateX][cellCoordinateY]=0;
+    }else{
+        this.setAttribute("class", "live");
+        grid[cellCoordinateX][cellCoordinateY]=0;
+    }
+}
+function playHandleClick(){
+
+    if(initSetting.playing){
+        initSetting.playing = false;
+    }else{
+        for(let i=0; i< initSetting.rows; i++){
+            for(let j=0; j< initSetting.cols; j++){
+                const isAlive = Math.round(Math.random());
+                if(isAlive === 1){
+                    const cell = document.getElementById(i + "_" + j);
+                    cell.setAttribute("class", "live");
+                    grid[i][j] = 1;
+                }
+            }
+        }
+        initSetting.playing = true;
+        playGame();
+    }
+
+
+}
+function playGame(){
+    evaluateNextGrid();
+    /* if(initSetting.playing){
+         timer = setTimeout(playGame, initSetting.timePlay);
+     }*/
+
+}
+const evaluateNextGrid = () => {
+    for (let i = 0; i < initSetting.rows; i++) {
+        for (let j = 0; j < initSetting.cols; j++) {
+            gameRules(i, j);
+        }
+    }
+    copyAndReset();
+    updateView();
+}
+const updateView = () =>{
+    for(let i=0; i < initSetting.rows; i++) {
+        for (let j = 0; j < initSetting.cols; j++) {
+            let cell = document.getElementById(`${i}_${j}`);
+
+            if(copyGrid[i][j] === 0){
+                cell.setAttribute("class", "" +
+                    "¡¡'");
+            }
+            /*    if (grid[i][j] === 1) {
+
+                } else {
+                    cell.setAttribute("class", "live");
+                }*/
+
+        }
+    }
 }
 
+const copyAndReset = () =>{
+    for(let i=0; i<initSetting.rows; i++){
+        for(let j=0; j<initSetting.cols; j++){
+            grid[i][j] = copyGrid[i][j];
+            copyGrid[i][j] = 0;
+        }
+    }
+}
+const gameRules = (rowIndex, colIndex) => {
 
-const initGame = () => {
-    const table = tableGame(40, 80);
-    const printable = document.querySelector('#app');
-    printable.appendChild(table);
+    const neighbors = countNeighbors(rowIndex, colIndex);
+
+    if (grid[rowIndex][colIndex] === 1) {
+        if (neighbors < 2) {
+            copyGrid[rowIndex][colIndex] = 0;
+        } else if (neighbors === 2 || neighbors === 3) {
+            copyGrid[rowIndex][colIndex] = 1;
+        } else if (neighbors > 3) {
+            copyGrid[rowIndex][colIndex] = 0;
+        }
+    } else if (grid[rowIndex][colIndex] === 0) {
+        if (neighbors === 3) {
+            copyGrid[rowIndex][colIndex] = 1;
+        }
+    }
+}
+const countNeighbors = (rowIndex, colIndex) => {
+    let count = 0;
+    if(rowIndex-1 >= 0){
+        if(grid[rowIndex-1][colIndex] === 1) count++;
+    }
+    if(rowIndex-1 >= 0 && colIndex-1 >=0){
+        if(grid[rowIndex-1][colIndex-1] === 1) count++;
+    }
+    if(rowIndex-1 >= 0 && colIndex+1 < initSetting.cols){
+        if(grid[rowIndex-1][colIndex+1] === 1) count++;
+    }if(colIndex-1 >=0){
+        if(grid[rowIndex][colIndex-1] === 1) count++;
+    }if(colIndex+1 <initSetting.cols){
+        if(grid[rowIndex][colIndex+1] === 1) count++;
+    }if(rowIndex+1 < initSetting.rows){
+        if(grid[rowIndex+1][colIndex] === 1) count++;
+    }if(rowIndex+1 < rowIndex && colIndex-1 >=0){
+        if(grid[rowIndex+1][colIndex-1] === 1) count++;
+    }if(rowIndex+1 < initSetting.rows && colIndex+1 < initSetting.cols){
+        if(grid[rowIndex+1][colIndex+1] === 1) count++;
+    }
+    return count;
+}
+const initGame = ()=>{
+    createTable();
+    initGrid();
+    controls();
+
 }
 
 window.onload = initGame;
--- Pablo-Garcia/precurso/bingoGame/app.js
+++ Pablo-Garcia/precurso/bingoGame/app.js
@@ -1,178 +1,57 @@
-/* User logic Game*/
-const initialScore = 100;
-function initPlayer(initialScore){
-    const scoring = initialScore;
-    const userName = prompt("What's your name?");
-    if(userName === null) {
-        initPlayer();
-    }else{
-        console.log(`Hi ${userName}, your score is: ${scoring}`);
-        return {userName, scoring, matched:false, numberBall:null, line:false, bingo:false};
-    }
-}
-/* Ranking players*/
-/*Numbers logic Game*/
-const generateRandomNumber = (max) => { const indexNumber = Math.floor(Math.random() * (max -1));return indexNumber;}
-function generateNumbers(min, max){
-    const numbers = [];
-    for(let i=min; i<=max; i++){
-        numbers.push(i);
-    }
-    return numbers;
-}
-function generateIndexNumbers(numbers){
-    const index = generateRandomNumber(numbers.length);
-    let number = numbers[index];
-    numbers.splice(index, 1);
-    return [number, numbers];
-}
 
-function generateNumberTable(numbers, rows, columns){
+const generateMatrix = (rows, columns)=> {
     const table = [];
     for(let i=0; i<rows; i++){
-        const row = [];
+        const  row = [];
         for(let j=0; j<columns; j++){
-            let number = 0;
-            [number, numbers] = generateIndexNumbers(numbers);
-            row.push(number)
+            const col = 0;
+            row.push(col);
         }
-        table.push(row);
+        table.push(row)
     }
     return table;
 }
-function printTable(table, rows, columns, printBingoCard){
-    let tableHtml = '<table>';
-    for(let i=0; i<rows; i++){
-        tableHtml += '<tr class="row-cell">';
-        for(let j=0; j<columns; j++){
-            tableHtml += `<td id="${table[i][j]}" class="column-cell">`;
-            tableHtml += table[i][j];
-            tableHtml += '</td>';
-        }
-    }
-    tableHtml += '</table>';
-    /*printBingoCard.innerHTML  = tableHtml;*/
-}
-function generateBingoCard(rows, columns){
-    /* const printBingoCard = document.getElementById('app');*/
-    const numbers = generateNumbers(1,100);
-    const table = generateNumberTable(numbers,3,5);
-    printTable(table, rows, columns);
-    console.log('Your bingo card is:')
-    console.table(table);
-    return table;
-}
-/*Init Logic Game*/
-const rows = {
-    0: 0,
-    1: 0,
-    2: 0,
-}
-const balls = generateNumbers(1,100);
-const players = [];
-function rankingPlayers (name, score) {
-    name && score ? players.push({name, score, }) : null;
-    players.sort((a, b) =>  (a.score < b.score) ? 1 : -1);
-    players.forEach((item, index) => {console.log(`Position:,  ${index += 1}, Player Name: ${item.name}, Points: ${item.score} `)})
-    return players;
-}
-function showRules(){
-    console.log(`
-   1. The game starts with 100 points.
-   2. Each turn subtracts 1 point.
-   3. When you complete a line on the card completely, you will have a line and add 15 points.
-   4. By completing the entire card, you will get bingo, add 20 points and the game will end. 
-   5. At the end of each game, the player ranking will be displayed.`);
-}
-function initPlay(user, numbers){
-    const table = generateBingoCard(3,5);
-    setTimeout(() => {
-        const isAgree = window.confirm('Do you like this cardboard?');
+const createBlinker = (table) =>{
+    const copyTable = [...table];
 
-        if(isAgree !== true){ initPlay(user, numbers) }
-        else{
-            showRules();
-            const startGame = window.confirm('Do you want to start playing?');
+    copyTable[1][2] = 1;
+    copyTable[2][2] = 1;
+    copyTable[3][2] = 1;
+    return copyTable;
 
-            startGame ? play(user, numbers, table) : endGame(user);
-        }}, );
 }
 
-function generateRandomBall(){
-    const indexBall = generateRandomNumber(balls.length);
-    const ball = balls[indexBall];
-    balls.splice(indexBall, 1);
-    return [ball, balls];
-}
+const gameRules = (copyTable) => {
+    copyTable.forEach((fila, filaIndex) => {
+        fila.forEach((columna, columnaIndex) => {
 
-function isInTable(table,ball){
-    let values = null;
-    table.forEach((row, rowIndex) => {
-        const colIndex = row.indexOf(ball);
-        if(colIndex !== -1){
-            values = {rowIndex, colIndex };
-        }
+            const counterNeighbors = evaluateElement(copyTable, filaIndex, columnaIndex)
+            console.log(filaIndex, columnaIndex,counterNeighbors)
+
+        })
     })
-    return values;
 }
+const evaluateElement = (matrixTable, filaIndex, columnaIndex)=> {
+    let counter = 0;
 
-const hasLine = rows => Object.values(rows).find(foundLine => foundLine === 5);
-const hasBingo = rows => Object.values(rows).reduce((a, b) => a + b);
+    /*   if(matrixTable[columnaIndex] && matrixTable[columnaIndex][filaIndex-1] && matrixTable[columnaIndex][filaIndex-1] === 1){
+           console.log(4)
+          counter++
+       }*/
 
-function play(user, numbers, table){
-    const [ball]= generateRandomBall();
-    user.numberBall = ball;
-    const found = isInTable(table, user.numberBall);
-    if(hasBingo(rows) < 15){
-        if(found){
-            const { rowIndex } = found;
-            rows[rowIndex] += 1;
-            user.matched = true;
+    if( matrixTable[columnaIndex][filaIndex-1] === 1){
 
-            /*document.getElementById(ball).innerHTML = 'X';*/
-            table.forEach(item => {
-                for(let i=0; i<item.length; i++){
-                    item[i] === ball ? item[i] = 'X' : null;
-                }
-            });
-            console.table(table);
-
-            if(!user.line && hasLine(rows)){
-                console.log('Line');
-                user.line = true;
-                user.scoring += 15;
-            }
-        }else{
-            user.matched = false;
-            user.scoring -= 1;
-            user.numberBall = ball;
-        }
-        setTimeout(() => {
-            const continuePlaying = window.confirm(`Do you want to continue playing ${user.userName} ?`);
-            continuePlaying ? play(user, numbers, table) : endGame(user, players);
-        }, 800)
-    }else{
-        alert('BINGO!');
-        user.bingo = true;
-        user.scoring += 20;
-        endGame(user, players);
+        counter = counter +1;
     }
-    console.log( `Your score: ${user.scoring}, The ball is: ${user.numberBall}, and checked with your carton is: ${user.matched} `);
+    /*    console.log('CONTADOR ' + counter)*/
+    return counter
 }
-
-function endGame(user){
-    console.log('%c Rankin players -> ', 'background: #222; color: #bada55')
-    rankingPlayers(user.userName, user.scoring);
-
-    user.bingo ? console.log(`Congratulations ${user.userName}, you have BINGO with a score of ${user.scoring}`) :
-    console.log(`Thanks for playing ${user.userName}, your score is:${user.scoring}`);
-    const restartGame = window.confirm('Do you like restart game?');
-    restartGame ? initGame() : null;
-}
-/*Init Program*/
-function initGame(){
-    const user = initPlayer();
-    initPlay(user);
+const initGame = () =>{
+    const rows = 5;
+    const columns = 5;
+    const matrix = generateMatrix(rows, columns);
+    const copyMatrix = createBlinker(matrix);
+    gameRules(copyMatrix)
 }
 
 window.onload = initGame;
