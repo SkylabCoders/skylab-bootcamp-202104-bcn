e1b72262092b613ec8b052b367afb0147c43b114
albertmasats <albertmasats@gmail.com>
2021-04-19 11:45:42 +0200

    Add corrections

--- albert-masats/challenge/game-of-life/gameoflife.js
+++ albert-masats/challenge/game-of-life/gameoflife.js
@@ -1,12 +1,12 @@
 
-let count = 0;
+let countNeighbour = 0;
 
 const createGrid = (rows, columns) => {
     let grid = new Array(rows);
     for (let i = 0; i < rows; i++) {
         grid[i] = new Array(columns).fill(0);
     }
-    return grid
+    return grid;
 }
 
 let arrayGameOfLife = createGrid(5,5);
@@ -25,21 +25,17 @@ const blinkerCreation = () => {
 blinkerCreation();
 
 function gol () {
-    
-    console.log(arrayGameOfLife)
     for(let i = 0; i<arrayGameOfLife.length; i++) {
         for(let j =0; j<arrayGameOfLife[i].length; j++) {
-            
             checkneighbours(i,j);
-            checkConditions(i,j)
-            
+            checkConditions(i,j);
         } 
     }
     changeArray();
-}    
+    return arrayGameOfLife;
+} 
 
 let timerGol = () => {
-
     setInterval(
        gol
     , 4000);
@@ -48,7 +44,7 @@ let timerGol = () => {
 let changeArray = () => {
     for (let i=0; i<grid2.length; i++) {
         for (let j=0; j<grid2[i].length; j++) {
-            arrayGameOfLife[i][j] = grid2 [i][j]
+            arrayGameOfLife[i][j] = grid2 [i][j];
         }
     }
 }
@@ -56,42 +52,42 @@ let changeArray = () => {
 
 let checkneighbours = function (i,j) {
     if (arrayGameOfLife[j+1] && arrayGameOfLife[i][j+1]  === 1) {
-        count++
+        countNeighbour++;
     } 
     if (arrayGameOfLife[j-1] && arrayGameOfLife[i][j-1] ===1) {
-        count++
+        countNeighbour++;
     }
     if (arrayGameOfLife[i-1] && arrayGameOfLife[j-1] && arrayGameOfLife[i-1][j-1] ===1) {
-        count++
+        countNeighbour++;
     }
     if (arrayGameOfLife[i-1] && arrayGameOfLife[i-1][j] ===1) {
-        count++
+        countNeighbour++;
     }
     if (arrayGameOfLife[i-1] && arrayGameOfLife[j+1] && arrayGameOfLife[i-1][j+1] ===1) {
-        count++
+        countNeighbour++;
     }
     if (arrayGameOfLife[i+1] && arrayGameOfLife[j-1] && arrayGameOfLife[i+1][j-1] ===1) {
-        count++
+        countNeighbour++;
     }
     if (arrayGameOfLife[i+1] && arrayGameOfLife[i+1][j] ===1) {
-        count++
+        countNeighbour++;
     }
     if (arrayGameOfLife[i+1] && arrayGameOfLife[j+1] && arrayGameOfLife[i+1][j+1] ===1){
-        count++
+        countNeighbour++;
     }
 }
 
 let checkConditions = function (i,j) {
     if (arrayGameOfLife[i][j] === 1){ 
-        if(count <= 1 || count >= 4){
-            grid2[i][j] = 0
+        if(countNeighbour <= 1 || countNeighbour >= 4){
+            grid2[i][j] = 0;
         }else{
-            grid2[i][j] = 1
+            grid2[i][j] = 1;
         }
     }else{
-        if(count === 3){
-            grid2[i][j] = 1
+        if(countNeighbour === 3){
+            grid2[i][j] = 1;
         }
     }
-    count = 0
+    countNeighbour = 0;
 }
