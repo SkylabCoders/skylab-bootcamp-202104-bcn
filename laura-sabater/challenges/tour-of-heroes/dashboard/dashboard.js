931033665307422ff4a5c89f2a56eea578badd82
Laura <laura.sabater.musach@gmail.com>
2021-04-19 20:10:32 +0200

    Create a divs

--- laura-sabater/challenges/tour-of-heroes/dashboard/dashboard.js
+++ laura-sabater/challenges/tour-of-heroes/dashboard/dashboard.js
@@ -5,11 +5,15 @@ const generateMainTitle = () => {
 };
 generateMainTitle();
 const generateMenu = () => {
+  const menuOptions = [{ name: 'Dashboard', link: 'dashboard.html' }, { name: 'Heroes', link: 'heroes.html' }];
   const menu = document.createElement('ul');
   menu.className = 'menu';
   for (let i = 0; i < 2; i += 1) {
     const nav = document.createElement('li');
-    nav.innerHTML = 'Dashboard';
+    nav.innerHTML = menuOptions[i].name;
+    const link = document.createElement('a');
+    link.href = menuOptions[i].link;
+    nav.appendChild(link);
     menu.appendChild(nav);
   }
   document.body.appendChild(menu);
@@ -21,3 +25,14 @@ const generateTitle2 = () => {
   document.body.appendChild(title2);
 };
 generateTitle2();
+const generateHeroesMenu = () => {
+  const heroesMenu = document.createElement('ul');
+  heroesMenu.className = 'heroes-menu';
+  for (let i = 0; i < 4; i += 1) {
+    const nav = document.createElement('li');
+    nav.innerHTML = 'Narco';
+    heroesMenu.appendChild(nav);
+  }
+  document.body.appendChild(heroesMenu);
+};
+generateHeroesMenu();
