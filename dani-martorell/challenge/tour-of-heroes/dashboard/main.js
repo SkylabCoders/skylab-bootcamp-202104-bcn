61ad4cb810f10243d30b472b7908f58795ac702f
Daniel Martorell <dani.martorell@gmail.com>
2021-04-19 19:35:21 +0200

    Create generateElement function

--- dani-martorell/challenge/tour-of-heroes/dashboard/main.js
+++ dani-martorell/challenge/tour-of-heroes/dashboard/main.js
@@ -10,27 +10,41 @@ const heroes = [
   { id: 19, name: 'Magma' },
   { id: 20, name: 'Tornado' },
 ];
+
 const mainContent = document.querySelector('.content');
-const headTitle = document.createElement('p');
-const selectorGroup = document.createElement('div');
-const navigatorDashboard = document.createElement('a');
-const navigatorHeroes = document.createElement('a');
-const mainTitle = document.createElement('h3');
-const heroesUl = document.createElement('ul');
+// const selectorGroup = document.createElement('div');
+// const navigatorDashboard = document.createElement('a');
+// const navigatorHeroes = document.createElement('a');
+// const mainTitle = document.createElement('h3');
+// const heroesUl = document.createElement('ul');
 
-function populateList(array) {
-
+function generateElement(typeOfElement, classNamesArray = '', context = '', hrefURL = '#') {
+  const element = document.createElement(typeOfElement);
+  element.classList.add(...classNamesArray);
+  if (typeOfElement === 'a') {
+    element.setAttribute('href', hrefURL);
+  }
+  element.textContent = context;
+  return element;
 }
+const headTitle = generateElement('div', ['head-title'], 'Tour of Heroes');
+mainContent.append(headTitle);
+const selectorGroup = generateElement('div', ['selector-group']);
+mainContent.append(selectorGroup);
+const linkDashboard = generateElement('a', ['selector-group__link-dasboard'], 'Dashboard', 'index.html');
+selectorGroup.append(linkDashboard);
+const linkHeroes = generateElement('a', ['selector-group__link-heroes'], 'Heroes', './../heroes/index.html');
+selectorGroup.append(linkHeroes);
+const mainTitle = generateElement('h3', [], 'Top Heroes');
+mainContent.append(mainTitle);
 
-headTitle.textContent = 'Tour of Heroes';
-mainContent.appendChild(headTitle);
-navigatorDashboard.setAttribute('href', 'index.html');
-navigatorDashboard.textContent = 'Navigator';
-navigatorDashboard.className = 'selector-group__navigator-dashboard';
-navigatorHeroes.setAttribute('href', './../heroes/index.html');
-navigatorHeroes.textContent = 'Heroes';
-navigatorHeroes.className = 'selector-group__navigator-heroes';
-selectorGroup.className = 'selector-group';
-selectorGroup.append(navigatorDashboard);
-selectorGroup.append(navigatorHeroes);
-mainContent.appendChild(selectorGroup);
+// function createheadTitle(name) {
+//   const headTitle = document.createElement('p');
+//   headTitle.textContent = 'Tour of Heroes';
+//   mainContent.appendChild(headTitle);
+// }
+// function createTitle(name) {
+//   const mainTitle = document.createElement('h3');
+//   mainTitle.textContent = name;
+//   mainContent.appendChild(mainTitle);
+// }
