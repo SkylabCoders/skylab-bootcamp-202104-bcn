761706d5ff104be86d68aaa285631e824c5f0e51
Gaspar Rodriguez <gaspar.unlp@gmail.com>
2021-04-19 14:20:03 +0200

    ESlint in main.js

--- Gaspar-R/challenge/game-of-life/script/main.js
+++ Gaspar-R/challenge/game-of-life/script/main.js
@@ -1,197 +1,187 @@
 const arrayWidth = 82;
 const arrayHeight = 70;
-
 let initialArray = [];
 let killerArray = [];
 let resurrectionArray = [];
-let nextGenArray = [];
+const nextGenArray = [];
 let lifeCicles = 0;
-let time ;
+let time;
 
-function createMatrix (width,height){
-    let array = new Array (height);
-    
- 
-     for (let i = 0; i < array.length; i++) {
-         array[i] = new Array(width);
-        }
+function createMatrix(width, height) {
+  const array = new Array(height);
 
-        for (let i = 0; i < array.length; i++) {    
-           
-             for (let j = 0; j < array[i].length; j++) {
-            array[i][j] = 0; 
-                }
-        }
-      
-        
-    return array
- }
- 
- let matrix = createMatrix (arrayWidth,arrayHeight)
- initialArray = matrix;
- createMatrixForHTML (matrix)
+  for (let i = 0; i < array.length; i++) {
+    array[i] = new Array(width);
+  }
 
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array[i].length; j++) {
+      array[i][j] = 0;
+    }
+  }
+
+  return array;
+}
+
+const matrix = createMatrix(arrayWidth, arrayHeight);
+initialArray = matrix;
+createMatrixForHTML(matrix);
 
 const isCellEmpty = (row, column, array) => {
-    if (array[row][column] !== undefined && array[row][column] === 0) {
-        return true;
-    } else {
-        return false;
-    }
+  if (array[row][column] !== undefined && array[row][column] === 0) {
+    return true;
+  }
+  return false;
 };
 
 const createVerticalBlinker = (column, row, array) => {
-    const createdVerticalArray = [...array];
-    let allOK = true;
-    isCellEmpty(column - 1, row -1 , array) ? (allOK = true) : (allOK = false);
-    isCellEmpty(column, row, array) ? (allOK = true) : (allOK = false);
-    isCellEmpty(column + 1, row, array) ? (allOK = true) : (allOK = false);
-    isCellEmpty(column, row -1, array) ? (allOK = true) : (allOK = false);
-    isCellEmpty(column, row + 1, array) ? (allOK = true) : (allOK = false);
-    if (allOK) {
-        createdVerticalArray[row - 1][column - 1] = 1;        
-        createdVerticalArray[row][column] = 1;
-        createdVerticalArray[row + 1][column] = 1;
-        createdVerticalArray[row - 1][column] = 1;
-        createdVerticalArray[row][column + 1] = 1;
+  const createdVerticalArray = [...array];
+  let allOK = true;
+  isCellEmpty(column - 1, row - 1, array) ? (allOK = true) : (allOK = false);
+  isCellEmpty(column, row, array) ? (allOK = true) : (allOK = false);
+  isCellEmpty(column + 1, row, array) ? (allOK = true) : (allOK = false);
+  isCellEmpty(column, row - 1, array) ? (allOK = true) : (allOK = false);
+  isCellEmpty(column, row + 1, array) ? (allOK = true) : (allOK = false);
+  if (allOK) {
+    createdVerticalArray[row - 1][column - 1] = 1;
+    createdVerticalArray[row][column] = 1;
+    createdVerticalArray[row + 1][column] = 1;
+    createdVerticalArray[row - 1][column] = 1;
+    createdVerticalArray[row][column + 1] = 1;
 
+    createMatrixForHTML(array);
 
-        createMatrixForHTML (array);
-
-        return createdVerticalArray;
-    } else {
-        console.log("No hay espacios disponibles");
-    }
+    return createdVerticalArray;
+  }
+  console.log('No hay espacios disponibles');
 };
 
-const newArray = createVerticalBlinker(arrayWidth/2, arrayHeight/2, initialArray);
-
+const newArray = createVerticalBlinker(arrayWidth / 2, arrayHeight / 2, initialArray);
 
 const countLiveCellsNeighbours = (row, column, array) => {
-    let neighbours = 0;
+  let neighbours = 0;
 
-    if (checkNeighbours(column, row, -1, -1, array) === true) {neighbours++;}
-    if (checkNeighbours(column, row, -1, +1, array) === true) {neighbours++;}
-    if (checkNeighbours(column, row, -1, 0, array) === true) {neighbours++;}
-    if (checkNeighbours(column, row, 0, -1, array) === true) {neighbours++;}
-    if (checkNeighbours(column, row, 0, +1, array) === true) {neighbours++;}
-    if (checkNeighbours(column, row, +1, -1, array) === true) {neighbours++;}
-    if (checkNeighbours(column, row, +1, 0, array) === true) {neighbours++;}
-    if (checkNeighbours(column, row, +1, +1, array) === true) {neighbours++;}
+  if (checkNeighbours(column, row, -1, -1, array) === true) { neighbours++; }
+  if (checkNeighbours(column, row, -1, +1, array) === true) { neighbours++; }
+  if (checkNeighbours(column, row, -1, 0, array) === true) { neighbours++; }
+  if (checkNeighbours(column, row, 0, -1, array) === true) { neighbours++; }
+  if (checkNeighbours(column, row, 0, +1, array) === true) { neighbours++; }
+  if (checkNeighbours(column, row, +1, -1, array) === true) { neighbours++; }
+  if (checkNeighbours(column, row, +1, 0, array) === true) { neighbours++; }
+  if (checkNeighbours(column, row, +1, +1, array) === true) { neighbours++; }
 
-    if (neighbours < 2 || neighbours >= 4) {
-        killerArray.push([column, row]);
-    }
+  if (neighbours < 2 || neighbours >= 4) {
+    killerArray.push([column, row]);
+  }
 };
 
-function checkNeighbours(column,row,columnDesplacement,rowDisplacement,array) {
-    let currentColumn = column + columnDesplacement;
-    let currentRow = row + rowDisplacement;
-    
-    if (currentColumn !== -1 && currentRow !== -1 && array[column] !== undefined && array[column][row] !== undefined && currentColumn < 
-        array[column].length && currentRow < array[row].length){
-        if (array[currentRow][currentColumn] === 0) {
-            checkNeighboursFromLiveNeighbours(currentColumn, currentRow, array);
-        }if (array[currentRow][currentColumn] === 1) {
-            return true;
-        }
+function checkNeighbours(column, row, columnDesplacement, rowDisplacement, array) {
+  const currentColumn = column + columnDesplacement;
+  const currentRow = row + rowDisplacement;
+
+  if (currentColumn !== -1 && currentRow !== -1 && array[column] !== undefined && array[column][row] !== undefined && currentColumn
+        < array[column].length && currentRow < array[row].length) {
+    if (array[currentRow][currentColumn] === 0) {
+      checkNeighboursFromLiveNeighbours(currentColumn, currentRow, array);
+    } if (array[currentRow][currentColumn] === 1) {
+      return true;
     }
+  }
 }
 
-
 const searchAlives = (array) => {
-     time = setInterval(()=> {
-    let oldArray = [...array];
-   
+  time = setInterval(() => {
+    const oldArray = [...array];
+
     console.log(array);
-    lifeCicles++
+    lifeCicles++;
 
-    
-        for (let column = 0; column < oldArray.length; column++) {
-            for (let row = 0; row < oldArray[column].length; row++) {
-                if (oldArray[column][row] === 1) {
-                    neighbours = countLiveCellsNeighbours(column, row, oldArray);
-                }
-            }
+    for (let column = 0; column < oldArray.length; column++) {
+      for (let row = 0; row < oldArray[column].length; row++) {
+        if (oldArray[column][row] === 1) {
+          neighbours = countLiveCellsNeighbours(column, row, oldArray);
         }
+      }
+    }
 
-        let nextGen = applyChanges(nextGenArray, oldArray);
-        
-        killerArray = [];
-        resurrectionArray = [];
-        createMatrixForHTML(array);
-        console.log(lifeCicles);
-        nextGen = newArray;
-    }, 100)
+    let nextGen = applyChanges(nextGenArray, oldArray);
+
+    killerArray = [];
+    resurrectionArray = [];
+    createMatrixForHTML(array);
+    console.log(lifeCicles);
+    nextGen = newArray;
+  }, 100);
 };
 
-
-
 function applyChanges(nextGenArray, newArray) {
-    nextGenArray = [...newArray];
-    killerArray.forEach((element) => {
-        nextGenArray[element[1]][element[0]] = 0;
-    });
-    resurrectionArray.forEach((element) => {
-        nextGenArray[element[1]][element[0]] = 1;
-    });
+  nextGenArray = [...newArray];
+  killerArray.forEach((element) => {
+    nextGenArray[element[1]][element[0]] = 0;
+  });
+  resurrectionArray.forEach((element) => {
+    nextGenArray[element[1]][element[0]] = 1;
+  });
 
-    return nextGenArray;
+  return nextGenArray;
 }
 
 function checkNeighboursFromLiveNeighbours(currentColumn, currentRow, array) {
-    let neighboursFromZeroCells = 0;
+  let neighboursFromZeroCells = 0;
 
-    if (countNeighboursFromLiveNeighbours(currentColumn,currentRow,-1,-1,array) === true) {
-        neighboursFromZeroCells++;}
-    if (countNeighboursFromLiveNeighbours(currentColumn,currentRow,-1,+1,array) === true) {
-        neighboursFromZeroCells++;}
-    if (countNeighboursFromLiveNeighbours(currentColumn,currentRow,-1,0,array) === true) {
-        neighboursFromZeroCells++;}
-    if (countNeighboursFromLiveNeighbours(currentColumn,currentRow,0,-1,array) === true) {
-        neighboursFromZeroCells++;}
-    if (countNeighboursFromLiveNeighbours(currentColumn,currentRow,0,+1,array) === true) {
-        neighboursFromZeroCells++;}
-    if (countNeighboursFromLiveNeighbours(currentColumn,currentRow,+1,-1,array) === true) {
-        neighboursFromZeroCells++;}
-    if (countNeighboursFromLiveNeighbours(currentColumn,currentRow,+1,0,array) === true) {
-        neighboursFromZeroCells++;}
-    if (countNeighboursFromLiveNeighbours(currentColumn,currentRow,+1,+1,array) === true) {
-        neighboursFromZeroCells++;}
+  if (countNeighboursFromLiveNeighbours(currentColumn, currentRow, -1, -1, array) === true) {
+    neighboursFromZeroCells++;
+  }
+  if (countNeighboursFromLiveNeighbours(currentColumn, currentRow, -1, +1, array) === true) {
+    neighboursFromZeroCells++;
+  }
+  if (countNeighboursFromLiveNeighbours(currentColumn, currentRow, -1, 0, array) === true) {
+    neighboursFromZeroCells++;
+  }
+  if (countNeighboursFromLiveNeighbours(currentColumn, currentRow, 0, -1, array) === true) {
+    neighboursFromZeroCells++;
+  }
+  if (countNeighboursFromLiveNeighbours(currentColumn, currentRow, 0, +1, array) === true) {
+    neighboursFromZeroCells++;
+  }
+  if (countNeighboursFromLiveNeighbours(currentColumn, currentRow, +1, -1, array) === true) {
+    neighboursFromZeroCells++;
+  }
+  if (countNeighboursFromLiveNeighbours(currentColumn, currentRow, +1, 0, array) === true) {
+    neighboursFromZeroCells++;
+  }
+  if (countNeighboursFromLiveNeighbours(currentColumn, currentRow, +1, +1, array) === true) {
+    neighboursFromZeroCells++;
+  }
 
-    if (neighboursFromZeroCells === 3) {
-        if (checkRepetedValues(resurrectionArray, currentColumn, currentRow) === true) {
-            resurrectionArray.push([currentColumn, currentRow]);
-        }
+  if (neighboursFromZeroCells === 3) {
+    if (checkRepetedValues(resurrectionArray, currentColumn, currentRow) === true) {
+      resurrectionArray.push([currentColumn, currentRow]);
     }
+  }
 }
 
-function countNeighboursFromLiveNeighbours(column,row,columnDesplacement,rowDisplacement,array) {
-    
-    let currentColumn = column + columnDesplacement;
-    let currentRow = row + rowDisplacement;
-    if (array[column] !== undefined && array[column][row] !== undefined && currentColumn !== 0 && currentRow !== 0 && currentColumn !== -1 && currentRow !== -1 &&  currentColumn !== array.length -1 && currentRow !== array[column].length -1){
-        if ( column< array[column].length && row < array[row].length ){
-
-        if (currentColumn !== -1 && currentRow !== -1 && array[currentColumn] !== undefined && array[currentColumn][currentRow] !== undefined
-              && array[currentRow][currentColumn] === 1 )
-            {
-                return true;
-            }
-        }
+function countNeighboursFromLiveNeighbours(column, row, columnDesplacement, rowDisplacement, array) {
+  const currentColumn = column + columnDesplacement;
+  const currentRow = row + rowDisplacement;
+  if (array[column] !== undefined && array[column][row] !== undefined && currentColumn !== 0 && currentRow !== 0 && currentColumn !== -1 && currentRow !== -1 && currentColumn !== array.length - 1 && currentRow !== array[column].length - 1) {
+    if (column < array[column].length && row < array[row].length) {
+      if (currentColumn !== -1 && currentRow !== -1 && array[currentColumn] !== undefined && array[currentColumn][currentRow] !== undefined
+              && array[currentRow][currentColumn] === 1) {
+        return true;
+      }
     }
+  }
 }
 
 function checkRepetedValues(resurrectionArray, currentColumn, currentRow) {
-    let currentPosition = (element) =>
-        element[0] === currentColumn && element[1] === currentRow;
+  const currentPosition = (element) => element[0] === currentColumn && element[1] === currentRow;
 
-    result = resurrectionArray.findIndex(currentPosition);
-    if (result === -1) {
-        return true;
-    }
+  result = resurrectionArray.findIndex(currentPosition);
+  if (result === -1) {
+    return true;
+  }
 }
-
 
 // function startGame(width,height){
 //     createMatrix (width,height)
@@ -203,36 +193,32 @@ function checkRepetedValues(resurrectionArray, currentColumn, currentRow) {
 //     searchAlives(secondArray,lifeCicles);
 // }
 
-function createMatrixForHTML(array){
+function createMatrixForHTML(array) {
+  let htmlGrid = '<table class="grid"><tbody>';
+  for (let i = 0; i < array.length; i++) {
+    htmlGrid += '<tr> ';
+    for (let j = 0; j < array[i].length; j++) {
+      if (array[i][j] === 0) {
+        htmlGrid += "<td class='deadCell'> ";
+        htmlGrid += '</td>';
+      } else {
+        htmlGrid += "<td class='aliveCell'> ";
+        htmlGrid += '</td>';
+      }
+    }
+  }
+  htmlGrid += '</tr>';
 
-        let htmlGrid= '<table class="grid"><tbody>';
-        for (let i = 0; i < array.length; i++) {    
-        htmlGrid+= "<tr> ";       
-            for (let j = 0; j < array[i].length; j++) {
+  htmlGrid += '</tbody></table>';
+  document.querySelector('.grid').innerHTML = htmlGrid;
+}
 
-                if(array[i][j] === 0){
-                     
-                    htmlGrid += "<td class='deadCell'> ";
-                     htmlGrid+= "</td>";
-                 }else{
-                    htmlGrid += "<td class='aliveCell'> ";
-                    htmlGrid+= "</td>";
-                 }
-                }
-            }
-        htmlGrid += "</tr>"
+function stopTime() {
+  clearInterval(time);
+}
 
-        htmlGrid += "</tbody></table>";
-        document.querySelector(".grid").innerHTML = htmlGrid;
-    }        
+const array6 = searchAlives(newArray);
 
-function stopTime (){
-    clearInterval(time);
-}   
-
-
-let array6 = searchAlives(newArray);
-
-function startGame (newArray){
-     searchAlives(newArray);
+function startGame(newArray) {
+  searchAlives(newArray);
 }
