5270fd0c5d4434098ce11287259ef19bf170dac3
gteixi <gemmateixido25@gmail.com>
2021-04-19 13:22:33 +0200

    create my constructor of objects.

---
+++ gemma-teixido/lectures/objects/objects.js
@@ -0,0 +1,11 @@
+function generatePizzas(ingredients, size) {
+    let pizza {
+        ingredients,
+        size
+    };
+    return pizza;
+}
+
+let orderPizza1 = generatePizzas('tomato, onion' ,'small');
+
+console.log(orderPizza1);
