19c8cdcaa0895b48bfc3db51e8fd33a29f0e40d0
gteixi <gemmateixido25@gmail.com>
2021-04-19 13:24:10 +0200

    Add = to create the obj.

--- gemma-teixido/lectures/objects/objects.js
+++ gemma-teixido/lectures/objects/objects.js
@@ -1,11 +1,11 @@
 function generatePizzas(ingredients, size) {
-    let pizza {
-        ingredients,
-        size
-    };
-    return pizza;
+  let pizza = {
+    ingredients,
+    size,
+  };
+  return pizza;
 }
 
-let orderPizza1 = generatePizzas('tomato, onion' ,'small');
+let orderPizza1 = generatePizzas("tomato, onion", "small");
 
 console.log(orderPizza1);
