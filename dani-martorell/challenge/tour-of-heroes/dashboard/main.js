a6f7749717f3c193671a33fc46eeb0199fde7cdf
Daniel Martorell <dani.martorell@gmail.com>
2021-04-19 19:56:27 +0200

    Add function populateList

--- dani-martorell/challenge/tour-of-heroes/dashboard/main.js
+++ dani-martorell/challenge/tour-of-heroes/dashboard/main.js
@@ -12,21 +12,6 @@ const heroes = [
 ];
 
 const mainContent = document.querySelector('.content');
-// const selectorGroup = document.createElement('div');
-// const navigatorDashboard = document.createElement('a');
-// const navigatorHeroes = document.createElement('a');
-// const mainTitle = document.createElement('h3');
-// const heroesUl = document.createElement('ul');
-
-function generateElement(typeOfElement, classNamesArray = '', context = '', hrefURL = '#') {
-  const element = document.createElement(typeOfElement);
-  element.classList.add(...classNamesArray);
-  if (typeOfElement === 'a') {
-    element.setAttribute('href', hrefURL);
-  }
-  element.textContent = context;
-  return element;
-}
 const headTitle = generateElement('div', ['head-title'], 'Tour of Heroes');
 mainContent.append(headTitle);
 const selectorGroup = generateElement('div', ['selector-group']);
@@ -37,14 +22,26 @@ const linkHeroes = generateElement('a', ['selector-group__link-heroes'], 'Heroes
 selectorGroup.append(linkHeroes);
 const mainTitle = generateElement('h3', [], 'Top Heroes');
 mainContent.append(mainTitle);
+const heroesList = generateElement('ul', ['heroes-list']);
+mainContent.appendChild(heroesList);
+const list = populateList(heroes, heroesList);
 
-// function createheadTitle(name) {
-//   const headTitle = document.createElement('p');
-//   headTitle.textContent = 'Tour of Heroes';
-//   mainContent.appendChild(headTitle);
-// }
-// function createTitle(name) {
-//   const mainTitle = document.createElement('h3');
-//   mainTitle.textContent = name;
-//   mainContent.appendChild(mainTitle);
-// }
+function generateElement(typeOfElement, classNamesArray = '', context = '', hrefURL = '#') {
+  const element = document.createElement(typeOfElement);
+  element.classList.add(...classNamesArray);
+  if (typeOfElement === 'a') {
+    element.setAttribute('href', hrefURL);
+  }
+  element.textContent = context;
+  return element;
+}
+
+function populateList(arrayOrigin, parentElement) {
+  const selectedItems = arrayOrigin.slice(1, 5);
+  selectedItems.forEach((element) => {
+    const item = generateElement('li', ['heroes-list__item']);
+    item.textContent = element.name;
+    parentElement.append(item);
+  });
+  return parentElement;
+}
