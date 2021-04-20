0174fb23a20d42c68ea7f2bffc9fcdd5d57ca7be
Isabel Gomez <belasi87@gmail.com>
2021-04-20 17:46:54 +0200

    Finish refactor dashboard

---
+++ Isabel-Gomez/challenge/tour-of-heroes/components/dashboard/dashboard.component.js
@@ -0,0 +1,6 @@
+createElement('h1', 'Tour of Heroes', mainSection);
+createElement('a', 'Dashboard', mainSection);
+createElement('a', 'Heroes', mainSection, './../heroes/heroes.html');
+createElement('h3', 'Top Heroes', mainSection);
+
+createSelectorHeroes(heroes);
--- Isabel-Gomez/challenge/tour-of-heroes/services/services.js
+++ Isabel-Gomez/challenge/tour-of-heroes/services/services.js
@@ -2,4 +2,16 @@ function fetchHeroes(url) {
   fetch(url).then((response) => response.json())
     .then((data) => data);
 }
-fetchHeroes('../../store/superHeroData.json');
+
+const createElement = (tag, innerHTML, parent, href) => {
+  const getTag = document.createElement(tag);
+  getTag.innerHTML = innerHTML;
+  parent.appendChild(getTag);
+  getTag.href = href;
+};
+
+const createSelectorHeroes = (array) => {
+  array.splice(1, 4).forEach((element) => {
+    createElement('a', `${element.name}`, mainSection, '../../heroes-details/heroes-details.html');
+  });
+};
