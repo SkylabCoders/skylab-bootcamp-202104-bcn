29b6dbe36097e2ef2f9c00ec841f86b68e20ae96
Daniel Martorell <dani.martorell@gmail.com>
2021-04-20 18:35:45 +0200

    Fix layout heroes

--- dani-martorell/challenge/tour-of-heroes/src/components/list/list.js
+++ dani-martorell/challenge/tour-of-heroes/src/components/list/list.js
@@ -1,4 +1,29 @@
 const mainContent = document.querySelector('.content');
+
+const headTitle = generateElement({ type: 'div', classes: ['head-title'], context: 'Tour of Heroes' });
+mainContent.append(headTitle);
+const selectorGroup = generateElement({ type: 'div', classes: ['selector-group'] });
+mainContent.append(selectorGroup);
+const linkDashboard = generateElement({
+  type: 'a', classes: ['selector-group__link-dasboard'], context: 'Dashboard', url: './../dashboard/index.html',
+});
+selectorGroup.append(linkDashboard);
+const linkHeroes = generateElement({
+  type: 'a', classes: ['selector-group__link-heroes'], context: 'Heroes', url: 'index.html',
+});
+selectorGroup.append(linkHeroes);
+
+const mainTitle = generateElement({ type: 'h3', classes: [], context: 'My Heroes' });
+mainContent.append(mainTitle);
+const highlitedHero = generateElement({ type: 'div', classes: ['higlited-hero'] });
+const highlightHeroName = generateElement({ type: 'p', classes: ['highlighted-hero__name'], context: 'Peter' });
+const highlightHeroDetailsButton = generateElement({
+  type: 'a',
+  context: 'View Details',
+  classes: ['highlighted-hero__details-btn', '--inactive'],
+  url: './../detalles/index.html',
+});
+
 const heroesList = generateElement({ type: 'ul', classes: ['heroes-list'] });
 mainContent.append(heroesList);
 
@@ -13,6 +38,7 @@ getAllHeroes('./../../store/superHeroData.json')
       const anchor = generateElement({
         type: 'a',
         classes: [],
+        url: [],
       });
       li.append(anchor);
       heroesList.append(li);
--- dani-martorell/challenge/tour-of-heroes/src/services/heroes.service.js
+++ dani-martorell/challenge/tour-of-heroes/src/services/heroes.service.js
@@ -14,31 +14,3 @@ function generateElement(elementProperties) {
   element.textContent = elementProperties.context;
   return element;
 }
-
-function populateListWithIds(elementProperties) {
-  const selectedItems = elementProperties.arrayOrigin
-    .slice(elementProperties.extract.from, elementProperties.extract.to);
-  selectedItems.forEach((element) => {
-    const item = generateElement({ type: 'li', classes: ['heroes-list__item', 'heroes-list__item--inactive'] });
-    const spanId = generateElement({ type: 'span', classes: ['heroes-list__item-id'] });
-    const spanName = generateElement({ type: 'span', classes: ['heroes-list__item-name'] });
-    item.onclick = function () {
-      const allItems = document.querySelectorAll('.heroes-list__item');
-      allItems.forEach((item) => {
-        if (item.classList.contains('heroes-list__item--active')) {
-          item.classList.remove('heroes-list__item--active');
-          item.classList.add('heroes-list__item--inactive');
-        }
-      });
-      item.classList.remove('heroes-list__item--inactive');
-      item.classList.add('heroes-list__item--active');
-      highlightHeroName.textContent = spanName.textContent;
-    };
-    spanId.textContent = element.id;
-    spanName.textContent = element.name;
-    item.append(spanId);
-    item.append(spanName);
-    elementProperties.parentElement.append(item);
-  });
-  return elementProperties.parentElement;
-}
