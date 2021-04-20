1c1c5f679ed905cb31966e9bea9a4314fc4dbf83
Daniel Martorell <dani.martorell@gmail.com>
2021-04-20 18:16:56 +0200

    Implementation create list with names from fetch

--- dani-martorell/challenge/tour-of-heroes/src/components/list/list.js
+++ dani-martorell/challenge/tour-of-heroes/src/components/list/list.js
@@ -1,4 +1,20 @@
+const mainContent = document.querySelector('.content');
+const heroesList = generateElement({ type: 'ul', classes: ['heroes-list'] });
+mainContent.append(heroesList);
+
 getAllHeroes('./../../store/superHeroData.json')
   .then((data) => {
-
+    data.slice(0, 10).forEach((heroe) => {
+      const li = generateElement({
+        type: 'li',
+        classes: ['heroes-list__item'],
+        context: heroe.name,
+      });
+      const anchor = generateElement({
+        type: 'a',
+        classes: [],
+      });
+      li.append(anchor);
+      heroesList.append(li);
+    });
   });
--- dani-martorell/challenge/tour-of-heroes/src/services/heroes.service.js
+++ dani-martorell/challenge/tour-of-heroes/src/services/heroes.service.js
@@ -3,3 +3,42 @@ function getAllHeroes(url) {
     .then((response) => response.json())
     .then((data) => data);
 }
+
+function generateElement(elementProperties) {
+  const element = document.createElement(elementProperties.type);
+  element.classList.add(...elementProperties.classes);
+  element.id = elementProperties.id || '';
+  if (elementProperties.type === 'a') {
+    element.setAttribute('href', elementProperties.url);
+  }
+  element.textContent = elementProperties.context;
+  return element;
+}
+
+function populateListWithIds(elementProperties) {
+  const selectedItems = elementProperties.arrayOrigin
+    .slice(elementProperties.extract.from, elementProperties.extract.to);
+  selectedItems.forEach((element) => {
+    const item = generateElement({ type: 'li', classes: ['heroes-list__item', 'heroes-list__item--inactive'] });
+    const spanId = generateElement({ type: 'span', classes: ['heroes-list__item-id'] });
+    const spanName = generateElement({ type: 'span', classes: ['heroes-list__item-name'] });
+    item.onclick = function () {
+      const allItems = document.querySelectorAll('.heroes-list__item');
+      allItems.forEach((item) => {
+        if (item.classList.contains('heroes-list__item--active')) {
+          item.classList.remove('heroes-list__item--active');
+          item.classList.add('heroes-list__item--inactive');
+        }
+      });
+      item.classList.remove('heroes-list__item--inactive');
+      item.classList.add('heroes-list__item--active');
+      highlightHeroName.textContent = spanName.textContent;
+    };
+    spanId.textContent = element.id;
+    spanName.textContent = element.name;
+    item.append(spanId);
+    item.append(spanName);
+    elementProperties.parentElement.append(item);
+  });
+  return elementProperties.parentElement;
+}
