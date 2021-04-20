a5abb9f4376f6817ceaa8a6e0f38a0859dfbef9b
blopez-dev <pjgl.frontend@gmail.com>
2021-04-20 18:55:47 +0200

    updated function fetch

--- Pablo-Garcia/lectures/tour-of-heroes/scripts/service.js
+++ Pablo-Garcia/lectures/tour-of-heroes/scripts/service.js
@@ -1,3 +1,10 @@
 function getHeroIDFromUrl(url) {
     return new URLSearchParams(url).get('heroID');
 }
+
+
+function fetcHeroes(){
+    return fetch('./assets/superHeroData.json')
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
