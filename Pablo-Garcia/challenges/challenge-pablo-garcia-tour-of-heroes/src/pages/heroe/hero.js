179ce1656a15fb0e40f8663c132a4a579dfa9e84
Pgarcia1989 <pjgl19@gmail.com>
2021-04-21 01:16:03 +0200

    Implemented fetch function to get data from JSON

--- Pablo-Garcia/challenges/challenge-pablo-garcia-tour-of-heroes/src/pages/heroe/hero.js
+++ Pablo-Garcia/challenges/challenge-pablo-garcia-tour-of-heroes/src/pages/heroe/hero.js
@@ -1,5 +1,29 @@
+const URL= '../../../src/common/utils/dataHeroes.json'
+const getDataJson = () => {
+    fetch(URL)
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(data){
+           const heroesSlide = data.slice(data.length-4, data.length);
+           const wrapperTopHeroesSlide = document.createElement('ul');
+            wrapperTopHeroesSlide.setAttribute('class', 'slideTopHeroes');
+            heroesSlide.forEach(({id, name}) =>{
+               const wrapperTopHeroesSlide__item = document.createElement('li');
+               const wrapperTopHeroesSlide__item__link = document.createElement('a');
+               wrapperTopHeroesSlide__item__link.setAttribute('href',`../../../src/pages/detail/detail.html?_${id}`);
+               wrapperTopHeroesSlide__item__link.innerHTML = `${name}`;
+               wrapperTopHeroesSlide__item.appendChild(wrapperTopHeroesSlide__item__link);
+               wrapperTopHeroesSlide.appendChild(wrapperTopHeroesSlide__item);
 
-const getDataFromJson = () =>{
-    fetch('../')
-        .then()
+           })
+            const node = document.getElementById('root');
+            node.append(wrapperTopHeroesSlide)
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+
+
 }
+
