d917ab8bbb9c7ac369bb49cccc37d4da90ae9b71
Emerson Fernandes <emerson_feral@hotmail.com>
2021-04-20 16:33:45 +0200

    adding json

--- emerson-fernandes/challenge-heroes/hero-details/hero-details.js
+++ emerson-fernandes/challenge-heroes/hero-details/hero-details.js
@@ -1,5 +1,27 @@
+
+const tourOfHerosH1 = document.createElement("h1");
+tourOfHerosH1.innerHTML ='Tour of Heroes'
+document.body.appendChild(tourOfHerosH1)
+
+const dashBoard = document.createElement("nav")
+document.body.appendChild(dashBoard)
+
+const buttonDashBoard = document.createElement("a")
+buttonDashBoard.setAttribute('href','../dashboard/dashboard.html')
+buttonDashBoard.innerHTML = "DashBoard"
+dashBoard.appendChild(buttonDashBoard)
+
+const buttonHeroes = document.createElement("a")
+buttonHeroes.setAttribute('href','../hero-list/hero-list.html')
+buttonHeroes.innerHTML = "Heroes"
+dashBoard.appendChild(buttonHeroes)
+
 const heroNameDetails = document.createElement("h2")
 document.body.appendChild(heroNameDetails)
+
+const divID = document.createElement("div")
+document.body.appendChild(divID)
+divID.innerHTML = "id:"
 
 const heroInput = document.createElement("div")
 document.body.appendChild(heroInput)
