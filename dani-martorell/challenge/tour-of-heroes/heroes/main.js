6fbe16b35747e0b27da74f95d6dae468fbe91f15
Daniel Martorell <dani.martorell@gmail.com>
2021-04-20 11:11:41 +0200

    Implementation heroe name appears highlighted when clicked

--- dani-martorell/challenge/tour-of-heroes/heroes/main.js
+++ dani-martorell/challenge/tour-of-heroes/heroes/main.js
@@ -21,6 +21,30 @@ function generateElement(elementProperties) {
   element.textContent = elementProperties.context;
   return element;
 }
+const mainContent = document.querySelector('.content');
+const headTitle = generateElement({ type: 'div', classes: ['head-title'], context: 'Tour of Heroes' });
+mainContent.append(headTitle);
+const selectorGroup = generateElement({ type: 'div', classes: ['selector-group'] });
+mainContent.append(selectorGroup);
+const linkDashboard = generateElement({
+  type: 'a', classes: ['selector-group__link-dasboard'], context: 'Dashboard', url: './../dashboard/index.html',
+});
+selectorGroup.append(linkDashboard);
+const linkHeroes = generateElement({
+  type: 'a', classes: ['selector-group__link-heroes'], context: 'Heroes', url: 'index.html',
+});
+selectorGroup.append(linkHeroes);
+
+const mainTitle = generateElement({ type: 'h3', classes: [], context: 'My Heroes' });
+mainContent.append(mainTitle);
+const highlitedHero = generateElement({ type: 'div', classes: ['higlited-hero'] });
+const highlightHeroName = generateElement({ type: 'p', classes: ['highlighted-hero__name'], context: 'Peter' });
+const highlightHeroDetailsButton = generateElement({
+  type: 'a',
+  context: 'View Details',
+  classes: ['highlighted-hero__details-btn', '--inactive'],
+  url: './../detalles/index.html',
+});
 
 function populateListWithIds(elementProperties) {
   const selectedItems = elementProperties.arrayOrigin
@@ -39,6 +63,7 @@ function populateListWithIds(elementProperties) {
       });
       item.classList.remove('heroes-list__item--inactive');
       item.classList.add('heroes-list__item--active');
+      highlightHeroName.textContent = spanName.textContent;
     };
     spanId.textContent = element.id;
     spanName.textContent = element.name;
@@ -48,22 +73,6 @@ function populateListWithIds(elementProperties) {
   });
   return elementProperties.parentElement;
 }
-
-const mainContent = document.querySelector('.content');
-const headTitle = generateElement({ type: 'div', classes: ['head-title'], context: 'Tour of Heroes' });
-mainContent.append(headTitle);
-const selectorGroup = generateElement({ type: 'div', classes: ['selector-group'] });
-mainContent.append(selectorGroup);
-const linkDashboard = generateElement({
-  type: 'a', classes: ['selector-group__link-dasboard'], context: 'Dashboard', url: './../dashboard/index.html',
-});
-selectorGroup.append(linkDashboard);
-const linkHeroes = generateElement({
-  type: 'a', classes: ['selector-group__link-heroes'], context: 'Heroes', url: 'index.html',
-});
-selectorGroup.append(linkHeroes);
-const mainTitle = generateElement({ type: 'h3', classes: [], context: 'My Heroes' });
-mainContent.append(mainTitle);
 const heroesList = generateElement({ type: 'ul', classes: ['heroes-list'] });
 mainContent.append(heroesList);
 populateListWithIds({
@@ -71,14 +80,7 @@ populateListWithIds({
   parentElement: heroesList,
   extract: { from: 0, to: 9 },
 });
-const highlitedHero = generateElement({ type: 'div', classes: ['higlited-hero'] });
-const highlightHeroName = generateElement({ type: 'p', classes: ['highlighted-hero__name'] });
-const highlightHeroDetailsButton = generateElement({
-  type: 'a',
-  context: 'View Details',
-  classes: ['highlighted-hero__details-btn', '--inactive'],
-  url: './../detalles/index.html',
-});
+
 highlitedHero.append(highlightHeroName);
 highlitedHero.append(highlightHeroDetailsButton);
 mainContent.append(highlitedHero);
