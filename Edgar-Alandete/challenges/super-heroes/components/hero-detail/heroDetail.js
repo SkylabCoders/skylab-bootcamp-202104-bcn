32cddcb98f19bc993d187089ccf3996c23f06813
Edgar Alandete <edgalandete@gmail.com>
2021-04-20 23:23:06 +0200

    Create hero detail BEM

--- Edgar-Alandete/challenges/super-heroes/components/hero-detail/heroDetail.js
+++ Edgar-Alandete/challenges/super-heroes/components/hero-detail/heroDetail.js
@@ -3,18 +3,19 @@ const heroDataUrl = '../../store/superHeroData.json';
 const urlParams = new URLSearchParams(window.location.search);
 
 const createNavigator = (parent) => {
-  const nav = createDomElement(parent, 'ul', '', '', '', 'header__main-navigation main-navigation');
-  const dashboardItem = createDomElement(nav, 'li', '', '', '', 'main-navigation__item');
-  createDomElement(dashboardItem, 'a', 'Dashboard', '../dashboard/dashboard.html', '', '', '');
-  const heroesItem = createDomElement(nav, 'li', '', '', '', 'main-navigation__item');
-  createDomElement(heroesItem, 'a', 'Heroes', '../heroes/heroes.html', '');
+  const nav = createDomElement(parent, 'ul', '', [{ name: 'class', value: 'header__main-navigation main-navigation' }]);
+  const dashboardItem = createDomElement(nav, 'li', '', [{ name: 'class', value: 'main-navigation__item' }]);
+  createDomElement(dashboardItem, 'a', 'Dashboard', [{ name: 'href', value: '../dashboard/dashboard.html' }]);
+  const heroesItem = createDomElement(nav, 'li', '', [{ name: 'class', value: 'main-navigation__item' }]);
+  createDomElement(heroesItem, 'a', 'Heroes', [{ name: 'href', value: '../heroes/heroes.html' }], '');
 
   return nav;
 };
 
 const createHeader = () => {
-  const header = createDomElement(hero, 'header', '', '', '', 'header');
-  createDomElement(header, 'h1', 'Tour of Heroes', '', '', 'header__main-title');
+  const header = createDomElement(hero, 'header', '',
+    [{ name: 'class', value: 'header' }]);
+  createDomElement(header, 'h1', 'Tour of Heroes', [{ name: 'class', value: 'header__main-title' }]);
   createNavigator(header);
 };
 
@@ -23,7 +24,7 @@ const heroDetail = () => {
   const heroInfo = getHeroById(heroDataUrl, urlParams.get('id'));
 
   heroInfo.then(({ name }) => {
-    createDomElement(hero, 'h2', `${name} details!`, '');
+    createDomElement(hero, 'h2', `${name} details!`, [{ name: 'class', value: 'hero-detail__title' }]);
   });
 };
 
