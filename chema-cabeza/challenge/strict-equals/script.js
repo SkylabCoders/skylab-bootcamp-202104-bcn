783be1b195e2b7ed21184c88c6250ea4e16c3812
CheCabeza <chemartinc@gmail.com>
2021-04-20 10:24:35 +0200

    Fixed string-string error

--- chema-cabeza/challenge/strict-equals/script.js
+++ chema-cabeza/challenge/strict-equals/script.js
@@ -1,5 +1,5 @@
 function strictEquals (a,b) {
-    if (isNaN(a)&&isNaN(b)) {
+    if (Object.is (a, NaN) && Object.is (NaN, b)) {
       return false;
   } else if (Object.is (a, -0) && Object.is (0, b)) {
       return true;
