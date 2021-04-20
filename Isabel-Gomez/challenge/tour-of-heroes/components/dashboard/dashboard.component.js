87b0c18cc176a639abe9f19900675e31993876c7
Isabel Gomez <belasi87@gmail.com>
2021-04-20 18:21:17 +0200

    Finish fetch in dashboard list

--- Isabel-Gomez/challenge/tour-of-heroes/components/dashboard/dashboard.component.js
+++ Isabel-Gomez/challenge/tour-of-heroes/components/dashboard/dashboard.component.js
@@ -1,6 +1,14 @@
+const heroes = fetchHeroes('../../store/superHeroData.json');
+
+const mainSection = document.querySelector('.container-dashboard');
+
 createElement('h1', 'Tour of Heroes', mainSection);
 createElement('a', 'Dashboard', mainSection);
 createElement('a', 'Heroes', mainSection, './../heroes/heroes.html');
 createElement('h3', 'Top Heroes', mainSection);
 
-createSelectorHeroes(heroes);
+heroes.then((info) => {
+  info.splice(1, 4).forEach((element) => {
+    createElement('a', `${element.name}`, mainSection, '../../heroes-details/heroes-details.html');
+  });
+});
--- Isabel-Gomez/challenge/tour-of-heroes/services/services.js
+++ Isabel-Gomez/challenge/tour-of-heroes/services/services.js
@@ -1,6 +1,6 @@
 function fetchHeroes(url) {
-  fetch(url).then((response) => response.json())
-    .then((data) => data);
+  return fetch(url).then((response) => response.json());
+  // .then((data) => data);
 }
 
 const createElement = (tag, innerHTML, parent, href) => {
@@ -8,10 +8,4 @@ const createElement = (tag, innerHTML, parent, href) => {
   getTag.innerHTML = innerHTML;
   parent.appendChild(getTag);
   getTag.href = href;
-};
-
-const createSelectorHeroes = (array) => {
-  array.splice(1, 4).forEach((element) => {
-    createElement('a', `${element.name}`, mainSection, '../../heroes-details/heroes-details.html');
-  });
 };
