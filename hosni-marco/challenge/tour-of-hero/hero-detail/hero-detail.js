f54efa0d7adae805fd2bd09c2934b3ba8e7afa61
Hosni <hosni.marco@gmail.com>
2021-04-20 23:52:24 +0200

    Asyn fixed service nd detail

--- hosni-marco/challenge/tour-of-hero/hero-detail/hero-detail.js
+++ hosni-marco/challenge/tour-of-hero/hero-detail/hero-detail.js
@@ -1,10 +1,20 @@
 const nodeContainer = document.getElementById('main');
+const heroID = new URLSearchParams(location.search).get('heroID');
+
+const getFetchHeroesData = async () => {
+  const data = await (await fetch('./../store/superHeroData.json')).json();
+  data.forEach((hero) => {
+    if (hero.id === parseInt(heroID)) {
+      document.getElementById('h2-hero-details').innerText = hero.name;
+      document.getElementById('text-id-hero').innerText = hero.id;
+    }
+  });
+};
 
 function addTittle(text) {
   const title = document.createElement('h1');
   title.innerHTML = text;
   title.classList.add = 'title';
-
   nodeContainer.appendChild(title);
 }
 
@@ -12,13 +22,11 @@ function addLink() {
   const createNav = document.createElement('nav');
   createNav.setAttribute('id', 'nav');
   nodeContainer.appendChild(createNav);
-
   const createLinkDashboard = document.createElement('a');
   createLinkDashboard.setAttribute('id', 'dashboard-link');
   createNav.appendChild(createLinkDashboard);
   createLinkDashboard.innerHTML = 'Dashboard';
   createLinkDashboard.href = './../dashboard/dashboard.html';
-
   const createLinkHeroDetails = document.createElement('a');
   createLinkHeroDetails.setAttribute('id', 'hero-list-link');
   createNav.appendChild(createLinkHeroDetails);
@@ -28,12 +36,9 @@ function addLink() {
 
 function addDetails() {
   const createH2 = document.createElement('h2');
-  createH2.innerHTML = 'HERO NAME! Details'; // intergrar name
   createH2.setAttribute('id', 'h2-hero-details');
   nodeContainer.appendChild(createH2);
-
   const createP = document.createElement('p');
-  createP.innerHTML = 'ID: value';
   createP.setAttribute('id', 'text-id-hero');
   nodeContainer.appendChild(createP);
 }
@@ -44,7 +49,6 @@ function searchHero() {
   createLabel.setAttribute('id', 'hero-name');
   createLabel.innerHTML = 'Hero name';
   nodeContainer.appendChild(createLabel);
-
   const createInput = document.createElement('input');
   createInput.setAttribute('type', 'text');
   createInput.setAttribute('id', 'input-hero-name');
@@ -56,7 +60,6 @@ function addButtons() {
   createGoBackButton.setAttribute('id', 'go-back-button');
   createGoBackButton.innerHTML = 'go back';
   nodeContainer.appendChild(createGoBackButton);
-
   const createSaveButton = document.createElement('button');
   createSaveButton.setAttribute('id', 'save-button');
   createSaveButton.innerHTML = 'save';
@@ -67,21 +70,17 @@ function messageDivContainer() {
   const createDiv = document.createElement('div');
   createDiv.setAttribute('id', 'message-container');
   nodeContainer.appendChild(createDiv);
-
   const createMessageP = document.createElement('p');
   createMessageP.setAttribute('id', 'messages');
   createMessageP.innerHTML = 'messages';
   createDiv.appendChild(createMessageP);
-
   const createButton = document.createElement('button');
   createButton.setAttribute('id', 'clear-message');
   createButton.innerHTML = 'clear messages';
   createDiv.appendChild(createButton);
-
   const createUl = document.createElement('ul');
   createUl.setAttribute('id', 'common-ul__message');
   createDiv.appendChild(createUl);
-
   for (let i = 0; i < 4; i += 1) {
     const createIl = document.createElement('il');
     createIl.setAttribute('id', `common-il__message-${i}`);
@@ -90,9 +89,15 @@ function messageDivContainer() {
   }
 }
 
+const getFetchData = async () => {
+  const data = await (await fetch('./../store/superHeroData.json')).json();
+  data.filter((hero) => hero.id === 1);
+};
+
 addTittle('tour of heroes');
 addLink();
 addDetails();
 searchHero();
 addButtons();
 messageDivContainer();
+getFetchHeroesData();
--- hosni-marco/challenge/tour-of-hero/hero-list/service.js
+++ hosni-marco/challenge/tour-of-hero/hero-list/service.js
@@ -1,23 +1,8 @@
-function nodeCreateElement(element, id, className) {
-  const node = document.createElement(element);
-  node.setAttribute('id', id);
-  node.setAttribute('class', className);
-  return node;
-}
+const getFetchHeroesData = async () => {
+  const data = await (await fetch('./../store/superHeroData.json')).json();
+  data.forEach((hero) => {
+    getList(hero);
+  });
+};
 
-const mainId = document.getElementById('main');
-const ul = nodeCreateElement('ul', 'id-ul', 'class-ul');
-mainId.append(ul);
-
-heroes.forEach((element) => {
-  const liElement = nodeCreateElement('li', `id-${element.name}`, `class-${element.name}`);
-  const aElement = nodeCreateElement('a', `id-link-${element.name}`, `class-link-${element.name}`);
-  const spanElement = nodeCreateElement('span', `id-span-${element.id}`, `class-span-${element.id}`);
-
-  spanElement.innerText = element.id;
-  aElement.innerText = element.name;
-  aElement.href = `./../hero-detail/hero-detail.html?heroID=${element.id}`;
-
-  liElement.append(spanElement, aElement);
-  ul.append(liElement);
-});
+getFetchHeroesData();
