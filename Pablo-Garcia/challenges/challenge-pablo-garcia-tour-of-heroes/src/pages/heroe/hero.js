95a47ebd71bda2a4a7eebcf0dfde72ec05c15739
Pgarcia1989 <pjgl19@gmail.com>
2021-04-21 03:16:40 +0200

    Implemented details hero

--- Pablo-Garcia/challenges/challenge-pablo-garcia-tour-of-heroes/src/pages/heroe/hero.js
+++ Pablo-Garcia/challenges/challenge-pablo-garcia-tour-of-heroes/src/pages/heroe/hero.js
@@ -2,7 +2,8 @@ const URL= '../../../src/common/utils/dataHeroes.json'
 const getDataJson = () => {
     fetch(URL)
         .then(function(res) {
-            return res.json();
+            const heroes = res.json();
+            return heroes;
         })
         .then(function(data){
            const heroesSlide = data.slice(data.length-4, data.length);
@@ -11,14 +12,15 @@ const getDataJson = () => {
             heroesSlide.forEach(({id, name}) =>{
                const wrapperTopHeroesSlide__item = document.createElement('li');
                const wrapperTopHeroesSlide__item__link = document.createElement('a');
-               wrapperTopHeroesSlide__item__link.setAttribute('href',`../../../src/pages/detail/detail.html?_${id}`);
+               wrapperTopHeroesSlide__item__link.setAttribute('href',`../../../src/pages/detail/detail.html?id=${id}`);
                wrapperTopHeroesSlide__item__link.innerHTML = `${name}`;
                wrapperTopHeroesSlide__item.appendChild(wrapperTopHeroesSlide__item__link);
                wrapperTopHeroesSlide.appendChild(wrapperTopHeroesSlide__item);
 
            })
-            const node = document.getElementById('root');
-            node.append(wrapperTopHeroesSlide)
+            const insertHeroes = document.querySelector('.header__wraper');
+            insertHeroes ? insertHeroes.append(wrapperTopHeroesSlide) : null;
+
         })
         .catch(function(error) {
             console.log(error);
