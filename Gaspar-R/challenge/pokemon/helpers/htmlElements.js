cd07dca14a52847efbff015afd695e906c514510
Gaspar Rodriguez <gaspar.unlp@gmail.com>
2021-04-21 23:41:27 +0200

    Images and details section added

--- Gaspar-R/challenge/pokemon/helpers/htmlElements.js
+++ Gaspar-R/challenge/pokemon/helpers/htmlElements.js
@@ -10,7 +10,7 @@ function createElement(tag, className = null, text = null, parent = null) {
 
 function createList(text, className = null, parent = null) {
   const newParent = document.querySelector(parent);
-  const list = document.createElement('ol');
+  const list = document.createElement('ul');
 
   const newElement = document.createElement('li');
   newElement.classList.add(className);
@@ -20,4 +20,25 @@ function createList(text, className = null, parent = null) {
 
   newParent.appendChild(list);
   return newElement;
+}
+
+function createImg(className = null, src, parent = null) {
+  const newImg = document.createElement('img');
+  newImg.classList.add(className);
+  newImg.setAttribute('src', src);
+
+  const newParent = document.querySelector(parent);
+  newParent.appendChild(newImg);
+  return newImg;
+}
+
+function createAnchor(className = null, href, text = null, parent = null) {
+  const newAnchor = document.createElement('a');
+  newAnchor.classList.add(className);
+  newAnchor.innerHTML = text;
+  newAnchor.setAttribute('href', href);
+
+  const newParent = document.querySelector(parent);
+  newParent.appendChild(newAnchor);
+  return newAnchor;
 }
---
+++ Gaspar-R/challenge/pokemon/src/pokemon-detail/pokemon-detail.js
@@ -0,0 +1,12 @@
+createElement('h1', 'title', 'Pokemon Pics', '.div__main');
+
+const listBtn = createElement('a', 'listBtn', 'List of Pokemons', '.div__main');
+listBtn.setAttribute('href', '../pokemon-list/pokemon-list.html');
+
+const dashboard = createElement('a', 'dashboardBtn', 'Dashboard', '.div__main');
+dashboard.setAttribute('href', '../dashboard/dashboard.html');
+
+pokemonList.then((data) => data.results.forEach((element) => {
+  const pokemonUrlDetails = fetchPokemon(element.url);
+  pokemonUrlDetails.then((data) => createImg('pokemonImg', data.sprites.front_default, 'main'));
+}));
