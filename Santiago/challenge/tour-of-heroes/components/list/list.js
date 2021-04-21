6e0d5d37512d313cc6a61de2cd29113c061e3856
Santiago.Martinez <infohere52@gmail.com>
2021-04-21 12:24:20 +0200

    Change oder of id and name in url

--- Santiago/challenge/tour-of-heroes/components/list/list.js
+++ Santiago/challenge/tour-of-heroes/components/list/list.js
@@ -46,7 +46,7 @@ const heroesList = () => {
     heroes.forEach((hero) => {
       const nameHero = document.createElement('li');
       const heroLink = document.createElement('a');
-      heroLink.setAttribute('href', `http://127.0.0.1:5500/skylab-bootcamp-202104-bcn/Santiago/challenge/tour-of-heroes/components/details/details.html?heroeName=${hero.name}&heroeID=${hero.id}&heroeFullName=${hero.biography.fullName}`);
+      heroLink.setAttribute('href', `http://127.0.0.1:5500/skylab-bootcamp-202104-bcn/Santiago/challenge/tour-of-heroes/components/details/details.html?heroeID=${hero.id}&heroeName=${hero.name}&heroeFullName=${hero.biography.fullName}`);
       nameHero.appendChild(heroLink);
       heroLink.innerHTML = hero.name;
       listDiv.appendChild(nameHero);
