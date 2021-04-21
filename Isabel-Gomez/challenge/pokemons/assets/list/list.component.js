0675f47ffb0ee95905cb510c1f777575b0490ceb
Isabel Gomez <belasi87@gmail.com>
2021-04-21 22:27:54 +0200

    Create pagination list

--- Isabel-Gomez/challenge/pokemons/assets/list/list.component.js
+++ Isabel-Gomez/challenge/pokemons/assets/list/list.component.js
@@ -1,10 +1,53 @@
-const pokemonsList = fetchListPokemons('https://pokeapi.co/api/v2/pokemon/?offset=30&limit=30');
-console.log('llamada a la API', pokemonsList);
-
 const mainSection = document.querySelector('.main-container');
+
+const createElement = (tag, parent, innerHTML, className, href) => {
+  const getTag = document.createElement(tag);
+  getTag.innerHTML = innerHTML;
+  parent.appendChild(getTag);
+  getTag.classList.add(className);
+  getTag.href = href;
+  return getTag;
+};
 
 createElement('h1', mainSection, 'Pokemons');
 createElement('a', mainSection, 'Dashboard', null, './../dashboard/dashboard.html');
 createElement('a', mainSection, 'List', null, './../list/list.html');
 createElement('h3', mainSection, 'List Pokemons');
 const getList = createElement('ul', mainSection, '');
+const numberPage = 1;
+
+const getAllPokemons = async (numPage) => {
+  const pokemonList = await fetchPagePokemons(numPage);
+  console.log('API', pokemonList);
+  pokemonList.results.forEach((element) => {
+    const newElementLi = createElement('li', getList, '', 'pokemon-item');
+    createElement('a', newElementLi, `${element.name.toUpperCase()}`, 'pokemon-detail', `../../details/details.html?pokemonName=${element.name}`);
+  });
+};
+getAllPokemons(0);
+
+const clickPages = async (numberPage) => {
+  const pokemonList = await fetchPagePokemons(numberPage);
+  pokemonList.results.forEach((element, position) => {
+    document.querySelectorAll('.pokemon-detail')[position].innerHTML = element.name.toUpperCase();
+  });
+};
+
+const previousButton = createElement('button', mainSection, 'Previous', null);
+const nextButton = createElement('button', mainSection, 'Next', null);
+
+const onChangePage = (numberPage) => {
+  previousButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    if (numberPage >= 0) {
+      clickPages(numberPage--);
+    }
+  });
+  nextButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    if (numberPage <= 5) {
+      clickPages(numberPage++);
+    }
+  });
+};
+onChangePage(numberPage);
--- Isabel-Gomez/challenge/pokemons/services/services.js
+++ Isabel-Gomez/challenge/pokemons/services/services.js
@@ -1,26 +1,13 @@
-// function fetchDashboardPokemons(url) {
-//   return fetch(url).then((response) => response.json())
-//     .then((data) => {
-//       data.results.slice(2, 7).forEach((element) => {
-//         const newElementLi = createElement('li', getDashboardList, '', 'pokemon-item');
-//         const firstPokemonNames = createElement('a', newElementLi, `${element.name.toUpperCase()}`, null, `../../details/details.html?pokemonName=${element.name}`);
-//       });
-//     });
-// }
-
 const fetchDashboardPokemons = async () => {
-  const url = 'https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20';
+  const url = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=40';
   const response = await fetch(url);
   const data = await response.json();
   return data;
 };
 
-function fetchListPokemons(url) {
-  return fetch(url).then((response) => response.json())
-    .then((data) => {
-      data.results.forEach((element) => {
-        const newLi = createElement('li', getList, '', 'pokemon-item');
-        const pokemonsNames = createElement('a', newLi, `${element.name.toUpperCase()}`, null, `../../details/details.html?pokemonName=${element.name}`);
-      });
-    });
-}
+const fetchPagePokemons = async (page) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/?offset=${page}0&limit=10`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
