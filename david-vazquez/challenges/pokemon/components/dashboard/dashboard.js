504ac95b5dfd313e8324d8c8b67472bea2df9dce
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-04-22 01:21:11 +0200

    Creating styles for dashboard screen

--- david-vazquez/challenges/pokemon/components/dashboard/dashboard.js
+++ david-vazquez/challenges/pokemon/components/dashboard/dashboard.js
@@ -2,10 +2,11 @@ const body = document.querySelector('.body');
 
 function createHeader() {
   const header = createHtmlElements(body, 'header', 'header', null);
-  const headerTitle = createHtmlElements(header, 'h1', 'header__title', 'POKEMON');
+  const headerLogo = createHtmlElements(header, 'img', 'header__logo', null);
+  headerLogo.setAttribute('src', './../../store/logo.png');
   const navBar = createHtmlElements(header, 'ul', 'header__navbar', null);
   createNavButton(navBar, 'Dashboard', './dashboard.html');
-  createNavButton(navBar, 'List', './../list/list.html');
+  createNavButton(navBar, 'Pokemons', './../list/list.html');
   createMain();
 }
 
@@ -17,12 +18,12 @@ function createNavButton(parent, text, url) {
 
 function createMain() {
   const dashboard = createHtmlElements(body, 'main', 'dashboard', null);
-  const dashboardTitle = createHtmlElements(dashboard, 'h2', 'dashboard__tittle', 'My Pokemons');
+  const dashboardTitle = createHtmlElements(dashboard, 'h2', 'dashboard__title', 'My Pokemons');
   const dashboardList = createHtmlElements(dashboard, 'ul', 'dashboard__top', null);
   const myPokemons = getData();
   myPokemons.then((element) => {
     element.results.slice(1, 5).forEach((firstPokemon) => {
-      const pokemon = createHtmlElements(dashboardList, 'li', 'dashboarad__top__item', null);
+      const pokemon = createHtmlElements(dashboardList, 'li', 'dashboard__top__item', null);
       const myPokemonsDetail = getDataPokemon(firstPokemon.name);
       myPokemonsDetail.then((element) => {
         const pokemonPic = createHtmlElements(pokemon, 'img', 'pokemon-detail__image', null);
