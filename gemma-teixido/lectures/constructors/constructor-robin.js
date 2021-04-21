03d46b94434d8f4fa006e68bd214790f8c512fd0
gteixi <gemmateixido25@gmail.com>
2021-04-21 13:31:06 +0200

    Added constructor of Batman and Robin.

---
+++ gemma-teixido/lectures/constructors/constructor-robin.js
@@ -0,0 +1,24 @@
+class Heroe { // la clase se escribe siempre con capitalize letter
+    constructor(heroeName, age, city, specialPower = null) {
+        this.heroeName = heroeName;
+        this.age = age;
+        this.city = city;
+        this.specialPower = specialPower;
+    }
+    lastComment = ''; //inicializar primero siempre para evitar errores
+    if (condicionTrue) {
+        this.goToAction();
+    }
+    goToAction = (message) => {
+        this.lastComment = message; //es una variable de clase => propiedad de la clase
+        function talk() {
+            this.lastComment = message;
+            console.log(this.lastComment);
+        }
+        // talk();
+        console.log(this.lastComment);
+    }
+}
+
+let robin = new Heroe ('Robin', 16, 'Gotham', 'sidekik')
+robin.goToAction('Santas cantimploras, Batman!');
