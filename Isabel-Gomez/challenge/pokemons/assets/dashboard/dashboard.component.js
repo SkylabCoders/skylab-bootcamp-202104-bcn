725b7bd08d0bc8f05da5ab36b52d87b38c4837dd
Isabel Gomez <belasi87@gmail.com>
2021-04-21 20:34:15 +0200

    Refactor dashboard

--- Isabel-Gomez/challenge/pokemons/assets/dashboard/dashboard.component.js
+++ Isabel-Gomez/challenge/pokemons/assets/dashboard/dashboard.component.js
@@ -1,10 +1,26 @@
-const pokemons = fetchDashboardPokemons('https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20');
-console.log('llamada a la API', pokemons);
-
 const mainSection = document.querySelector('.main-container');
+
+const createElement = (tag, parent, innerHTML, className, href) => {
+  const getTag = document.createElement(tag);
+  getTag.innerHTML = innerHTML;
+  parent.appendChild(getTag);
+  getTag.classList.add(className);
+  getTag.href = href;
+  return getTag;
+};
 
 createElement('h1', mainSection, 'Pokemons');
 createElement('a', mainSection, 'Dashboard', null, './../dashboard/dashboard.html');
 createElement('a', mainSection, 'List', null, './../list/list.html');
 createElement('h3', mainSection, 'Top 5 Pokemons');
 const getDashboardList = createElement('ul', mainSection, '');
+
+const getRandomPokemons = async () => {
+  const pokemonList = await fetchDashboardPokemons();
+  console.log('API', pokemonList);
+  pokemonList.results.slice(2, 7).forEach((element) => {
+    const newElementLi = createElement('li', getDashboardList, '', 'pokemon-item');
+    createElement('a', newElementLi, `${element.name.toUpperCase()}`, null, `../../details/details.html?pokemonName=${element.name}`);
+  });
+};
+getRandomPokemons();
--- Isabel-Gomez/challenge/pokemons/services/services.js
+++ Isabel-Gomez/challenge/pokemons/services/services.js
@@ -1,26 +1,24 @@
-function fetchDashboardPokemons(url) {
-  return fetch(url).then((response) => response.json())
-    .then((data) => {
-      data.results.slice(2, 7).forEach((element) => {
-        const newElementLi = createElement('li', getDashboardList, '', 'pokemon-item');
-        const firstPokemonNames = createElement('a', newElementLi, `${element.name.toUpperCase()}`, null, `../../details/details.html?pokemonName=${element.name}`);
-      });
-    });
-}
+// function fetchDashboardPokemons(url) {
+//   return fetch(url).then((response) => response.json())
+//     .then((data) => {
+//       data.results.slice(2, 7).forEach((element) => {
+//         const newElementLi = createElement('li', getDashboardList, '', 'pokemon-item');
+//         const firstPokemonNames = createElement('a', newElementLi, `${element.name.toUpperCase()}`, null, `../../details/details.html?pokemonName=${element.name}`);
+//       });
+//     });
+// }
 
-const createElement = (tag, parent, innerHTML, className, href) => {
-  const getTag = document.createElement(tag);
-  getTag.innerHTML = innerHTML;
-  parent.appendChild(getTag);
-  getTag.classList.add(className);
-  getTag.href = href;
-  return getTag;
+const fetchDashboardPokemons = async () => {
+  const url = 'https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20';
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 };
 
 function fetchListPokemons(url) {
   return fetch(url).then((response) => response.json())
     .then((data) => {
-      data.forEach((element) => {
+      data.results.forEach((element) => {
         const newLi = createElement('li', getList, '', 'pokemon-item');
         const pokemonsNames = createElement('a', newLi, `${element.name.toUpperCase()}`, null, `../../details/details.html?pokemonName=${element.name}`);
       });
