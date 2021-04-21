7e21e516f3bc5a3fc55c6feb9bb0fca3b4b99e36
Hosni <hosni.marco@gmail.com>
2021-04-22 00:28:43 +0200

    Set details abilities and table

---
+++ hosni-marco/challenge/pokemon/script/components.js
@@ -0,0 +1,49 @@
+function createList(name) {
+  const ulElement = document.getElementById('list-container');
+  const listElement = document.createElement('li');
+  const anchorElement = document.createElement('a');
+
+  anchorElement.href = `./../html/pokemon-details.html?name=${name}`;
+  anchorElement.innerText = name;
+
+  listElement.setAttribute('id', `id-${name}`);
+
+  ulElement.append(listElement);
+  listElement.append(anchorElement);
+}
+
+// const setDetailsName = (ability) => {
+//   const ulElementDetails = document.getElementById('abilities');
+//   const listElementDetails = document.createElement('li');
+//   listElementDetails.innerText = ability;
+//   ulElementDetails.append(listElementDetails);
+// };
+
+const setAtributes = (ulID, atribute) => {
+  const ulAbilities = document.getElementById(ulID);
+  const liAbilities = document.createElement('li');
+  liAbilities.setAttribute('id', `atribute-${atribute}`);
+  liAbilities.innerText = atribute;
+  ulAbilities.append(liAbilities);
+};
+
+const setTitlePokemon = (name) => {
+  const pokeName = document.getElementById('pokemon-name');
+  pokeName.innerText = name;
+};
+
+const setTable = (nameStat, valueStat) => {
+  const table = document.getElementById('stats');
+  const tRow = document.createElement('tr');
+  tRow.setAttribute('id', nameStat);
+
+  const tdName = document.createElement('td');
+  tdName.innerText = nameStat;
+  const tdValue = document.createElement('td');
+  tdValue.innerText = valueStat;
+
+  tRow.append(tdName);
+  tRow.append(tdValue);
+
+  table.append(tRow);
+};
---
+++ hosni-marco/challenge/pokemon/script/services.js
@@ -0,0 +1,28 @@
+const getPokemonList = async () => {
+  const pokemons = await fetch('https://pokeapi.co/api/v2/pokemon/');
+  const pokemonList = await pokemons.json();
+  pokemonList.results.forEach((element) => {
+    createList(element.name);
+  });
+};
+
+const getabilities = async (name, list) => {
+  const pokemons = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  const pokemonList = await pokemons.json();
+  pokemonList.abilities.forEach((element) => {
+    setAtributes(list, element.ability.name);
+  });
+};
+
+const getStats = async (URLname) => {
+  debugger;
+  const pokemons = await fetch(`https://pokeapi.co/api/v2/pokemon/${URLname}`);
+  const pokemonList = await pokemons.json();
+  pokemonList.stats.forEach((element) => {
+    setTable(element.stat.name, element.base_stat);
+  });
+};
+
+function getPokemonIDFromUrl(url) {
+  return new URLSearchParams(url).get('name');
+}
