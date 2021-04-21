dcca5ce8d02a193cf623e504904106b2dce92f8b
Edgar Alandete <edgalandete@gmail.com>
2021-04-22 00:38:16 +0200

    Create paginated pokemons service

--- Edgar-Alandete/challenges/pokemon/services/services.js
+++ Edgar-Alandete/challenges/pokemon/services/services.js
@@ -7,3 +7,13 @@ const getPokemonById = async (pokemonId) => {
     console.warning('-', error);
   }
 };
+
+const getPaginatedPokemons = async (limit, set) => {
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/pokemon?limit=${limit}&offset=${set}`);
+    const pokemons = await response.json();
+    return pokemons;
+  } catch (error) {
+
+  }
+};
