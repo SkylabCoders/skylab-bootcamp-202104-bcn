77efd5022648773f31c249b5e34f28daf2485255
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-04-22 19:00:59 +0200

    Create getPokemonDetails function

--- alfredo-zimperz/challenges/pokemon/src/pokemon-list/pokemon-list.js
+++ alfredo-zimperz/challenges/pokemon/src/pokemon-list/pokemon-list.js
@@ -1,24 +1,35 @@
 const POKEMON_LIST_BASE_URL = 'https://pokeapi.co/api/v2/';
+const POKEMON_URL = 'pokemon/';
 const MAIN_ELEMENT = document.querySelector('main');
 const listPagination = { offset: 0, limit: 20 };
 
 createElement('h1', null, 'Pokemon list', MAIN_ELEMENT);
 const pokemonListUl = createElement('ul', 'pokemon-list', null, MAIN_ELEMENT);
 
-const getPokemonList = (offset, limit) => {
+const getPokemonDetails = async function (pokemonName) {
+  const pokemonInfo = await fetchData(POKEMON_LIST_BASE_URL + POKEMON_URL + pokemonName);
+  const pokemonListLi = createElement('li', 'pokemon-list__item');
+  const pokemonListA = createElement('a', 'list-item__a');
+  pokemonListA.href = `./../pokemon-detail/?name=${pokemonName}`;
+  const pokemonListImg = createElement('img', 'list-item__img');
+  pokemonListImg.src = pokemonInfo.sprites.front_default;
+  const pokemonListP = createElement('p', 'list-item__img', pokemonName);
+  pokemonListLi.appendChild(pokemonListA);
+  pokemonListA.appendChild(pokemonListImg);
+  pokemonListA.appendChild(pokemonListP);
+  pokemonListUl.appendChild(pokemonListLi);
+};
+
+const getPokemonList = async function (offset, limit) {
   const paginatedURL = `${POKEMON_LIST_BASE_URL}pokemon?limit=${listPagination.limit}
   &offset=${listPagination.offset}`;
 
   pokemonListUl.innerHTML = '';
 
-  fetchData(paginatedURL).then((pokemons) => {
-    pokemons.results.forEach((pokemon) => {
-      const pokemonListLi = createElement('li', 'pokemon-list__item');
-      const pokemonListA = createElement('a', null, pokemon.name);
-      pokemonListA.href = `./../pokemon-detail/?name=${pokemon.name}`;
-      pokemonListLi.appendChild(pokemonListA);
-      pokemonListUl.appendChild(pokemonListLi);
-    });
+  const pokemons = await fetchData(paginatedURL);
+
+  pokemons.results.forEach(async (pokemon) => {
+    await getPokemonDetails(pokemon.name);
   });
 };
 
