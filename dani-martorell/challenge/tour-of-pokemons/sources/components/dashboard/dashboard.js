cd8199768c9290376a645dae09c21c49ce64a6ba
Daniel Martorell <dani.martorell@gmail.com>
2021-04-22 20:48:07 +0200

    Added Eslint and jest to new branch v2

--- dani-martorell/challenge/tour-of-pokemons/sources/components/dashboard/dashboard.js
+++ dani-martorell/challenge/tour-of-pokemons/sources/components/dashboard/dashboard.js
@@ -17,17 +17,24 @@ mainContent.append(mainTitle);
 const pokemonsList = generateElement({ type: 'ul', classes: ['pokemons-list'] });
 mainContent.append(pokemonsList);
 
-pokemonsShortList
-  .then((list) => {
-    const randomList = list.sort(randomNum).slice(0, 5);
-    randomList.forEach((poke) => {
-      getPokemonId(poke.name).then((id) => {
-        const li = generateElement({ type: 'li', classes: ['pokemons-list__element'] });
-        const anchor = generateElement({
-          type: 'a', classes: [], context: `${poke.name}`, url: `./../details/details.html?ID=${id}&NAME=${poke.name}`,
-        });
-        li.append(anchor);
-        mainContent.append(li);
-      });
-    });
-  });
+const getPokemons = async() => {
+  const response = await fetchPokemons(5);
+  const list = await response.json();
+  console.log(list);
+
+
+}
+
+getPokemons();
+  // await getPokemons().results.sort(randomNum.slice(0,5));
+  //   randomList.forEach((poke) => {
+  //     getPokemonId(poke.name).then((id) => {
+  //       const li = generateElement({ type: 'li', classes: ['pokemons-list__element'] });
+  //       const anchor = generateElement({
+  //         type: 'a', classes: [], context: `${poke.name}`, url: `./../details/details.html?ID=${id}&NAME=${poke.name}`,
+  //       });
+  //       li.append(anchor);
+  //       mainContent.append(li);
+  //     });
+  //   });
+  // });
