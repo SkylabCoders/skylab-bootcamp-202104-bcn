a69705003257032508fcbef1e9cc499f4c8318e6
IrenePerezAlonso <irperal@hotmail.com>
2021-04-22 17:51:44 +0200

    Start new proyecy

--- irene-perez/challenge/challenge-irene-perez-pokemon/scr/components/dashboard/dashboard.comp.js
+++ irene-perez/challenge/challenge-irene-perez-pokemon/scr/components/dashboard/dashboard.comp.js
@@ -1,15 +1,15 @@
-const main = document.querySelector('.dashboard');
+// const main = document.querySelector('.dashboard');
 
-const header = createElementHtml('header', null, null, main);
-createElementHtml('h1', 'POKEMON', null, header);
-createElementHtml('h2', 'Pokemons List:', null, header);
-const ul = createElementHtml('ul', null, null, header);
+// const header = createElementHtml('header', null, null, main);
+// createElementHtml('h1', 'POKEMON', null, header);
+// createElementHtml('h2', 'Pokemons List:', null, header);
+// const ul = createElementHtml('ul', null, null, header);
 
-result.then((list) => {
-  const arrayPokemons = list;
-  const newArray = chooseNumberPoke(arrayPokemons, 10);
-  arrayPokemons.forEach((element) => {
-    const li = createElementHtml('li', null, null, ul);
-    createElementHtml('a', element.url, '../../details/details.html', li);
-  });
-});
+// result.then((list) => {
+//   const arrayPokemons = list;
+//   const newArray = chooseNumberPoke(arrayPokemons, 10);
+//   arrayPokemons.forEach((element) => {
+//     const li = createElementHtml('li', null, null, ul);
+//     createElementHtml('a', element.url, '../../details/details.html', li);
+//   });
+// });
--- irene-perez/challenge/challenge-irene-perez-pokemon/scr/service/service.js
+++ irene-perez/challenge/challenge-irene-perez-pokemon/scr/service/service.js
@@ -1,30 +1,30 @@
-const createElementHtml = (element, text, href, parent, id) => {
-  const elementToCreate = document.createElement(element);
-  if (text) {
-    elementToCreate.innerText = text;
-  }
-  if (href) {
-    elementToCreate.setAttribute('href', href);
-  }
-  if (id) {
-    elementToCreate.setAttribute('id', id);
-  }
-  parent.appendChild(elementToCreate);
-  return elementToCreate;
-};
+// const createElementHtml = (element, text, href, parent, id) => {
+//   const elementToCreate = document.createElement(element);
+//   if (text) {
+//     elementToCreate.innerText = text;
+//   }
+//   if (href) {
+//     elementToCreate.setAttribute('href', href);
+//   }
+//   if (id) {
+//     elementToCreate.setAttribute('id', id);
+//   }
+//   parent.appendChild(elementToCreate);
+//   return elementToCreate;
+// };
 
-async function getPokemons(url) {
-  const list = await fetch(url);
-  const parsedList = await list.json();
-  console.log(parsedList);
-}
+// async function getPokemons(url) {
+//   const list = await fetch(url);
+//   const parsedList = await list.json();
+//   console.log(parsedList);
+// }
 
-const result = getPokemons('https://pokeapi.co/api/v2/pokemon/');
+// const result = getPokemons('https://pokeapi.co/api/v2/pokemon/');
 
-function chooseNumberPoke(array, num) {
-  const newArray = [];
-  for (let i = 0; i < num; i + 1) {
-    newArray.push(array[i]);
-  }
-  return newArray;
-}
+// function chooseNumberPoke(array, num) {
+//   const newArray = [];
+//   for (let i = 0; i < num; i + 1) {
+//     newArray.push(array[i]);
+//   }
+//   return newArray;
+// }
