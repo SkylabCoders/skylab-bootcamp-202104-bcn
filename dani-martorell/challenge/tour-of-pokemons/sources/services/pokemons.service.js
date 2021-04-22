6fbb11321e4c9a390a4c8d3e3b6a5f3bff44e327
Daniel Martorell <dani.martorell@gmail.com>
2021-04-22 23:15:13 +0200

    Implementation pagination and set css

--- dani-martorell/challenge/tour-of-pokemons/sources/services/pokemons.service.js
+++ dani-martorell/challenge/tour-of-pokemons/sources/services/pokemons.service.js
@@ -1,10 +1,13 @@
+let currentPagination = 0;
+const listElementsLimit = 20;
+
 async function fetchPokemons(num = 0) {
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=${num}`);
   const data = await response.json();
   return data;
 }
 
-async function getPokemonId(name) {
+async function fetchPokemonId(name) {
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
   const data = await response.json();
   return data.id;
@@ -16,7 +19,10 @@ async function getPokemonDetails(name) {
   return data;
 }
 
-const pokemonsLongList = fetchPokemons(20);
+const getPokemonId = async (name) => {
+  const pokemonId = await fetchPokemonId(name);
+  return pokemonId;
+};
 
 function generateElement(elementProperties) {
   const element = document.createElement(elementProperties.type);
@@ -41,3 +47,26 @@ const paramId = getPokemon('ID', location.search);
 function randomNum(a, b) {
   return 0.5 - Math.random();
 }
+
+const getPokemonsWithOffset = (offset, limit) => fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`)
+  .then((response) => response.json())
+  .then((data) => {
+    data.results.forEach((pokemon) => {
+      const li = generateElement({ type: 'li', classes: ['pokemons-list__element'] });
+      getPokemonId(pokemon.name).then((id) => {
+        const anchor = generateElement({
+          type: 'a', classes: [], context: `${pokemon.name}`, url: `./../details/details.html?ID=${id}&NAME=${pokemon.name}`,
+        });
+        li.append(anchor);
+        mainContent.append(li);
+      });
+    });
+  });
+
+const nextPagination = () => {
+  pokemonsList.innerHTML = '';
+  const buttonNext = document.querySelector('buttons-wrapper__next');
+  // buttonNext.innerHTML = 'Next';
+  currentPagination += 10;
+  getPokemonsWithOffset(currentPagination, listElementsLimit);
+};
