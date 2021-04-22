6c7a6c7d802b9b3eb7b1d6befb3f6ee7738b2f85
Santiago.Martinez <infohere52@gmail.com>
2021-04-22 17:35:13 +0200

    Create pagination

--- Santiago/challenge/pokemon/components/pokemons/pokemons.js
+++ Santiago/challenge/pokemon/components/pokemons/pokemons.js
@@ -1,16 +1,20 @@
 const main = document.querySelector('.pokemons');
+const pagedList = { offset: 0, limit: 20 };
 
 const createHeader = () => {
   const header = createDomElements('header', '', '', main);
   createDomElements('h1', 'Pokemons - List', '', header);
   createDomElements('a', 'DASHBOARD', 'http://127.0.0.1:5500/challenge/pokemon/components/dashboard/dashboard.html', header);
 };
+createHeader();
+
+const listDiv = createDomElements('div', '', '', main);
+const ul = createDomElements('ul', '', '', listDiv);
 
 const createList = () => {
-  const pagination = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=15';
+  const pagination = `https://pokeapi.co/api/v2/pokemon?limit=${pagedList.limit}&offset=${pagedList.offset}`;
 
-  const listDiv = createDomElements('div', '', '', main);
-  const ul = createDomElements('ol', '', '', listDiv);
+  ul.innerHTML = '';
 
   getPokemonData(pagination).then((pokemons) => {
     pokemons.results.forEach((pokemon) => {
@@ -20,8 +24,25 @@ const createList = () => {
   });
 };
 
+const createButtons = () => {
+  const foot = createDomElements('footer', '', '', main);
+  const btnBack = createDomElements('button', 'back', '', foot);
+
+  btnBack.onclick = () => {
+    if (pagedList.offset >= 20) {
+      pagedList.offset -= 20;
+      createList(pagedList.offset, pagedList.limit);
+    }
+  };
+  const btnNext = createDomElements('button', 'next', '', foot);
+  btnNext.onclick = () => {
+    pagedList.offset += 20;
+    createList(pagedList.offset, pagedList.limit);
+  };
+};
+
 const createPokemonsDashboard = () => {
-  createHeader();
-  createList();
+  createList(pagedList.offset, pagedList.limit);
+  createButtons();
 };
 createPokemonsDashboard();
