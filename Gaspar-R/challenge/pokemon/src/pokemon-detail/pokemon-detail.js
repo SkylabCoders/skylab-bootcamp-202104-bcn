d2d3b71be6e87b998a06dbe4d2e0a496009055c9
Gaspar Rodriguez <gaspar.unlp@gmail.com>
2021-04-22 14:47:50 +0200

    Links, images and background added

--- Gaspar-R/challenge/pokemon/src/pokemon-detail/pokemon-detail.js
+++ Gaspar-R/challenge/pokemon/src/pokemon-detail/pokemon-detail.js
@@ -1,4 +1,4 @@
-createElement('h1', 'title', 'Pokemon Pics', '.div__main');
+// createElement('h1', 'title', 'Pokemon Pics', '.div__main');
 
 const listBtn = createElement('a', 'listBtn', 'List of Pokemons', '.div__main');
 listBtn.setAttribute('href', '../pokemon-list/pokemon-list.html');
@@ -8,5 +8,11 @@ dashboard.setAttribute('href', '../dashboard/dashboard.html');
 
 pokemonList.then((data) => data.results.forEach((element) => {
   const pokemonUrlDetails = fetchPokemon(element.url);
-  pokemonUrlDetails.then((data) => createImg('pokemonImg', data.sprites.front_default, 'main'));
+
+  pokemonUrlDetails.then((data) => {
+    createAnchor(`${data.name}`, '../pokemon-list/pokemon-list.html', `${data.name}`, 'main');
+    console.log(data);
+    createImg('pokemonImg', data.sprites.front_default, `.${data.name}`);
+    console.log(`name: ${data.name}`);
+  });
 }));
