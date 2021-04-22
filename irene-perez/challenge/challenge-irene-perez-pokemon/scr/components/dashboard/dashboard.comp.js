18e8e504d8564a9998d5be546be775d1db78a2f0
IrenePerezAlonso <irperal@hotmail.com>
2021-04-22 19:54:44 +0200

    create list with images, names and logo

--- irene-perez/challenge/challenge-irene-perez-pokemon/scr/components/dashboard/dashboard.comp.js
+++ irene-perez/challenge/challenge-irene-perez-pokemon/scr/components/dashboard/dashboard.comp.js
@@ -1,15 +1,30 @@
-// const main = document.querySelector('.dashboard');
+const main = document.querySelector('.dashboard');
 
-// const header = createElementHtml('header', null, null, main);
-// createElementHtml('h1', 'POKEMON', null, header);
-// createElementHtml('h2', 'Pokemons List:', null, header);
-// const ul = createElementHtml('ul', null, null, header);
+function createDashboard() {
+  const header = createElementHtml('header', null, null, main);
+  const logoPoke = createElementHtml('img', null, null, header);
+  logoPoke.setAttribute('src', './../../img/pokemon-logo.png');
+  const prevButton = createElementHtml('button', 'Atrás', null, header);
+  const nextButton = createElementHtml('button', 'Siguiente', null, header);
+  prevButton.onClick = (() => paginate(store.previous));
+  nextButton.onClick = (() => paginate(store.next));
+  createElementHtml('h2', 'Pokemons List:', null, header);
+  const ul = createElementHtml('ul', null, null, header);
+  createPokeList(ul);
+}
+createDashboard();
 
-// result.then((list) => {
-//   const arrayPokemons = list;
-//   const newArray = chooseNumberPoke(arrayPokemons, 10);
-//   arrayPokemons.forEach((element) => {
-//     const li = createElementHtml('li', null, null, ul);
-//     createElementHtml('a', element.url, '../../details/details.html', li);
-//   });
-// });
+function createPokeList(ul) {
+  const result = getData('https://pokeapi.co/api/v2/pokemon?limit=150&offset=0');
+  result.then((element) => {
+    element.results.forEach((pokemon) => {
+      const pokemonItem = createElementHtml('li', null, null, ul);
+      const pokemonLink = createElementHtml('a', pokemon.name, '../../details/details.html', pokemonItem);
+      const detailsPoke = getDataDetails(pokemon.name);
+      detailsPoke.then((element) => {
+        const pokeImg = createElementHtml('img', null, null, pokemonItem);
+        pokeImg.setAttribute('src', `${element.sprites.front_shiny}`);
+      });
+    });
+  });
+}
--- irene-perez/challenge/challenge-irene-perez-pokemon/scr/service/service.js
+++ irene-perez/challenge/challenge-irene-perez-pokemon/scr/service/service.js
@@ -1,30 +1,26 @@
-// const createElementHtml = (element, text, href, parent, id) => {
-//   const elementToCreate = document.createElement(element);
-//   if (text) {
-//     elementToCreate.innerText = text;
-//   }
-//   if (href) {
-//     elementToCreate.setAttribute('href', href);
-//   }
-//   if (id) {
-//     elementToCreate.setAttribute('id', id);
-//   }
-//   parent.appendChild(elementToCreate);
-//   return elementToCreate;
-// };
+const createElementHtml = (element, text, href, parent, id) => {
+  const elementToCreate = document.createElement(element);
+  if (text) {
+    elementToCreate.innerText = text;
+  }
+  if (href) {
+    elementToCreate.setAttribute('href', href);
+  }
+  if (id) {
+    elementToCreate.setAttribute('id', id);
+  }
+  parent.appendChild(elementToCreate);
+  return elementToCreate;
+};
 
-// async function getPokemons(url) {
-//   const list = await fetch(url);
-//   const parsedList = await list.json();
-//   console.log(parsedList);
-// }
+function getData(url) {
+  return fetch(url)
+    .then((response) => response.json())
+    .catch(() => { console.error('You are gonna die, muahahahaha!!'); });
+}
 
-// const result = getPokemons('https://pokeapi.co/api/v2/pokemon/');
-
-// function chooseNumberPoke(array, num) {
-//   const newArray = [];
-//   for (let i = 0; i < num; i + 1) {
-//     newArray.push(array[i]);
-//   }
-//   return newArray;
-// }
+function getDataDetails(name) {
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    .then((response) => response.json())
+    .catch(() => { console.error('You are gonna die, muahahahaha!!'); });
+}
