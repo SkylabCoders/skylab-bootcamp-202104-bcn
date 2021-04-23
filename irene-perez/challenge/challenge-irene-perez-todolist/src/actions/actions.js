57857fc08e225d37b88fe0cea883d469b1a10483
IrenePerezAlonso <irperal@hotmail.com>
2021-04-23 19:00:17 +0200

    Finished the first functions

--- irene-perez/challenge/challenge-irene-perez-todolist/src/actions/actions.js
+++ irene-perez/challenge/challenge-irene-perez-todolist/src/actions/actions.js
@@ -1,10 +1,23 @@
-
+const createElementHtml = (element, text, href, parent, id) => {
+  const elementToCreate = document.createElement(element);
+  if (text) {
+    elementToCreate.innerText = text;
+  }
+  if (href) {
+    elementToCreate.setAttribute('href', href);
+  }
+  if (id) {
+    elementToCreate.setAttribute('id', id);
+  }
+  parent.appendChild(elementToCreate);
+  return elementToCreate;
+};
 //   function getData(url) {
 //     return fetch(url)
 //       .then((response) => response.json())
 //       .catch(() => { console.error('You are gonna die, muahahahaha!!'); });
 //   }
-  
+
 //   function getDataDetails(name) {
 //     return fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
 //       .then((response) => response.json())
