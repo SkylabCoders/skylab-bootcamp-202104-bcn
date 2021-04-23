2c86bb24b7ae697dd699280b8753e6c265880a5b
Anna Sala <anna.sala.mercade@gmail.com>
2021-04-23 09:44:02 +0200

    returned to no images list was not working propertly

--- anna-sala/challenges/pokeapi/list/list.js
+++ anna-sala/challenges/pokeapi/list/list.js
@@ -28,6 +28,7 @@ function fetchApiList(limit, offset, callback) {
 function iterationList(obj) {
   obj.results.forEach((element) => {
     printOnListPage('a', element.name, 'pokemons__list--item', `./../detail/detail.html?pokemonName=${element.name}`);
+    )
   });
 }
 
