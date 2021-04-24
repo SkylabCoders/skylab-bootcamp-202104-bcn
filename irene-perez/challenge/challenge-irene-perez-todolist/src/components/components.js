c627b4f681ffe3518ef6d11022671e41c15dc6b7
IrenePerezAlonso <irperal@hotmail.com>
2021-04-24 19:58:30 +0200

    created the functions modifyElement and addNewValue and it worksgit status

--- irene-perez/challenge/challenge-irene-perez-todolist/src/components/components.js
+++ irene-perez/challenge/challenge-irene-perez-todolist/src/components/components.js
@@ -17,7 +17,8 @@ function addToList(element, array) {
   const newElementList = element.value;
   array.push(newElementList);
   const li = createElement('li', `${newElementList}`, null, ul, `${array.length}`);
-  createElement('button', 'Modify', null, li);
+  const modifyButton = createElement('button', 'Modify', null, li);
+  modifyButton.onclick = (() => modifyElement(elementId));
   const deleteButton = createElement('button', 'Delete', null, li);
   const elementId = li.id;
   deleteButton.onclick = (() => deleteTask(myArray, elementId));
@@ -37,4 +38,20 @@ function deleteTask(array, id) {
 function addSyleElement(id) {
   const elementToMark = document.getElementById(id);
   elementToMark.style = 'text-decoration:line-through';
+}
+
+function modifyElement(id) {
+  const elementToModify = document.getElementById(id);
+  const input = createElement('input', null, null, elementToModify);
+  input.getAttribute('value');
+  const buttonInput = createElement('button', 'Change', null, elementToModify);
+  buttonInput.onclick = (() => addNewValue(elementToModify, input, myArray));
+}
+
+function addNewValue(oldTask, newTask, array) {
+  const value = oldTask.innerHTML;
+  const elementIndex = array.indexOf(value);
+  const newValue = newTask.value;
+  array.splice(elementIndex, 1, newValue);
+  oldTask.innerHTML = newTask.value;
 }
