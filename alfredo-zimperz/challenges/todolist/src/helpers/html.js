f50664193c2bc1d8271f5a86200d3471f309ac54
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-04-25 00:08:39 +0200

    Create scss styles

--- alfredo-zimperz/challenges/todolist/src/helpers/html.js
+++ alfredo-zimperz/challenges/todolist/src/helpers/html.js
@@ -16,17 +16,19 @@ const createElement = (
 };
 
 const createTaskListItem = (id, title, description, done, parent) => {
-  const item = createElement('div', parent, 'todo-list_item', null, null);
+  const item = createElement('div', parent, 'todo-list__item', null, null);
   const itemTextContainer = createElement('div', item, 'item__text-container', null, null);
   const itemActionsContainer = createElement('div', item, 'item__actions-container', null, null);
-  const btnDelete = createElement('button', itemActionsContainer, 'actions__button--delete', id, 'Delete');
+  const btnDelete = createElement('button', itemActionsContainer, 'actions__button--delete', id);
   btnDelete.onclick = () => handleDelete(id);
+  btnDelete.innerHTML = '<i class="far fa-trash-alt"></i>';
   const btnModify = createElement('button', itemActionsContainer, 'actions__button--modify', id, 'Modify');
   btnModify.onclick = () => handleModify(id);
+  btnModify.innerHTML = '<i class="far fa-edit"></i>';
   const btnToogleState = createElement('button', itemActionsContainer, 'actions__button--toogle', id, 'Complete');
   btnToogleState.onclick = () => handleComplete(id);
-  const taskh1 = createElement('h1', itemTextContainer, null, null, title);
+  btnToogleState.innerHTML = '<i class="far fa-check-square"></i>';
+  const taskh1 = createElement('h2', itemTextContainer, null, null, title);
   if (done) { taskh1.classList.add('done'); }
   createElement('p', itemTextContainer, null, null, description);
-  createElement('p', itemTextContainer, null, null, `done: ${done}`);
 };
