ee29563449819e80b594796306e489988603f6fe
Hosni <hosni.marco@gmail.com>
2021-04-24 14:48:07 +0200

    Creating store

---
+++ hosni-marco/challenge/todo-list/src/store.js
@@ -0,0 +1,28 @@
+const store = [
+    {
+        type: "CLIENT-CREATE",
+        cient: {
+            fname: "",
+            lname: "",
+    }
+    },
+    {
+        type: "OBTAIN-CLIENT"
+    },
+    {
+        type: "UPDATE-CLIENT",
+    },
+    {
+        type: "DELETE-CLIENT"
+    },
+    {
+        clients:[
+            {
+            clientID,
+            clientFname,
+            clientLname,
+            }
+        ]
+    }
+]
+  
