f3222c4ba5cd2c2b0685da256b8987361131d9e0
ulldecorb <ausias.bertran@gmail.com>
2021-04-25 18:18:23 +0200

    Implement id search

--- ausias-bertran/challenge/todo-list/services/service.js
+++ ausias-bertran/challenge/todo-list/services/service.js
@@ -1,30 +1,26 @@
 function createNode(elementType, fatherNode = '', classAtribute, innerData = '', position = '') {
     let newNode = document.createElement(`${elementType}`);
-    newNode.className = (`'${classAtribute}'`);
+    newNode.className = `${classAtribute}`;
     newNode.innerHTML = `${innerData}`; 
     fatherNode.insertAdjacentElement(position, newNode); 
     return newNode;
 }
 
 function createTodoList() {
-    const mainList = createNode('ul', main, 'main-list', '', 'afterbegin');
+    const mainList = createNode('ul', main,'main-list', '', 'afterbegin');
     const listAtributeName = Object.values(state);
     const stateLenght = Object.values(state).length;
-    for (let i = 0; i < stateLenght; i++) {
-        const todoTask = createNode('li', mainList, `main-list__task`, listAtributeName[i], 'beforeend');
-
+    for (let i = 0; i < stateLenght; i++) {   
+        const todoTask = createNode('li', mainList, `main-list__task`, '', 'beforeend');
+        const showValue = createNode('div', todoTask, 'main-task', state.task[`task${i}`].taskTodo, 'afterbegin')
         const updateInput = createNode('input', todoTask, 'main-list__update-input', '', 'afterbegin');        
-        updateInput.value = listAtributeName[i];
-
-        const updateButton = createNode('button', todoTask, 'main-list__update-button', 'CHANGE', 'beforeend');
-        updateButton.onclick = () => updateTask(listAtributeName[i], updateInput.value);
-        
-        const showUpdateButton = createNode('button', updateInput, 'main-list__show-update-input-button', 'UPDATE', 'afterend');
-        showUpdateButton.onclick = () => showUpdateTask(updateInput, updateButton);
-        
-        const deleteButton = createNode('button', todoTask, 'main-list__delete-button', 'DELETE', 'beforeend');
-        deleteButton.onclick = () => deleteTask(listAtributeName[i]);
-        
+        const updateButton = createNode('button', todoTask, 'main-list__update-button', '✅', 'beforeend');
+        const showUpdateButton = createNode('button', showValue, 'main-list__show-update-input-button', '🔄', 'afterend');
+        const deleteButton = createNode('button', todoTask, 'main-list__delete-button', '❌', 'beforeend');
+        updateInput.value = state.task[`task${i}`].taskTodo;
+        updateButton.onclick = () => updateTask(listAtributeName[i], state.task[`task${i}`].taskTodo);
+        showUpdateButton.onclick = () => showUpdateTask(updateInput, updateButton, showValue);
+        deleteButton.onclick = () => deleteTask(`task${i}`);
         updateInput.style.display = 'none';
         updateButton.style.display = 'none';
     }
@@ -33,7 +29,7 @@ function createTodoList() {
 
 function createNewTaskBox() {
     const newTaskBox = createNode('div', main, 'new-task','', 'beforeend');
-    const newTaskButton = createNode('button', newTaskBox, 'new-task__button', 'NEW', 'beforeend');
+    const newTaskButton = createNode('button', newTaskBox, 'new-task__button', 'ADD', 'beforeend');
     newTaskButton.onclick = () => addTask();
     const newTaskValue = createNode('input', newTaskBox, 'new-task__input', '', 'afterbegin');
     return newTaskBox;
@@ -41,32 +37,38 @@ function createNewTaskBox() {
 
 function addTask() {
     const newTaskValue = document.getElementsByTagName('input');
-    state[`${newTaskValue[newTaskValue.length -1].value}`] = newTaskValue[newTaskValue.length -1].value;
+    state.task[`task${state.taskListCounter}`] = {
+        taskTodo: newTaskValue[newTaskValue.length - 1].value,
+        id : state.taskListCounter,
+    }
     document.getElementsByTagName('ul')[0].remove();
     createTodoList();
-    newTaskValue[newTaskValue.length -1].value = '';
+    newTaskValue[newTaskValue.length - 1].value = '';
+    state.taskListCounter += 1;
 }
 
 function deleteTask(atribute) {
-    delete state[`${atribute}`];
+    delete state.task[`${atribute}`];
+
     document.getElementsByTagName('ul')[0].remove();
     createTodoList();
 }
 
-function updateTask( atribute, newValue) {
-    state[`${atribute}`] = newValue;
+function updateTask( id, newValue) {
+    state.task[`task${id}`].taskTodo = newValue;
     document.getElementsByTagName('ul')[0].remove();
     createTodoList();
 }
 
-function showUpdateTask(input,button) {
+function showUpdateTask(input, button, currentTask) {
     if (input.style.display === 'none') {
         input.style.display = 'block';
         button.style.display = 'block';
-    }
-    if (input.style.display === 'block') {
+        currentTask.style.display = 'none';
+    } else if (input.style.display === 'block') {
         input.style.display = 'none';
         button.style.display = 'none';
+        currentTask.style.display = 'block';
     }
 }
 
--- ausias-bertran/challenge/todo-list/store/store.js
+++ ausias-bertran/challenge/todo-list/store/store.js
@@ -1,6 +1,26 @@
 const state = {
-    ['Upload Button']: 'Upload Button',
-    ['Pomodoro Break']: 'Pomodoro Break'
-};
+    task: {
+        task0: {
+            id: 0,
+            taskTodo: 'Upload Button'
+        },
+        task1: {
+            id: 1,
+            taskTodo: 'Pomodoro Break'
+        },
+    },
+    taskListCounter: 2,
+
+
+
+
+
+
+
+
+
+
+    
+}
 
 
