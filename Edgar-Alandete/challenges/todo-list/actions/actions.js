f4fc1e30de8cb228f29b056aa614e9838bb18940
Edgar Alandete <edgalandete@gmail.com>
2021-04-25 16:41:53 +0200

    Create tests

--- Edgar-Alandete/challenges/todo-list/actions/actions.js
+++ Edgar-Alandete/challenges/todo-list/actions/actions.js
@@ -31,27 +31,3 @@ const createError = () => {
   const payload = { type: 'ERROR', data: 'NO SE PUEDE CREAR LA TAREA' };
   return payload;
 };
-function dispatcher({ data, type }) {
-  switch (type) {
-    case 'CREATE_TASK':
-      tasksReducer(data, type);
-      break;
-    case 'DELETE_TASK':
-      tasksReducer(data, type);
-      break;
-    case 'LOAD_TASKS':
-      tasksReducer(data, type);
-      break;
-    case 'UPDATE_TASK':
-      tasksReducer(data, type);
-      break;
-    case 'ERROR':
-      generalReducer(data, type);
-      break;
-    default:
-
-      break;
-  }
-}
-
-// module.exports = { getTask, getTasks };
--- Edgar-Alandete/challenges/todo-list/actions/actions.test.js
+++ Edgar-Alandete/challenges/todo-list/actions/actions.test.js
@@ -1,4 +1,60 @@
-test('Given a getTask function creator', () => {
-  const result = getTask();
-  expect(actionTypes.GET_TASK).toBe(result.type);
+const GET_TASK = 'GET_TASK';
+const LOAD_TASKS = 'LOAD_TASKS';
+const CREATE_TASK = 'CREATE_TASK';
+const DELETE_TASK = 'DELETE_TASK';
+const UPDATE_TASK = 'UPDATE_TASK';
+const ERROR = 'ERROR';
+
+const createTask = (taskToCreate) => {
+  const payload = { type: 'CREATE_TASK', data: taskToCreate };
+  return payload;
+};
+
+const deleteTask = (taskId) => {
+  const payload = { type: DELETE_TASK, data: taskId };
+  return payload;
+};
+
+describe('Given a createTask function creator', () => {
+  const scenarios = [
+    {
+      task: { taskId: '', value: 'Task for the test', isFinished: false },
+      result: { type: CREATE_TASK, data: { taskId: '', value: 'Task for the test', isFinished: false } },
+    },
+  ];
+
+  scenarios.forEach((scenario) => {
+    describe(`When invoked with values task: ${scenario.task}`, () => {
+      test(`Then return ${scenario.result}`, () => {
+        const taskToCreate = scenario.task;
+        const resultScenario = scenario.result;
+        // Act
+        const result = createTask(taskToCreate);
+        // Assert
+        expect(resultScenario).toEqual(result);
+      });
+    });
+  });
+});
+
+describe('Given a createTask function creator', () => {
+  const scenarios = [
+    {
+      task: 2,
+      result: { type: DELETE_TASK, data: 2 },
+    },
+  ];
+
+  scenarios.forEach((scenario) => {
+    describe(`When invoked with values task: ${scenario.task}`, () => {
+      test(`Then return ${scenario.result}`, () => {
+        const taskToCreate = scenario.task;
+        const resultScenario = scenario.result;
+        // Act
+        const result = deleteTask(taskToCreate);
+        // Assert
+        expect(resultScenario).toEqual(result);
+      });
+    });
+  });
 });
