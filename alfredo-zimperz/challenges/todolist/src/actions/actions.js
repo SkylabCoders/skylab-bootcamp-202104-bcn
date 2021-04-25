58dc35ad9809436b5829b1ad76f347e1b5420943
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-04-25 13:04:57 +0200

    Convert createTask function to a promise

--- alfredo-zimperz/challenges/todolist/src/actions/actions.js
+++ alfredo-zimperz/challenges/todolist/src/actions/actions.js
@@ -1,3 +1,5 @@
+const LA_PROGRAMACION_MOLA = true;
+
 const getTasks = () => store.dispatch(
   { type: GET_TASKS },
 );
@@ -6,9 +8,26 @@ const deleteTask = (taskId) => store.dispatch(
   { type: DELETE_TASK, taskId },
 );
 
-const createTask = (taskData) => store.dispatch(
-  { type: CREATE_TASK, taskData },
-);
+// const createTask = (taskData) => {
+//   store.dispatch(
+//     { type: CREATE_TASK, taskData },
+//   );
+// };
+
+const createTask = (taskData) => new Promise((resolve, reject) => {
+  const { title } = taskData;
+  const { description } = taskData;
+  if (LA_PROGRAMACION_MOLA === true) {
+    setTimeout(() => {
+      resolve({ type: CREATE_TASK, taskData: { title, description, id: null } });
+    }, 2000);
+  } else {
+    reject({
+      type: 'ERROR',
+      message: 'La programación no mola',
+    });
+  }
+});
 
 const modifyTask = (taskData) => store.dispatch(
   { type: MODIFY_TASK, taskData },
