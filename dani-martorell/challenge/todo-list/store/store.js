9909e11923c71950538e1f588d7a40652a3fde7c
Daniel Martorell <dani.martorell@gmail.com>
2021-04-25 10:59:46 +0200

    Refractor reducers

--- dani-martorell/challenge/todo-list/store/store.js
+++ dani-martorell/challenge/todo-list/store/store.js
@@ -16,3 +16,46 @@ const state = {
     },
   ],
 };
+
+const reducer = ({ type, data }) => {
+  switch (type) {
+    case 'ADD_TASK':
+      addTaskToStore(data);
+      break;
+
+    case 'REMOVE_TASK':
+      removeTaskFromStore(data);
+      break;
+
+    case 'OPEN_EDIT_FRAME':
+      modifyEditElement(data);
+      break;
+
+    case 'CONFIRM_EDIT':
+      modifyTask(data);
+      break;
+
+    default:
+      break;
+  }
+};
+
+const addTaskToStore = ({ task, id }) => {
+  state.toDoList.push({ task, id });
+  state.lastId = id;
+  renderList(state.toDoList);
+};
+
+const removeTaskFromStore = (id) => {
+  const list = state.toDoList;
+  const index = list.findIndex((element) => element.id === +id);
+  list.splice(index, 1);
+  renderList(state.toDoList);
+};
+
+const modifyTask = ({ task, id }) => {
+  const list = state.toDoList;
+  const [selectedElement] = list.filter((element) => element.id === +id);
+  selectedElement.task = task;
+  renderList(state.toDoList);
+};
