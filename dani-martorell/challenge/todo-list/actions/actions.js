9d1d406d8a5befd3657906e86a25f8d8b5d5d482
Daniel Martorell <dani.martorell@gmail.com>
2021-04-25 13:29:47 +0200

    Add setTimeOut on add new task

--- dani-martorell/challenge/todo-list/actions/actions.js
+++ dani-martorell/challenge/todo-list/actions/actions.js
@@ -1,20 +1,23 @@
 const addTask = (task) => {
   if (!task) return;
   const myPromise = new Promise((resolve, reject) => {
-    const index = state.toDoList.find((element) => element.task === task);
-    if (index) {
-      reject('Task is already listed');
-    } else {
-      resolve(
-        {
-          type: 'ADD_TASK',
-          data: {
-            task,
-            id: state.lastId + 1,
+    const index = state.toDoList.find((element) => element.task.toUpperCase() === task.toUpperCase());
+    setTimeout(() => {
+      if (index) {
+        task = task.toUpperCase();
+        reject(`"${task}" is already listed`);
+      } else {
+        resolve(
+          {
+            type: 'ADD_TASK',
+            data: {
+              task,
+              id: state.lastId + 1,
+            },
           },
-        },
-      );
-    }
+        );
+      }
+    }, 600);
   });
   myPromise.then((data) => reducer(data)).catch((error) => alert(error));
 };
