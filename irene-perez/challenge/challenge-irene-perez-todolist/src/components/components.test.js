e1923cae10229a178ef69e419b8a9cffcf19ac18
IrenePerezAlonso <irperal@hotmail.com>
2021-04-25 19:15:21 +0200

    Add testing document

---
+++ irene-perez/challenge/challenge-irene-perez-todolist/src/components/components.test.js
@@ -0,0 +1,14 @@
+const createElement = (element, text, href, parent, id) => {
+  const elementToCreate = document.createElement(element);
+  if (text) {
+    elementToCreate.innerText = text;
+  }
+  if (href) {
+    elementToCreate.setAttribute('href', href);
+  }
+  if (id) {
+    elementToCreate.setAttribute('id', id);
+  }
+  parent.appendChild(elementToCreate);
+  return elementToCreate;
+};
