1ae0ef7478dd7162d16e52e91666668a43d15e39
Gaspar Rodriguez <gaspar.unlp@gmail.com>
2021-04-25 23:35:59 +0200

    Style added, and doneBtn finished

--- Gaspar-R/challenge/to-do-list/src/components/todolist.js
+++ Gaspar-R/challenge/to-do-list/src/components/todolist.js
@@ -7,35 +7,55 @@ const todoUl = document.querySelector('.todo-list');
 const handlerAddTask = () => {
   const newTask = addTask(inputBtn.value);
   dispatcher(newTask);
-  console.log('asd', state);
+  updateHTML();
 };
 
 const handlerDeleteTask = (taskId) => {
   const delTaskId = deleteTask(taskId);
   dispatcher(delTaskId);
-//   console.log(taskId);
+  updateHTML();
+};
+
+const handlerDoneTask = (taskId) => {
+  const completeTask = completedTask(taskId);
+  dispatcher(completeTask);
+  updateHTML();
 };
 
 function updateHTML() {
   todoUl.innerText = '';
   state.forEach((task) => {
     const li = document.createElement('li');
+    li.classList.add('task_li');
     li.innerText = `${task.title}`;
 
     todoUl.appendChild(li);
     delBtnCreator(li, task);
+    doneBtn(li, task);
   });
 }
 
-updateHTML();
-
-console.log(state);
-
 function delBtnCreator(li, task) {
   const delBtn = document.createElement('button');
-  delBtn.classList.add('delBtn', `id${task.id}`);
+  delBtn.classList.add('delBtn');
   delBtn.onclick = (() => handlerDeleteTask(task.id));
 
   delBtn.innerText = '-';
   li.appendChild(delBtn);
 }
+
+function doneBtn(li, task) {
+  const doneBtn = document.createElement('button');
+  if (task.complete === true) {
+    doneBtn.classList.add('completedTaskBtn');
+    doneBtn.setAttribute('disable', '');
+    li.classList.add('completedTask_li');
+  } else { doneBtn.classList.add('doneBtn'); }
+  doneBtn.onclick = (() => handlerDoneTask(task.id));
+
+  doneBtn.innerText = '✓';
+  li.appendChild(doneBtn);
+}
+
+updateHTML();
+console.log(state);
