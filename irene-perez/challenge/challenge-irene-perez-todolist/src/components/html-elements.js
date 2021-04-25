c0015e258f87c3162d776a1792e2fb9bc0d5aa8f
IrenePerezAlonso <irperal@hotmail.com>
2021-04-25 18:29:08 +0200

    created a css document

--- irene-perez/challenge/challenge-irene-perez-todolist/src/components/html-elements.js
+++ irene-perez/challenge/challenge-irene-perez-todolist/src/components/html-elements.js
@@ -1,22 +1,22 @@
 const main = document.querySelector('main');
 
 const header = createElement('header', null, null, main);
-header.setAttribute('className', 'header');
+header.setAttribute('class', 'header');
 
-const tittle = createElement('h2', 'To Do list:', null, header);
-tittle.setAttribute('className', 'header__tittle');
+const tittle = createElement('h2', '✔️ To Do list ', null, header);
+tittle.setAttribute('class', 'header__tittle');
 
 const ul = createElement('ul', null, null, header);
-ul.setAttribute('className', 'header__ul');
+ul.setAttribute('class', 'header__ul');
 
 const input = createElement('input', null, null, header);
-input.getAttribute('className', 'header__input');
+input.getAttribute('class', 'header__input');
 input.getAttribute('value');
 
 const addButton = createElement('button', 'Add to list', null, header);
-addButton.setAttribute('className', 'header__addButton');
+addButton.setAttribute('class', 'header__addButton');
 addButton.onclick = function () { addToList(input, myArray); };
 
 const resetButton = createElement('button', 'Clean all', null, main);
-resetButton.setAttribute('className', 'resetButton');
+resetButton.setAttribute('class', 'resetButton');
 resetButton.onclick = function () { resetList(myArray, ul); };
