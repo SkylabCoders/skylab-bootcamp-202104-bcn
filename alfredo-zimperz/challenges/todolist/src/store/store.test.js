64612d943461f889ac31badb46ca3851a8b75703
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-04-25 17:05:22 +0200

    Create reducer test

--- alfredo-zimperz/challenges/todolist/src/store/store.test.js
+++ alfredo-zimperz/challenges/todolist/src/store/store.test.js
@@ -1,20 +1,36 @@
-const tasksReducer = (tasks = this.tasks, action) => {
-  let newTasks = [...tasks];
+const testingTasks = [
+  {
+    id: 0,
+    title: 'Tarea 1',
+    description: 'Descripcion tarea 1',
+    done: false,
+  },
+  {
+    id: 1,
+    title: 'Tarea 2',
+    description: 'Descripcion tarea 2',
+    done: true,
+  },
+];
+const nextTaskId = 2;
+
+const tasksReducer = (action, testTasks = testingTasks) => {
+  let newTasks = [...testTasks];
 
   switch (action.type) {
-    case GET_TASKS:
+    case 'GET_TASKS':
       break;
-    case DELETE_TASK:
+    case 'DELETE_TASK':
       newTasks = newTasks.filter((task) => task.id !== action.taskId);
       break;
-    case CREATE_TASK:
+    case 'CREATE_TASK':
       newTasks = [
         ...newTasks,
-        { ...action.taskData, done: false, id: this.nextTaskId },
+        { ...action.taskData, done: false, id: nextTaskId },
       ];
-      this.nextTaskId++;
+      // nextTaskId++;
       break;
-    case MODIFY_TASK:
+    case 'MODIFY_TASK':
       newTasks = newTasks.map(
         (task) => {
           if (task.id === action.taskData.id) {
@@ -40,7 +56,51 @@ const tasksReducer = (tasks = this.tasks, action) => {
   return newTasks;
 };
 
-test('Given a tasksReducer function', () => {
-  result = true;
-  expect(result).toBe(true);
+test('Given a tasksReducer function, receiving an DELETE_TASK action', () => {
+  result = tasksReducer({ type: 'DELETE_TASK', taskId: 1 });
+  expect(result).toEqual(
+    [
+      {
+        id: 0,
+        title: 'Tarea 1',
+        description: 'Descripcion tarea 1',
+        done: false,
+      },
+    ],
+  );
+});
+
+test('Given a tasksReducer function, receiving an CREATE_TASK action', () => {
+  result = tasksReducer(
+    {
+      type: 'CREATE_TASK',
+      taskData: {
+        title: 'Tarea 3',
+        description: 'Descripcion tarea 3',
+      },
+    },
+  );
+  console.log(result);
+  expect(result).toEqual(
+    [
+      {
+        id: 0,
+        title: 'Tarea 1',
+        description: 'Descripcion tarea 1',
+        done: false,
+      },
+      {
+        id: 1,
+        title: 'Tarea 2',
+        description: 'Descripcion tarea 2',
+        done: true,
+      },
+      {
+        title: 'Tarea 3',
+        description: 'Descripcion tarea 3',
+        done: false,
+        id: 2,
+      },
+    ],
+  );
 });
