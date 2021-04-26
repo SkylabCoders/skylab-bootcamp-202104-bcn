b6e84549a347e83efb2ee2f1b17d0a93fee85dd9
Isabel Gomez <belasi87@gmail.com>
2021-04-26 17:56:23 +0200

    Install esLint and everything is ok

--- Isabel-Gomez/challenge/mychallenge/src/component.jsx
+++ Isabel-Gomez/challenge/mychallenge/src/component.jsx
@@ -1,24 +1,23 @@
-import {useState} from "react";
+import { useState } from 'react';
 
 function ButtonComponent() {
+  const [counter, setCounter] = useState(0);
 
-const [counter, setCounter] = useState(0);
+  const increment = () => {
+    setCounter(counter + 1);
+  };
 
-const increment = () => {
-    setCounter(counter+1);
-}
+  const decrement = () => {
+    setCounter(counter - 1);
+  };
 
-const decrement = () => {
-    setCounter(counter-1);
-}
-
-return (
+  return (
     <div>
-        <button onClick={increment}>+</button>
-        <p>{counter}</p>
-        <button onClick={decrement}>-</button>
-    </div>)
-
+      <button onClick={increment}>+</button>
+      <p>{counter}</p>
+      <button onClick={decrement}>-</button>
+    </div>
+  );
 }
 
 export default ButtonComponent;
