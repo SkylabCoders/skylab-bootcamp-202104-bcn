3a32523af2cc70881ad87e4ead780bc3eb8f0762
Daniel Martorell <dani.martorell@gmail.com>
2021-04-26 19:08:59 +0200

    Set useState

--- dani-martorell/challenge/clickerApp/clicker/src/App.jsx
+++ dani-martorell/challenge/clickerApp/clicker/src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './button';
+import Screen from './resultScreen';
 
 function App() {
-  const increment = () => console.log('sumando');
-  const decrement = () => console.log('restando');
+  const [counter, setCounter] = useState(0);
+  const increment = () => setCounter(counter + 1);
+  const decrement = () => setCounter(counter - 1);
   return (
     <div className="App">
       <header className="App-header">
         <Button operation={increment} sign="+" />
+        <Screen value={counter} />
         <Button operation={decrement} sign="-" />
 
       </header>
--- dani-martorell/challenge/clickerApp/clicker/src/resultScreen.jsx
+++ dani-martorell/challenge/clickerApp/clicker/src/resultScreen.jsx
@@ -1,1 +1,7 @@
 import React from 'react';
+
+const Screen = ({ value }) => (
+  <h2>{value}</h2>
+);
+
+export default Screen;
