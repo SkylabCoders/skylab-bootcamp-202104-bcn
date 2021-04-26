7013846208777875937ab7988b06894766906bd7
Isabel Gomez <belasi87@gmail.com>
2021-04-26 17:49:35 +0200

    Finish counter button

--- Isabel-Gomez/challenge/mychallenge/src/App.js
+++ Isabel-Gomez/challenge/mychallenge/src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import createButton from './component';
+import ButtonComponent from './component';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>The Button</h1>
-        <createButton getClick={(counter) => {console.log(counter++)}} />
+        <ButtonComponent/>
       </header>
     </div>
   );
--- Isabel-Gomez/challenge/mychallenge/src/component.jsx
+++ Isabel-Gomez/challenge/mychallenge/src/component.jsx
@@ -1,6 +1,24 @@
-function createButton({getClick}) {
-let counter = 0;
-return <button onClick={(e) => {getClick(counter++)}}>counter</button>
+import {useState} from "react";
+
+function ButtonComponent() {
+
+const [counter, setCounter] = useState(0);
+
+const increment = () => {
+    setCounter(counter+1);
 }
 
-export default createButton;
+const decrement = () => {
+    setCounter(counter-1);
+}
+
+return (
+    <div>
+        <button onClick={increment}>+</button>
+        <p>{counter}</p>
+        <button onClick={decrement}>-</button>
+    </div>)
+
+}
+
+export default ButtonComponent;
