ec123bdec2a4c096616f1d9d81de7910ebdd8134
Fernando Gomez <shadyfast8.services@gmail.com>
2021-04-26 19:37:47 +0200

    solved eslint exceptins

--- fernando-gomez/challenge/tour-of-heroes-react/src/HeroDetailComponent.jsx
+++ fernando-gomez/challenge/tour-of-heroes-react/src/HeroDetailComponent.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable react/prop-types */
 import React from 'react';
 
 const HeroDetail = ({ currentHero }) => (
--- fernando-gomez/challenge/tour-of-heroes-react/src/HeroesListComponents.jsx
+++ fernando-gomez/challenge/tour-of-heroes-react/src/HeroesListComponents.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable react/prop-types */
 import React from 'react';
 
 const HeroesList = ({ heroesList, setAsSelected }) => (
