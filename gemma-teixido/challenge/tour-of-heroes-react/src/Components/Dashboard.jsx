0b91e41e259c948a6140aac503da82012a824f4f
gteixi <gemmateixido25@gmail.com>
2021-04-27 09:17:50 +0200

    Ready for pull review.

---
+++ gemma-teixido/challenge/tour-of-heroes-react/src/Components/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <header className="header">
+      <h1 className="header-title">
+        Dashboard of Heroes
+        <button type="button">List</button>
+      </h1>
+    </header>
+  );
+}
+
+export default Dashboard;
