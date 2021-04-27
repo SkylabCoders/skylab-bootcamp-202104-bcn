96343744e8dabfdfb2bf2fbfde11a0fb4b355530
Santiago.Martinez <infohere52@gmail.com>
2021-04-27 11:22:29 +0200

    Delete a commented line in list.jsx

--- Santiago/challenge/tour-oh-react/src/components/list/list.jsx
+++ Santiago/challenge/tour-oh-react/src/components/list/list.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import heroes from '../../store/store';
 
 const List = () => {
-  //   heroes.map((hero) => console.log(({ value: hero.id, text: hero.name })));
   const myHereos = heroes.map((hero) => (
     <li>
       {hero.id}
