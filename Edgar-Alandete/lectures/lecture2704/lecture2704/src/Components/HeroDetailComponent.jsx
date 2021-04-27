60e8d6706ce495127e127a8c30c6dcd305044bdc
Edgar Alandete <edgalandete@gmail.com>
2021-04-27 16:19:29 +0200

    Remove console logs

--- Edgar-Alandete/lectures/lecture2704/lecture2704/src/Components/HeroDetailComponent.jsx
+++ Edgar-Alandete/lectures/lecture2704/lecture2704/src/Components/HeroDetailComponent.jsx
@@ -4,12 +4,9 @@ import heroes from '../assets/heroes';
 
 const HeroDetailComponent = () => {
   const { heroId } = useParams();
-  console.log(heroId);
   const myHero = heroes.find((hero) => hero.id === heroId);
-  console.log(myHero);
   return (
     <article>
-      <button type="button" onClick={() => { console.log('closo'); }}>Close</button>
       <h2>{myHero.superhero}</h2>
       <p>ghiobgjidfjisfvisufnsnvsinvusivfsiun</p>
     </article>
