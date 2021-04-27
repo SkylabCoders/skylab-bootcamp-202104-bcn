2937c09a11fb782eca410af4f5ad2772fda15c3a
Santiago.Martinez <infohere52@gmail.com>
2021-04-27 11:07:08 +0200

    Create folder store with store.js

--- Santiago/challenge/tour-oh-react/src/components/dashboard/dashboard.jsx
+++ Santiago/challenge/tour-oh-react/src/components/dashboard/dashboard.jsx
@@ -1,4 +1,6 @@
 /* eslint-disable react/react-in-jsx-scope */
+import React from 'react';
+
 const Dashboard = () => (
   <header>
     <h1> TOUR OF HEROES </h1>
--- Santiago/challenge/tour-oh-react/src/components/list/list.jsx
+++ Santiago/challenge/tour-oh-react/src/components/list/list.jsx
@@ -1,18 +1,8 @@
 /* eslint-disable react/react-in-jsx-scope */
-const List = () => {
-  const heroes = [
-    { id: 11, name: 'Dr Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-  ];
+import React from 'react';
+import heroes from '../../store/store';
 
+const List = () => {
   //   heroes.map((hero) => console.log(({ value: hero.id, text: hero.name })));
   const myHereos = heroes.map((hero) => (
     <li>
