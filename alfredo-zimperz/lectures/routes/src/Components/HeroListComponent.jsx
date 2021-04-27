d4902bfb64c214f236ff0cc876eca1f4e98334c3
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-04-27 12:53:48 +0200

    Create HeroDetailComponent

--- alfredo-zimperz/lectures/routes/src/Components/HeroListComponent.jsx
+++ alfredo-zimperz/lectures/routes/src/Components/HeroListComponent.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import HeroDetailComponent from './HeroDetailComponent';
 
 const HeroListComponent = ({ li1 }) => (
   <ul>
+    <li>
+      <HeroDetailComponent id={1} name="Elvis" />
+    </li>
     <li>{li1}</li>
     <li>Hola</li>
     <li>Hola</li>
@@ -12,11 +16,11 @@ const HeroListComponent = ({ li1 }) => (
 );
 
 HeroListComponent.propTypes = {
-  li1: PropTypes.string.isRequired,
+  li1: PropTypes.string,
 };
 
 HeroListComponent.defaultProps = {
-  //
+  li1: 'Hola',
 };
 
 export default HeroListComponent;
