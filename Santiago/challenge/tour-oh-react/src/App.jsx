8e8791be0af802e503a7d2d8c2b2ad025fe0570e
Santiago.Martinez <infohere52@gmail.com>
2021-04-27 10:30:53 +0200

    Change list from dashboard to list

--- Santiago/challenge/tour-oh-react/src/App.jsx
+++ Santiago/challenge/tour-oh-react/src/App.jsx
@@ -2,11 +2,13 @@
 import './App.css';
 import React from 'react';
 import Dashboard from './components/dashboard/dashboard';
+import List from './components/list/list';
 
 function App() {
   return (
     <div className="App">
       <Dashboard />
+      <List />
     </div>
   );
 }
---
+++ Santiago/challenge/tour-oh-react/src/components/list/list.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/react-in-jsx-scope */
+const List = () => {
+  const heroes = [
+    { id: 11, name: 'Dr Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+  ];
+
+  //   heroes.map((hero) => console.log(({ value: hero.id, text: hero.name })));
+  const myHereos = heroes.map((hero) => (
+    <li>
+      {hero.id}
+      {' - '}
+      {hero.name}
+    </li>
+  ));
+
+  return (
+    <div>
+      <ul>
+        {myHereos}
+      </ul>
+    </div>
+  );
+};
+export default List;
