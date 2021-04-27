1b5f05677793130c15595b14ce25883d001be8cb
Edgar Alandete <edgalandete@gmail.com>
2021-04-27 15:03:58 +0200

    Import react dom libraries

--- Edgar-Alandete/lectures/lecture2704/lecture2704/src/App.jsx
+++ Edgar-Alandete/lectures/lecture2704/lecture2704/src/App.jsx
@@ -1,4 +1,10 @@
 import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import HeroDetailComponent from './Components/HeroDetailComponent';
 import HeroListComponent from './Components/HeroListComponent';
 
 function App() {
@@ -19,6 +25,12 @@ function App() {
       {(currentId !== 0)
         ? <HeroListComponent onOpen={handleSelectHero} />
         : <p>No hero selected</p>}
+      <Router>
+        <Switch>
+          <Route extact path="/heroes/" component={HeroDetailComponent} />
+          <Route extact path="/heroes/:heroId" component={HeroDetailComponent} />
+        </Switch>
+      </Router>
     </>
   );
 }
