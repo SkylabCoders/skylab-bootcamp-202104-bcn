1d159e636a72690f9afafc782c8ad6fa10604461
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-04-27 09:41:57 +0200

    Clean files and move heroes data

--- alfredo-zimperz/challenges/react-heroes/src/App.jsx
+++ alfredo-zimperz/challenges/react-heroes/src/App.jsx
@@ -1,22 +1,10 @@
 import React, { useState } from 'react';
 import './App.css';
+import heroes from './data/heroesData';
 import HeroDetail from './components/HeroDetail';
 import HeroesList from './components/HeroesList';
 
 function App() {
-  const heroes = [
-    { id: 11, name: 'Dr Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' },
-  ];
-
   const [selectedHero, setSelectedHero] = useState(null);
 
   return (
