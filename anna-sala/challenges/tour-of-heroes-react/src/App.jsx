68ffa396e2ca00dd37025a62a674c17e17a37134
Anna Sala <anna.sala.mercade@gmail.com>
2021-04-27 09:38:35 +0200

    created separate file -store- for constants

--- anna-sala/challenges/tour-of-heroes-react/src/App.jsx
+++ anna-sala/challenges/tour-of-heroes-react/src/App.jsx
@@ -2,19 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import HeroesList from './components/HeroesList';
 import HeroesDetail from './components/HeroesDetail';
-
-const heroes = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' },
-];
+import heroes from './store/store';
 
 function App() {
   const [currentSelectedHero, selectedHero] = useState(null);
