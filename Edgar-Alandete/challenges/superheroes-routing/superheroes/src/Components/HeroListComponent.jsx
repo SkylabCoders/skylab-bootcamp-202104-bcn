20920ce2b9a9edbb75747c231111f0c063efff90
Edgar Alandete <edgalandete@gmail.com>
2021-04-27 22:01:34 +0200

    Print hero info with button

--- Edgar-Alandete/challenges/superheroes-routing/superheroes/src/Components/HeroListComponent.jsx
+++ Edgar-Alandete/challenges/superheroes-routing/superheroes/src/Components/HeroListComponent.jsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import heroes from '../assets/heroes';
 import HeroListItemComponent from './HeroListItemComponent';
 
-const HeroListComponent = () => (
-  <ul className="list__container">
-    {
-          heroes
-          && heroes.map(
-            ({ id, superhero }) => <HeroListItemComponent id={id} superhero={superhero} />,
-          )
-    }
-  </ul>
-);
+const HeroListComponent = () => {
+  const [selectedHero, selectHero] = useState(null);
+
+  function onHandleSelectedHero(hero) {
+    selectHero(hero);
+  }
+
+  return (
+    <ul className="list__container">
+      {
+        heroes
+        && heroes.map(
+          // eslint-disable-next-line camelcase
+          ({
+            // eslint-disable-next-line camelcase
+            id, superhero, publisher, alter_ego,
+          }) => (
+            <HeroListItemComponent
+              id={id}
+              superhero={superhero}
+              publisher={publisher}
+              // eslint-disable-next-line camelcase
+              alterEgo={alter_ego}
+              selectHero={onHandleSelectedHero}
+              selectedHero={selectedHero}
+            />
+          ),
+        )
+  }
+    </ul>
+  );
+};
 
 export default HeroListComponent;
--- Edgar-Alandete/challenges/superheroes-routing/superheroes/src/Components/HeroListItemComponent.jsx
+++ Edgar-Alandete/challenges/superheroes-routing/superheroes/src/Components/HeroListItemComponent.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const HeroListItemComponent = ({ id, superhero }) => (
-  <li key={id} superhero={superhero} id={id}>{superhero}</li>
+const HeroListItemComponent = ({
+  // eslint-disable-next-line camelcase
+  id, superhero, selectHero, publisher, alterEgo, selectedHero,
+}) => (
+  <li key={id} superhero={superhero} id={id}>
+    <button type="button" onClick={() => selectHero(id)}>Boton</button>
+    {superhero}
+    {selectedHero && selectedHero === id && (
+    <>
+      <p>
+        Alter Ego:
+        {' '}
+        {alterEgo}
+      </p>
+      <p>
+        Published by:
+        {' '}
+        {publisher}
+      </p>
+    </>
+    )}
+  </li>
 );
 
 HeroListItemComponent.propTypes = {
   id: PropTypes.string.isRequired,
   superhero: PropTypes.string.isRequired,
+  publisher: PropTypes.string.isRequired,
+  alterEgo: PropTypes.string.isRequired,
+  selectHero: PropTypes.func.isRequired,
+  selectedHero: PropTypes.string.isRequired,
 };
 
 export default HeroListItemComponent;
