52d8dccf6208df1d32c59afe0e45ac46336b9e91
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-04-27 16:20:15 +0200

    Intall prop-types dependency

---
+++ alfredo-zimperz/challenges/heroes-react-routes/src/Components/HeroesListComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const HeroesListComponent = (props) => (
+  <div />
+);
+
+HeroesListComponent.propTypes = {
+
+};
+
+export default HeroesListComponent;
