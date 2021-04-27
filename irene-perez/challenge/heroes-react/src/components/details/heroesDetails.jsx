6efa2ff0f7da6ea6637c4df80e15c3d2b897f3ef
IrenePerezAlonso <irperal@hotmail.com>
2021-04-27 20:12:09 +0200

    Finished the useParams but cant finished the links :(

--- irene-perez/challenge/heroes-react/src/components/details/heroesDetails.jsx
+++ irene-perez/challenge/heroes-react/src/components/details/heroesDetails.jsx
@@ -3,16 +3,16 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import heroes from '../elements/heroes';
 
-function HeroesDetails({ superhero }) {
+function HeroesDetails() {
   const { heroId } = useParams();
   const hero = heroes.find((id) => (id.toString() === heroId));
   return (
     <article>
-      <p>{`${hero.superhero} id:`${hero.id}}</p>
-      <p>{`${hero.superhero} publiseh:`${hero.published}</p>
-      <p>{`${hero.superhero} alter-ego:`${hero.alter_ego}</p>
+      <p>{`${hero.superhero} id:${hero.id}`}</p>
+      <p>{`${hero.superhero} publiseh:${hero.published}`}</p>
+      <p>{`${hero.superhero} alter-ego:${hero.alter_ego}`}</p>
     </article>
-  )
+  );
 }
 
 export default HeroesDetails;
