6acf509de705d78fefb81257a248c302cd8fa483
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-04-27 18:18:57 +0200

    Dashboard have 5 items and they work

--- david-vazquez/challenges/react-tour-of-heroes/my-app/src/components/dashboard/Dashboard.jsx
+++ david-vazquez/challenges/react-tour-of-heroes/my-app/src/components/dashboard/Dashboard.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import heroes from '../../assets/heroes';
+import HeroListItemComponent from '../list/heroListItemComponen';
 
 function Dashboard() {
   return (
     <div>
       <h2>My top Heroes</h2>
+      <ul className="list__container">{heroes.slice(1, 6).map(({ id, superhero }) => <HeroListItemComponent id={id} name={superhero} key={`${id}`} />)}</ul>
     </div>
   );
 }
