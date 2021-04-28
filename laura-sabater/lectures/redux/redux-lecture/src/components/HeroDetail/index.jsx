b8cb85a6a0d6750e343914fad5ef8924cc8a64a3
Laura <laura.sabater.musach@gmail.com>
2021-04-28 13:01:31 +0200

    Install redux and react-redux

--- laura-sabater/lectures/redux/redux-lecture/src/components/HeroDetail/index.jsx
+++ laura-sabater/lectures/redux/redux-lecture/src/components/HeroDetail/index.jsx
@@ -12,14 +12,51 @@ function HeroDetail() {
     setSelectedHero(HEROES.find((hero) => hero.id === +heroId));
   }, [heroId]);
 
+  function handleNameChange(event) {
+    setSelectedHero({
+      ...selectedHero,
+      name: event.target.value,
+    });
+  }
+
   return (
-    <div>
-      <h2>
-        {selectedHero?.name}
-        {' '}
-        details
-      </h2>
-    </div>
+    selectedHero
+      ? (
+        <div>
+          <div>
+            <h2>
+              {selectedHero.name}
+              {' '}
+              details
+            </h2>
+          </div>
+          <div>
+            <span>id: </span>
+            {selectedHero.id}
+          </div>
+          <div>
+            <label htmlFor="hero-name">
+              Hero name:
+              <input
+                id="hero-name"
+                value={selectedHero?.value}
+                onChange={handleNameChange}
+                placeholder="Hero name"
+              />
+            </label>
+          </div>
+          <button type="button">go back</button>
+          <button type="button">save</button>
+        </div>
+
+      )
+      : (
+        <h3>
+          There is no hero with id=
+          {heroId}
+        </h3>
+      )
+
   );
 }
 
