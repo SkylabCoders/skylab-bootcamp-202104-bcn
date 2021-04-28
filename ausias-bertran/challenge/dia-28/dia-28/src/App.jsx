45d7495c58ca59cd823676cce63cf1d79659fb0b
ulldecorb <ausias.bertran@gmail.com>
2021-04-28 10:59:51 +0200

    Add Links

--- ausias-bertran/challenge/dia-28/dia-28/src/App.jsx
+++ ausias-bertran/challenge/dia-28/dia-28/src/App.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import './styles.css';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Dashboard from './components/Dashboard/index';
+import HeroDetail from './components/HeroDetail';
 
 function App() {
   return (
     <>
-      <h1>Tour of Heroes</h1>
-      <nav>
-        <a href="  ">Dashbooard</a>
-        <a href="  ">Heroes</a>
-      </nav>
-      <Dashboard />
+      <BrowserRouter>
+        <h1>Tour of Heroes</h1>
+        <nav>
+          <a href="  ">Dashbooard</a>
+          <a href="  ">Heroes</a>
+        </nav>
+        <Switch>
+          <Route path="/" component={Dashboard} />
+          <Route path="/detail" component={HeroDetail} />
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
