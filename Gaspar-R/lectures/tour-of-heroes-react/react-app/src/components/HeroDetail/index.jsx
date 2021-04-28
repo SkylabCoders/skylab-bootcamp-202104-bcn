305aef6997d11d5ffcadb9188f7a2887be0a01a0
Gaspar Rodriguez <gaspar.unlp@gmail.com>
2021-04-28 13:07:08 +0200

    Details done and redux installed

--- Gaspar-R/lectures/tour-of-heroes-react/react-app/src/components/HeroDetail/index.jsx
+++ Gaspar-R/lectures/tour-of-heroes-react/react-app/src/components/HeroDetail/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {useParams} from 'react-router-dom';
 import HEROES from '../constants/heroes.mock'
 
@@ -6,21 +6,47 @@ import HEROES from '../constants/heroes.mock'
 function HeroDetail() {
 
 const {heroId} = useParams();
- const selectedHero = HEROES.find((hero) => hero.id === +heroId) ;  
+const [selectedHero,setSelectedHero]= useState() ;  
+
+ useEffect(()=>{
+     setSelectedHero (HEROES.find((hero) => hero.id === +heroId));
+ }, [heroId]);
+ 
+ function handleNameChange(event){
+     setSelectedHero({
+        ...selectedHero,
+        name :event.target.value
+       
+     });
+     
+ }
     return (
-    
+            selectedHero
+            ?
+            (
+            <div>
+            <div>
+            <div>
+            <h2>
+                    <label htmlFor='hero-name'>
+                    Hero name:
+                        <input id="hero-name" 
+                        value= {selectedHero.name}
+                        onChange={handleNameChange}
+                        placeholder="Hero name"
+                        />
+                        </h2>
+                    </label>
+            </div>
+            <button type='button'>go back</button>
+            <button type='button'>save</button>
+           
+            </div>  
+    ) :  (<h3> No hay hero con ese id </h3> )
+    </div>
 
-        <div>
-             <h2>{selectedHero?.name} 
-
-        {' '}
-        Details
-        </h2>
-        </div>
-
+    )
     
-    
-    );
 }
 
 export default HeroDetail;
