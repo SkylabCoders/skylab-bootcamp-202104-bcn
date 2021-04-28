b18e5dbcb767a901565bdf20f04ef4e113c24144
Edgar Alandete <edgalandete@gmail.com>
2021-04-28 15:49:25 +0200

    Add action types to redux configuration

--- Edgar-Alandete/lectures/toh-redux/src/redux/reducers/heroesReducer.js
+++ Edgar-Alandete/lectures/toh-redux/src/redux/reducers/heroesReducer.js
@@ -1,17 +1,18 @@
 import HEROES from '../../constants/heroes.mock';
+import actionTypes from '../actions/actionTypes';
 
 function heroesReducer(heroes = [], action) {
   switch (action.type) {
-    case 'LOAD_HEROES':
+    case actionTypes.LOAD_HEROES:
       return HEROES;
-    case 'DELETE_HERO':
+    case actionTypes.DELETE_HERO:
       return heroes.filter((hero) => hero.id !== action.action.heroId);
-    case 'ADD_HERO':
+    case actionTypes.ADD_HERO:
       return [
         ...heroes,
         action.hero,
       ];
-    case 'UPDATE_HERO':
+    case actionTypes.UPDATE_HERO:
       return heroes.map((hero) => (hero.id === action.hero.id
         ? { ...hero, ...action.hero } : hero));
     default:
