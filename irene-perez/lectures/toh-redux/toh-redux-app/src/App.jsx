4fab83cbb76f361f5990943843f32a7f2d005231
IrenePerezAlonso <irperal@hotmail.com>
2021-04-28 15:49:09 +0200

    Added redux configuration

--- irene-perez/lectures/toh-redux/toh-redux-app/src/App.jsx
+++ irene-perez/lectures/toh-redux/toh-redux-app/src/App.jsx
@@ -4,26 +4,30 @@ import './App.css';
 import {
   BrowserRouter, Route, Switch, Link
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Dashboard from './components/Dashboard';
 import HeroDetail from './components/HeroDetail';
 import NotFound from './components/NotFound';
+import store from './redux/stores';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <h1>Tour of heroes</h1>
-        <nav>
-          <Link to="/">Dashboard</Link>
-          <Link to="/detail">Details</Link>
-          <Link to="/404">404</Link>
-        </nav>
-        <Switch>
-          <Route path="/" exact component={Dashboard} />
-          <Route path="/detail/:heroId" component={HeroDetail} />
-          <Route component={NotFound} />
-        </Switch>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <h1>Tour of heroes</h1>
+          <nav>
+            <Link to="/">Dashboard</Link>
+            <Link to="/detail">Details</Link>
+            <Link to="/404">404</Link>
+          </nav>
+          <Switch>
+            <Route path="/" exact component={Dashboard} />
+            <Route path="/detail/:heroId" component={HeroDetail} />
+            <Route component={NotFound} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
--- irene-perez/lectures/toh-redux/toh-redux-app/src/components/HeroDetail/index.jsx
+++ irene-perez/lectures/toh-redux/toh-redux-app/src/components/HeroDetail/index.jsx
@@ -1,20 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import HEROES from '../../constans/heroes';
 import './HeroDetail.css';
 
 function HeroDetail() {
   const { heroId } = useParams();
-  const selectedHero = HEROES.find((hero) => hero.id === +heroId);
+  const [selectedHero, setSelectHero] = useState();
+  useEffect(() => {
+    setSelectHero(HEROES.find((hero) => hero.id === +heroId));
+  }, [heroId]);
+
+  function handleNameChange(event) {
+    setSelectHero(
+      {
+        ...selectedHero,
+        name: event.target.value
+      }
+    );
+  }
+
   return (
-    <div>
-      <h2>
-        {selectedHero?.name}
-        {' '}
-        Details
-      </h2>
-    </div>
+    selectedHero
+      ? (
+        <div>
+          <h2>
+            {selectedHero.name}
+            {' '}
+            details
+          </h2>
+          <div>
+            <span>id: </span>
+            {selectedHero.id}
+          </div>
+          <div>
+            <label htmlFor="hero-name">
+              Hero name:
+              <input
+                id="hero-name"
+                value={selectedHero.name}
+                onChange={handleNameChange}
+                placeholder="Hero name"
+              />
+            </label>
+          </div>
+          <div>
+            <button type="button">go back</button>
+            <button type="button">save</button>
+          </div>
+        </div>
+      )
+      : (
+        <div>
+          <h3>
+            No hay heroe con la id=
+            {heroId}
+          </h3>
+        </div>
+      )
   );
 }
-
 export default HeroDetail;
