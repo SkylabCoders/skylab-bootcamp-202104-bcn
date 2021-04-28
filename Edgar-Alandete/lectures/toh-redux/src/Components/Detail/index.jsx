0416a6234801e753c4f7427dda6f559530ba8e46
Edgar Alandete <edgalandete@gmail.com>
2021-04-28 12:52:52 +0200

    Handle onChange name event

--- Edgar-Alandete/lectures/toh-redux/src/Components/Detail/index.jsx
+++ Edgar-Alandete/lectures/toh-redux/src/Components/Detail/index.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import heroes from '../../constants/heroes.mock';
 import './HeroDetail.css';
 
 function Detail() {
   const { heroId } = useParams();
-  const selectedHero = heroes.find((hero) => hero.id === +heroId);
+  const [selectedHero, setSelectedHero] = useState();
+
+  useEffect(() => {
+    setSelectedHero(heroes.find((hero) => hero.id === +heroId));
+  }, [heroId]);
+
+  function handleNameChange(event) {
+    setSelectedHero({
+      ...selectedHero,
+      name: event.target.value,
+    });
+  }
   return (
     <>
       <div>
@@ -14,6 +25,16 @@ function Detail() {
           {' '}
           Details
         </h2>
+        <div>
+          <span>id: </span>
+          {selectedHero?.id}
+        </div>
+        <div>
+          <label htmlFor="hero-name">
+            Hero Name:
+            <input id="hero-name" onChange={handleNameChange} value={selectedHero.name} placeholder="Hero Name" />
+          </label>
+        </div>
       </div>
     </>
   );
