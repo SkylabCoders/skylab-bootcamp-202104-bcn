4e0dff9928d2990134411cbd514f2e2fb61abae3
Santiago.Martinez <infohere52@gmail.com>
2021-04-28 10:34:02 +0200

    Create dashboard with four heroes

--- Santiago/lectures/tour-oh-redux/src/App.jsx
+++ Santiago/lectures/tour-oh-redux/src/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import './App.css';
 import './index.css';
+import './App.css';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <>
       <h1>Tour of heroes redux</h1>
-
       <nav type="button">
         <a href=" ">Dashboard</a>
         <a href=" ">Heroes</a>
       </nav>
+      <Dashboard />
     </>
   );
 }
