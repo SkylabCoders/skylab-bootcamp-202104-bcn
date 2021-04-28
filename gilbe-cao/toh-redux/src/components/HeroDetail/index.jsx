201d9d1f4bf0c15e12556db7ee3ccf60cf59e241
Gilberto Cao <gilbe.cao@gmail.com>
2021-04-28 12:52:24 +0200

    Handle name change

--- gilbe-cao/toh-redux/src/components/HeroDetail/index.jsx
+++ gilbe-cao/toh-redux/src/components/HeroDetail/index.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-debugger */
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import HEROES from '../../constants/heroes.mock';
@@ -10,6 +12,13 @@ function HeroDetail() {
   useEffect(() => {
     setSelectedHero(HEROES.find((hero) => hero.id === +heroId));
   }, [heroId]);
+
+  function handleNameChange(event) {
+    setSelectedHero({
+      ...selectedHero,
+      name: event.target.value,
+    });
+  }
 
   return (
     selectedHero
@@ -27,7 +36,12 @@ function HeroDetail() {
           <div>
             <label htmlFor="hero-name">
               Hero name:
-              <input id="hero-name" value={selectedHero.name} placeholder="Hero name" />
+              <input
+                id="hero-name"
+                value={selectedHero.name}
+                onChange={handleNameChange}
+                placeholder="Hero name"
+              />
             </label>
           </div>
           <button type="button">go back</button>
