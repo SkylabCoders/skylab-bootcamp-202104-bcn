dae30beeeb14b61ed5a842782fe76b1ac06f7c7d
Hosni <hosni.marco@gmail.com>
2021-04-28 12:54:48 +0200

    Input HeroDetail

--- hosni-marco/lecture/tour-of-heroes-redux/my-toh-redux/src/App.jsx
+++ hosni-marco/lecture/tour-of-heroes-redux/my-toh-redux/src/App.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import './styles.css';
 import './App.css';
-import Dashboard from './components/Dashboard/index';
+import {
+  BrowserRouter, Switch, Route, Link
+} from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import HeroDetail from './components/HeroDetail';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <>
-      <h1>Tour of Heroes</h1>
-      <nav>
-        <a href="Dashboard">Dashboard</a>
-        <a href="Heroes">Heroes</a>
-      </nav>
-      <Dashboard />
+      <BrowserRouter>
+        <nav>
+          <h1>Tour of Heroes</h1>
+          <Link to="/">Dashboard</Link>
+          <Link to="/Heroes">Heroes</Link>
+        </nav>
+        <Switch>
+          <Route path="/" exact component={Dashboard} />
+          <Route path="/Detail/:heroId" exact component={HeroDetail} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
---
+++ hosni-marco/lecture/tour-of-heroes-redux/my-toh-redux/src/components/HeroDetail/index.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import './HeroDetail.css';
+import HEROES from '../../constants/heroes.mock';
+
+function HeroDetail() {
+  const { heroId } = useParams();
+  const [selectedHero, setSelectedHero] = useState();
+  useEffect(() => {
+    setSelectedHero(HEROES.find((hero) => hero.id === +heroId));
+  }, [heroId]);
+
+  function handleNameChange(event) {
+    setSelectedHero({
+      ...selectedHero,
+      name: event.target.value
+    });
+  }
+  return (
+    <>
+      <div>
+        <h2>
+          {selectedHero?.name}
+          {' '}
+          Details
+        </h2>
+        <div>
+          <span>id: </span>
+          {selectedHero?.id}
+        </div>
+        <div>
+          <label htmlFor="hero-name">
+            Hero name:
+            <input
+              id="hero-name"
+              value={selectedHero?.name}
+              onChange={handleNameChange}
+              placeholder="Hero name"
+            />
+          </label>
+        </div>
+        <button type="button">go back</button>
+        <button type="button">save</button>
+      </div>
+    </>
+  );
+}
+export default HeroDetail;
