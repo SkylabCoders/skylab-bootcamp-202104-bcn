f7c7968f79db21b8468d0a9cf3319322dabf8d0f
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-04-28 13:00:34 +0200

    install react & react-redux dependencies

--- alfredo-zimperz/lectures/tour-of-heroes-redux/src/components/HeroDetail/index.jsx
+++ alfredo-zimperz/lectures/tour-of-heroes-redux/src/components/HeroDetail/index.jsx
@@ -1,32 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import HEROES from '../../constants/heroes.mock';
 import './heroDetail.css';
 
 const HeroDetail = () => {
   const { heroId } = useParams();
-  const { id, name } = HEROES.find((hero) => hero.id === +heroId);
-  return (
-    <section className="hero">
-      <h2>
-        {name.toUpperCase()}
-        {' '}
-        Details
-      </h2>
-      <div>
-        <span>id: </span>
-        {id}
-      </div>
-      <div>
-        <label htmlFor="hero-name">
-          Hero name:
-          <input htmlFor="hero-name" id="hero-name" placeholder="Hero name" />
-        </label>
+  const [selectedHero, setSelectedHero] = useState();
 
-      </div>
-      <button type="button">go back</button>
-      <button type="button">save</button>
-    </section>
+  useEffect(() => {
+    setSelectedHero(HEROES.find((hero) => hero.id === +heroId));
+  }, [heroId]);
+
+  const handleNameChange = (event) => {
+    console.log(event);
+    setSelectedHero(
+      { ...selectedHero, name: event.target.name },
+    );
+  };
+
+  return (
+    selectedHero ? (
+      <section className="hero">
+        <h2>
+          {selectedHero?.name.toUpperCase()}
+          {' '}
+          Details
+        </h2>
+        <div>
+          <span>id: </span>
+          {selectedHero?.id}
+        </div>
+        <div>
+          <label htmlFor="hero-name">
+            Hero name:
+            <input
+              id="hero-name"
+              placeholder="Hero name"
+              value={selectedHero?.name}
+              onChange={handleNameChange}
+            />
+          </label>
+
+        </div>
+        <button type="button">go back</button>
+        <button type="button">save</button>
+      </section>
+    ) : (
+      <h2>
+        There is no hero with id
+        {' '}
+        {heroId}
+      </h2>
+    )
 
   );
 };
