2546170fce3081e520f7217a79925f32a71d6735
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-04-28 10:58:36 +0200

    Add react-router

--- david-vazquez/lectures/react-redux/my-app/src/App.jsx
+++ david-vazquez/lectures/react-redux/my-app/src/App.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import './App.css';
 import './styles.css';
+import {
+  BrowserRouter, Route, Switch, Link,
+} from 'react-router-dom';
 import Dashboard from './components/Dashboard';
+import HeroDetail from './components/HeroDetail';
 
 function App() {
   return (
     <>
-      <h1>Tour of Heroes</h1>
-      <nav>
-        <a href=" ">Dashboard</a>
-        <a href=" ">Heroes</a>
-      </nav>
-      <Dashboard />
+      <BrowserRouter>
+        <h1>Tour of Heroes</h1>
+        <nav>
+          <Link to="/">Dashboard</Link>
+          <Link to="/detail">Details</Link>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route exact path="/detail" component={HeroDetail} />
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
---
+++ david-vazquez/lectures/react-redux/my-app/src/components/HeroDetail/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HeroDetail() {
+  return (
+    <>
+      <h2>Hero Detail</h2>
+    </>
+  );
+}
+
+export default HeroDetail;
