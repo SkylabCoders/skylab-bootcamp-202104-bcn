e53cf5f0b6ad2220615b9f4b7c77baf48400ea26
cristian-bars <patxi.cristian@gmail.com>
2021-04-28 10:47:20 +0200

    Creating routes

--- cristian-bars/lectures/react-redux/toh-redux/src/App.jsx
+++ cristian-bars/lectures/react-redux/toh-redux/src/App.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import './styles.css';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Dashboard from './componentes/Dashboard';
 
 function App() {
   return (
     <>
-      <h1>Tour of Heroes</h1>
-      <nav>
-        <a href=" ">Dashboard</a>
-        <a href=" ">Heroes</a>
-      </nav>
-      <Dashboard />
+      <BrowserRouter>
+        <h1>Tour of Heroes</h1>
+        <nav>
+          <a href=" ">Dashboard</a>
+          <a href=" ">Heroes</a>
+        </nav>
+        <Route path="/" component={Dashboard} />
+      </BrowserRouter>
     </>
   );
 }
