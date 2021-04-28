89f13ed5721e6237acd0567ec3740067fb21ea7d
cristian-bars <patxi.cristian@gmail.com>
2021-04-28 10:14:44 +0200

    Creating the first hero

---
+++ cristian-bars/lectures/react-redux/toh-redux/src/componentes/Dashboard/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Dashboard.css';
+
+function Dashboard() {
+  return (
+    <>
+      <h2>Top Heroes</h2>
+
+      <div className="heroes-menu">
+        <a href=" ">Heroe</a>
+      </div>
+    </>
+  );
+}
+export default Dashboard;
