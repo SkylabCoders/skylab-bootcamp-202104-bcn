b48ddc348ef3ede3c25289f12d6a051678ae6e54
Anna Sala <anna.sala.mercade@gmail.com>
2021-04-28 12:53:02 +0200

    handeled input updating on change

--- anna-sala/lectures/react-redux/src/components/HeroDetail/index.jsx
+++ anna-sala/lectures/react-redux/src/components/HeroDetail/index.jsx
@@ -1,20 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import './herodetail.css';
 import heroes from '../../assets/heroes';
 
 function HeroDetail() {
   const { heroId } = useParams();
-  const myHero = heroes.find((hero) => hero.id === +heroId);
+  const [selectedHero, setSelectedHero] = useState();
+  useEffect(() => {
+    setSelectedHero(heroes.find((hero) => hero.id === +heroId));
+  }, [heroId]);
+  function handleNameChange() {
+    setSelectedHero({
+      ...selectedHero,
+      // eslint-disable-next-line no-restricted-globals
+      name: event.target.value,
+    });
+  }
 
   return (
-    <div>
-      <h4>
-        This is my hero
-        {' '}
-        {myHero?.name}
-      </h4>
-    </div>
+    selectedHero ? (
+      <div>
+        <h3>
+          Hero Name:
+          {' '}
+          {selectedHero.name}
+        </h3>
+        <h4>
+          Hero ID:
+          {' '}
+          {selectedHero.id}
+        </h4>
+        <label htmlFor="hero-name">
+          Hero Name:
+          <input
+            id="hero-name"
+            value={selectedHero.name}
+            onChange={handleNameChange}
+          />
+        </label>
+        <button type="button">Forward</button>
+        <button type="button">Back</button>
+      </div>
+    ) : <h3>Hero not Found</h3>
   );
 }
 
