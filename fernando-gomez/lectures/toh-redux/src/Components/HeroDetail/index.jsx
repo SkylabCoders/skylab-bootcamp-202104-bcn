8d1b4424970adda7b75568c628a61c16410a3515
Fernando Gomez <shadyfast8.services@gmail.com>
2021-04-28 12:52:35 +0200

    handleNameChange function implemented at hero details component

--- fernando-gomez/lectures/toh-redux/src/Components/HeroDetail/index.jsx
+++ fernando-gomez/lectures/toh-redux/src/Components/HeroDetail/index.jsx
@@ -1,18 +1,56 @@
+import { useState, useEffect } from 'react';
 import React, { useParams } from 'react-router-dom';
 import HEROES from '../../heroes.mock';
+import './heroDetail.css';
 
 const HeroDetail = () => {
   const { heroId } = useParams();
-  console.log(heroId);
-  const selectedHero = HEROES.find((hero) => hero.id === +heroId);
+  const [selectedHero, setSelectedHero] = useState();
+
+  useEffect(() => {
+    setSelectedHero(HEROES.find((hero) => hero.id === +heroId));
+  }, [heroId]);
+
+  const handleNameChange = (event) => {
+    setSelectedHero({
+      ...selectedHero,
+      name: event.target.value,
+    });
+  };
+
   return (
-    <>
-      <h2>
-        {selectedHero.name}
-        {' '}
-        details
-      </h2>
-    </>
+    selectedHero
+      ? (
+        <>
+          <h2>
+            { selectedHero?.name }
+            {' '}
+            Details
+          </h2>
+          <div>
+            <span>id: </span>
+            { selectedHero?.id }
+          </div>
+          <label htmlFor="hero-name">
+            Hero name:
+            <input
+              id="hero-name"
+              placeholder="Hero name"
+              value={selectedHero.name}
+              onChange={handleNameChange}
+            />
+          </label>
+          <button type="button">go back</button>
+          <button type="button">save</button>
+        </>
+      )
+      : (
+        <h3>
+          There is no hero with id
+          {' '}
+          {heroId}
+        </h3>
+      )
   );
 };
 export default HeroDetail;
--- fernando-gomez/lectures/toh-redux/src/Components/HeroSearch/index.jsx
+++ fernando-gomez/lectures/toh-redux/src/Components/HeroSearch/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './heroSearch.css';
 
 const HeroSearch = () => (
   <>
