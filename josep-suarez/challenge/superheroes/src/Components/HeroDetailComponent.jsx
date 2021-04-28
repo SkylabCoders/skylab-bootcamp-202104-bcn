7fee7039b0a11839b03522fa79008f257cd63549
Josep Suarez <djose.anton@gmail.com>
2021-04-28 09:00:32 +0200

    end of project

---
+++ josep-suarez/challenge/superheroes/src/Components/HeroDetailComponent.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {useParams} from "react-router"
+import {Link} from "react-router-dom"
+import heroes from "./superherosData";
+import '../heroDetails.css'
+
+function HeroDetail (){
+    const {heroId} = useParams();
+    const selectedhero = heroes.find((hero) => hero.id === heroId)
+    return (
+        <ul class="listDetails">
+            <li>{selectedhero.superhero}</li>
+            <li>{selectedhero.id}</li>
+            <li>{selectedhero.publisher}</li>
+            <li>{selectedhero.alter_ego}</li>
+            <li>{selectedhero.first_appearance}</li>
+            <li>{selectedhero.characters}</li>
+            <Link to={`/`}>Atràs</Link>
+        </ul>
+    
+    )   
+}
+
+
+export default HeroDetail
