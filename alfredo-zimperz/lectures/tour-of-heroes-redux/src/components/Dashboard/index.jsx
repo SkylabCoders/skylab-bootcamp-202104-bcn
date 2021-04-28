2a206fdf05bb0a850849dc23173d5fed30568b4c
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-04-28 11:51:46 +0200

    Move HEROES to constant

--- alfredo-zimperz/lectures/tour-of-heroes-redux/src/components/Dashboard/index.jsx
+++ alfredo-zimperz/lectures/tour-of-heroes-redux/src/components/Dashboard/index.jsx
@@ -1,34 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import HEROES from '../../constants/heroes.mock';
 import './dashboard.css';
 
-const index = () => {
-  const HEROES = [
-    { id: 11, name: 'Dr Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' },
-  ];
+const index = () => (
+  <>
+    <h2>Top Heroes</h2>
+    <div className="heroes-menu">
+      {HEROES.slice(1, 5).map((hero) => (
+        <Link to={`/heroes/${hero.id}`} key={hero.id}>
+          {hero.name}
+        </Link>
+      ))}
 
-  return (
-    <>
-      <h2>Top Heroes</h2>
-      <div className="heroes-menu">
-        {HEROES.slice(1, 5).map((hero) => (
-          <Link to={`/heroes/${hero.id}`} key={hero.id}>
-            {hero.name}
-          </Link>
-        ))}
-
-      </div>
-    </>
-  );
-};
+    </div>
+  </>
+);
 
 export default index;
--- alfredo-zimperz/lectures/tour-of-heroes-redux/src/components/Heroes/index.jsx
+++ alfredo-zimperz/lectures/tour-of-heroes-redux/src/components/Heroes/index.jsx
@@ -1,56 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import HEROES from '../../constants/heroes.mock';
 import './heroes.css';
 
-const Heroes = () => {
-  const HEROES = [
-    { id: 11, name: 'Dr Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' },
-  ];
+const Heroes = () => (
+  <>
+    <h2>My Heroes</h2>
 
-  return (
-    <>
-      <h2>My Heroes</h2>
+    <div>
+      <label htmlFor="new-hero" id="new-hero">
+        Hero name:
+        <input htmlFor="new-hero" />
+      </label>
+      <button className="add-button" type="button">
+        Add hero
+      </button>
+    </div>
 
-      <div>
-        <label htmlFor="new-hero" id="new-hero">
-          Hero name:
-          <input htmlFor="new-hero" />
-        </label>
-        <button className="add-button" type="button">
-          Add hero
-        </button>
-      </div>
+    <ul className="heroes">
+      {HEROES.map((hero) => (
+        <li key={hero.id}>
+          <Link to={`/heroes/${hero.id}`}>
+            <span className="badge">{hero.id}</span>
+            {' '}
+            {hero.name}
+          </Link>
+          <button
+            className="delete"
+            title="delete hero"
+            type="button"
+          >
+            x
+          </button>
+        </li>
+      ))}
 
-      <ul className="heroes">
-        {HEROES.map((hero) => (
-          <li key={hero.id}>
-            <Link to={`/heroes/${hero.id}`}>
-              <span className="badge">{hero.id}</span>
-              {' '}
-              {hero.name}
-            </Link>
-            <button
-              className="delete"
-              title="delete hero"
-              type="button"
-            >
-              x
-            </button>
-          </li>
-        ))}
-
-      </ul>
-    </>
-  );
-};
+    </ul>
+  </>
+);
 
 export default Heroes;
