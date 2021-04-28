a56724e649f79cf28fe3e8d36e16bd4e256b6758
cristian-bars <patxi.cristian@gmail.com>
2021-04-28 11:51:55 +0200

    Move heroes to constants

--- cristian-bars/lectures/react-redux/toh-redux/src/componentes/Dashboard/index.jsx
+++ cristian-bars/lectures/react-redux/toh-redux/src/componentes/Dashboard/index.jsx
@@ -1,27 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Dashboard.css';
+import HEROES from '../Constants/Heroes.mock';
 
 function Dashboard() {
-  const HEROES = [
-    { id: 11, name: 'Dr Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-  ];
-
   return (
     <>
       <h2>Top Heroes</h2>
 
       <div className="heroes-menu">
-        {HEROES.slice(1, 5).map((heroe) => <Link to="/detail">{heroe.name}</Link>)}
+        {HEROES.slice(1, 5).map((heroe) => <Link to={`/detail/${heroe.id}`}>{heroe.name}</Link>)}
       </div>
     </>
   );
