4d9c92cb3dee424270a200807e43c166f0ac2040
Santiago.Martinez <infohere52@gmail.com>
2021-04-28 14:01:18 +0200

    Configure the structure of redux

--- Santiago/lectures/tour-oh-redux/src/App.jsx
+++ Santiago/lectures/tour-oh-redux/src/App.jsx
@@ -4,28 +4,32 @@ import './App.css';
 import {
   Switch, Route, BrowserRouter, Link
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Dashboard from './components/Dashboard';
 import HeroDetails from './components/HeroDetails';
 import NotFound from './components/NotFound';
 import HeroList from './components/HeroList';
+import store from './redux/stores/index';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <h1>Tour of heroes</h1>
-        <nav type="button">
-          <Link to="/">Dashboard</Link>
-          <Link to="/list">Hero list</Link>
-        </nav>
+      <Provider store={store}>
+        <BrowserRouter>
+          <h1>Tour of heroes</h1>
+          <nav type="button">
+            <Link to="/">Dashboard</Link>
+            <Link to="/list">Hero list</Link>
+          </nav>
 
-        <Switch>
-          <Route exact path="/" component={Dashboard} />
-          <Route path="/detail/:heroId" component={HeroDetails} />
-          <Route path="/list" component={HeroList} />
-          <Route component={NotFound} />
-        </Switch>
-      </BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route path="/detail/:heroId" component={HeroDetails} />
+            <Route path="/list" component={HeroList} />
+            <Route component={NotFound} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
