e27312a17f43c3c42ddbee431fcda6c212a90863
cristian-bars <patxi.cristian@gmail.com>
2021-04-28 12:12:33 +0200

    Editing details page with superhero id

--- cristian-bars/lectures/react-redux/toh-redux/src/componentes/HeroDetail/Index.jsx
+++ cristian-bars/lectures/react-redux/toh-redux/src/componentes/HeroDetail/Index.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import './HeroDetail.css';
+import { useParams } from 'react-router';
+import HEROES from '../Constants/Heroes.mock';
 
 function HeroDetail() {
-  return <h1>Hero Detail</h1>;
+  const { heroId } = useParams();
+  const selectedHero = HEROES.find((hero) => hero.id === +heroId);
+  return (
+    <div>
+      <h2>
+        {selectedHero.name}
+        {' '}
+        Details
+      </h2>
+    </div>
+  );
 }
 
 export default HeroDetail;
