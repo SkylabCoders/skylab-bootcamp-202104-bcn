9bc6b411ab12d9b591f833222c087c8a1a1a0936
Gilberto Cao <gilbe.cao@gmail.com>
2021-04-28 12:42:49 +0200

    Validate template for no hero details

--- gilbe-cao/toh-redux/src/components/HeroDetail/index.jsx
+++ gilbe-cao/toh-redux/src/components/HeroDetail/index.jsx
@@ -1,20 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import HEROES from '../../constants/heroes.mock';
 import './HeroDetail.css';
 
 function HeroDetail() {
   const { heroId } = useParams();
-  const selectedHero = HEROES.find((hero) => hero.id === +heroId);
+  const [selectedHero, setSelectedHero] = useState();
+
+  useEffect(() => {
+    setSelectedHero(HEROES.find((hero) => hero.id === +heroId));
+  }, [heroId]);
 
   return (
-    <div>
-      <h2>
-        {selectedHero?.name}
-        {' '}
-        Details
-      </h2>
-    </div>
+    selectedHero
+      ? (
+        <div>
+          <h2>
+            {selectedHero.name}
+            {' '}
+            Details
+          </h2>
+          <div>
+            <span>id: </span>
+            {selectedHero.id}
+          </div>
+          <div>
+            <label htmlFor="hero-name">
+              Hero name:
+              <input id="hero-name" value={selectedHero.name} placeholder="Hero name" />
+            </label>
+          </div>
+          <button type="button">go back</button>
+          <button type="button">save</button>
+        </div>
+      )
+      : (
+        <h3>
+          There is no hero with id=
+          {heroId}
+        </h3>
+      )
+
   );
 }
 
