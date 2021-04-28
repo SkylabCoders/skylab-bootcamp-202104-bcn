2470590cf42f2ba13033b6741e8aff1ccec8da89
Edgar Alandete <edgalandete@gmail.com>
2021-04-28 12:09:49 +0200

    Print Hero name to details

--- Edgar-Alandete/lectures/toh-redux/src/Components/Detail/index.jsx
+++ Edgar-Alandete/lectures/toh-redux/src/Components/Detail/index.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import heroes from '../../constants/heroes.mock';
+import './HeroDetail.css';
 
 function Detail() {
+  const { heroId } = useParams();
+  const selectedHero = heroes.find((hero) => hero.id === +heroId);
   return (
     <>
-      <h1>Hero Detail</h1>
+      <div>
+        <h2>
+          {selectedHero?.name}
+          {' '}
+          Details
+        </h2>
+      </div>
     </>
   );
 }
