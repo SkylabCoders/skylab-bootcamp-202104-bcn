d7614ff42f069a827d9f19be7d25f1d3ed74f214
cristian-bars <patxi.cristian@gmail.com>
2021-04-28 12:53:36 +0200

    Editing on changing hero name

--- cristian-bars/lectures/react-redux/toh-redux/src/componentes/HeroDetail/Index.jsx
+++ cristian-bars/lectures/react-redux/toh-redux/src/componentes/HeroDetail/Index.jsx
@@ -10,14 +10,49 @@ function HeroDetail() {
   useEffect(() => {
     setSelectedHero(HEROES.find((hero) => hero.id === +heroId));
   }, [heroId]);
+
+  function handleNameChange(event) {
+    setSelectedHero({
+      ...selectedHero,
+      name: event.target.value
+    });
+  }
   return (
-    <div>
-      <h2>
-        {selectedHero?.name}
-        {' '}
-        Details
-      </h2>
-    </div>
+    selectedHero
+      ? (
+        <div>
+          <h2>
+            {selectedHero?.name}
+            {' '}
+            Details
+          </h2>
+          <div>
+            <span>id: </span>
+            {selectedHero.id}
+          </div>
+          <div>
+            <label htmlFor="hero-name">
+              Hero name:
+              {' '}
+              <input
+                id="hero-name"
+                value={selectedHero.name}
+                onChange={handleNameChange}
+                placeholder="Hero name"
+              />
+            </label>
+          </div>
+          <button type="button">go back</button>
+          <button type="button">save</button>
+        </div>
+      )
+      : (
+        <h3>
+          There is no hero with id=
+          {' '}
+          {heroId}
+        </h3>
+      )
   );
 }
 
