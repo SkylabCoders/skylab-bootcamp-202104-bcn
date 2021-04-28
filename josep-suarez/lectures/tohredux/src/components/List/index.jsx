20ba7fe67cb70d9c7edb0f2fc9017bcb639c02eb
Josep Suarez <djose.anton@gmail.com>
2021-04-28 19:52:35 +0200

    consigo borrar de la lista

--- josep-suarez/lectures/tohredux/src/components/List/index.jsx
+++ josep-suarez/lectures/tohredux/src/components/List/index.jsx
@@ -1,12 +1,43 @@
+/* eslint-disable no-console */
+/* eslint-disable no-debugger */
 import React from 'react';
 import { Link } from 'react-router-dom';
-import HEROES from '../../services/heroes';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { deleteHero } from '../../redux/actions/actionCreators';
 
-function ListHeroes() {
+function ListHeroes({ dispatch, heroes }) {
+  // const [newHeroes, setHeroesNow] = useState(heroes);
+
+  // useEffect(() => {
+  //   setHeroesNow();
+  // }, [newHeroes]);
+
+  function deleteSelect(heroId) {
+    console.log(heroId);
+    dispatch(deleteHero(heroId));
+  }
   return (
     <>
-      {HEROES.map((hero) => (<Link to={`/detail/${hero.id}`}>{`${hero.id}-${hero.name}`}</Link>))}
+      <button type="button">Add</button>
+      {heroes.map((hero) => (
+        <li>
+          <Link to={`/detail/${hero.id}`}>{`${hero.id}-${hero.name}`}</Link>
+          <button type="button" onClick={() => deleteSelect(hero.id)}>Delete</button>
+          <button type="button">Modify</button>
+        </li>
+      ))}
     </>
   );
 }
-export default ListHeroes;
+ListHeroes.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  heroes: PropTypes.shape([]).isRequired
+};
+function mapStateToProps(store) {
+  return {
+    heroes: store.heroes
+  };
+}
+
+export default connect(mapStateToProps)(ListHeroes);
