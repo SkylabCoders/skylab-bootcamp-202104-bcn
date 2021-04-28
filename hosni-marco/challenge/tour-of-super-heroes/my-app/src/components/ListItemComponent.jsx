11c10d78d6752803d8eb2f655d400201958e8f4a
Hosni <hosni.marco@gmail.com>
2021-04-28 09:17:18 +0200

    somthing wrong

--- hosni-marco/challenge/tour-of-super-heroes/my-app/src/components/ListItemComponent.jsx
+++ hosni-marco/challenge/tour-of-super-heroes/my-app/src/components/ListItemComponent.jsx
@@ -7,50 +7,35 @@ import {
   Route,
   Redirect
 } from 'react-router-dom';
-
 import InfoHeroComponent from './InfoHeroComponent';
 // import Heroes from '../assets/Heroes';
-
 function ListItemComponent({
   superhero, id
 }) {
   // const { heroId } = useParams();
   // const searchHero = Heroes.find((hero) => hero.id === heroId);
-
   return (
     <div>
       <li className={`list__hero-item-${id}`}>
-
         <Link className="list__hero-item--link" to={`/heroes/${id}`}>View more</Link>
-
         {superhero}
       </li>
-      {
-
-        <Switch>
-          {/* <Route extact path="/heroes/:heroId" component={DetailHeroComponent} /> */}
-          <Route extact path="/heroes/:heroId" component={InfoHeroComponent} />
-          <Redirect to="/heroes" />
-        </Switch>
-
-      }
-
+      <Switch>
+        <Route extact path="/heroes/:heroId" component={InfoHeroComponent} />
+        <Redirect to="/heroes" />
+      </Switch>
       {/* {heroId !== undefined ? (
         <InfoHeroComponent name={superhero} alterEgo={alterEgo} publisher={publisher} />
       ) : (
         null
       )} */}
     </div>
-
   );
 }
 
 ListItemComponent.propTypes = {
   id: PropTypes.string.isRequired,
   superhero: PropTypes.string.isRequired
-  // alterEgo: PropTypes.string.isRequired,
-  // publisher: PropTypes.string.isRequired
-
 };
 
 export default ListItemComponent;
