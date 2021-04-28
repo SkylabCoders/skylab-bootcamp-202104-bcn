d59399ca3e8d7ea9cd3e322b3fdf2e8802a95d5f
cristian-bars <patxi.cristian@gmail.com>
2021-04-28 10:22:48 +0200

    Print five heroes

--- cristian-bars/lectures/react-redux/toh-redux/src/componentes/Dashboard/index.jsx
+++ cristian-bars/lectures/react-redux/toh-redux/src/componentes/Dashboard/index.jsx
@@ -2,12 +2,25 @@ import React from 'react';
 import './Dashboard.css';
 
 function Dashboard() {
+  const HEROES = [
+    { id: 11, name: 'Dr Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+  ];
+
   return (
     <>
       <h2>Top Heroes</h2>
 
       <div className="heroes-menu">
-        <a href=" ">Heroe</a>
+        {HEROES.slice(1, 5).map((heroe) => <a href=" ">{heroe.name}</a>)}
       </div>
     </>
   );
