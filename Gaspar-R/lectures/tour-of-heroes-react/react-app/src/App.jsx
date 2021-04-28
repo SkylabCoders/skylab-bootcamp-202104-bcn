78ad76766bd717cabb4b72916a8fb6b326e07f63
Gaspar Rodriguez <gaspar.unlp@gmail.com>
2021-04-28 11:00:09 +0200

    Replacement anchor for routes

--- Gaspar-R/lectures/tour-of-heroes-react/react-app/src/App.jsx
+++ Gaspar-R/lectures/tour-of-heroes-react/react-app/src/App.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import './style.css'
 import './App.css';
+import {BrowserRouter, Route, Switch} form 'react-router-dom';
 import Dashboard from './components/Dashborad';
 // import <index className="css"></index>
 
 function App() {
   return (
     <> 
+    <BrowserRouter>  
     <h1> Tour of Heroes</h1>
     <nav>
       <a href="">Dashboard</a>
       <a href="">Heroes </a>
     </nav>
-    <Dashboard></Dashboard>
+    <Switch>  
+    <Route path="/components/Dashboard" component={Dashboard}></Route>
+    <Route path="/components/Dashboard" component={HeroDetail}></Route>
+    </Switch>
+    </BrowserRouter>
     </>
   );
 }
