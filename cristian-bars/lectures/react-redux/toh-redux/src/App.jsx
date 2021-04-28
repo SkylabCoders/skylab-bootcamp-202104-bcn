d0fd38590762db1e536bc90bfe9b715c096b3707
cristian-bars <patxi.cristian@gmail.com>
2021-04-28 10:14:44 +0200

    Creating the first hero

--- cristian-bars/lectures/react-redux/toh-redux/src/App.jsx
+++ cristian-bars/lectures/react-redux/toh-redux/src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import './App.css';
 import './styles.css';
+import './App.css';
+import Dashboard from './componentes/Dashboard';
 
 function App() {
   return (
     <>
-      <h1>hola mundo</h1>
+      <h1>Tour of Heroes</h1>
       <nav>
-        <a href="fds">Dashboard</a>
-        <a href="dfsa">Heroes</a>
+        <a href=" ">Dashboard</a>
+        <a href=" ">Heroes</a>
       </nav>
+      <Dashboard />
     </>
   );
 }
