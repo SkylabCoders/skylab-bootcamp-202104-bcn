f7d1bfa9ee7c56884e1beecae2179d4ef147a83d
IrenePerezAlonso <irperal@hotmail.com>
2021-04-28 11:52:06 +0200

    Create a constants folder with heroes.js document

--- irene-perez/lectures/toh-redux/toh-redux-app/src/App.jsx
+++ irene-perez/lectures/toh-redux/toh-redux-app/src/App.jsx
@@ -6,6 +6,7 @@ import {
 } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import HeroDetail from './components/HeroDetail';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
@@ -15,10 +16,12 @@ function App() {
         <nav>
           <Link to="/">Dashboard</Link>
           <Link to="/detail">Details</Link>
+          <Link to="/404">404</Link>
         </nav>
         <Switch>
           <Route path="/" exact component={Dashboard} />
           <Route path="/detail" component={HeroDetail} />
+          <Route component={NotFound} />
         </Switch>
       </BrowserRouter>
     </>
