b128ded741affed25594cc914e2f356ea2dbe00e
Gilberto Cao <gilbe.cao@gmail.com>
2021-04-28 12:18:41 +0200

    Find hero by id on details

--- gilbe-cao/toh-redux/src/components/HeroDetail/index.jsx
+++ gilbe-cao/toh-redux/src/components/HeroDetail/index.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import HEROES from '../../constants/heroes.mock';
+import './HeroDetail.css';
 
 function HeroDetail() {
-  return <h1>Hero Detail</h1>;
+  const { heroId } = useParams();
+  const selectedHero = HEROES.find((hero) => hero.id === +heroId);
+
+  return (
+    <div>
+      <h2>
+        {selectedHero?.name}
+        {' '}
+        Details
+      </h2>
+    </div>
+  );
 }
 
 export default HeroDetail;
