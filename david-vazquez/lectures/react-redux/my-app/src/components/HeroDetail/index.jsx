d3162f288b7f2abae220ab3818f2a75024d5c982
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-04-28 12:52:37 +0200

    handle name change

--- david-vazquez/lectures/react-redux/my-app/src/components/HeroDetail/index.jsx
+++ david-vazquez/lectures/react-redux/my-app/src/components/HeroDetail/index.jsx
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import './herodetail.css';
 import HEROES from '../../constants/heroes.mock';
 
 function HeroDetail() {
   const { heroId } = useParams();
-  const selectedHero = HEROES.find((hero) => hero.id === +heroId);
+  const [selectedHero, setSelectedHero] = useState();
+  useEffect(() => {
+    setSelectedHero(HEROES.find((hero) => hero.id === +heroId));
+  }, [heroId]);
+
+  function handleNameChange(event) {
+    setSelectedHero({
+      ...selectedHero,
+      name: event.target.value,
+    });
+  }
+
   return (
-    <div>
-      <h2>
-        {selectedHero?.name}
-        {' '}
-        Details
-      </h2>
-    </div>
+    selectedHero ? (
+      <div>
+        <h2>
+          {selectedHero?.name}
+          {' '}
+          Details
+        </h2>
+        <div>
+          <span>id: </span>
+          {selectedHero?.id}
+        </div>
+        <div>
+          <label htmlFor="hero-name">
+            Hero name:
+            <input id="hero-name" value={selectedHero.name} onChange={handleNameChange} placeholder="Hero name" />
+          </label>
+        </div>
+        <button type="button">go back</button>
+        <button type="button">save</button>
+      </div>
+    )
+      : (
+        <h3>No hay heroe</h3>
+      )
+
   );
 }
 
