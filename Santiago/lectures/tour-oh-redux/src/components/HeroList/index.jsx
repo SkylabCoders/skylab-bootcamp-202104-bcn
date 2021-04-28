65cb02d375a7a2747c5bc41f91aa9c82d13ddf2e
Santiago.Martinez <infohere52@gmail.com>
2021-04-28 20:25:44 +0200

    Delete button working

--- Santiago/lectures/tour-oh-redux/src/components/HeroList/index.jsx
+++ Santiago/lectures/tour-oh-redux/src/components/HeroList/index.jsx
@@ -1,16 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import HEROES from '../../Constants/heroes-mock';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import { loadHeroes, deleteHero } from '../../redux/actions/actionCreators';
 import './heroList.css';
 
-function HeroList() {
+function HeroList({ heroes, dispatch }) {
+  useEffect(() => {
+    if (!heroes.length) dispatch(loadHeroes());
+  }, []);
+
+  function handleDelete(heroId) {
+    dispatch(deleteHero(heroId));
+  }
+
   return (
     <>
-      <h2>Heroes List</h2>
-      <div className="heroes-menu">
-        {HEROES.map((hero) => <Link to={`/detail/${hero.id}`}>{hero.name}</Link>)}
+      <h2>Top Heroes</h2>
+      <div>
+        <label htmlFor="hero-input">
+          Hero name:
+          <input id="hero-input" placeholder="Hero name" />
+        </label>
+        <button type="button">
+          Add
+        </button>
       </div>
+      <ul className="heroes">
+        {heroes.map((hero) => (
+          <li className="heroes_li">
+            <Link to={`/detail/${hero.id}`}>
+              {hero.id}
+              {' - '}
+              {hero.name}
+            </Link>
+            <button type="button" className="delete" onClick={() => handleDelete(hero.id)} title="delete hero">x</button>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
-export default HeroList;
+
+HeroList.propTypes = {
+  heroes: PropTypes.shape([]).isRequired,
+  dispatch: PropTypes.func.isRequired
+};
+
+function mapStateToProps(store) {
+  return {
+    heroes: store.heroes
+  };
+}
+export default connect(mapStateToProps)(HeroList);
