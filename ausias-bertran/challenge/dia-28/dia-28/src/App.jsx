d3776068dfd4f39da59c8e7ccb711a3121bbac3c
ulldecorb <ausias.bertran@gmail.com>
2021-04-28 10:35:26 +0200

    Add Dashboard component

---
+++ ausias-bertran/challenge/dia-28/dia-28/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles.css';
+import './App.css';
+import Dashboard from './components/Dashboard/index';
+
+function App() {
+  return (
+    <>
+      <h1>Tour of Heroes</h1>
+      <nav>
+        <a href="  ">Dashbooard</a>
+        <a href="  ">Heroes</a>
+      </nav>
+      <Dashboard />
+    </>
+  );
+}
+
+export default App;
