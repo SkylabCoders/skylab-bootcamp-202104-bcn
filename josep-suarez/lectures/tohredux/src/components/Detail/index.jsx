c89c08204f8684841483dcdb81c66874609018b3
Josep Suarez <djose.anton@gmail.com>
2021-04-28 12:32:12 +0200

    cambio params por useEffect

--- josep-suarez/lectures/tohredux/src/components/Detail/index.jsx
+++ josep-suarez/lectures/tohredux/src/components/Detail/index.jsx
@@ -1,28 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import './detail.css';
 import heroes from '../../services/heroes';
 
 function Detail() {
   const { heroId } = useParams();
-  {
-    const selectedHero = heroes.find((hero) => hero.id.toString() === heroId);
-    return (
-      <>
-        <h2>Hero Details</h2>
-        <h3>
-          name:
-          {' '}
-          {selectedHero.name}
-        </h3>
-        <h3>
-          id:
-          {' '}
-          {selectedHero.id}
-        </h3>
-      </>
-    );
-  }
+  const [selectedHero, setSelectedHero] = useState();
+  useEffect(() => {
+    setSelectedHero(heroes.find((hero) => hero.id === +heroId));
+  }, [heroId]);
+  return (
+    <>
+      <h2>Hero Details</h2>
+      <h3>
+        name:
+        {' '}
+        {selectedHero?.name}
+      </h3>
+      <h3>
+        id:
+        {' '}
+        {selectedHero?.id}
+      </h3>
+    </>
+  );
 }
 
 export default Detail;
