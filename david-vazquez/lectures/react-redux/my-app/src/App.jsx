a9ebc23e33832bde50bdf7f9a07e634aaf689940
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-04-28 15:49:12 +0200

    Added redux configuration

--- david-vazquez/lectures/react-redux/my-app/src/App.jsx
+++ david-vazquez/lectures/react-redux/my-app/src/App.jsx
@@ -4,25 +4,29 @@ import './styles.css';
 import {
   BrowserRouter, Route, Switch, Link,
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Dashboard from './components/Dashboard';
 import HeroDetail from './components/HeroDetail';
 import NotFound from './components/NotFound';
+import store from './redux/stores';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <h1>Tour of Heroes</h1>
-        <nav>
-          <Link to="/">Dashboard</Link>
-          <Link to="/detail">Details</Link>
-        </nav>
-        <Switch>
-          <Route exact path="/" component={Dashboard} />
-          <Route path="/detail/:heroId" component={HeroDetail} />
-          <Route component={NotFound} />
-        </Switch>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <h1>Tour of Heroes</h1>
+          <nav>
+            <Link to="/">Dashboard</Link>
+            <Link to="/detail">Details</Link>
+          </nav>
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route path="/detail/:heroId" component={HeroDetail} />
+            <Route component={NotFound} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
