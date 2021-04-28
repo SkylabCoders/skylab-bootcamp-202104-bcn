ed8ebb15be767783655ee6fb3a5a94a3b672d6f4
Hosni <hosni.marco@gmail.com>
2021-04-28 10:35:47 +0200

    Style import

--- hosni-marco/lecture/tour-of-heroes-redux/my-toh-redux/src/App.jsx
+++ hosni-marco/lecture/tour-of-heroes-redux/my-toh-redux/src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import './styles.css';
 import './App.css';
+import Dashboard from './components/Dashboard/index';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div />
-      </header>
-    </div>
+    <>
+      <h1>Tour of Heroes</h1>
+      <nav>
+        <a href="Dashboard">Dashboard</a>
+        <a href="Heroes">Heroes</a>
+      </nav>
+      <Dashboard />
+    </>
   );
 }
 
