7848c8000174c0bf46ff6e3b24a5c02ffcd79bd7
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-04-29 20:43:08 +0200

    Can add, delete and load tasks

--- david-vazquez/challenges/to-do-list-redux/src/App.jsx
+++ david-vazquez/challenges/to-do-list-redux/src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
 import InputTask from './components/InputTask';
+import store from './redux/store/index';
 
 function App() {
   return (
     <>
-      <div className="App">
-        <h1>To Do List</h1>
-        <InputTask />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <h1>To Do List</h1>
+          <InputTask />
+        </div>
+      </Provider>
     </>
   );
 }
--- david-vazquez/challenges/to-do-list-redux/src/components/InputTask.jsx
+++ david-vazquez/challenges/to-do-list-redux/src/components/InputTask.jsx
@@ -1,21 +1,44 @@
-import React from 'react';
-import TASKS from '../constants/task.mock';
+import React, { useEffect, useState } from 'react';
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
+import { loadTasks, deleteTask, addTask } from '../redux/actions/actionCreator';
 
-function InputTask() {
+function InputTask({ tasks, dispatch }) {
+  const [taskText, setTaskText] = useState();
+  useEffect(() => {
+    if (!tasks.length) dispatch(loadTasks());
+  });
+
+  function handleAdd() {
+    let newId = 1;
+    if (tasks.length !== 0) newId = tasks[tasks.length - 1].id + 1;
+    const newTask = {
+      id: newId,
+      text: taskText,
+    };
+    dispatch(addTask(newTask));
+  }
+  function getTask(event) {
+    setTaskText(event.target.value);
+  }
+
+  function selectDeleteTask(id) {
+    dispatch(deleteTask(id));
+  }
   return (
     <div>
       <label htmlFor="input-task">
         Introduce tu tarea:
-        <input type="text" id="input-task" value="" placeholder="Introduce tu tarea" />
-        <button type="button">Añadir</button>
+        <input id="input-task" onChange={getTask} />
+        <button type="button" onClick={() => handleAdd()}>Añadir</button>
       </label>
-      <ul>
+      <ul className="task-list">
         <h2>List of task: </h2>
-        {TASKS.map((task) => (
-          <li>
+        {tasks.map((task) => (
+          <li className="task-item">
             {task.text}
             <button type="button">DONE</button>
-            <button type="button">DELETE</button>
+            <button type="button" onClick={() => selectDeleteTask(task.id)}>DELETE</button>
           </li>
         ))}
       </ul>
@@ -23,4 +46,15 @@ function InputTask() {
   );
 }
 
-export default InputTask;
+InputTask.propTypes = {
+  tasks: PropTypes.shape([]).isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+function mapStateToProps(tasks) {
+  return {
+    tasks,
+  };
+}
+
+export default connect(mapStateToProps)(InputTask);
