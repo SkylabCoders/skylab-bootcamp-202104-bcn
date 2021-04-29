c0c2e0f6b549ca9111c8f88061ef8f5a16b0076a
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-04-29 18:05:36 +0200

    Finishing, dont work create heroes

--- david-vazquez/lectures/tour-of-heroes-review/src/redux/reducers/heroesReducers.js
+++ david-vazquez/lectures/tour-of-heroes-review/src/redux/reducers/heroesReducers.js
@@ -5,6 +5,8 @@ function heroesReducers(heroes = [], action) {
   switch (action.type) {
     case actionTypes.LOAD_HEROES:
       return HEROES;
+    case actionTypes.CREATE_HERO:
+      return heroes;
     default:
       return heroes;
   }
