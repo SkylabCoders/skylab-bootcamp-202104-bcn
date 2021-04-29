4e8903bda72858e7b7d02f6f93158f2f30013f2f
cristian-bars <patxi.cristian@gmail.com>
2021-04-29 15:57:08 +0200

    Configuration redux devtools extension

--- cristian-bars/lectures/review-toh/review-toh/src/components/heroes/HeroList.jsx
+++ cristian-bars/lectures/review-toh/review-toh/src/components/heroes/HeroList.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
+import { loadHeroes } from '../../redux/actions/action-creator';
 
-const HeroList = () => (
-  <main>
-    <h2> Top Heroes</h2>
-    <div className="heroes-menu">
-      Llista
-    </div>
-  </main>
-);
+const HeroList = ({ heroes, dispatch }) => {
+  useEffect(() => {
+    if (!heroes.length) {
+      dispatch(loadHeroes());
+    }
+  }, []);
 
-export default HeroList;
+  return (
+    <main>
+      <h2> Top Heroes</h2>
+      <div className="heroes-menu">
+        Llista
+      </div>
+    </main>
+  );
+};
+
+HeroList.propTypes = {
+  heroes: PropTypes.shape([]).isRequired,
+  dispatch: PropTypes.func.isRequired
+};
+
+function mapStateToProps(store) {
+  return {
+    heroes: store.heroes
+  };
+}
+export default connect(mapStateToProps)(HeroList);
