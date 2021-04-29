26587ce226a5cf94881d5c915fa5c9e4498b519d
Isabel Gomez <belasi87@gmail.com>
2021-04-29 19:06:29 +0200

    Create: - store - rootReducer but empty, only structure

--- Isabel-Gomez/challenge/todo-list-react-redux/src/App.jsx
+++ Isabel-Gomez/challenge/todo-list-react-redux/src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/stores/index';
+
 import './App.css';
+import CreateTask from './components/createTask/CreateTask';
 
 function App() {
   return (
-    <header>
-      <h2>To Do List</h2>
-    </header>
+
+    <Provider store={store}>
+      <header>
+        <h2>To Do List</h2>
+      </header>
+      <main>
+        <CreateTask />
+      </main>
+    </Provider>
+
   );
 }
 
---
+++ Isabel-Gomez/challenge/todo-list-react-redux/src/components/createTask/CreateTask.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import createTask from './createTask.css';
+
+const CreateTask = () => {
+
+};
+
+export default CreateTask;
