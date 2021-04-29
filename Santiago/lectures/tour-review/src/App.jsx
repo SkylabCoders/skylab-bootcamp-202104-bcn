62d2e2b194c4b171ef89fe829f04b92dac3f2803
Santiago.Martinez <infohere52@gmail.com>
2021-04-29 13:03:43 +0200

    Add dashboard and create structure folders.

--- Santiago/lectures/tour-review/src/App.jsx
+++ Santiago/lectures/tour-review/src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
+import Navbar from './components/UI/Nav';
+import Dashboard from './components/dashboard/Dashboard';
+import HeroList from './components/heroes/HeroList';
+import HeroDetail from './components/heroes/HeroDetail';
 
 function App() {
   return (
     <div className="App">
-      <h1>HOLA</h1>
+      <BrowserRouter>
+        <h1>TOUR OF HEROES</h1>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route exact path="/heroes" component={HeroList} />
+          <Route exact path="/heroes/:heroId" component={HeroDetail} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ Santiago/lectures/tour-review/src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Dashboard = () => (
+  <main>
+    <h2>HEROES LIST</h2>
+    <div className="heroes-menu"> LISTA DE HEROES</div>
+  </main>
+);
+
+export default Dashboard;
