a0dc31b653d5adf10abd31ac825951bfcf3ae8b3
Anna Sala <anna.sala.mercade@gmail.com>
2021-04-29 19:52:36 +0200

    started redux on component

--- anna-sala/challenges/to-do-list-redux/src/components/Dashboard/index.jsx
+++ anna-sala/challenges/to-do-list-redux/src/components/Dashboard/index.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { PropTypes } from 'prop-types';
 
-function Dashboard() {
+function Dashboard({ tasks }) {
   return (
     <ul>
-      <li>This will be the task list</li>
+      {tasks.map((task) => (
+        <li>
+          {' '}
+          <Link to={`/detail/${task.id}`}>{task.taskName}</Link>
+        </li>
+      ))}
     </ul>
   );
 }
+Dashboard.propTypes = {
+  tasks: PropTypes.shape([]).isRequired,
+  // dispatch: PropTypes.func.isRequired,
+};
 
-export default Dashboard;
+function mapStateToProps(store) {
+  return {
+    tasks: store.tasks,
+  };
+}
+export default connect(mapStateToProps)(Dashboard);
--- anna-sala/challenges/to-do-list-redux/src/constants/tasks.mock.js
+++ anna-sala/challenges/to-do-list-redux/src/constants/tasks.mock.js
@@ -1,9 +1,9 @@
 const TASKS = [
-  { id: 1, task: 'Buy Milk' },
-  { id: 2, task: 'Walk the Dog' },
-  { id: 3, task: 'Finish to-do-list' },
-  { id: 4, task: 'Go to the bank' },
-  { id: 6, task: 'Update Escalidraw' },
+  { id: 1, taskName: 'Buy Milk' },
+  { id: 2, taskName: 'Walk the Dog' },
+  { id: 3, taskName: 'Finish to-do-list' },
+  { id: 4, taskName: 'Go to the bank' },
+  { id: 6, taskName: 'Update Escalidraw' },
 ];
 
 export default TASKS;
