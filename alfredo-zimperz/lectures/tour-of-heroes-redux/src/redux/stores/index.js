664d268b1c9f1fb8cd221134ea831fac8d54d61f
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-04-29 15:58:25 +0200

    Add ReduxDevTools script to store

--- alfredo-zimperz/lectures/tour-of-heroes-redux/src/redux/stores/index.js
+++ alfredo-zimperz/lectures/tour-of-heroes-redux/src/redux/stores/index.js
@@ -3,6 +3,8 @@ import rootReducers from '../reducers';
 
 const store = createStore(
   rootReducers,
+  // eslint-disable-next-line no-underscore-dangle
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
 export default store;
