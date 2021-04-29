4056f9432547004fb14b1a2e7ad6300a115531c5
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-04-29 18:25:06 +0200

    React and eslint installed and configured

---
+++ david-vazquez/challenges/to-do-list-redux/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        Hola
+      </div>
+    </>
+  );
+}
+
+export default App;
