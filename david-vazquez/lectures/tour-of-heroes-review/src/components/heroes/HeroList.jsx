8ec64a6d02cebb0c930428d3c7f1962a82190380
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-04-29 16:17:14 +0200

    Display heroes on screen

--- david-vazquez/lectures/tour-of-heroes-review/src/components/heroes/HeroList.jsx
+++ david-vazquez/lectures/tour-of-heroes-review/src/components/heroes/HeroList.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
+import { loadHeroes } from '../../redux/actions/actionCreator';
 
-const HeroList = () => (
-  <main>
-    <h2>List of heroes</h2>
-    <div className="heroes-menu">
-      Aca vienen los heroes
-    </div>
-  </main>
-);
+function HeroList({ heroes, dispatch }) {
+  useEffect(() => {
+    if (!heroes.length) {
+      dispatch(loadHeroes());
+    }
+  }, []);
+  return (
+    <main>
+      <h2>List of heroes</h2>
+      <div className="heroes-menu">
+        {heroes.length && heroes.map((hero) => <li key={hero.id}>{hero.name}</li>)}
+      </div>
+    </main>
+  );
+}
 
-export default HeroList;
+HeroList.propTypes = {
+  heroes: PropTypes.shape([]).isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+function mapStateToProps(store) {
+  return {
+    heroes: store.heroes,
+  };
+}
+
+export default connect(mapStateToProps)(HeroList);
