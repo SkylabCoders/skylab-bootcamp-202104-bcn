6e3a0533371d2482ab692f8c4bccaf38518700aa
Daniel Martorell <dani.martorell@gmail.com>
2021-04-29 18:23:42 +0200

    Set up Project

---
+++ dani-martorell/challenge/todo-list-redux/todo-list/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hola
+    </div>
+  );
+}
+
+export default App;
---
+++ dani-martorell/challenge/todo-list-redux/todo-list/src/constants/TASKS.js
@@ -0,0 +1,9 @@
+const TASKS = [
+  { id: 1, task: 'Comprar leche' },
+  { id: 2, task: 'Sacar al perro' },
+  { id: 3, task: 'Llamar a Juan' },
+  { id: 4, task: 'Comprar leche' },
+  { id: 5, task: 'Comprar leche' },
+  { id: 6, task: 'Comprar leche' },
+
+];
