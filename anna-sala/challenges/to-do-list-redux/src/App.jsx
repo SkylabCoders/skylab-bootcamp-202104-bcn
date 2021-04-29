311530beb99f072c580e40bb06fc2ddfcb41c483
Anna Sala <anna.sala.mercade@gmail.com>
2021-04-29 18:55:48 +0200

    created routes

--- anna-sala/challenges/to-do-list-redux/src/App.jsx
+++ anna-sala/challenges/to-do-list-redux/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  Link,
+} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <h1>TO DO LIST</h1>
+    <>
+      <Router>
+        <h1>TO DO LIST</h1>
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route exact path="/:taskId" component={Task} />
+          <Redirect to="/" />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
