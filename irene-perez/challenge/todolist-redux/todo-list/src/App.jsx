4e2e10d35aabdfd5484ceff3d30a36ec49f66efc
IrenePerezAlonso <irperal@hotmail.com>
2021-04-29 19:47:37 +0200

    import the store at the App

--- irene-perez/challenge/todolist-redux/todo-list/src/App.jsx
+++ irene-perez/challenge/todolist-redux/todo-list/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import InputTask from './components/InputTask';
+import store from './redux/store/index';
 import './App.css';
 
 function App() {
   return (
     <>
-      <Provider>
+      <Provider store={store}>
         <div className="App">
           <h1>To do List</h1>
           <InputTask />
