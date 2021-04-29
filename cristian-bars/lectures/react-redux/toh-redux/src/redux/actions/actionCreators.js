77ac8f69f145b65972db14bdc507ca8c947aa590
cristian-bars <patxi.cristian@gmail.com>
2021-04-29 08:49:24 +0200

    Changing Dashboard

---
+++ cristian-bars/lectures/react-redux/toh-redux/src/redux/actions/actionCreators.js
@@ -0,0 +1,35 @@
+import actionTypes from './actionTypes';
+
+export function loadHeroes() {
+  return {
+    type: actionTypes.LOAD_HEROES
+  };
+}
+
+export function addHero(hero) {
+  return {
+    type: actionTypes.ADD_HERO,
+    hero
+  };
+}
+
+export function deleteHero(heroId) {
+  return {
+    type: actionTypes.DELETE_HERO,
+    heroId
+  };
+}
+
+export function updateHero(hero) {
+  return {
+    type: actionTypes.UPDATE_HERO,
+    hero
+  };
+}
+
+export function loadHero(hero) {
+  return {
+    type: actionTypes.LOAD_HERO,
+    hero
+  };
+}
