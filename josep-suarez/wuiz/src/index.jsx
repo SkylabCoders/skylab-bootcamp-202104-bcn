fd607a47e5dde6fc90e52a0f8b3c75fb087ea93f
Josep Suarez <djose.anton@gmail.com>
2021-04-29 12:36:15 +0200

    estructura de carpetas creada

--- josep-suarez/wuiz/src/index.jsx
+++ josep-suarez/wuiz/src/index.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+// import { store } from './redux/store/store';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    ,
+  </Provider>,
   document.getElementById('root')
 );
 
