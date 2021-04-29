97a5491d6c7322938858a5319ed5e1a0d3cb8062
Anna Sala <anna.sala.mercade@gmail.com>
2021-04-29 23:22:32 +0200

    printed list with redux

--- anna-sala/challenges/to-do-list-redux/src/components/Dashboard/index.jsx
+++ anna-sala/challenges/to-do-list-redux/src/components/Dashboard/index.jsx
@@ -1,33 +1,45 @@
-import React from 'react';
-// import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-// import { PropTypes } from 'prop-types';
-import TASKS from '../../constants/tasks.mock';
+import { PropTypes } from 'prop-types';
+import { loadTasks, deleteTask } from '../../redux/actions/actionCreators';
 
-function Dashboard() {
+function Dashboard({ tasks, dispatch }) {
+  useEffect(() => {
+    if (!tasks.length) dispatch(loadTasks());
+  }, []);
+  function deleteClick(id) {
+    dispatch(deleteTask(id));
+  }
   return (
     <>
       <ul className="to-do-list">
-        {TASKS.map((task) => (
+        {tasks.map((task) => (
           <li className="to-do-list__task">
             {' '}
             <Link to={`/${task.id}`} className="task-name">{task.taskName}</Link>
-            <button type="button" className="delete">X</button>
+            <button
+              type="button"
+              className="delete"
+              onClick={() => deleteClick(task.id)}
+            >
+              X
+            </button>
           </li>
         ))}
       </ul>
     </>
   );
 }
-// Dashboard.propTypes = {
-//   tasks: PropTypes.shape([]).isRequired,
-//   // dispatch: PropTypes.func.isRequired,
-// };
+Dashboard.propTypes = {
+  tasks: PropTypes.shape([]).isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
 
-// function mapStateToProps(store) {
-//   return {
-//     tasks: store.tasks,
-//   };
-// }
-// connect(mapStateToProps)
-export default (Dashboard);
+function mapStateToProps(store) {
+  return {
+    tasks: store.tasks,
+  };
+}
+
+export default connect(mapStateToProps)(Dashboard);
