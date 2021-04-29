12688def87c1cfc6199bcb4d8eb801bccd8afadd
Santiago.Martinez <infohere52@gmail.com>
2021-04-29 17:07:09 +0200

    Show five heroes in dashboardd

--- Santiago/lectures/tour-review/src/components/dashboard/Dashboard.jsx
+++ Santiago/lectures/tour-review/src/components/dashboard/Dashboard.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import loadHeroes from '../../redux/actions/actionCreator';
 
-const Dashboard = () => (
-  <main>
-    <h2>Dashboard</h2>
-    <h3>Top Heroes</h3>
-    <div className="heroes-menu"> </div>
-  </main>
-);
+const Dashboard = ({ heroes, dispatch }) => {
+  useEffect(() => {
+    if (!heroes.length) dispatch(loadHeroes());
+  }, []);
 
-export default Dashboard;
+  return (
+    <main>
+      <h2>DASHBOARD</h2>
+      <h3>Top Heroes</h3>
+      <div>
+        {
+            heroes.slice(0, 5).map((hero) => (
+              <li key={hero.id}>{hero.name}</li>
+            ))
+        }
+      </div>
+    </main>
+  );
+};
+
+Dashboard.propTypes = {
+  heroes: PropTypes.shape([]).isRequired,
+  dispatch: PropTypes.func.isRequired
+};
+
+// Conectar store con el componente
+function mapStateToProps(store) {
+  return {
+    heroes: store.heroes
+  };
+}
+
+export default connect(mapStateToProps)(Dashboard);
