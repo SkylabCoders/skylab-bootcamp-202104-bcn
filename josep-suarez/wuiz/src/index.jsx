cd58137f9a4f40e6aacc81d6c80d58b43481b95e
Josep Suarez <djose.anton@gmail.com>
2021-04-29 19:56:01 +0200

    end project with css, and favicon

--- josep-suarez/wuiz/src/index.jsx
+++ josep-suarez/wuiz/src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store/store';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
