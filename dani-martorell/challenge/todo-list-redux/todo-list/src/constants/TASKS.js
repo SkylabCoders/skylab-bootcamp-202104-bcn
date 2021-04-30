017cb6fc049130f5f2f10c00ba31363a9cb43237
Daniel Martorell <dani.martorell@gmail.com>
2021-04-30 08:44:07 +0200

    Update TASKS file

--- dani-martorell/challenge/todo-list-redux/todo-list/src/constants/TASKS.js
+++ dani-martorell/challenge/todo-list-redux/todo-list/src/constants/TASKS.js
@@ -1,9 +1,9 @@
 const TASKS = [
-  { id: 1, task: 'Comprar leche' },
-  { id: 2, task: 'Sacar al perro' },
-  { id: 3, task: 'Comprar medias' },
-  { id: 4, task: 'Ir al banco' },
-  { id: 5, task: 'Atracar banco' },
+  { id: 1, task: 'Comprar leche', timeStamp: 1619764872302 },
+  { id: 2, task: 'Sacar al perro', timeStamp: 1519764872302 },
+  { id: 3, task: 'Comprar medias', timeStamp: 1419764872302 },
+  { id: 4, task: 'Ir al banco', timeStamp: 1319764872302 },
+  { id: 5, task: 'Atracar banco', timeStamp: 1219764872302 },
 
 ];
 
