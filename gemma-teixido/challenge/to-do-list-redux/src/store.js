3638d78736168cc6b3fea8751e3a58d523b34f23
gteixi <gemmateixido25@gmail.com>
2021-04-30 12:21:03 +0200

    installed thunk.

--- gemma-teixido/challenge/to-do-list-redux/src/store.js
+++ gemma-teixido/challenge/to-do-list-redux/src/store.js
@@ -1,8 +1,13 @@
-import { createStore } from 'redux';
+/* eslint-disable no-underscore-dangle */
+import { applyMiddleware, createStore, compose } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from './redux/reducers';
 
+const composeEnhancers = (typeof window !== 'undefined'
+&& window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE_)
+|| compose;
 const store = createStore(
-  rootReducer
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
 );
-
 export default store;
