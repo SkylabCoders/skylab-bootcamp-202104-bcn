20823cce180806bef2ca980c97feab29560ba842
Fernando Gomez <shadyfast8.services@gmail.com>
2021-04-30 12:19:26 +0200

    added redux thunk middleware

--- fernando-gomez/lectures/toh-redux/src/redux/stores/index.js
+++ fernando-gomez/lectures/toh-redux/src/redux/stores/index.js
@@ -1,6 +1,12 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
-const store = createStore(rootReducer);
-
+const composeEnhancers = (typeof window !== 'undefined'
+&& window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)
+|| compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk)),
+);
 export default store;
