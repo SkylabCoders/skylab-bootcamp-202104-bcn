825752e8dd04e95ecea51209464fc6271a8400f1
Isabel Gomez <belasi87@gmail.com>
2021-05-01 10:48:55 +0200

    Create main structure with link, and create Dashboard component

--- Isabel-Gomez/lectures/countrys-app/src/App.jsx
+++ Isabel-Gomez/lectures/countrys-app/src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, Route, Switch, Link,
+} from 'react-router-dom';
 import './App.css';
+import Dashboard from './components/dashboard/Dashboard';
 
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <header>
+        <h1>Countries App</h1>
+        <Link to="/">Dashboard</Link>
+        <Link to="/countries">Countries</Link>
+      </header>
+      <main>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route path="/countries" component={Countries} />
+          </Switch>
+        </Router>
+      </main>
+    </div>
   );
 }
 
---
+++ Isabel-Gomez/lectures/countrys-app/src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './dashboard.scss';
+
+const Dashboard = () => {
+
+};
+
+export default Dashboard;
