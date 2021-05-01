5765e1dc4ab2b738e65b4e248b15eb56444ef2e7
Daniel Martorell <dani.martorell@gmail.com>
2021-05-01 10:48:01 +0200

    Set up routes

---
+++ dani-martorell/lectures/redux-freddy/countries-app/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router, Switch, Route, Link,
+} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Hello</h1>
+        <Link to="/">Dashboard</Link>
+        <Link to="/countries">COuntries</Link>
+      </header>
+      <main>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={Dashboard} />
+            <Route path="/countries" exact component={Countries} />
+          </Switch>
+        </Router>
+      </main>
+    </div>
+  );
+}
+
+export default App;
