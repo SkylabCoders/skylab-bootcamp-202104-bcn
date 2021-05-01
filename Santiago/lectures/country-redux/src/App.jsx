0318ddc43e88bd094f5e2fafccf1050065a362e3
Santiago.Martinez <infohere52@gmail.com>
2021-05-01 10:58:56 +0200

    Import components to app.jsx

--- Santiago/lectures/country-redux/src/App.jsx
+++ Santiago/lectures/country-redux/src/App.jsx
@@ -3,23 +3,25 @@ import {
   BrowserRouter as Router, Switch, Route, Link
 } from 'react-router-dom';
 import './App.css';
+import Dashboard from './components/Dashboard';
+import Countries from './components/Countries';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>HELLO</h1>
-        <Link to="/">Dashboard</Link>
-        <Link to="/countries">Countries</Link>
-      </header>
-      <main>
-        <Router>
+      <Router>
+        <header>
+          <h1>HELLO</h1>
+          <Link to="/">Dashboard</Link>
+          <Link to="/countries">Countries</Link>
+        </header>
+        <main>
           <Switch>
             <Route path="/" exact component={Dashboard} />
             <Route path="/countries" component={Countries} />
           </Switch>
-        </Router>
-      </main>
+        </main>
+      </Router>
     </div>
   );
 }
