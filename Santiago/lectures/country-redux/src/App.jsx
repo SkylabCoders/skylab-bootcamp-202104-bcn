56dbb5df66d45c24464db20fd82953eebe4c7c37
Santiago.Martinez <infohere52@gmail.com>
2021-05-01 11:53:53 +0200

    Create laod action

--- Santiago/lectures/country-redux/src/App.jsx
+++ Santiago/lectures/country-redux/src/App.jsx
@@ -3,7 +3,7 @@ import {
   BrowserRouter as Router, Switch, Route, Link
 } from 'react-router-dom';
 import './App.css';
-import Provider from 'react-redux';
+import { Provider } from 'react-redux';
 import Dashboard from './components/Dashboard';
 import Countries from './components/Countries';
 import store from './redux/store';
