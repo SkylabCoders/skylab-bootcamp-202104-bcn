8258bdfe9163148ee128b51edc898ff840084101
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-05-01 11:53:45 +0200

    Create countries actionCreator

---
+++ alfredo-zimperz/lectures/countries-redux-api/src/redux/reducers/countriesReducer.js
@@ -0,0 +1,16 @@
+import actionTypes from '../actions/actionTypes';
+
+function countriesReducer(initialCountries = [], action) {
+  let updatedCountries = [...initialCountries];
+  switch (action.type) {
+    case actionTypes.LOAD_COUNTRIES:
+      updatedCountries = [...action.countries];
+      break;
+
+    default:
+      break;
+  }
+  return updatedCountries;
+}
+
+export default countriesReducer;
