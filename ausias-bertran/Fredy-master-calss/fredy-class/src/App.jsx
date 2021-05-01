f7a8a36a8e94b957c92776ab9df29560e3667c7f
ulldecorb <ausias.bertran@gmail.com>
2021-05-01 11:51:24 +0200

    Add reducers

--- ausias-bertran/Fredy-master-calss/fredy-class/src/App.jsx
+++ ausias-bertran/Fredy-master-calss/fredy-class/src/App.jsx
@@ -5,6 +5,8 @@ import {
   Route,
   Link,
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store/index';
 import Dashboard from './componentes/Dashborad/index';
 import Countries from './componentes/Countries';
 import './App.css';
@@ -12,19 +14,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Router>
-        <header className="App-header">
-          <h1>Hello, Skylab</h1>
-          <Link to="/">Dashboard</Link>
-          <Link to="/countries">Countries</Link>
-        </header>
-        <main>
-          <Switch>
-            <Route path="/" exact component={Dashboard} />
-            <Route path="/countries" exact component={Countries} />
-          </Switch>
-        </main>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <header className="App-header">
+            <h1>Hello, Skylab</h1>
+            <Link to="/">Dashboard</Link>
+            <Link to="/countries">Countries</Link>
+          </header>
+          <main>
+            <Switch>
+              <Route path="/" exact component={Dashboard} />
+              <Route path="/countries" exact component={Countries} />
+            </Switch>
+          </main>
+        </Router>
+      </Provider>
     </div>
   );
 }
