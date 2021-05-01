56d2dbdc954e42c871cad55121d2f2ec373803c2
gteixi <gemmateixido25@gmail.com>
2021-05-01 11:12:04 +0200

    Installed react-redux and imported Provider.

--- gemma-teixido/lectures/class-fredy/src/App.jsx
+++ gemma-teixido/lectures/class-fredy/src/App.jsx
@@ -6,27 +6,30 @@ import
   Route,
   Link
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Countries from './components/Countries';
 import Dashboard from './components/Dashboard';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <header>
-          <h1>Hola</h1>
-          <Link to="/">Dashboard</Link>
-          <Link to="/countries">Countries</Link>
-        </header>
-        <main>
-          <Switch>
-            <Route path="/" exact component={Dashboard} />
-            <Route path="/countries" component={Countries} />
-          </Switch>
-        </main>
-      </div>
-    </Router>
+    <Provider>
+      <Router>
+        <div className="App">
+          <header>
+            <h1>Hola</h1>
+            <Link to="/">Dashboard</Link>
+            <Link to="/countries">Countries</Link>
+          </header>
+          <main>
+            <Switch>
+              <Route path="/" exact component={Dashboard} />
+              <Route path="/countries" component={Countries} />
+            </Switch>
+          </main>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
