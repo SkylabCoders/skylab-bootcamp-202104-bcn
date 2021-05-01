6da37b670a9100c5c4fe96f77bd89c83b896ee6e
Santiago.Martinez <infohere52@gmail.com>
2021-05-01 10:54:09 +0200

    Create dashboard and countries components.

---
+++ Santiago/lectures/country-redux/src/components/Countries/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+const Countries = () => {
+  const h1 = 'Countries';
+  return (
+    <div>
+      {h1}
+    </div>
+  );
+};
+
+export default Countries;
