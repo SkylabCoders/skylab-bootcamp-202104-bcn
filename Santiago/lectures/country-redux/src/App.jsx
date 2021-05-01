de7b18165f5048594d960116b4887144b6325a2a
Santiago.Martinez <infohere52@gmail.com>
2021-05-01 10:54:09 +0200

    Create dashboard and countries components.

--- Santiago/lectures/country-redux/src/App.jsx
+++ Santiago/lectures/country-redux/src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, Switch, Route, Link
+} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>HELLO</h1>
+      <header>
+        <h1>HELLO</h1>
+        <Link to="/">Dashboard</Link>
+        <Link to="/countries">Countries</Link>
+      </header>
+      <main>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={Dashboard} />
+            <Route path="/countries" component={Countries} />
+          </Switch>
+        </Router>
+      </main>
     </div>
   );
 }
