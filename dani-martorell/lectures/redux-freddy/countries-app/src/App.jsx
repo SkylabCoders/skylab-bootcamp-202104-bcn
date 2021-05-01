8f42d93a3093d04a4871bf5243ec214152a6a87a
Daniel Martorell <dani.martorell@gmail.com>
2021-05-01 11:11:23 +0200

    Install Provider

--- dani-martorell/lectures/redux-freddy/countries-app/src/App.jsx
+++ dani-martorell/lectures/redux-freddy/countries-app/src/App.jsx
@@ -3,26 +3,29 @@ import React from 'react';
 import {
   BrowserRouter as Router, Switch, Route, Link,
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Dashboard from './components/Dashboard/index';
 import Countries from './components/Countries/index';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <header>
-          <h1>Hello</h1>
-          <Link to="/">Dashboard</Link>
-          <Link to="/countries">Countries</Link>
-        </header>
-        <main>
-          <Switch>
-            <Route path="/" exact component={Dashboard} />
-            <Route path="/countries" exact component={Countries} />
-          </Switch>
-        </main>
-      </Router>
-    </div>
+    <Provider>
+      <div className="App">
+        <Router>
+          <header>
+            <h1>Hello</h1>
+            <Link to="/">Dashboard</Link>
+            <Link to="/countries">Countries</Link>
+          </header>
+          <main>
+            <Switch>
+              <Route path="/" exact component={Dashboard} />
+              <Route path="/countries" exact component={Countries} />
+            </Switch>
+          </main>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 export default App;
