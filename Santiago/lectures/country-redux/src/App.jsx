6d78602c94b41999b3d80b57847113cecf679e52
Santiago.Martinez <infohere52@gmail.com>
2021-05-01 11:23:46 +0200

    Create redux folder.

--- Santiago/lectures/country-redux/src/App.jsx
+++ Santiago/lectures/country-redux/src/App.jsx
@@ -3,25 +3,29 @@ import {
   BrowserRouter as Router, Switch, Route, Link
 } from 'react-router-dom';
 import './App.css';
+import Provider from 'react-redux';
 import Dashboard from './components/Dashboard';
 import Countries from './components/Countries';
+import store from './redux/store';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <header>
-          <h1>HELLO</h1>
-          <Link to="/">Dashboard</Link>
-          <Link to="/countries">Countries</Link>
-        </header>
-        <main>
-          <Switch>
-            <Route path="/" exact component={Dashboard} />
-            <Route path="/countries" component={Countries} />
-          </Switch>
-        </main>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <header>
+            <h1>HELLO</h1>
+            <Link to="/">Dashboard</Link>
+            <Link to="/countries">Countries</Link>
+          </header>
+          <main>
+            <Switch>
+              <Route path="/" exact component={Dashboard} />
+              <Route path="/countries" component={Countries} />
+            </Switch>
+          </main>
+        </Router>
+      </Provider>
     </div>
   );
 }
