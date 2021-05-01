fb1f5524836a84ee8023b3eb66b6419e7cfd508a
Santiago.Martinez <infohere52@gmail.com>
2021-05-01 13:02:21 +0200

    Show list to the view.

--- Santiago/lectures/country-redux/src/redux/reducers/countriesReducer.js
+++ Santiago/lectures/country-redux/src/redux/reducers/countriesReducer.js
@@ -4,6 +4,7 @@ function countriesReducer(initialCountries = [], action) {
   let updatedCountries = [...initialCountries];
   switch (action.type) {
     case actionTypes.LOAD_COUNTRIES:
+      // el countries de [...action.countries] es el countries en loadCountries countries: COUNTRIES
       updatedCountries = [...action.countries];
       break;
     default:
