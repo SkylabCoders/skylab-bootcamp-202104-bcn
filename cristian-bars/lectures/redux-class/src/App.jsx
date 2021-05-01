170bc05d34f9146e659f9e195806b0227595de13
cristian-bars <patxi.cristian@gmail.com>
2021-05-01 10:47:47 +0200

    Creating components

---
+++ cristian-bars/lectures/redux-class/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  BrouserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Hello, Skylab</h1>
+        <Link to="/">Dashboard</Link>
+        <Link to="/countries">Countries</Link>
+      </header>
+      <main>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={Dashboard} />
+            <Route path="/countries" exact component={Countries} />
+          </Switch>
+        </Router>
+      </main>
+    </div>
+  );
+}
+
+export default App;
