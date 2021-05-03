eaba3d56e3a4f1f1fdd1b304304bd1d30f777415
Laura <laura.sabater.musach@gmail.com>
2021-05-03 17:49:14 +0200

    create footer component with scss

---
+++ 00-TEAMS/03-TEAM/skylart/src/components/Common/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.scss';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <span className="footer__content">© 2000–2021 Skyl’Art. All rights reserved.</span>
+    </footer>
+  );
+}
+
+export default Footer;
