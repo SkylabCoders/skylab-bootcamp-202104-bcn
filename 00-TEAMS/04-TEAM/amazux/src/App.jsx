de32c3ed754e49c17a448b3432cf1f47af2c5588
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-05-03 17:13:27 +0200

    Create errorMessage component

--- 00-TEAMS/04-TEAM/amazux/src/App.jsx
+++ 00-TEAMS/04-TEAM/amazux/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from './components/Header';
+import ErrorMessage from './components/ErrorMessage';
 import Dashboard from './components/Dashboard';
 import './App.css';
 
-function App() {
+function App({ error }) {
   return (
     <>
+      {error && <ErrorMessage message="Hay un error" />}
       <Header />
       <main className="main">
         <Switch>
@@ -18,4 +22,12 @@ function App() {
   );
 }
 
-export default App;
+App.propTypes = {
+  error: PropTypes.string.isRequired
+};
+
+function mapStateToProps({ error }) {
+  return { error };
+}
+
+export default connect(mapStateToProps)(App);
