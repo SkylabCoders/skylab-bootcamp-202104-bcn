d1273f74664eca3a02d13497fe87b6dfa52964c1
ulldecorb <ausias.bertran@gmail.com>
2021-05-03 20:12:53 +0200

    Add SideNav list

--- 00-TEAMS/04-TEAM/amazux/src/components/Header/CategoriesNav/SideNav/index.jsx
+++ 00-TEAMS/04-TEAM/amazux/src/components/Header/CategoriesNav/SideNav/index.jsx
@@ -3,21 +3,25 @@ import PropTypes from 'prop-types';
 import './sidenav.css';
 
 const SideNav = ({ closeSideNav }) => (
-  <div id="sidenav" className="sidenav">
+  <>
+    <div id="sidenav" className="sidenav">
+      <div className="sidenav__user-bar">
+        {/* <img clasName="sidenav__user-icon" /> */}
+        Hola User
+      </div>
+      <ul>
+        <li>Lorem ipsum dolor sit amet</li>
+        <li>Maiores a quia, non quo debitis</li>
+        <li>blanditiis fugiat voluptates</li>
+        <li>doloremque eaque</li>
+      </ul>
+    </div>
     <button type="button" className="sidenav__close-button" onClick={closeSideNav}>×</button>
-
-    Lorem ipsum dolor sit amet
-    consectetur adipisicing elit.
-    Maiores a quia, non quo debitis
-    doloremque eaque, blanditiis fugiat voluptates
-    vel ad enim id obcaecati delectus
-    assumenda reiciendis facilis explicabo animi.
-  </div>
+  </>
 );
 
 SideNav.propTypes = {
   closeSideNav: PropTypes.func.isRequired
-
 };
 
 export default SideNav;
