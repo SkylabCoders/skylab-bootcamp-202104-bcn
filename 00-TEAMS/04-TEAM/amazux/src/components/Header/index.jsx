e15c7563e06d2da9f802fe5ce9adc3e3d29cdb4e
ulldecorb <ausias.bertran@gmail.com>
2021-05-03 18:50:18 +0200

    Create CategoriesNav and SIdeNav

--- 00-TEAMS/04-TEAM/amazux/src/components/Header/index.jsx
+++ 00-TEAMS/04-TEAM/amazux/src/components/Header/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 // import PropTypes from 'prop-types'
 import UserNav from './UserNav/index';
+import CategoriesNav from './CategoriesNav';
 
 const Header = () => (
   <header>
     <UserNav />
+    <CategoriesNav />
   </header>
 );
 
