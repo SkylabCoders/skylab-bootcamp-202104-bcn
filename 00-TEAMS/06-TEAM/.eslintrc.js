65197a84b02870a5f1d99235d6ea70cdb96eb467
blopez-dev <pjgl.frontend@gmail.com>
2021-05-03 12:04:19 +0200

    Configrued eslint

--- 00-TEAMS/06-TEAM/.eslintrc.js
+++ 00-TEAMS/06-TEAM/.eslintrc.js
@@ -20,5 +20,11 @@ module.exports = {
     'react',
   ],
   rules: {
+    'class-methods-use-this': 0,
+    'comma-dangle': [2, 'never'],
+    'linebreak-style': 0,
+    'global-require': 0,
+    'eslint linebreak-style': [0, 'error', 'windows'],
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
   },
 };
