85d951071017343e2856feab71570b4d2d75e132
JDSanchon <juanda.sanchon@gmail.com>
2021-05-03 16:47:51 +0200

    fix header

--- 00-TEAMS/02-TEAM/Chejuja-Team/trivial/src/App.jsx
+++ 00-TEAMS/02-TEAM/Chejuja-Team/trivial/src/App.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import {
-  BrowserRouter, Route, Switch
+  BrowserRouter, Switch // Route
 } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import Header from './components/Header';
-import Dashboard from './components/Dashboard';
-import GameMode from './components/GameMode';
-import WelcomeRules from './components/WelcomeRules';
-import Game from './components/Game';
-import Login from './components/Login';
-import Ranking from './components/Ranking';
+// import Dashboard from './components/Dashboard';
+// import GameMode from './components/GameMode';
+// import WelcomeRules from './components/WelcomeRules';
+// import Game from './components/Game';
+// import Login from './components/Login';
+// import Ranking from './components/Ranking';
 
 function App() {
   return (
     <Provider store={store}>
-      <Header />
-      <Dashboard />
       <BrowserRouter>
+        <Header />
+        {/* <Dashboard /> */}
         <Switch>
-          <Route path="/" exact component={WelcomeRules} />
+          {/* <Route path="/" exact component={WelcomeRules} />
           <Route path="/game-mode" exact component={GameMode} />
           <Route path="/play" exact component={Game} />
           <Route path="/log-in" exact component={Login} />
-          <Route path="/ranking" exact component={Ranking} />
+          <Route path="/ranking" exact component={Ranking} /> */}
         </Switch>
       </BrowserRouter>
     </Provider>
