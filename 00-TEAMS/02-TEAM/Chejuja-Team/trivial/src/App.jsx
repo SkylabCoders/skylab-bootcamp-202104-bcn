ae7172751b6de40e5deeb286eb8c2639fd1c507a
JDSanchon <juanda.sanchon@gmail.com>
2021-05-03 17:04:58 +0200

    Fix conflicts

--- 00-TEAMS/02-TEAM/Chejuja-Team/trivial/src/App.jsx
+++ 00-TEAMS/02-TEAM/Chejuja-Team/trivial/src/App.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import {
-  BrowserRouter, Switch // Route
+  BrowserRouter, Switch, Route // Route
 } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-<<<<<<< HEAD
 import Header from './components/Header';
 // import Dashboard from './components/Dashboard';
 // import GameMode from './components/GameMode';
-=======
 // import Header from './components/Header';
 // import Dashboard from './components/Dashboard';
 import GameMode from './components/GameMode';
->>>>>>> deda3a7920402e3c94a43a13cf2f520a79ae98e8
 // import WelcomeRules from './components/WelcomeRules';
 // import Game from './components/Game';
 // import Login from './components/Login';
@@ -21,20 +18,14 @@ import GameMode from './components/GameMode';
 function App() {
   return (
     <Provider store={store}>
-<<<<<<< HEAD
-=======
       {/* <Header />
       <Dashboard /> */}
->>>>>>> deda3a7920402e3c94a43a13cf2f520a79ae98e8
       <BrowserRouter>
         <Header />
         {/* <Dashboard /> */}
         <Switch>
-<<<<<<< HEAD
           {/* <Route path="/" exact component={WelcomeRules} />
-=======
           {/* <Route path="/" exact component={WelcomeRules} /> */}
->>>>>>> deda3a7920402e3c94a43a13cf2f520a79ae98e8
           <Route path="/game-mode" exact component={GameMode} />
           {/* <Route path="/play" exact component={Game} />
           <Route path="/log-in" exact component={Login} />
