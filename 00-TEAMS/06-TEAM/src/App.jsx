d985c2aa1ef4e7cc61b023b13d032c1e1ea18e21
blopez-dev <pjgl.frontend@gmail.com>
2021-05-03 16:20:49 +0200

    Configured redux

--- 00-TEAMS/06-TEAM/src/App.jsx
+++ 00-TEAMS/06-TEAM/src/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, Switch, Route, Link
+} from 'react-router-dom';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import Movies from './common/components/movies';
 
 function App() {
   return (
     <div className="App">
-      <h1>Team</h1>
+      <Provider store={store}>
+        <Router>
+          <header>
+            <h1>HELLO</h1>
+            <Link to="/countries">Countries</Link>
+          </header>
+          <main>
+            <Switch>
+              <Route path="/countries" component={Movies} />
+            </Switch>
+          </main>
+        </Router>
+      </Provider>
     </div>
   );
 }
