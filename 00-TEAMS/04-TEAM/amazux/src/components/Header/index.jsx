11984d7df52214eca95816add917158f3d67e3c4
cristian-bars <patxi.cristian@gmail.com>
2021-05-03 12:40:01 +0200

    creating header component

---
+++ 00-TEAMS/04-TEAM/amazux/src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import PropTypes from 'prop-types'
+import UserNav from './UserNav/index';
+
+const Header = () => (
+  <header>
+    <UserNav />
+  </header>
+);
+
+Header.propTypes = {
+
+};
+
+export default Header;
