ea396859a6931d1684edfdcb52403d79342b4848
Josep Suarez <djose.anton@gmail.com>
2021-05-03 15:22:33 +0200

    finish gameMode

--- 00-TEAMS/02-TEAM/Chejuja-Team/trivial/src/components/GameMode/index.jsx
+++ 00-TEAMS/02-TEAM/Chejuja-Team/trivial/src/components/GameMode/index.jsx
@@ -1,14 +1,19 @@
 /* eslint-disable no-console */
 import React, { useRef } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import loadGame from '../../redux/reducers/trivialReducer';
 import './gameMode.css';
 
-function GameMode() {
+function GameMode(dispatch) {
   const myRefDifficult = useRef(null);
   const myRefType = useRef(null);
+  function loadingGame(selectDiff, selectType) {
+    return dispatch(loadGame(selectDiff, selectType));
+  }
   return (
     <main className="mode-game">
-      <h1>GAME MODE</h1>
+      <h2>GAME MODE</h2>
       <select className="mode-game__difficult" ref={myRefDifficult}>
         <option value="easy">EASY</option>
         <option value="medium">MEDIUM</option>
@@ -18,10 +23,18 @@ function GameMode() {
         <option value="boolean">BOOLEAN</option>
         <option value="multiple">MULTIPLE</option>
       </select>
-      <button type="button" className="mode-game__accept" onClick={() => loadGame(myRefDifficult, myRefType)}>
-        <img src="https://img.icons8.com/doodle/48/000000/ok.png" alt="OK" />
-      </button>
+      <Link to="./play">
+        <button type="button" className="mode-game__accept" onClick={() => loadingGame(myRefDifficult, myRefType)}>
+          <img src="https://img.icons8.com/doodle/48/000000/ok.png" alt="OK" />
+        </button>
+
+      </Link>
     </main>
   );
 }
-export default GameMode;
+function mapStateToProps(store) {
+  return {
+    game: store.gameData
+  };
+}
+export default connect(mapStateToProps)(GameMode);
