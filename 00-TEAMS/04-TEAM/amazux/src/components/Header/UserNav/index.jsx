409c6c3a626bd08fe9a472b9d726f8c0b7a98299
cristian-bars <patxi.cristian@gmail.com>
2021-05-03 16:13:07 +0200

    Create styles files to User bar

--- 00-TEAMS/04-TEAM/amazux/src/components/Header/UserNav/index.jsx
+++ 00-TEAMS/04-TEAM/amazux/src/components/Header/UserNav/index.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 // import PropTypes from 'prop-types'
+import logo from '../../../images/amazon_logo_RGB.jpg';
+import login from '../../../images/login-white.png';
+import basket from '../../../images/shopping-basket-white.png';
 
 const UserNav = () => (
   <div className="navigation">
     <div className="navigation__img">
-      <img src="../../images/amazon.jpg" alt="" />
+      <img src={logo} alt="company-logo" />
     </div>
     <div className="navigation__search">
       <input type="text" placeholder="serach product" />
+    </div>
+    <div className="navigation__login">
+      <img src={login} alt="Login" />
+    </div>
+    <div className="navigation__basket">
+      <img src={basket} alt="Shopping basket" />
     </div>
   </div>
 
