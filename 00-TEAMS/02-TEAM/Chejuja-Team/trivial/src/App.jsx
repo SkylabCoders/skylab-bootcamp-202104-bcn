690f2d6695bada1d9e0609c182085c3d45166c46
Josep Suarez <djose.anton@gmail.com>
2021-05-03 13:21:57 +0200

    hooks useRef, fixed_

--- 00-TEAMS/02-TEAM/Chejuja-Team/trivial/src/App.jsx
+++ 00-TEAMS/02-TEAM/Chejuja-Team/trivial/src/App.jsx
@@ -4,26 +4,26 @@ import {
 } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import Header from './components/Header';
-import Dashboard from './components/Dashboard';
+// import Header from './components/Header';
+// import Dashboard from './components/Dashboard';
 import GameMode from './components/GameMode';
-import WelcomeRules from './components/WelcomeRules';
-import Game from './components/Game';
-import Login from './components/Login';
-import Ranking from './components/Ranking';
+// import WelcomeRules from './components/WelcomeRules';
+// import Game from './components/Game';
+// import Login from './components/Login';
+// import Ranking from './components/Ranking';
 
 function App() {
   return (
     <Provider store={store}>
-      <Header />
-      <Dashboard />
+      {/* <Header />
+      <Dashboard /> */}
       <BrowserRouter>
         <Switch>
-          <Route path="/" exact component={WelcomeRules} />
+          {/* <Route path="/" exact component={WelcomeRules} /> */}
           <Route path="/game-mode" exact component={GameMode} />
-          <Route path="/play" exact component={Game} />
+          {/* <Route path="/play" exact component={Game} />
           <Route path="/log-in" exact component={Login} />
-          <Route path="/ranking" exact component={Ranking} />
+          <Route path="/ranking" exact component={Ranking} /> */}
         </Switch>
       </BrowserRouter>
     </Provider>
--- 00-TEAMS/02-TEAM/Chejuja-Team/trivial/src/components/GameMode/index.jsx
+++ 00-TEAMS/02-TEAM/Chejuja-Team/trivial/src/components/GameMode/index.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+/* eslint-disable no-console */
+import React, { useRef } from 'react';
+// import loadGame from '../../redux/reducers/trivialReducer';
 
 function GameMode() {
+  const myRefDifficult = useRef(null);
+  const myRefType = useRef(null);
   return (
-    <h1>MODO DE JUEGO</h1>
+    <main className="mode-game">
+      <h1>GAME MODE</h1>
+      <select className="mode-game__difficult" ref={myRefDifficult}>
+        <option value="easy">EASY</option>
+        <option value="medium">MEDIUM</option>
+        <option value="hard">HARD</option>
+      </select>
+      <select className="mode-game__type" ref={myRefType}>
+        <option value="boolean">BOOLEAN</option>
+        <option value="multiple">MULTIPLE</option>
+      </select>
+      <button type="button" onClick={() => console.log(myRefDifficult.current.value, myRefType.current.value)}>
+        <img src="https://img.icons8.com/doodle/48/000000/ok.png" alt="OK" />
+      </button>
+    </main>
   );
 }
 export default GameMode;
