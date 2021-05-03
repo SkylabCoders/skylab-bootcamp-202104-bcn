cd03301c8bb7fe12369302db9769fe5ac7c3e32c
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-05-03 18:56:01 +0200

    Working in css header style

--- 00-TEAMS/03-TEAM/skylart/src/components/Common/Header/index.jsx
+++ 00-TEAMS/03-TEAM/skylart/src/components/Common/Header/index.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
-import logo from '../../../assets/logo.png';
+import logo from '../../../assets/logo.svg';
 import './Header.scss';
 
 function Header() {
   return (
     <header className="header">
-      <ul className="header__left-elements">
-        <img src={logo} alt="logo" />
-        <li className="left-elements">Home</li>
-        <li className="left-elements">
-          <ul>Category</ul>
-          <li className="left-elements__item">Egyptian Art</li>
-          <li className="left-elements__item">Asian Art</li>
-          <li className="left-elements__item">Arts of Africa, Oceania and America</li>
-          <li className="left-elements__item">Euro paintings</li>
-          <li className="left-elements__item">Greek and Roman</li>
-          <li className="left-elements__item">The American Wing</li>
-        </li>
-        <li className="left-elements">Favorites</li>
-      </ul>
-      <ul className="header__right-elements">
-        <li className="right-elements">Login</li>
-      </ul>
+      <div className="header__container">
+        <ul className="container__left-elements">
+          <img src={logo} alt="logo" />
+          <li className="left-elements">Home</li>
+          <li className="left-elements">
+            Category
+            <ul className="left-elements__list">
+              <li className="left-elements__item">Egyptian Art</li>
+              <li className="left-elements__item">Asian Art</li>
+              <li className="left-elements__item">Arts of Africa, Oceania and America</li>
+              <li className="left-elements__item">Euro paintings</li>
+              <li className="left-elements__item">Greek and Roman</li>
+              <li className="left-elements__item">The American Wing</li>
+            </ul>
+          </li>
+          <li className="left-elements">Favorites</li>
+        </ul>
+        <ul className="container__right-elements">
+          <li className="right-elements">Login</li>
+        </ul>
+      </div>
     </header>
   );
 }
