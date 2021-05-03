269e19fafb1e1c9b7ac2b4c24ac847e5563136b5
Laura <laura.sabater.musach@gmail.com>
2021-05-03 20:41:19 +0200

    Change botton tag for Link

--- 00-TEAMS/03-TEAM/skylart/src/components/Common/GoBackButton/index.jsx
+++ 00-TEAMS/03-TEAM/skylart/src/components/Common/GoBackButton/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './GoBack.scss';
 import goBackArrow from '../../../assets/goBackArrow.svg';
 
 function GoBackButton() {
   return (
-    <button className="go-back-button" type="button">
+    <Link to="/" className="go-back-button">
       Go Back
       <img src={goBackArrow} alt="arrow" />
-    </button>
+    </Link>
   );
 }
 
