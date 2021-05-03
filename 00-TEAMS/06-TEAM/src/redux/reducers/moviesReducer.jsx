48763d58ec14bb4c21de756629c5ca23e3b67a28
blopez-dev <pjgl.frontend@gmail.com>
2021-05-03 16:27:46 +0200

    Updated moviesReducer.jsx

--- 00-TEAMS/06-TEAM/src/redux/reducers/moviesReducer.jsx
+++ 00-TEAMS/06-TEAM/src/redux/reducers/moviesReducer.jsx
@@ -4,7 +4,6 @@ function moviesReducer(initialMovies = [], action) {
   let updatedMovies = [...initialMovies];
   switch (action.type) {
     case actionTypes.LOAD_MOVIES:
-      // el countries de [...action.countries] es el countries en loadCountries countries: COUNTRIES
       updatedMovies = [...action.movies];
       break;
     default:
