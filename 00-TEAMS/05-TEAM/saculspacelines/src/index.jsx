f76ca1e5a4c3bd794e3cb12ea6e3a9be58c3316b
Fernando Gomez <shadyfast8.services@gmail.com>
2021-05-03 11:56:47 +0200

    Configured store for the provider

--- 00-TEAMS/05-TEAM/saculspacelines/src/index.jsx
+++ 00-TEAMS/05-TEAM/saculspacelines/src/index.jsx
@@ -4,10 +4,11 @@ import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import configureStore from './redux/store/configureStore';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={configureStore}>
       <App />
     </Provider>
   </React.StrictMode>,
