81085f3db3f112b93196c0e2c0519d8c86c1b49d
cristian-bars <patxi.cristian@gmail.com>
2021-05-03 17:21:44 +0200

    Edit basket shopping

--- 00-TEAMS/04-TEAM/amazux/src/components/Header/UserNav/index.jsx
+++ 00-TEAMS/04-TEAM/amazux/src/components/Header/UserNav/index.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 // import PropTypes from 'prop-types'
 import logo from '../../../images/amazon_logo_RGB.jpg';
 import login from '../../../images/login-white.png';
-import basket from '../../../images/shopping-basket-white.png';
 // import search from '../../../images/search-slim.svg';
 
 const UserNav = () => (
@@ -17,13 +16,14 @@ const UserNav = () => (
       <input type="text" placeholder="Search product" />
     </div>
     <div className="navigation__login">
-      <img src={login} alt="Login" />
+      <Link to="/">
+        <img src={login} alt="Login" />
+      </Link>
     </div>
     <div className="navigation__basket">
-      <img src={basket} alt="Shopping basket" />
+      <button type="button">basket</button>
     </div>
   </div>
-
 );
 
 UserNav.propTypes = {
