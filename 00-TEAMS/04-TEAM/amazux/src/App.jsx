56a2ab1c692047a9a206378a803a810195e9362c
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-05-03 11:48:13 +0200

    Configure and clean initial proyect: - Configure Eslint - Clean autogenerated files - Create main folder structure - Change site title and favicon

---
+++ 00-TEAMS/04-TEAM/amazux/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const title = 'Dashboard';
+  return (
+    <div className="App">
+      <h1>{title}</h1>
+    </div>
+  );
+}
+
+export default App;
