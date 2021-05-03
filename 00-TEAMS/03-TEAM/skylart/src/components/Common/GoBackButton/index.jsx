4d34c7bd55c049bb5620b0ec46e8ac106b204adb
Laura <laura.sabater.musach@gmail.com>
2021-05-03 19:41:01 +0200

    Add goBackArrow vector

--- 00-TEAMS/03-TEAM/skylart/src/components/Common/GoBackButton/index.jsx
+++ 00-TEAMS/03-TEAM/skylart/src/components/Common/GoBackButton/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './GoBack.scss';
+import goBackArrow from '../../../assets/goBackArrow.svg';
 
 function GoBackButton() {
   return (
-    <button className="go-back-button" type="button">Go Back</button>
+    <button className="go-back-button" type="button">
+      Go Back
+      <img src={goBackArrow} alt="arrow" />
+    </button>
   );
 }
 
