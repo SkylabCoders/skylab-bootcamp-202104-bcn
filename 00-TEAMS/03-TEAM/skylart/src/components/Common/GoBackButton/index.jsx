28565bb66efb13489b6257e9ff9f6204644ffcd2
Laura <laura.sabater.musach@gmail.com>
2021-05-03 19:05:15 +0200

    create GoBackButton component with scss

---
+++ 00-TEAMS/03-TEAM/skylart/src/components/Common/GoBackButton/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './GoBack.scss';
+
+function GoBackButton() {
+  return (
+    <button className="go-back-button" type="button">Go Back</button>
+  );
+}
+
+export default GoBackButton;
