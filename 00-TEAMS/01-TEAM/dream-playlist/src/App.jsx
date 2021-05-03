0491925347830afb68478e148cde5047a07260df
gteixi <gemmateixido25@gmail.com>
2021-05-03 14:45:16 +0200

    Link components to dashboard and login.

--- 00-TEAMS/01-TEAM/dream-playlist/src/App.jsx
+++ 00-TEAMS/01-TEAM/dream-playlist/src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import
+{
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 import Dashboard from './components/dashboard';
+import LogIn from './components/login';
 import './App.css';
 
 function App() {
   return (
-    <Dashboard />
+    <>
+      <Router>
+        <Link to="/" exact>Dashboard</Link>
+        <Link to="/login">LogIn</Link>
+        <Switch>
+          <Route path="/login" component={LogIn} />
+          <Route path="/" component={Dashboard} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ 00-TEAMS/01-TEAM/dream-playlist/src/components/login/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function LogIn() {
+  return (
+    <h3>This is the log-in</h3>
+  );
+}
+
+export default LogIn;
