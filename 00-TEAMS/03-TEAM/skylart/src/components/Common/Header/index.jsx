40a635fd212e2a9d8ec18d79b19ac82b98706372
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-05-03 20:20:41 +0200

    Finish header and hover display

--- 00-TEAMS/03-TEAM/skylart/src/components/Common/Header/index.jsx
+++ 00-TEAMS/03-TEAM/skylart/src/components/Common/Header/index.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import logo from '../../../assets/logo.svg';
+import iconLogin from '../../../assets/iconLogin.svg';
 import './Header.scss';
 
 function Header() {
   return (
     <header className="header">
-      <div className="header__container">
+      <div className="container">
         <ul className="container__left-elements">
           <img src={logo} alt="logo" />
           <li className="left-elements">Home</li>
           <li className="left-elements">
             Category
+            {' '}
+            <span className="arrow-category">▼</span>
             <ul className="left-elements__list">
               <li className="left-elements__item">Egyptian Art</li>
               <li className="left-elements__item">Asian Art</li>
@@ -23,7 +26,10 @@ function Header() {
           <li className="left-elements">Favorites</li>
         </ul>
         <ul className="container__right-elements">
-          <li className="right-elements">Login</li>
+          <li className="right-elements">
+            Login
+            <img src={iconLogin} alt="login" />
+          </li>
         </ul>
       </div>
     </header>
