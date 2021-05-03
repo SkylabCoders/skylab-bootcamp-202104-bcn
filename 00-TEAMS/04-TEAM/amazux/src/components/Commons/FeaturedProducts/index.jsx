3896018a58ea01fe03931ca41971c1e9838cfbf9
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-05-03 22:22:29 +0200

    Connect products from store to feacturedProducts component & add CSS styles

--- 00-TEAMS/04-TEAM/amazux/src/components/Commons/FeaturedProducts/index.jsx
+++ 00-TEAMS/04-TEAM/amazux/src/components/Commons/FeaturedProducts/index.jsx
@@ -1,10 +1,28 @@
+/* eslint-disable no-underscore-dangle */
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import './featuredProducts.css';
 
 const FeaturedProducts = ({ products }) => (
   <section className="feactured-products">
     {
-        products.length ? (<h2>Hay products</h2>) : (<h2>No hay products</h2>)
+        products.length ? (
+          <div className="product-list">
+            {products.slice(0, 4).map((product) => (
+              <div className="product" key={product._id}>
+                <Link to={`/products/${product._id}`}>
+                  <h3>{product.name}</h3>
+                  <img src={product.img.url} alt={product.name} />
+                  <p>
+                    {`${product.cost} €`}
+                  </p>
+                </Link>
+              </div>
+            ))}
+          </div>
+
+        ) : (<h2>No hay products</h2>)
     }
   </section>
 );
