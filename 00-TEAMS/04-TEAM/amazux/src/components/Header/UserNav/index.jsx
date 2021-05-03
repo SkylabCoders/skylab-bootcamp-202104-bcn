38915f130d881ed6c8d22de5490ef1ffa01496db
cristian-bars <patxi.cristian@gmail.com>
2021-05-03 18:49:32 +0200

    Edit styles User bar

--- 00-TEAMS/04-TEAM/amazux/src/components/Header/UserNav/index.jsx
+++ 00-TEAMS/04-TEAM/amazux/src/components/Header/UserNav/index.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 // import PropTypes from 'prop-types'
-import logo from '../../../images/amazon_logo_RGB.jpg';
-import login from '../../../images/login-white.png';
 // import search from '../../../images/search-slim.svg';
 
 const UserNav = () => (
   <nav className="navigation">
-    <div className="navigation__img">
+    <div className="navigation__contentImg">
       <Link to="/">
-        <img src={logo} alt="company-logo" />
+        <div className="navigation__img" />
       </Link>
+    </div>
+
+    <div className="navigation__location">
+      Enviar a Joseluis
     </div>
     <div className="navigation__search">
       <input type="text" placeholder="Search product" />
     </div>
-    <div className="navigation__login">
-      <Link to="/">
-        <img src={login} alt="Login" />
-      </Link>
-    </div>
+    <Link to="/">
+      <div className="navigation__login" />
+    </Link>
     <div className="navigation__cart">
       <button type="button">Cart</button>
     </div>
