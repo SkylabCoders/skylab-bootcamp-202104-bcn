b2c831e9d0eec1496f79f99034ab1246be853f6a
JDSanchon <juanda.sanchon@gmail.com>
2021-05-04 11:04:30 +0200

    Add rules and index style

--- 00-TEAMS/02-TEAM/Chejuja-Team/trivial/src/components/WelcomeRules/index.jsx
+++ 00-TEAMS/02-TEAM/Chejuja-Team/trivial/src/components/WelcomeRules/index.jsx
@@ -3,15 +3,13 @@ import './welcomeRules.css';
 
 function WelcomeRules() {
   return (
-    <main>
-      <p className="welcomeRules">
+    <main className="main">
+      <p className="main-text">
         Bienvenidos al Trivial de CHEJUJA S.A!
         Dale a jugar , indica la dificultad, el tipo de preguntas y disfruta de la experiencia.
         Podrás alcanzar la primera posición en nuestro ranking?
-
         <br />
         CHEJUJA mejor que el tren de la bruja!
-
       </p>
     </main>
 
