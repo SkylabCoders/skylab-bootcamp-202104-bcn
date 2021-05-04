d914ccab64973a70083646374f7e767301bf27fa
CheCabeza <chemartinc@gmail.com>
2021-05-04 10:20:45 +0200

    Made some style changes on game component

--- 00-TEAMS/02-TEAM/Chejuja-Team/trivial/src/components/Game/index.jsx
+++ 00-TEAMS/02-TEAM/Chejuja-Team/trivial/src/components/Game/index.jsx
@@ -3,14 +3,20 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
+import './game.css';
 
 function Game({ game }) {
   console.log(game);
+  function decodeHtml(html) {
+    const txt = document.createElement('textarea');
+    txt.innerHTML = html;
+    return txt.value;
+  }
   return (
-    <main className="mode-game">
-      {
-        game.length ? <p>{game[0][0].question}</p> : <p>Cargando</p>
-      }
+    <main className="game">
+      <div className="question">
+        {game.length ? <p>{decodeHtml(game[0][0].question)}</p> : <p>Cargando</p>}
+      </div>
     </main>
   );
 }
