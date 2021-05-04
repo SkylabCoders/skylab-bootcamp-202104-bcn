d215f223e1a0210761119aaef50f9ae42157856b
Gaspar Rodriguez <gaspar.unlp@gmail.com>
2021-05-04 20:28:58 +0200

    Add not working logo

--- 00-TEAMS/08-TEAM/src/components/Header/index.jsx
+++ 00-TEAMS/08-TEAM/src/components/Header/index.jsx
@@ -6,7 +6,7 @@ import SearchInput from '../SearchInput';
 const Header = () => (
   <nav>
     <Link className="nav-element" to="/">
-      <img className="nav-icon" src="logo.svg" alt="icon" />
+      <img className="nav-icon" src="../../assets" alt="icon" />
     </Link>
     <Link className="nav-element" to="/">Dashboard</Link>
     <Link className="nav-element" to="/global">Global</Link>
