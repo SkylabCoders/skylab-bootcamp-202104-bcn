f8f538d7bfe362405eecdc55255c35a0bd3cff7c
cristian-bars <patxi.cristian@gmail.com>
2021-05-04 10:11:51 +0200

    Refactoring style files & edit footer bar

---
+++ 00-TEAMS/04-TEAM/amazux/src/components/Footer/FooterBar/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const FooterBar = () => (
+  <>
+    <div className="footer__links">
+      <div className="footer__item">
+        Conocenos
+      </div>
+      <div className="footer__item">
+        Nuestros productos
+      </div>
+      <div className="footer__item">
+        Siguenos
+      </div>
+      <div className="footer__item">
+        Contacto
+      </div>
+    </div>
+  </>
+);
+
+FooterBar.propTypes = {
+
+};
+
+export default FooterBar;
