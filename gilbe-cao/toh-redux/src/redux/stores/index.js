aa288d0622806ddacc3c3df4e5d686a307f31879
Gilberto Cao <gilbe.cao@gmail.com>
2021-05-04 11:26:15 +0200

    Add redux thunk and compose

--- gilbe-cao/toh-redux/src/redux/stores/index.js
+++ gilbe-cao/toh-redux/src/redux/stores/index.js
@@ -1,8 +1,12 @@
-import { createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
+  composeEnhancers(applyMiddleware(thunk)),
 );
 
 export default store;
