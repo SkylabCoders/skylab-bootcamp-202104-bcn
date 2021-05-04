ac08202a0c5933a550bd149f7faac30ec0892aab
Josep Suarez <djose.anton@gmail.com>
2021-05-04 19:44:37 +0200

    adding effects to click on aboutus

--- 00-TEAMS/02-TEAM/Chejuja-Team/trivial/src/components/About-us/index.jsx
+++ 00-TEAMS/02-TEAM/Chejuja-Team/trivial/src/components/About-us/index.jsx
@@ -12,20 +12,20 @@ function AboutUs() {
         <div className="about-us__info--chema">
           <img src="https://img.icons8.com/color/96/000000/superman-dc.png" alt="chemaHero" />
           <p>Chema</p>
-          <a href="https://github.com/CheCabeza" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/fluent/48/000000/github.png" alt="github" /></a>
-          <a href="https://www.instagram.com/chemartinc/" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png" alt="instagram" /></a>
+          <a href="https://github.com/CheCabeza" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/fluent/48/000000/github.png" alt="github" className="about-us__info--socialmedia" /></a>
+          <a href="https://www.instagram.com/chemartinc/" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png" alt="instagram" className="about-us__info--socialmedia" /></a>
         </div>
         <div className="about-us__info--juanda">
           <img src="https://img.icons8.com/color/96/000000/deadpool.png" alt="juandaHero" />
           <p>Juanda</p>
-          <a href="https://github.com/JDSanchon" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/fluent/48/000000/github.png" alt="github" /></a>
-          <a href="https://www.instagram.com/juandadesafio/" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png" alt="instagram" /></a>
+          <a href="https://github.com/JDSanchon" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/fluent/48/000000/github.png" alt="github" className="about-us__info--socialmedia" /></a>
+          <a href="https://www.instagram.com/juandadesafio/" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png" alt="instagram" className="about-us__info--socialmedia" /></a>
         </div>
         <div className="about-us__info--chema">
           <img src="https://img.icons8.com/color/96/000000/hawkeye.png" alt="josepHero" />
           <p>Josep</p>
-          <a href="https://github.com/jasuarez21" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/fluent/48/000000/github.png" alt="github" /></a>
-          <a href="https://www.instagram.com/jasuarez21/" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png" alt="instagram" /></a>
+          <a href="https://github.com/jasuarez21" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/fluent/48/000000/github.png" alt="github" className="about-us__info--socialmedia" /></a>
+          <a href="https://www.instagram.com/jasuarez21/" target="_blank" rel="noreferrer"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png" alt="instagram" className="about-us__info--socialmedia" /></a>
         </div>
       </section>
       <div className="about-us__footer">
