17d9cfecc75ae81e2e378512dcacac2959b2a97f
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-05-04 19:21:34 +0200

    Add carousel in home page

--- 00-TEAMS/06-TEAM/src/pages/home/Home.jsx
+++ 00-TEAMS/06-TEAM/src/pages/home/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import MostViwedMovies from '../../common/components/slider-most-viewed/MostViwedMovies';
 
 const Home = () => (
-  <h1>Home</h1>
+  <>
+    <h1>Home</h1>
+    <MostViwedMovies />
+  </>
 );
 
 export default Home;
