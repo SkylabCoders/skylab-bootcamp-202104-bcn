1055b60a3485932cad7bb18ced120009e228ac3f
Anna Sala <anna.sala.mercade@gmail.com>
2021-05-04 12:45:36 +0200

    connect auth0 provider

--- 00-TEAMS/03-TEAM/skylart/src/App.jsx
+++ 00-TEAMS/03-TEAM/skylart/src/App.jsx
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import {
   BrowserRouter, Switch, Route, Redirect,
 } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 import configureStore from './redux/store';
 import Header from './components/Common/Header';
 import Home from './components/Home';
@@ -14,21 +15,26 @@ import './styles/styles.scss';
 
 function App() {
   return (
-    <Provider store={configureStore}>
-
-      <BrowserRouter>
-        <Header />
-        <LogIn />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/category" component={Category} />
-          <Route path="/detail/:artworkId" component={Detail} />
-          <Route path="/favorites" component={Favorites} />
-          <Route path="/login" component={LogIn} />
-          <Redirect to="/" />
-        </Switch>
-      </BrowserRouter>
-    </Provider>
+    <Auth0Provider
+      domain="dev-ovw36gxc.eu.auth0.com"
+      clientId="j3N82SWz5nKFRVUsRsaaesV95fEChY0Q"
+      redirectUri={window.location.origin}
+    >
+      <Provider store={configureStore}>
+        <BrowserRouter>
+          <Header />
+          <LogIn />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/category" component={Category} />
+            <Route path="/detail/:artworkId" component={Detail} />
+            <Route path="/favorites" component={Favorites} />
+            <Route path="/login" component={LogIn} />
+            <Redirect to="/" />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
+    </Auth0Provider>
   );
 }
 
