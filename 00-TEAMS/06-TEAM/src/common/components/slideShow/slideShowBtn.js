b8cb1e55d569d94e3c37b6c2c127a7d4b8d6ee78
Hosni <hosni.marco@gmail.com>
2021-05-04 02:28:14 +0200

    Clean comments

--- 00-TEAMS/06-TEAM/src/common/components/slideShow/slideShowBtn.js
+++ 00-TEAMS/06-TEAM/src/common/components/slideShow/slideShowBtn.js
@@ -1,15 +1,6 @@
-// if (slider !== null) {
-//   const sliderSection = document.querySelectorAll('.slider__section');
-//   const sliderSectionLast = sliderSection[sliderSection.length - 1];
-//   slider.insertAdjacentElement('afterbegin', sliderSectionLast);
-// }
 function next(position) {
-  //   // eslint-disable-next-line no-debugger
-  // eslint-disable-next-line no-debugger
-  debugger;
   const slider = document.querySelector('#slider');
   const sliderSectionFirst = document.querySelectorAll('.slider__section')[0];
-  //   if (slider !== undefined) {
   switch (position) {
     case 'RIGHT':
       slider.style.marginLeft = '-200%';
@@ -27,7 +18,6 @@ function next(position) {
     slider.insertAdjacentElement('beforeend', sliderSectionFirst);
     slider.style.marginLeft = '-100%';
   }, 500);
-//   }
 }
 
 export default next;
