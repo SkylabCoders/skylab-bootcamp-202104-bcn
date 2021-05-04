feec0a9831a3161a6c52ad68fc72a81f4016a745
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-05-04 12:42:07 +0200

    Add oAuth provider

--- 00-TEAMS/04-TEAM/amazux/src/index.jsx
+++ 00-TEAMS/04-TEAM/amazux/src/index.jsx
@@ -2,18 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/store';
+import App from './App';
+import './index.css';
 
+// TODO Move credentiales to .env
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>
+    <Auth0Provider
+      domain="fredyzz.eu.auth0.com"
+      clientId="G7hYqsbR0ORjzsSs2IHUiZ5tWd91QfRS"
+      redirectUri={window.location.origin}
+    >
+      <Provider store={store}>
+        <Router>
+          <App />
+        </Router>
+      </Provider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
