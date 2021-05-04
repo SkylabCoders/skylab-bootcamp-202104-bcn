60fd4bd841b29bdd21ee90bbaad5de2c2e0798d6
albertmasats <albertmasats@gmail.com>
2021-05-04 18:06:29 +0200

    Create header template

---
+++ 00-TEAMS/01-TEAM/dream-playlist/src/components/header/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+function Header() {
+  return (
+    <header>
+      <div>
+        img
+      </div>
+      <ul>
+        <li>Name</li>
+        <li>photo</li>
+        <li>Logout</li>
+      </ul>
+    </header>
+  );
+}
+
+Header.propTypes = {
+
+};
+
+export default Header;
