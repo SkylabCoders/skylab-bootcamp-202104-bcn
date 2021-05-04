1516abff8607b9776285e35853ca00f63a2befd5
cristian-bars <patxi.cristian@gmail.com>
2021-05-04 18:50:26 +0200

    Create pagination products in dashboard

--- 00-TEAMS/04-TEAM/amazux/src/components/Commons/ProductsList/index.jsx
+++ 00-TEAMS/04-TEAM/amazux/src/components/Commons/ProductsList/index.jsx
@@ -1,20 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Loader from '../Loader/index';
+import './productList.css';
 
-const ProductList = ({ products }) => (
-  <div className="product-list">
-    {
+const ProductList = ({ products, productsToShow }) => {
+  const [initialPosition, setInitialPosition] = useState(0);
+  const [lastPosition, setLastPosition] = useState(productsToShow);
+
+  const prev = () => {
+    setInitialPosition(initialPosition - (productsToShow * 2));
+    setLastPosition(lastPosition - (productsToShow * 2));
+  };
+
+  const next = () => {
+    setInitialPosition(initialPosition + (productsToShow * 2));
+    setLastPosition(lastPosition + (productsToShow * 2));
+  };
+
+  // const list = productsToShow;
+  // const min = 0;
+  return (
+    <div className="product-list">
+      <button type="button" onClick={prev}>{'<'}</button>
+      {
       products.length ? (
-        <div className="product-list">Hay productos</div>
+        <div className="products">
+          <ul className="products__list">
+            {products.slice(initialPosition, lastPosition).map((element) => (
+              <li key={element.name} className="products__item">
+                <img className="products__img" src={element.img.url} alt={element.name} />
+                <div className="products__details">
+                  <span className="products__name">{element.name}</span>
+                  <span className="products__price">
+                    {element.cost}
+                    {' '}
+                    €
+                  </span>
+                </div>
+              </li>
+            ))}
+          </ul>
+          <ul className="products__list">
+            {products.slice(initialPosition + productsToShow, lastPosition + productsToShow)
+              .map((element) => (
+                <li key={element.name} className="products__item">
+                  <img className="products__img" src={element.img.url} alt={element.name} />
+                  <div className="products__details">
+                    <span className="products__name">{element.name}</span>
+                    <span className="products__price">
+                      {element.cost}
+                      {' '}
+                      €
+                    </span>
+                  </div>
+                </li>
+              ))}
+          </ul>
+        </div>
       ) : <Loader />
     }
-  </div>
-);
+      <button type="button" onClick={next}>{'>'}</button>
+    </div>
+  );
+};
 
 ProductList.propTypes = {
-  products: PropTypes.shape([]).isRequired
-  // productsToShow: PropTypes.number
+  products: PropTypes.shape([]).isRequired,
+  productsToShow: PropTypes.number.isRequired
 };
 
 export default ProductList;
--- 00-TEAMS/04-TEAM/amazux/src/redux/store/index.js
+++ 00-TEAMS/04-TEAM/amazux/src/redux/store/index.js
@@ -1,9 +1,10 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducers from '../reducers';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
-  rootReducers, applyMiddleware(thunk)
+  rootReducers, composeEnhancers(applyMiddleware(thunk))
 );
 
 export default store;
