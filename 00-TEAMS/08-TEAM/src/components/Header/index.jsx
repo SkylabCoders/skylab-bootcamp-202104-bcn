cc482df1c76523a696f7bc8396a05a51b64d4447
Gaspar Rodriguez <gaspar.unlp@gmail.com>
2021-05-04 20:22:19 +0200

    SASS and logo added

--- 00-TEAMS/08-TEAM/src/components/Header/index.jsx
+++ 00-TEAMS/08-TEAM/src/components/Header/index.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './style.scss';
+import SearchInput from '../SearchInput';
 
 const Header = () => (
   <nav>
-    <Link to="/">Dashboard</Link>
-    <Link to="/global">Global</Link>
-    <Link to="/mysavedplaces">My Saved Places</Link>
-    <Link to="/advices">Advices</Link>
-    <Link to="/login">Login</Link>
+    <Link className="nav-element" to="/">
+      <img className="nav-icon" src="logo.svg" alt="icon" />
+    </Link>
+    <Link className="nav-element" to="/">Dashboard</Link>
+    <Link className="nav-element" to="/global">Global</Link>
+    <Link className="nav-element" to="/mysavedplaces">My Saved Places</Link>
+    <Link className="nav-element" to="/advices">Advices</Link>
+    <SearchInput />
+    <Link className="nav-element" to="/login">Login</Link>
   </nav>
 );
 
