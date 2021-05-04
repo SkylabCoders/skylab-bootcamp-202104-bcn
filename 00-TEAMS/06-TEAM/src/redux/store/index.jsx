827e13d1b42169801958de93a1f93d3fa24562a5
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-05-04 18:30:00 +0200

    Correct index.jsx from store folder

--- 00-TEAMS/06-TEAM/src/redux/store/index.jsx
+++ 00-TEAMS/06-TEAM/src/redux/store/index.jsx
@@ -1,10 +1,14 @@
-import { createStore, applyMiddleware } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../reducers';
 
-const store = createStore(rootReducer, composeWithDevTools(
-  applyMiddleware(thunk)
-));
+const composeEnhancers = (typeof window !== 'undefined'
+&& window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)
+|| compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 export default store;
