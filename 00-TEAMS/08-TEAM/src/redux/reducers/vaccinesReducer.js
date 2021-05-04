95597d2d1e4dbc80f64467618a4a792cad3f4dfd
Gaspar Rodriguez <gaspar.unlp@gmail.com>
2021-05-04 20:13:49 +0200

    Add correct file

---
+++ 00-TEAMS/08-TEAM/src/redux/reducers/vaccinesReducer.js
@@ -0,0 +1,15 @@
+import actionTypes from '../actions/actionTypes';
+
+const VaccinesReducers = (initialVaccinesContinentData = {}, action) => {
+  let updatedVaccinesContinent;
+  switch (action.type) {
+    case actionTypes.LOAD_VACCINES:
+      updatedVaccinesContinent = action.data;
+      break;
+    default:
+      updatedVaccinesContinent = initialVaccinesContinentData;
+      break;
+  }
+  return updatedVaccinesContinent;
+};
+export default VaccinesReducers;
