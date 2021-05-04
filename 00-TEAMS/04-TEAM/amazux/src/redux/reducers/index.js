b69f052579f5d19b65b90e9f2f467fcba3a541a3
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-05-04 11:38:59 +0200

    Create auth reducer & actionCreator

--- 00-TEAMS/04-TEAM/amazux/src/redux/reducers/index.js
+++ 00-TEAMS/04-TEAM/amazux/src/redux/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import user from './userReducer';
 import products from './productsReducer';
 import error from './errorReducer';
+import auth from './authReducer';
 
 const rootReducer = combineReducers({
   user,
+  auth,
   error,
   products
 });
