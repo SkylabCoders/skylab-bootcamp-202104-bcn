b4b746ee679a1caa7fc65b973b125371d2377775
Gaspar Rodriguez <gaspar.unlp@gmail.com>
2021-05-04 19:53:30 +0200

    Delete wrong changes

--- 00-TEAMS/08-TEAM/src/components/Country/index.jsx
+++ 00-TEAMS/08-TEAM/src/components/Country/index.jsx
@@ -39,7 +39,6 @@ function Country({ dispatch, countryData, vaccineByCountryData }) {
     <>
       <div>
         <h1>{country}</h1>
-<<<<<<< HEAD
         <ul>
           {myCountryStats.map((value) => (
             <li key={`${value}`}>
@@ -54,32 +53,21 @@ function Country({ dispatch, countryData, vaccineByCountryData }) {
             </li>
           ))}
         </ul>
-=======
-
->>>>>>> Add isabel files
       </div>
     </>
   );
 }
 
 Country.propTypes = {
-<<<<<<< HEAD
   dispatch: PropTypes.func.isRequired,
   countryData: PropTypes.shape({}).isRequired,
   vaccineByCountryData: PropTypes.shape({}).isRequired
-=======
-  dispatch: PropTypes.func.isRequired
->>>>>>> Add isabel files
 };
 
 function mapStateToProps(store) {
   return {
-<<<<<<< HEAD
     countryData: store.countryData,
     vaccineByCountryData: store.vaccineByCountryData
-=======
-    tasks: store.tasks
->>>>>>> Add isabel files
   };
 }
 
