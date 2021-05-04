794180b3e28b2ffec4bae1db20c2e74671817bae
Hosni <hosni.marco@gmail.com>
2021-05-04 11:08:18 +0200

    Feature Slide, carusel, button with redux

--- 00-TEAMS/06-TEAM/src/common/components/slideShow/slideShow.js
+++ 00-TEAMS/06-TEAM/src/common/components/slideShow/slideShow.js
@@ -7,48 +7,38 @@ import {
   ButtonNext
 } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
-
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import loadMovies from '../../../redux/actions/actionsCreator';
 import './style.css';
-import next from './slideShowBtn';
 
 function slideShow({ myMovies, dispatch }) {
-  const baseImgUrl = 'https://image.tmdb.org/t/p/w780';
+  const baseImgUrl = 'https://image.tmdb.org/t/p/original';
   useEffect(() => {
     dispatch(loadMovies());
   }, []);
 
-  // return (
-  //   <section className="container-slider">
-  //     <div className="slider" id="slider">
-  //       {
-  //       myMovies.slice(0, 4).map((element) => (
-  //         <div key={element.id} className="slider__section">
-  //           <img className="slider__image" key={element.id} src={`${baseImgUrl}${element.poster_path}`} alt="" />
-  //         </div>
-  //       ))
-  //     }
-  //     </div>
-  //     <button
-  //       type="button"
-  //       onClick={() => next('RIGHT')}
-  //       className="slider__btn slider__btn--right"
-  //       id="btn-right"
-  //     >
-  //       &#62;
-  //     </button>
-  //     <button
-  //       type="button"
-  //       onClick={() => next('LEFT')}
-  //       className="slider__btn slider__btn--left"
-  //       id="btn-left"
-  //     >
-  //       &#60;
-  //     </button>
-  //   </section>
-  // );
+  return (
+    <CarouselProvider
+      naturalSlideWidth={1000}
+      naturalSlideHeight={650}
+      totalSlides={4}
+      visibleSlides={1}
+      currentSlide={0}
+    >
+      <Slider>
+        { myMovies.slice(0, 4).map((element) => (
+          <Slide key={element.id} className="slider__section">
+            <Link to={`${element.id}`}><img className="slider__image" key={element.id} src={`${baseImgUrl}${element.backdrop_path}`} alt="" /></Link>
+            {' '}
+          </Slide>
+        ))}
+      </Slider>
+      <ButtonBack>&#60;</ButtonBack>
+      <ButtonNext>&#62;</ButtonNext>
+    </CarouselProvider>
+  );
 }
 
 slideShow.propTypes = {
