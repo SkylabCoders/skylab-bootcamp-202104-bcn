704515ca763a487c0bb214d227cfb1a6d3f5a22a
ulldecorb <ausias.bertran@gmail.com>
2021-05-04 02:17:34 +0200

    Add icons on sidenav

--- 00-TEAMS/04-TEAM/amazux/src/components/Header/CategoriesNav/SideNav/index.jsx
+++ 00-TEAMS/04-TEAM/amazux/src/components/Header/CategoriesNav/SideNav/index.jsx
@@ -3,13 +3,17 @@ import PropTypes from 'prop-types';
 import './sidenav.css';
 
 const SideNav = ({ closeSideNav }) => (
-  <>
+  <div className="sidenav-box">
+    <div className="sidenav__blackground"> </div>
     <div id="sidenav" className="sidenav">
       <div className="sidenav__user-bar">
-        {/* <img clasName="sidenav__user-icon" /> */}
-        Hola User
+        <div className="sidenav__user-icon"> </div>
+        <div clasName="sidenav__user-welcome">
+          Hola, User
+        </div>
       </div>
-      <ul>
+      <menu className="sidenav__menu-header">Menu</menu>
+      <ul className="sidenav__menu-list">
         <li>Lorem ipsum dolor sit amet</li>
         <li>Maiores a quia, non quo debitis</li>
         <li>blanditiis fugiat voluptates</li>
@@ -17,7 +21,7 @@ const SideNav = ({ closeSideNav }) => (
       </ul>
     </div>
     <button type="button" className="sidenav__close-button" onClick={closeSideNav}>×</button>
-  </>
+  </div>
 );
 
 SideNav.propTypes = {
