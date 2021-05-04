bc663ba90688a53c3ee079a767f52f5337131b3d
albertmasats <albertmasats@gmail.com>
2021-05-04 19:13:43 +0200

    Add styles to header

--- 00-TEAMS/01-TEAM/dream-playlist/src/components/header/index.jsx
+++ 00-TEAMS/01-TEAM/dream-playlist/src/components/header/index.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import './index-header.css';
 // import PropTypes from 'prop-types';
 
 function Header() {
   return (
     <header>
-      <div>
-        img
+      <div className="icon">
+        <span />
       </div>
-      <ul>
-        <li>Name</li>
-        <li>photo</li>
-        <li>Logout</li>
-      </ul>
+      <div className="header-container">
+        <ul className="header-list">
+          <li>Name</li>
+          <li>photo</li>
+          <li>Logout</li>
+        </ul>
+      </div>
     </header>
   );
 }
