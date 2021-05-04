c46fad2fd72b3f61283dd5e8e1f28d454da38a77
Edgar Alandete <edgalandete@gmail.com>
2021-05-04 18:51:12 +0200

    Remove space

--- 00-TEAMS/01-TEAM/dream-playlist/src/redux/store/index.js
+++ 00-TEAMS/01-TEAM/dream-playlist/src/redux/store/index.js
@@ -1,6 +1,5 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-
 import rootReducer from '../reducers';
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
