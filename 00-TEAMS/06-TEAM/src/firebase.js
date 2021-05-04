116aa0102078dfabbb4b7c9c95f6a9234804e664
blopez-dev <pjgl.frontend@gmail.com>
2021-05-04 11:13:36 +0200

    upadted composer

--- 00-TEAMS/06-TEAM/src/firebase.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: 'AIzaSyA4SpUjU6y5TPTVRvtOf9tCUCMvDu2N1k0',
-  authDomain: 'skyfilms-team-6.firebaseapp.com',
-  projectId: 'skyfilms-team-6',
-  storageBucket: 'skyfilms-team-6.appspot.com',
-  messagingSenderId: '961529725949',
-  appId: '1:961529725949:web:324ceac7ce6eb41da18f94'
-};
