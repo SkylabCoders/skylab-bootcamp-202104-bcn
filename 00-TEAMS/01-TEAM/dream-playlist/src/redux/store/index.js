3eecec41485b8d59547f9e2878f67aa37059e604
gteixi <gemmateixido25@gmail.com>
2021-05-04 11:26:32 +0200

    Add composeEnhancer in store.

--- 00-TEAMS/01-TEAM/dream-playlist/src/redux/store/index.js
+++ 00-TEAMS/01-TEAM/dream-playlist/src/redux/store/index.js
@@ -1,10 +1,12 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 import rootReducer from '../reducers';
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
-  rootReducer, applyMiddleware(thunk)
+  rootReducer, composeEnhancer(applyMiddleware(thunk));
 );
 
 export default store;
