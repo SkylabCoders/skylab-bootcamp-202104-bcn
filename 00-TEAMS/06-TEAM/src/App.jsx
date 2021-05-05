18b675d0f4cbd6c33e8dd1d5194abf3bed84e926
Hosni <hosni.marco@gmail.com>
2021-05-05 09:44:56 +0200

    Updated package json

--- 00-TEAMS/06-TEAM/src/App.jsx
+++ 00-TEAMS/06-TEAM/src/App.jsx
@@ -1,32 +1,21 @@
 import React from 'react';
-import {
-  BrowserRouter as Router, Switch, Route, Link
-} from 'react-router-dom';
 import './App.css';
 import { Provider } from 'react-redux';
+import { Auth0Provider } from '@auth0/auth0-react';
 import store from './redux/store';
-import Movies from './common/components/movies';
-import slideShow from './common/components/slideShow/slideShow';
+import HeaderLayout from './application/components/layout/Layout';
 
 function App() {
   return (
-    <div className="App">
+    <Auth0Provider
+      domain="blopez-devv.eu.auth0.com"
+      clientId="6LYLNXO67WSQsZKKBqxW4TZPjTL9waa7"
+      redirectUri={window.location.origin}
+    >
       <Provider store={store}>
-        <Router>
-          <header>
-            <h1>HELLO</h1>
-            <Link to="/countries">Countries</Link>
-          </header>
-          <main>
-            <Switch>
-              <Route path="/" component={slideShow} />
-              <Route path="/countries" component={Movies} />
-            </Switch>
-          </main>
-        </Router>
+        <HeaderLayout />
       </Provider>
-    </div>
+    </Auth0Provider>
   );
 }
-
 export default App;
