0dbb78fbd36b35847143cac120e7ff7db8a10f8f
albertmasats <albertmasats@gmail.com>
2021-05-05 09:58:35 +0200

    Create all header structure

--- 00-TEAMS/01-TEAM/dream-playlist/src/components/header/index.jsx
+++ 00-TEAMS/01-TEAM/dream-playlist/src/components/header/index.jsx
@@ -1,26 +1,21 @@
 import React from 'react';
 import './index-header.css';
-// import PropTypes from 'prop-types';
 
 function Header() {
   return (
     <header>
-      <div className="icon">
-        <span />
-      </div>
       <div className="header-container">
+        <div className="icon">
+          <span />
+        </div>
         <ul className="header-list">
           <li>Name</li>
-          <li>photo</li>
+          <li />
           <li>Logout</li>
         </ul>
       </div>
     </header>
   );
 }
-
-Header.propTypes = {
-
-};
 
 export default Header;
