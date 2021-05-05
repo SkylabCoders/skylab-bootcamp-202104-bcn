9ed8831744e605427d5a57d95f276c8f1953730c
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-05-05 17:34:08 +0200

    Move test-utils to src

---
+++ 00-TEAMS/04-TEAM/amazux/src/components/Dashboard/Dashboard.test.js
@@ -0,0 +1,36 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Dashboard from './index';
+import { render, screen } from '../../test-utils';
+
+test('should return a div with class products__list', () => {
+  const { container } = render(<Dashboard />, {
+    initialState: {
+      products: [
+        {
+          img: {
+            url: 'https://picsum.photos/200',
+            hdUrl: 'https://picsum.photos/1000'
+          },
+          _id: 1,
+          name: 'Iphone 8',
+          cost: 800,
+          category: 'Cellphones'
+        },
+        {
+          img: {
+            url: 'https://picsum.photos/200',
+            hdUrl: 'https://picsum.photos/1000'
+          },
+          _id: 2,
+          name: 'Iphone 10',
+          cost: 1200,
+          category: 'Cellphones'
+        }
+      ]
+    }
+  });
+
+  expect(screen.getByText(/Slider/i)).toBeInTheDocument();
+});
