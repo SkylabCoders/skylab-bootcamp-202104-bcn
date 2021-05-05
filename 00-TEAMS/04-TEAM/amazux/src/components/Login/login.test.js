b45f3123ca3f850fbdd334e105786bba0e7cc236
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-05-05 15:12:58 +0200

    Test login

--- 00-TEAMS/04-TEAM/amazux/src/components/Login/login.test.js
+++ 00-TEAMS/04-TEAM/amazux/src/components/Login/login.test.js
@@ -1,22 +1,46 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 import Login from './index';
-import { render, screen } from './test-utils';
+import {
+  render, screen, fireEvent
+} from './test-utils';
+
+jest.mock('@auth0/auth0-react');
+
+const user = {
+  email: 'johndoe@me.com',
+  email_verified: true,
+  sub: 'google-oauth2|12345678901234'
+};
 
 describe('Login Component', () => {
-  test('should contain pepe', () => {
+  beforeEach(() => {
+    useAuth0.mockReturnValue({
+      isAuthenticated: false,
+      user,
+      logout: jest.fn(),
+      loginWithRedirect: jest.fn()
+    });
+  });
+
+  test('should contain Welcome', () => {
     render(<Login />, {
       initialState: {
         auth: {
           isLoggedIn: true,
-          user: { given_name: 'pepe' }
+          user: {
+            given_name: 'Fredy',
+            picture: './../images/amazon_logo_RGB.jpg'
+          }
         }
       }
     });
 
-    expect(screen.getByText(/pepe/i)).toBeInTheDocument();
+    expect(screen.getByText(/Welcome/i)).toBeInTheDocument();
   });
-  test('should contain stranger', () => {
+
+  test('should contain Please login', () => {
     render(<Login />, {
       initialState: {
         auth: {
@@ -26,5 +50,26 @@ describe('Login Component', () => {
     });
 
     expect(screen.getByText(/Please login/i)).toBeInTheDocument();
+  });
+  test('...', () => {
+    const { getAllByRole } = render(<Login />, {
+      initialState: {
+        auth: {
+          isLoggedIn: true,
+          user: {
+            given_name: 'asdasd',
+            picture: 'asdasd'
+          }
+        }
+      }
+    });
+
+    // const btn = ----
+    //    fireEvent.click(btn);
+
+    const { logout } = useAuth0();
+    const btn = getAllByRole('button')[0];
+    fireEvent.click(btn);
+    expect(logout).toHaveBeenCalled();
   });
 });
