cdf1d6273dbdbae0684a75a245161b9b8318c2e1
Isabel Gomez <belasi87@gmail.com>
2021-05-05 19:47:36 +0200

    Create function to get  Americas data, summing dates from North and South America

--- 00-TEAMS/08-TEAM/src/components/Map/map.jsx
+++ 00-TEAMS/08-TEAM/src/components/Map/map.jsx
@@ -1,31 +1,38 @@
 /* eslint-disable no-console */
 import React from 'react';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
 import { Chart } from 'react-google-charts';
 
-const Map = () => (
-  <>
-    <div style={{ display: 'flex', maxWidth: 900 }}>
-      <Chart
-        width="500px"
-        height="300px"
-        chartType="GeoChart"
-        data={[
-          ['Region Code', 'Continent', 'People vaccinated', 'People partially vaccinated'],
-          ['142', 'Asia', 200, 200],
-          ['150', 'Europe', 300, 300],
-          ['019', 'Americas', 400, 400],
-          ['009', 'Oceania', 600, 600],
-          ['002', 'Africa', 700, 700]
-        ]}
-        options={{
-          resolution: 'continents',
-          colors: ['#FAB06C', '#FF3939']
-        }}
-        mapsApiKey={process.env.REACT_APP_GOOGLE_MAP_KEY}
-        rootProps={{ 'data-testid': '1' }}
-      />
-    </div>
-  </>
-);
+const Map = ({ vaccinesContinentData }) => {
+  console.log(vaccinesContinentData);
+  return (
+    <>
+      <div style={{ display: 'flex', maxWidth: 900 }}>
+        <Chart
+          width="500px"
+          height="300px"
+          chartType="GeoChart"
+          data={vaccinesContinentData}
+          options={{
+            resolution: 'continents',
+            colors: ['#FAB06C', '#FF3939'],
+            backgroundColor: 'transparent'
+          }}
+          mapsApiKey={process.env.REACT_APP_GOOGLE_MAP_KEY}
+          rootProps={{ 'data-testid': '1' }}
+        />
+      </div>
+    </>
+  );
+};
 
-export default Map;
+Map.propTypes = {
+  vaccinesContinentData: PropTypes.shape([]).isRequired
+};
+
+const mapStateToProps = ({ vaccinesContinentData }) => ({
+  vaccinesContinentData
+});
+
+export default connect(mapStateToProps)(Map);
--- 00-TEAMS/08-TEAM/src/redux/reducers/vaccinesReducer.js
+++ 00-TEAMS/08-TEAM/src/redux/reducers/vaccinesReducer.js
@@ -1,11 +1,11 @@
 import actionTypes from '../actions/actionTypes';
 
 const VaccinesReducers = (initialVaccinesContinentData = [], action) => {
-  let updatedVaccinesContinent = [['Region Code', 'Continent', 'People vaccinated', 'People partially vaccinated']];
+  const chartHeader = [['Region Code', 'Continent', 'People vaccinated', 'People partially vaccinated']];
+  let updatedVaccinesContinent;
   switch (action.type) {
-    case actionTypes.LOAD_VACCINES:
-      updatedVaccinesContinent.push(action.data);
-      break;
+    case actionTypes.LOAD_VACCINES_MAP:
+      return [...chartHeader, ...action.data];
     default:
       updatedVaccinesContinent = initialVaccinesContinentData;
       break;
