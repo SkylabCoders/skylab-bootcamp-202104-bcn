ab5b3d6217c6eb27fbeca34799bb489f528d8bb3
Edgar Alandete <edgalandete@gmail.com>
2021-05-05 16:06:46 +0200

    Remove comments from test-utils

--- 00-TEAMS/01-TEAM/dream-playlist/src/components/form/dropdown/test-utils.js
+++ 00-TEAMS/01-TEAM/dream-playlist/src/components/form/dropdown/test-utils.js
@@ -1,10 +1,8 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-filename-extension */
-// test-utils.js
 import React from 'react';
 import { render as rtlRender } from '@testing-library/react';
 import { Provider } from 'react-redux';
-// Import your own reducer
 import configureStore from '../../../redux/store';
 
 function render(
@@ -20,7 +18,5 @@ function render(
   return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
 }
 
-// re-export everything
 export * from '@testing-library/react';
-// override render method
 export { render };
