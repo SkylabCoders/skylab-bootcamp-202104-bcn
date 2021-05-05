bc1b26a72ec94ab0127346e5c1b7858d6b3ca56f
cristian-bars <patxi.cristian@gmail.com>
2021-05-05 16:29:55 +0200

    Testing UserNav & footercopyright nav

---
+++ 00-TEAMS/04-TEAM/amazux/src/components/Header/UserNav/userNav.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render, screen } from './test-utils';
+import UserNav from './index';
+
+describe('UserNav Component', () => {
+  test('has an tag text', () => {
+    // eslint-disable-next-line react/jsx-filename-extension
+    const { container } = render(<UserNav />);
+    const asd = container.querySelector('.navigation__location-name');
+    expect(asd.textContent).toBe('Enviar a Joseluis');
+  });
+
+  test('has an nav tag', () => {
+    // eslint-disable-next-line react/jsx-filename-extension
+    render(<UserNav />);
+    expect(screen.getByText(/enviar/i)).toBeInTheDocument();
+  });
+});
