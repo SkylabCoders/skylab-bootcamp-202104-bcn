bc0149c15894b9ea18c3fc09cdd7b449c1292929
Edgar Alandete <edgalandete@gmail.com>
2021-05-05 17:04:53 +0200

    Change const store to function store

--- 00-TEAMS/01-TEAM/dream-playlist/src/redux/store/index.js
+++ 00-TEAMS/01-TEAM/dream-playlist/src/redux/store/index.js
@@ -4,8 +4,8 @@ import rootReducer from '../reducers';
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(
-  rootReducer, composeEnhancer(applyMiddleware(thunk))
+const configureStore = (initialState) => createStore(
+  rootReducer, initialState, composeEnhancer(applyMiddleware(thunk))
 );
 
-export default store;
+export default configureStore;
