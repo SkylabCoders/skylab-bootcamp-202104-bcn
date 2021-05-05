9e76d58d419c3846d42a0fdbe79fff168a29eaad
cristian-bars <patxi.cristian@gmail.com>
2021-05-05 16:29:55 +0200

    Testing UserNav & footercopyright nav

---
+++ 00-TEAMS/04-TEAM/amazux/src/components/Footer/FooterCopyright/footerCopyright.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render, screen } from '../test-utils';
+import FooterCopyright from './index';
+
+describe('UserNav Component', () => {
+  test('has an tag text', () => {
+    // eslint-disable-next-line react/jsx-filename-extension
+    const { container } = render(<FooterCopyright />);
+    const asd = container.querySelector('.footer__copyrigth');
+    expect(asd.textContent).toBe('@TheFAC team - Amazux');
+  });
+
+  test('has an copyright text', () => {
+    // eslint-disable-next-line react/jsx-filename-extension
+    render(<FooterCopyright />);
+    expect(screen.getByText(/@/i)).toBeInTheDocument();
+  });
+});
