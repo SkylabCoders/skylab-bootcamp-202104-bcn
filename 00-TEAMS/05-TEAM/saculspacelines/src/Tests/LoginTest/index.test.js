a8db692e12400af798d61698b3b4cfe533552845
Fernando Gomez <shadyfast8.services@gmail.com>
2021-05-05 16:28:29 +0200

    Provider envolving Login test

--- 00-TEAMS/05-TEAM/saculspacelines/src/Tests/LoginTest/index.test.js
+++ 00-TEAMS/05-TEAM/saculspacelines/src/Tests/LoginTest/index.test.js
@@ -1,17 +1,23 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
+import { Provider } from 'react-redux';
 import { render } from '../TestUtils';
 import Login from '../../Components/Login';
+import configureStore from '../../redux/store/configureStore';
 
 describe('Given a Login component', () => {
   test('Should print Login section with a loggedOutTemplate when loged out', () => {
-    const { container } = render(<Login />, {
-      initialState: {
-        auth: {
-          isLoggedIn: false
+    const { container } = render(
+      <Provider store={configureStore()}>
+        <Login />
+      </Provider>, {
+        initialState: {
+          auth: {
+            isLoggedIn: false
+          }
         }
       }
-    });
+    );
 
     const loginLogOutTitle = container.querySelector('.auth__title');
     expect(loginLogOutTitle.textContent).toBe('Sacul spacelines');
