9d64fc5ba657108d29e0af4844bf7b1f03ed6006
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-05-05 19:41:50 +0200

    Create loadCategoryFromApi test, before install mock

--- 00-TEAMS/03-TEAM/skylart/src/redux/actions/actionCretators.test.js
+++ 00-TEAMS/03-TEAM/skylart/src/redux/actions/actionCretators.test.js
@@ -1,5 +1,11 @@
-import * as actions from './actionCreators';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import fetchMock from 'fecth-mock';
 import actionTypes from './actionTypes';
+import * as actions from './actionCreators';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 describe('actions', () => {
   it('should create an action to load a detail', () => {
@@ -23,5 +29,24 @@ describe('actions', () => {
       type: actionTypes.AUTH_LOGOUT,
     };
     expect(actions.logout()).toEqual(expectedAction);
+  });
+});
+
+describe('async actions', () => {
+  afterEach(() => {
+    fetchMock.restore();
+  });
+  it('creates LOAD_ARTWORKS when async await has been done', () => {
+    const url = 'https://collectionapi.metmuseum.org/public/collection/v1/departments';
+    fetchMock.getOnce('/departments', {
+      artwork: data.departments,
+    });
+    const expectedActions = [
+      { type: actionTypes.LOAD_ARTWORKS, artwork: data.department },
+    ];
+    const store = mockStore({ artworks: [] });
+    return store.dispatch(actions.loadArtworksFromApi(url)).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
   });
 });
