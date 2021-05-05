cc44a04cc058031d520d9a8484af2f80adaee842
Emerson Fernandes <emerson_feral@hotmail.com>
2021-05-06 00:05:11 +0200

    reworking styles and connecting component with local user

--- 00-TEAMS/05-TEAM/saculspacelines/src/Components/WishList/index.jsx
+++ 00-TEAMS/05-TEAM/saculspacelines/src/Components/WishList/index.jsx
@@ -1,30 +1,33 @@
 import React from 'react';
-import Avatar from '../../Images/dorin.png';
 import './style/style.css';
+import USERS from '../../redux/store/userList';
+
+const { username, avatar, wishlist } = USERS[0];
+const { planet, starship, price } = wishlist[0];
 
 function WishList() {
   return (
     <div>
       <div className="header-container">
-        <img src={Avatar} alt="avatar" className="avatar" />
+        <img src={avatar} alt="avatar" className="avatar" />
         <div className="header-container__item">
-          <h2>User</h2>
+          <h2>{username}</h2>
         </div>
       </div>
-      <div className="button-container">
-        <button type="button" className="button-container__item-changename">Change name</button>
-        <button type="button" className="button-container__item-changefaction">Change Faction</button>
+      <div className="change-container">
+        <button type="button" className="change-container__item">Change name</button>
+        <button type="button" className="change-container__item">Change Faction</button>
       </div>
       <div className="main-container">
         <h3 className="main-container__title">Wishlist</h3>
         <div className="wishlist-container">
           <tr className="wishlist-container__tablerow">
             <td className="wishlist-container__tabledata">
-              planet
+              {planet}
             </td>
-            <td>starship</td>
+            <td>{starship}</td>
             <td>
-              price
+              {price}
               {' '}
               <button type="button" className="wishlist-container__button">modify</button>
               {' '}
