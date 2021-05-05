f498f279cc08a322b1ba09e5a993a8e5f33bea7f
Hosni <hosni.marco@gmail.com>
2021-05-05 10:24:26 +0200

    Update slide-show

--- 00-TEAMS/06-TEAM/src/App.jsx
+++ 00-TEAMS/06-TEAM/src/App.jsx
@@ -3,12 +3,7 @@ import './App.css';
 import { Provider } from 'react-redux';
 import { Auth0Provider } from '@auth0/auth0-react';
 import store from './redux/store';
-<<<<<<< HEAD
-import Movies from './common/components/movies';
-import slideShow from './common/components/slideShow/slideShow';
-=======
 import HeaderLayout from './application/components/layout/Layout';
->>>>>>> 116aa0102078dfabbb4b7c9c95f6a9234804e664
 
 function App() {
   return (
@@ -18,22 +13,7 @@ function App() {
       redirectUri={window.location.origin}
     >
       <Provider store={store}>
-<<<<<<< HEAD
-        <Router>
-          <header>
-            <h1>HELLO</h1>
-            <Link to="/countries">Countries</Link>
-          </header>
-          <main>
-            <Switch>
-              <Route path="/" component={slideShow} />
-              <Route path="/countries" component={Movies} />
-            </Switch>
-          </main>
-        </Router>
-=======
         <HeaderLayout />
->>>>>>> 116aa0102078dfabbb4b7c9c95f6a9234804e664
       </Provider>
     </Auth0Provider>
   );
