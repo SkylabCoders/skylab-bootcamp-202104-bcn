f3bf0ae66019bc6ef228e2aea58e9070f7fee63a
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-05-05 15:33:16 +0200

    Add the second banner carousel, all from the same films page from URL

--- 00-TEAMS/06-TEAM/src/pages/home/Home.jsx
+++ 00-TEAMS/06-TEAM/src/pages/home/Home.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import MostViwedMovies from '../../common/components/slider-most-viewed/MostViwedMovies';
 import SlideShow from '../../common/components/slide-show/slideShow';
+import './home.css';
 
 const Home = () => (
   <>
     <SlideShow />
-    <MostViwedMovies />
+    <div className="banner-container">
+      <div className="banner-container__first">
+        <h1>TEMP</h1>
+        <MostViwedMovies />
+      </div>
+      <div className="banner-container__second">
+        <h1>TEMP</h1>
+        <MostViwedMovies />
+      </div>
+    </div>
   </>
 );
 
