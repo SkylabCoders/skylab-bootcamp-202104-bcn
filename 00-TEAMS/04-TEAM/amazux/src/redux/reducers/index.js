d5bac135dce85a81a3951211190475f4f5e63f04
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-05-06 10:30:08 +0200

    Delete unnecessary files

--- 00-TEAMS/04-TEAM/amazux/src/redux/reducers/index.js
+++ 00-TEAMS/04-TEAM/amazux/src/redux/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
-import user from './userReducer';
 import products from './productsReducer';
 import error from './errorReducer';
 import auth from './authReducer';
 
 const rootReducer = combineReducers({
-  user,
   auth,
   error,
   products
