7ad76e13fab6909220fe0f05454e35957d3ee570
Isabel Gomez <belasi87@gmail.com>
2021-05-06 18:31:00 +0200

    Change button name to Login/Logout

--- 00-TEAMS/08-TEAM/src/components/Header/index.jsx
+++ 00-TEAMS/08-TEAM/src/components/Header/index.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
 import './style.scss';
 import SearchInput from '../SearchInput';
 import logo from '../../assets/logoC19.png';
 import userAvatar from '../../assets/user-avatar.png';
 
-const Header = () => (
-  <nav>
-    <Link className="nav-element" to="/">
-      <img className="nav-icon" src={logo} alt="icon" />
-    </Link>
-    <Link className="nav-element" to="/global">Global</Link>
-    <Link className="nav-element" to="/mysavedplaces">My Saved Places</Link>
-    <Link className="nav-element" to="/advices">Advices</Link>
-    <SearchInput />
-    <Link className="nav-element" to="/login">
-      Login
-      <img className="user-avatar" src={userAvatar} alt="icon" />
-    </Link>
-  </nav>
-);
+const Header = () => {
+  const { isAuthenticated } = useAuth0();
+  return (
+    <nav>
+      <Link className="nav-element" to="/">
+        <img className="nav-icon" src={logo} alt="icon" />
+      </Link>
+      <Link className="nav-element" to="/global">Global</Link>
+      <Link className="nav-element" to="/mysavedplaces">My Saved Places</Link>
+      <Link className="nav-element" to="/advices">Advices</Link>
+      <SearchInput />
 
-export default Header;
+      <Link className="nav-element" to="/login">
+        { !isAuthenticated ? 'Login' : 'Logout'}
+        <img className="user-avatar" src={userAvatar} alt="icon" />
+      </Link>
+    </nav>
+  );
+};
+
+export default (Header);
