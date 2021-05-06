e9dcb127688615fbba64c3accc0464e75ba89655
Anna Sala <anna.sala.mercade@gmail.com>
2021-05-06 15:33:46 +0200

    home component test passed

--- 00-TEAMS/03-TEAM/skylart/src/redux/store/index.js
+++ 00-TEAMS/03-TEAM/skylart/src/redux/store/index.js
@@ -2,13 +2,14 @@ import { applyMiddleware, createStore, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
-function configureStore() {
+function configureStore(initialState) {
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
   || compose;
   return createStore(
     rootReducer,
+    initialState,
     composeEnhancers(applyMiddleware(thunk)),
   );
 }
 
-export default configureStore();
+export default configureStore;
