a892374af8ba76310a7235c50d390cc79ba701c5
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-05-06 19:16:31 +0200

    Add all info from the details

--- 00-TEAMS/06-TEAM/src/pages/movie-details/MovieDetails.jsx
+++ 00-TEAMS/06-TEAM/src/pages/movie-details/MovieDetails.jsx
@@ -7,21 +7,47 @@ import { getMovieById } from '../../application/store/actions/actionsCreator';
 
 function MovieDetail({ selectedMovie, dispatch }) {
   const { movieId } = useParams();
-
   useEffect(() => {
     dispatch(getMovieById(movieId));
   }, [movieId]);
 
-  console.log({ selectedMovie });
+  console.log(selectedMovie);
+  const baseImgUrl = 'https://image.tmdb.org/t/p/w200/';
 
   return (
     selectedMovie.id
       ? (
         <div>
-          MOVIE ID:
-          {selectedMovie.id}
-          ORIGINAL TITLE:
-          {selectedMovie.original_title}
+          <span>
+            TITULO:
+            {selectedMovie.title}
+          </span>
+          <br />
+          <span>
+            TITULO ORIGINAL:
+            {selectedMovie.original_title}
+          </span>
+          <br />
+          <span>
+            GENERO:
+            {selectedMovie.genres.map((element) => element.name)}
+          </span>
+          <br />
+          <span>
+            DURACION:
+            {selectedMovie.runtime}
+          </span>
+          <br />
+          <span>
+            RESUMEN:
+            {selectedMovie.overview}
+          </span>
+          <br />
+          <span>
+            BANNER:
+            <br />
+            <img className="slider__img" src={`${baseImgUrl}${selectedMovie.backdrop_path}`} alt="" />
+          </span>
         </div>
       )
       : (
@@ -38,7 +64,17 @@ MovieDetail.propTypes = {
   dispatch: PropTypes.func.isRequired,
   selectedMovie: PropTypes.shape({
     id: PropTypes.number,
-    original_title: PropTypes.string
+    original_title: PropTypes.string,
+    overview: PropTypes.string,
+    genres: PropTypes.shape({
+      map: PropTypes.func.isRequired,
+      id: PropTypes.number,
+      name: PropTypes.string
+    }),
+    runtime: PropTypes.number,
+    title: PropTypes.string,
+    backdrop_path: PropTypes.string
+
   }).isRequired
 };
 
