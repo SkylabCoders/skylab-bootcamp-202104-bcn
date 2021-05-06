706c173fce6713fec792c721f9d7dcd8ea107dee
Josep Suarez <djose.anton@gmail.com>
2021-05-06 18:18:42 +0200

    ranking function OK

--- 00-TEAMS/02-TEAM/Chejuja-Team/trivial/src/components/Ranking/index.jsx
+++ 00-TEAMS/02-TEAM/Chejuja-Team/trivial/src/components/Ranking/index.jsx
@@ -7,35 +7,61 @@ import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
 import './ranking.css';
 import Confeti from './confeti';
+import rankingDefault from '../../services/rankingDefault';
 
 function Ranking({ auth, ranking }) {
+  const arrayToPush = {
+    name: auth.user.name,
+    correctAnswers: ranking[0].correctAnswers,
+    incorrectAnswers: ranking[0].incorrectAnswers
+  };
+  const newArray = [...rankingDefault, arrayToPush];
+  newArray.sort((a, b) => {
+    if (a.correctAnswers > b.correctAnswers) {
+      return -1;
+    }
+    if (a.correctAnswers > b.correctAnswers) {
+      return 1;
+    }
+    return 0;
+  });
   return (
     <main className="ranking-game">
       <Confeti />
       <div className="ranking-game__avatar">
         <img src={`${auth.user.picture}`} alt="avatar" className="ranking-game__avatar--photoUser" />
-        {console.log(auth)}
-        {console.log(ranking[0].incorrectAnswers)}
         <p className="ranking-game__text">{`Enhorabuena ${auth.user.name}, has acertado ${ranking[0].correctAnswers} y fallado ${ranking[0].incorrectAnswers}`}</p>
       </div>
       <div className="ranking-game__container-gif">
         <Link to="/">
           <img src="https://i.pinimg.com/originals/15/32/04/1532041b7388db634ff593b7d6d7e8bc.gif" alt="brujaGif" />
-          <p>BACK</p>
         </Link>
+        <p className="backButton">BACK</p>
       </div>
       <div className="ranking-game__podium">
         <div className="ranking-game__podium--second">
           <img src="https://img.icons8.com/doodle/48/000000/second-place-ribbon.png" alt="second" />
-          <p>JOSEP</p>
+          <p>{`${newArray[1].name}`}</p>
+          <p>
+            {' '}
+            {`${newArray[1].correctAnswers} aciertos`}
+          </p>
         </div>
         <div className="ranking-game__podium--first">
           <img src="https://img.icons8.com/doodle/48/000000/first-place-ribbon--v1.png" alt="first" />
-          <p>Chejuja</p>
+          <p>{`${newArray[0].name}`}</p>
+          <p>
+            {' '}
+            {`${newArray[0].correctAnswers} aciertos`}
+          </p>
         </div>
         <div className="ranking-game__podium--third">
           <img src="https://img.icons8.com/doodle/48/000000/third-place-ribbon.png" alt="third" />
-          <p>Chema</p>
+          <p>{`${newArray[2].name}`}</p>
+          <p>
+            {' '}
+            {`${newArray[2].correctAnswers} aciertos`}
+          </p>
         </div>
 
       </div>
--- 00-TEAMS/02-TEAM/Chejuja-Team/trivial/src/services/rankingDefault.js
+++ 00-TEAMS/02-TEAM/Chejuja-Team/trivial/src/services/rankingDefault.js
@@ -1,13 +1,17 @@
-export default [
+const rankingDefault = [
   {
     name: 'Chema',
-    correctAnswers: 3
+    correctAnswers: 3,
+    incorretAnswers: 7
   },
   {
     name: 'Juanda',
-    correctAnswers: 2
+    correctAnswers: 2,
+    incorretAnswers: 8
   },
   {
-    name: 'Josep',
-    correctAnswers: 1
+    name: 'Nil',
+    correctAnswers: 7,
+    incorretAnswers: 3
   }];
+export default rankingDefault;
