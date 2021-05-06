5628831ae491a6c2506358937f5b6f07bd7a962a
Daniel Martorell <dani.martorell@gmail.com>
2021-05-06 15:59:50 +0200

    Create historyGraph component with fake data

--- 00-TEAMS/08-TEAM/src/components/HistoryGraph/index.jsx
+++ 00-TEAMS/08-TEAM/src/components/HistoryGraph/index.jsx
@@ -1,23 +1,52 @@
 /* eslint-disable no-console */
-import React from 'react';
+import { React, useState } from 'react';
 import { connect } from 'react-redux';
-import { PropTypes } from 'prop-types';
+// import { PropTypes } from 'prop-types';
 import './style.scss';
+import { Line } from 'react-chartjs-2';
 
-const HistoryGraph = ({ globalData }) => (
-  <section className="global-graphs">
-    <h2>HistoryGraph component</h2>
-    <p>
-      Confirmed:
-      {' '}
-      {globalData}
-    </p>
-  </section>
-);
+const HistoryGraph = () => {
+  const [state] = useState(
+    {
+      labels: ['January', 'February', 'March',
+        'April', 'May'],
+      datasets: [
+        {
+          label: 'Rainfall',
+          fill: false,
+          lineTension: 0.5,
+          backgroundColor: 'rgba(75,192,192,1)',
+          borderColor: 'rgba(0,0,0,1)',
+          borderWidth: 2,
+          data: [65, 59, 80, 81, 56]
+        }
+      ]
+    }
+  );
 
-HistoryGraph.propTypes = {
-  globalData: PropTypes.shape([]).isRequired
+  return (
+    <section className="global-graphs">
+      <Line
+        data={state}
+        options={{
+          title: {
+            display: true,
+            text: 'Average Rainfall per month',
+            fontSize: 20
+          },
+          legend: {
+            display: true,
+            position: 'right'
+          }
+        }}
+      />
+    </section>
+  );
 };
+
+// HistoryGraph.propTypes = {
+//   globalData: PropTypes.shape([]).isRequired
+// };
 
 const mapStateToProps = ({ globalData }) => (
   {
