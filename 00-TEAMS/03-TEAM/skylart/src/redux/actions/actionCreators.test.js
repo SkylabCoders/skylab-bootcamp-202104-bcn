20b397b0a3c166712d09d3601b262214a7e98653
Laura <laura.sabater.musach@gmail.com>
2021-05-06 16:31:23 +0200

    Add loadCategory test to actionCreators test file and reorder test

--- 00-TEAMS/03-TEAM/skylart/src/redux/actions/actionCreators.test.js
+++ 00-TEAMS/03-TEAM/skylart/src/redux/actions/actionCreators.test.js
@@ -1,42 +1,13 @@
 import axios from 'axios';
 import actionTypes from './actionTypes';
 import * as actions from './actionCreators';
-import { loadArtworksFromApi } from './actionCreators';
+import { loadArtworksFromApi, loadCategory } from './actionCreators';
 
 jest.mock('axios');
 
-describe('actions', () => {
-  it('should create an action to load a detail', () => {
-    const detail = {};
-    const expectedAction = {
-      type: actionTypes.LOAD_DETAIL,
-      detail,
-    };
-    expect(actions.loadDetail(detail)).toEqual(expectedAction);
-  });
-  test('should return action', () => {
-    const detail = { name: 'ArtWorkName' };
-    const expectedAction = {
-      type: actionTypes.LOAD_DETAIL,
-      detail,
-    };
-    expect(actions.loadDetail(detail)).toEqual(expectedAction);
-  });
-  it('should create an action to login user', () => {
-    const user = 'Laura';
-    const expectedAction = {
-      type: actionTypes.AUTH_LOGIN,
-      user,
-    };
-    expect(actions.login(user)).toEqual(expectedAction);
-  });
-  it('should create an action to logout', () => {
-    const expectedAction = {
-      type: actionTypes.AUTH_LOGOUT,
-    };
-    expect(actions.logout()).toEqual(expectedAction);
-  });
+// Test Anna
 
+describe('loadArtworksFromApi function', () => {
   test('should dispatch LOAD_ARTWORKS', async () => {
     axios.mockResolvedValue({ data: { objectIDs: 10 } });
     const dispatch = jest.fn();
@@ -48,6 +19,7 @@ describe('actions', () => {
       artwork: 10,
     });
   });
+
   test('should dispatch LOAD_ERROR', async () => {
     axios.mockRejectedValue();
     const dispatch = jest.fn();
@@ -56,5 +28,52 @@ describe('actions', () => {
     expect(dispatch).toHaveBeenCalledWith({
       type: 'LOAD_ERROR',
     });
+  });
+});
+
+describe('loadCategory function', () => {
+  test('should dispatch LOAD_CATEGORY', async () => {
+    axios.mockResolvedValue({ data: ['hello world'] });
+    const dispatch = jest.fn();
+    const artworkId = 10;
+
+    await loadCategory(artworkId)(dispatch);
+
+    expect(dispatch).toHaveBeenCalledWith({
+      type: actionTypes.LOAD_CATEGORY,
+      detail: ['hello world'],
+      artworkId: 10,
+    });
+  });
+});
+
+describe('loadDetail function', () => {
+  test('should return action', () => {
+    const detail = { name: 'ArtWorkName' };
+    const expectedAction = {
+      type: actionTypes.LOAD_DETAIL,
+      detail,
+    };
+    expect(actions.loadDetail(detail)).toEqual(expectedAction);
+  });
+});
+
+describe('login function', () => {
+  test('should create an action to login user', () => {
+    const user = 'Laura';
+    const expectedAction = {
+      type: actionTypes.AUTH_LOGIN,
+      user,
+    };
+    expect(actions.login(user)).toEqual(expectedAction);
+  });
+});
+
+describe('logout function', () => {
+  test('should create an action to logout', () => {
+    const expectedAction = {
+      type: actionTypes.AUTH_LOGOUT,
+    };
+    expect(actions.logout()).toEqual(expectedAction);
   });
 });
