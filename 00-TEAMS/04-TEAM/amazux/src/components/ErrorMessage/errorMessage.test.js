b3a6f6149a5100f3a0bf08ca12dfc558b47a0ed2
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-05-06 09:44:41 +0200

    ModifyErrorMessage test

--- 00-TEAMS/04-TEAM/amazux/src/components/ErrorMessage/errorMessage.test.js
+++ 00-TEAMS/04-TEAM/amazux/src/components/ErrorMessage/errorMessage.test.js
@@ -1,12 +1,29 @@
+/* eslint-disable no-console */
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import {
-  render, screen
+  render, screen, fireEvent
 } from './test-utils';
 import ErrorMessage from './index';
+import { deleteError } from '../../redux/actions/actionCreators';
 
-test('should return an error message', () => {
-  render(<ErrorMessage message="Hola Josep" />);
+jest.mock('../../redux/actions/actionCreators');
 
-  expect(screen.getByText(/Hola Josep/i)).toBeInTheDocument();
+beforeEach(() => {
+  deleteError.mockReturnValue(jest.fn);
+});
+
+describe('<ErrorMessage/>', () => {
+  test('should return an error message', () => {
+    render(<ErrorMessage message="this is an error" />);
+    expect(screen.getByText(/this is an error/i)).toBeInTheDocument();
+  });
+
+  test('should call deleteError actionCreator', () => {
+    const { getAllByRole } = render(<ErrorMessage message="Hola Josep" />);
+    const btn = getAllByRole('button')[0];
+    // console.log(btn);
+    fireEvent.click(btn);
+    expect(deleteError).toHaveBeenCalled();
+  });
 });
