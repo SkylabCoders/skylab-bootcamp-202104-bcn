3356e7e76f271a00db6a6aa796aaaefe4a718825
Isabel Gomez <belasi87@gmail.com>
2021-05-06 18:03:46 +0200

    Show login in My Saved Places if the user is not logged

--- 00-TEAMS/08-TEAM/src/components/Mysavedplaces/index.jsx
+++ 00-TEAMS/08-TEAM/src/components/Mysavedplaces/index.jsx
@@ -2,19 +2,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+
 import SearchInput from '../SearchInput';
 import './style.scss';
 
 const MySavedPlaces = ({ favoriteCountry }) => {
-  console.log(favoriteCountry);
+  const history = useHistory();
+  const { isAuthenticated } = useAuth0();
+
+  if (!isAuthenticated) {
+    history?.push('/login');
+  }
 
   return (
     <>
       <div className="mySavedPlaces-cointainer">
         <h1>My Saved Places</h1>
         <SearchInput className="mySavedPlaces-cointainer__searchInput" />
-
         <div className="countriesSaved-cointainer">
           <ul>
             { favoriteCountry.map((element) => (
