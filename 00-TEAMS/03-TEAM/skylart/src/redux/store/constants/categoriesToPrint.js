0f9f603dd35af1c5b9bcd8da6db811f0d7facce8
Anna Sala <anna.sala.mercade@gmail.com>
2021-05-06 19:38:15 +0200

    updated constant for categories to Print

--- 00-TEAMS/03-TEAM/skylart/src/redux/store/constants/categoriesToPrint.js
+++ 00-TEAMS/03-TEAM/skylart/src/redux/store/constants/categoriesToPrint.js
@@ -1,5 +1,34 @@
 const categoriesToPrint = [
-  1, 5, 6, 10, 11, 13,
+  {
+    id: 1,
+    imageUrl: '',
+    department: 'American Decorative Arts',
+  },
+  {
+    id: 5,
+    imageUrl: '',
+    department: 'Arts of Africa, Oceania, and the Americas',
+  },
+  {
+    id: 6,
+    imageUrl: '',
+    department: 'Asian Art',
+  },
+  {
+    id: 10,
+    imageUrl: '',
+    department: 'Egyptian Art',
+  },
+  {
+    id: 11,
+    imageUrl: '',
+    department: 'European Paintings',
+  },
+  {
+    id: 13,
+    imageUrl: '',
+    department: 'Greek and Roman Art',
+  },
 ];
 
 export default categoriesToPrint;
