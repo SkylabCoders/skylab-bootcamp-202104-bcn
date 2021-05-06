b35d9bc464337d2cacbfe44b31297dc08d1a7d80
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-05-06 13:52:27 +0200

    Create Product test

--- 00-TEAMS/04-TEAM/amazux/src/components/Products/index.jsx
+++ 00-TEAMS/04-TEAM/amazux/src/components/Products/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-// import PropTypes from 'prop-types';
 
 const Products = () => {
   const { id } = useParams();
@@ -15,10 +14,6 @@ const Products = () => {
       </h3>
     </section>
   );
-};
-
-Products.propTypes = {
-
 };
 
 export default Products;
