fda8b035ccd7779a4a31610af00a3a04ff0c6112
Laura <laura.sabater.musach@gmail.com>
2021-05-07 13:24:14 +0200

    Delete comments from index

--- 00-TEAMS/03-TEAM/skylart/src/index.jsx
+++ 00-TEAMS/03-TEAM/skylart/src/index.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import configureStore from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
