ad93f8fada5392748ddca095d6df6424891006b9
Laura <laura.sabater.musach@gmail.com>
2021-05-07 12:59:55 +0200

    Delete import favorites from rootReducer

--- 00-TEAMS/03-TEAM/skylart/src/redux/reducers/index.js
+++ 00-TEAMS/03-TEAM/skylart/src/redux/reducers/index.js
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux';
 import artworks from './artworksReducer';
 import category from './categoryReducer';
-import favorites from './favoritesReducer';
 import detail from './detailReducer';
 import auth from './authReducer';
 
 const rootReducer = combineReducers({
   artworks,
-  favorites,
   detail,
   category,
   auth,
