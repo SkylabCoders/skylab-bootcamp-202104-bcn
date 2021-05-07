1646783443224a8fadd384f34df13753fb7be1c7
Anna Sala <anna.sala.mercade@gmail.com>
2021-05-07 12:01:10 +0200

    fix categories page for fetching to api

--- 00-TEAMS/03-TEAM/skylart/src/components/Category/index.jsx
+++ 00-TEAMS/03-TEAM/skylart/src/components/Category/index.jsx
@@ -3,22 +3,29 @@ import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router';
+import categoriesToPrint from '../../redux/store/constants/categoriesToPrint';
 import { loadArtworksFromApi, loadCategory } from '../../redux/actions/actionCreators';
 import './Category.scss';
 
+let alreadyCalledId = [2];
 function Category({ dispatch, artworks, category }) {
   const { categoryId } = useParams();
+  const departmentProp = categoriesToPrint.find((element) => element.id === +categoryId);
   useEffect(() => {
-    dispatch(loadArtworksFromApi(categoryId));
-  }, []);
-
+    if (!alreadyCalledId.includes(+categoryId)) {
+      alreadyCalledId = [...alreadyCalledId, +categoryId];
+      dispatch(loadArtworksFromApi(categoryId));
+    }
+  }, [categoryId]);
   useEffect(() => {
     artworks.slice(1, 16).map((artworkId) => dispatch(loadCategory(artworkId)));
   }, [artworks]);
   return (
     <>
       <ul className="category">
-        {category.length && category.map((individualDetail) => (
+        {category.length && category.filter(
+          (element) => departmentProp.department === element.department,
+        ).map((individualDetail) => (
           <li className="category__item">
             <Link to={`/detail/${individualDetail.objectID}`}>
               <img
