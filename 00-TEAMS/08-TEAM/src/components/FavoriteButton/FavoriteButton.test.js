83e7444d889ea44bf97f8fd67ea7379be1b6657f
Gaspar Rodriguez <gaspar.unlp@gmail.com>
2021-05-07 11:20:56 +0200

    FavoriteButton component tested

--- 00-TEAMS/08-TEAM/src/components/FavoriteButton/FavoriteButton.test.js
+++ 00-TEAMS/08-TEAM/src/components/FavoriteButton/FavoriteButton.test.js
@@ -2,29 +2,26 @@
 import React from 'react';
 import FavoriteButton from './index';
 import { addCountryToFav } from '../../redux/actions/actionCreators';
-import { render } from './test-utils';
+import { render, fireEvent } from './test-utils';
 // import { useParams } from 'react-router';
 
 jest.mock('./../../redux/actions/actionCreators');
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
-  useParams: jest.fn().mockReturnValue('France')
+  useParams: () => ({ country: 'France' })
 }));
 
 describe('FavoriteButton Component', () => {
-  test.only('should dispatch addCountryToFav action', () => {
-    addCountryToFav.mockReturnValue({
-      type: 'ADD_FAV',
-      data: 'France'
-    });
+  test('should dispatch addCountryToFav action', () => {
+    addCountryToFav.mockReturnValue(jest.fn);
 
-    render(<FavoriteButton />, {
+    const { container } = render(<FavoriteButton />, {
       initialState: {
         favoriteCountry: ['France']
-
       }
     });
-
-    expect(addCountryToFav()).toHaveBeenCalled();
+    const btn = container.querySelector('button');
+    fireEvent.click(btn);
+    expect(addCountryToFav).toHaveBeenCalled();
   });
 });
