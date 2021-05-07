f34d31eaaf0aa7fe123e3a3e379234287b7e79d9
Hosni <hosni.marco@gmail.com>
2021-05-07 14:35:52 +0200

    Feature Films show

--- 00-TEAMS/06-TEAM/src/common/services/films.js
+++ 00-TEAMS/06-TEAM/src/common/services/films.js
@@ -13,3 +13,4 @@ export const getGenres = (entryPoint) => http.get(`${entryPoint}?api_key=${API_K
 export const getCast = (entryPoint) => http.get(`${entryPoint}/credits?api_key=${API_KEY}&language=en-US&page=1`);
 export const getRecomended = (entryPoint) => http.get(`${entryPoint}/recommendations?api_key=${API_KEY}&language=en-US&page=1`);
 export const getShows = `https://api.themoviedb.org/3/tv/on_the_air?api_key=${API_KEY}`;
+export const getGrid = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`;
--- 00-TEAMS/06-TEAM/src/pages/films/Films.js
+++ 00-TEAMS/06-TEAM/src/pages/films/Films.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { loadGridMovies } from '../../application/store/actions/actionsCreator';
+import '../tv-shows/styles/styles.css';
 
-const Films = () => (
-  <h1>Films</h1>
-);
+const showMoviesGrid = ({ myGrid, dispatch }) => {
+  const baseImgUrl = 'https://image.tmdb.org/t/p/w200';
 
-export default Films;
+  useEffect(() => {
+    dispatch(loadGridMovies());
+  }, []);
+
+  return (
+    <div className="shows-conatiner">
+      {myGrid.map((element) => (
+        <img key={element.id} src={`${baseImgUrl}${element.poster_path}`} alt="" />
+      ))}
+    </div>
+  );
+};
+
+showMoviesGrid.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  myGrid: PropTypes.shape({
+    map: PropTypes.func.isRequired,
+    poster_path: PropTypes.string
+  }).isRequired
+};
+
+const mapStateToProps = ({ moviesGrid }) => ({
+  myGrid: moviesGrid
+});
+
+export default connect(mapStateToProps)(showMoviesGrid);
