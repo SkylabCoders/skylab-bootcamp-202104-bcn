9b5e8d4510477b0bc665cef290a32768f77c0f2b
Hosni <hosni.marco@gmail.com>
2021-05-08 15:46:23 +0200

    Deleted usseles files, fix magic strings

--- 00-TEAMS/06-TEAM/src/pages/home/Home.jsx
+++ 00-TEAMS/06-TEAM/src/pages/home/Home.jsx
@@ -4,17 +4,18 @@ import SlideShow from '../../common/components/slide-show/slideShow';
 import { TYPE_PARAMS } from '../../common/services/films';
 import './home.css';
 import Heading from '../../common/components/heading/Heading';
+import actionTypes from '../../application/store/actions/actionTypes';
 
 const Home = () => (
   <>
     <SlideShow />
     <div className="banner-container">
       <div className="banner-container__first">
-        <Heading content="Las más vistas" type={2} />
+        <Heading content={actionTypes.LOAD_TITLE_MOST_VIEWED} type={2} />
         <SliderCarousel type={TYPE_PARAMS.popular} />
       </div>
       <div className="banner-container__second">
-        <Heading content="Las más valoradas" type={2} />
+        <Heading content={actionTypes.LOAD_TITLE_MOST_VALUED} type={2} />
         <SliderCarousel type={TYPE_PARAMS.topRated} />
       </div>
     </div>
