3821125f8ee8a1211bbe7452cc37cd1c9aee09f0
Isabel Gomez <belasi87@gmail.com>
2021-05-08 15:26:13 +0200

    Aprove total header test

--- 00-TEAMS/08-TEAM/src/components/Header/Header.test.js
+++ 00-TEAMS/08-TEAM/src/components/Header/Header.test.js
@@ -1,12 +1,12 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import { unmountComponentAtNode } from 'react-dom';
-import Header from './index';
+import { useAuth0 } from '@auth0/auth0-react';
 import { render, screen } from '../../utils/test-utils';
+import Header from './index';
 
 let container = null;
 beforeEach(() => {
-  jest.resetModules();
   container = document.createElement('div');
   document.body.appendChild(container);
 });
@@ -17,21 +17,20 @@ afterEach(() => {
   container = null;
 });
 
+jest.mock('@auth0/auth0-react');
+
 describe('Header Component', () => {
-  test('should contain Global', () => {
-    render(<Header />, container);
-    expect(screen.getByText(/Global/i)).toBeInTheDocument();
+  test('should contain Login when the user is authenticated', () => {
+    useAuth0.mockImplementation(() => ({ isAuthenticated: true }));
+    render(<Header />,
+      container);
+    expect(screen.getByText(/Logout/i)).toBeInTheDocument();
   });
 });
 
-jest.mock('@auth0/auth0-react', () => ({
-  useAuth0: () => ({
-    isAuthenticated: false
-  })
-}));
-
 describe('Header Component', () => {
   test('should contain Login when the user is not authenticated', () => {
+    useAuth0.mockImplementation(() => ({ isAuthenticated: false }));
     render(<Header />,
       container);
     expect(screen.getByText(/Login/i)).toBeInTheDocument();
