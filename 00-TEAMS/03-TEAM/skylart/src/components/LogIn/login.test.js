6d63a224fc7599803b58d3857a6e97905b602290
Anna Sala <anna.sala.mercade@gmail.com>
2021-05-09 08:42:45 +0200

    login tohavebeen called to action creator test

--- 00-TEAMS/03-TEAM/skylart/src/components/LogIn/login.test.js
+++ 00-TEAMS/03-TEAM/skylart/src/components/LogIn/login.test.js
@@ -1,10 +1,45 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 import Login from './index';
 import { render, screen } from '../../utils/test-utils';
+import { login } from '../../redux/actions/actionCreators';
+
+jest.mock('@auth0/auth0-react');
+jest.mock('../../redux/actions/actionCreators');
+
+const user = {
+  email: 'johndoe@me.com',
+  email_verified: true,
+  sub: 'google-oauth2|12345678901234',
+};
 
 describe('Login Component', () => {
-  test('render should contain HolaQTal', () => {
+  beforeEach(() => {
+    useAuth0.mockReturnValue({
+      isAuthenticated: true,
+      user,
+      logout: jest.fn(),
+      loginWithRedirect: jest.fn(),
+    });
+    login.mockReturnValue(jest.fn);
+  });
+
+  test('when isAuthenticated && user, login should be called', () => {
+    render(<Login />, {
+      initialState: {
+        auth: {
+          isLoggedIn: true,
+          user: { name: 'ComStas' },
+        },
+      },
+    });
+    expect(login).toHaveBeenCalled();
+  });
+});
+
+describe('Given a Login Component', () => {
+  test('when logged in render should contain HolaQTal', () => {
     render(<Login />, {
       initialState: {
         auth: {
@@ -16,7 +51,7 @@ describe('Login Component', () => {
 
     expect(screen.getByText(/HolaQTal/i)).toBeInTheDocument();
   });
-  test('should contain Log In', () => {
+  test('when logged out render should contain "Log In"', () => {
     render(<Login />, {
       initialState: {
         auth: {
