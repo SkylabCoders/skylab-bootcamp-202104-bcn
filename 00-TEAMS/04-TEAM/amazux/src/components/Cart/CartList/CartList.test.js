7e30a15a247fd9f14efc97ddcda5c758e054eafa
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-05-09 15:10:19 +0200

    Create CarList test

--- 00-TEAMS/04-TEAM/amazux/src/components/Cart/CartList/CartList.test.js
+++ 00-TEAMS/04-TEAM/amazux/src/components/Cart/CartList/CartList.test.js
@@ -1,7 +1,14 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import { render } from '../../../test-utils';
+import { render, fireEvent } from '../../../test-utils';
 import CartList from './index';
+import { deleteProduct } from '../../../redux/actions/actionCreators';
+
+jest.mock('../../../redux/actions/actionCreators');
+
+beforeEach(() => {
+  deleteProduct.mockReturnValue(jest.fn);
+});
 
 describe('<CartList/>', () => {
   const scenarios = [
@@ -27,9 +34,18 @@ describe('<CartList/>', () => {
       `should return ${scenario.result} <li> elements with className main-cart__item`, () => {
         const { container } = render(<CartList />, scenario.state);
         const result = container.querySelectorAll('.main-cart__item').length;
-        console.log('----------->', result);
         expect(result).toBe(scenario.result);
       }
     );
+  });
+  test('should call deleteProduct actionCreator', () => {
+    const { getAllByRole } = render(<CartList />, {
+      initialState: {
+        cart: [{ name: 'iPhone', cost: 100, img: { url: 'fakeUrl' } }, { name: 'Ipad', cost: 300, img: { url: 'fakeUrl' } }]
+      }
+    });
+    const btn = getAllByRole('button')[0];
+    fireEvent.click(btn);
+    expect(deleteProduct).toHaveBeenCalled();
   });
 });
