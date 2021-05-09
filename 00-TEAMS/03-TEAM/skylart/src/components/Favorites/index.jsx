6fba722f6ff1304f56ecccf61b2d4a65b334cd61
Anna Sala <anna.sala.mercade@gmail.com>
2021-05-09 13:05:21 +0200

    print on favorites component without connecting with real data

---
+++ 00-TEAMS/03-TEAM/skylart/src/components/Favorites/index.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable max-len */
+import React from 'react';
+
+function Favorites() {
+  return (
+    <>
+      <h2>Favorites</h2>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique voluptatum impedit eum, perspiciatis debitis nobis cumque doloribus quas quibusdam commodi. Molestias nemo quis voluptatem expedita voluptate dicta dolor neque ullam.</p>
+    </>
+  );
+}
+
+export default Favorites;
