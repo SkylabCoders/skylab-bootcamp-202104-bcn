838f5d110f88e1c8c5147afdbde7d4dd9e8cc26f
Daniel Martorell <dani.martorell@gmail.com>
2021-05-10 20:10:46 +0200

    Add routes and controller

---
+++ dani-martorell/challenge/toh-rest-api/src/controllers/heroesController.js
@@ -0,0 +1,69 @@
+function heroesController(heroes) {
+  let heroesList = [...heroes];
+
+  function getAll(req, res) {
+    res.json(heroes);
+  }
+
+  function createOne(req, res) {
+    const newHeroBody = req.body;
+    const { id: newHeroId } = req.body;
+    const isHeroIdAlreadyInUse = heroes.find((hero) => hero.id === +newHeroId);
+    if (!isHeroIdAlreadyInUse) {
+      heroes.push(newHeroBody);
+      res.json(heroes);
+    } else {
+      res.status(400).json({ message: 'Invalid Id.' });
+    }
+  }
+
+  function getById(req, res) {
+    const { heroId } = req.params;
+    const heroToGet = heroes.find((hero) => hero.id === +heroId);
+    if (heroToGet) {
+      res.json(heroToGet);
+    } else {
+      res.status(404).send({ message: 'Invalid Id.' });
+    }
+  }
+
+  function updateById(req, res) {
+    const { heroId } = req.params;
+    const { body } = req;
+    const heroToModify = heroes.find((hero) => hero.id === +heroId);
+    if (heroToModify) {
+      const updatedHeroes = heroes.map((hero) => {
+        if (hero.id === +heroId) {
+          return { ...hero, ...body };
+        }
+        return hero;
+      });
+      heroesList = [...updatedHeroes];
+      res.json(heroesList);
+    } else {
+      res.status(404).json({ message: 'Invalid Id.' });
+    }
+  }
+
+  function deleteById(req, res) {
+    const { heroId } = req.params;
+    const heroToDelete = heroes.find((hero) => hero.id === +heroId);
+    if (heroToDelete) {
+      const updatedHeroes = heroes.filter((hero) => hero.id !== +heroId);
+      heroesList = [...updatedHeroes];
+      res.json(heroesList);
+    } else {
+      res.status(404).json({ message: 'Invalid Id.' });
+    }
+  }
+
+  return {
+    getAll,
+    createOne,
+    getById,
+    updateById,
+    deleteById,
+  };
+}
+
+module.exports = heroesController;
--- dani-martorell/challenge/toh-rest-api/src/index.js
+++ dani-martorell/challenge/toh-rest-api/src/index.js
@@ -1,63 +1,9 @@
 const express = require('express');
-let heroes = require('./heroes2');
 
 const server = express();
 server.use(express.json());
+const heroesRouter = require('./routes/heroesRouter');
 
-server.get('/heroes', (req, res) => {
-  res.send(heroes);
-});
-
-server.get('/heroes/:heroId', (req, res) => {
-  const { heroId } = req.params;
-  const heroToGet = heroes.find((hero) => hero.id === +heroId);
-  if (heroToGet) {
-    res.json(heroToGet);
-  } else {
-    res.status(404).send({ message: 'Invalid Id.' });
-  }
-});
-
-server.delete('/heroes/:heroId', (req, res) => {
-  const { heroId } = req.params;
-  const heroToDelete = heroes.find((hero) => hero.id === +heroId);
-  if (heroToDelete) {
-    const updatedHeroes = heroes.filter((hero) => hero.id !== +heroId);
-    heroes = [...updatedHeroes];
-    res.json(heroes);
-  } else {
-    res.status(404).json({ message: 'Invalid Id.' });
-  }
-});
-
-server.post('/heroes', (req, res) => {
-  const newHeroBody = req.body;
-  const { id: newHeroId } = req.body;
-  const isHeroIdAlreadyInUse = heroes.find((hero) => hero.id === +newHeroId);
-  if (!isHeroIdAlreadyInUse) {
-    heroes.push(newHeroBody);
-    res.json(heroes);
-  } else {
-    res.status(400).json({ message: 'Invalid Id.' });
-  }
-});
-
-server.put('/heroes/:heroId', (req, res) => {
-  const { heroId } = req.params;
-  const { body } = req;
-  const heroToModify = heroes.find((hero) => hero.id === +heroId);
-  if (heroToModify) {
-    const updatedHeroes = heroes.map((hero) => {
-      if (hero.id === +heroId) {
-        return { ...hero, ...body };
-      }
-      return hero;
-    });
-    heroes = [...updatedHeroes];
-    res.json(heroes);
-  } else {
-    res.status(404).json({ message: 'Invalid Id.' });
-  }
-});
+server.use('/heroes', heroesRouter);
 
 server.listen('2021');
