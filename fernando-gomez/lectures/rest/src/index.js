1a52a251efd83b1419b69c628547affbc67eeeb4
Fernando Gomez <shadyfast8.services@gmail.com>
2021-05-10 15:14:06 +0200

    changes at index.js

--- fernando-gomez/lectures/rest/src/index.js
+++ fernando-gomez/lectures/rest/src/index.js
@@ -1,21 +1,9 @@
 const express = require('express');
+let heroes = require('./heroes');
 
 const server = express();
 
 server.use(express.json());
-
-let heroes = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' },
-];
 
 server.get('/', (req, res) => res.send('Hey que pasa fer'));
 server.get('/fer', (req, res) => res.send('Bienvenido crack'));
