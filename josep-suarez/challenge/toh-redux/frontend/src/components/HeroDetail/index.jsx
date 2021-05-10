2eec88b1fc50b0cdbe622c5893fe78586baa1c2a
Josep Suarez <djose.anton@gmail.com>
2021-05-10 20:07:51 +0200

    all tests ok , and app running

---
+++ josep-suarez/challenge/toh-redux/frontend/src/components/HeroDetail/index.jsx
@@ -0,0 +1,82 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-debugger */
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { PropTypes } from 'prop-types';
+import { getHeroById, updateHero } from '../../redux/actions/actionCreators';
+import './HeroDetail.css';
+
+function HeroDetail({ selectedHero, dispatch }) {
+  const { heroId } = useParams();
+  const [heroName, setHeroName] = useState(selectedHero?.name);
+
+  useEffect(() => {
+    dispatch(getHeroById(heroId));
+  }, [heroId]);
+
+  useEffect(() => {
+    setHeroName(selectedHero?.name);
+  }, [selectedHero]);
+
+  function handleNameChange(event) {
+    setHeroName(event.target.value);
+  }
+
+  function save() {
+    dispatch(updateHero({ id: selectedHero.id, name: heroName }));
+  }
+
+  return (
+    selectedHero?.id
+      ? (
+        <div>
+          <h2>
+            {heroName}
+            {' '}
+            Details
+          </h2>
+          <div>
+            <span>id: </span>
+            {selectedHero.id}
+          </div>
+          <div>
+            <label htmlFor="hero-name">
+              Hero name:
+              <input
+                id="hero-name"
+                value={heroName}
+                onChange={handleNameChange}
+                placeholder="Hero name"
+              />
+            </label>
+          </div>
+          <button type="button">go back</button>
+          <button onClick={save} type="button">save</button>
+        </div>
+      )
+      : (
+        <h3>
+          There is no hero with id=
+          {heroId}
+        </h3>
+      )
+
+  );
+}
+
+HeroDetail.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  selectedHero: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string
+  }).isRequired
+};
+
+function mapStateToProps({ selectedHero }) {
+  return {
+    selectedHero
+  };
+}
+
+export default connect(mapStateToProps)(HeroDetail);
