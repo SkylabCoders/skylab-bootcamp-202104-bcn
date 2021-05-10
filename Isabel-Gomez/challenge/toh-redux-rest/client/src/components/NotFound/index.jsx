95d3c90e88d65ef73bd28b737bfb4bfb20bd0ca2
Isabel Gomez <belasi87@gmail.com>
2021-05-10 17:20:37 +0200

    Create new project, toh with redux and rest api

--- Isabel-Gomez/challenge/toh-redux-rest/client/src/components/NotFound/index.jsx
+++ Isabel-Gomez/challenge/toh-redux-rest/client/src/components/NotFound/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const NotFound = () => (
-  <>
-    <h1>Error 404</h1>
-    <h2>Nothing to see here...</h2>
-  </>
-);
+function NotFound() {
+  return (
+    <>
+      <h1>Error - 404</h1>
+      <h3>Nothing to see here...</h3>
+      <Link to="/">Back to safety</Link>
+    </>
+  );
+}
 
 export default NotFound;
