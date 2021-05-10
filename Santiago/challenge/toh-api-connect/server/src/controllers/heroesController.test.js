400d32a204aeb28220a2f6225d11a3cfbca314cc
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-05-10 22:18:41 +0200

    Add heroesController.test.js

--- Santiago/challenge/toh-api-connect/server/src/controllers/heroesController.test.js
+++ Santiago/challenge/toh-api-connect/server/src/controllers/heroesController.test.js
@@ -15,3 +15,87 @@ describe('getAll', () => {
     expect(res.json).toHaveBeenCalledWith([{ name: 'Pepe' }]);
   });
 });
+
+describe('createOne', () => {
+  test('shoud create one heroe', () => {
+    // arrange
+    const req = {
+      body: { id: 10, name: 'hedi' }
+    };
+
+    const res = {
+      json: jest.fn()
+    };
+
+    // act
+    const { createOne } = heroesController([]);
+    createOne(req, res);
+
+    // assert
+    expect(res.json).toHaveBeenCalledWith([{ id: 10, name: 'hedi' }]);
+  });
+});
+
+describe('getById', () => {
+  test('shoud get a hero', () => {
+    // arrange
+    const req = {
+      params: { heroId: 10 }
+    };
+
+    const res = {
+      json: jest.fn(),
+      status: jest.fn()
+    };
+
+    // act
+    const { getById } = heroesController([{ id: 10, name: 'hedi' }, { id: 11, name: 'eude' }]);
+    getById(req, res);
+
+    // assert
+    expect(res.json).toHaveBeenCalledWith({ id: 10, name: 'hedi' });
+  });
+});
+
+describe('updateById', () => {
+  test('shoud update a hero', () => {
+    // arrange
+    const req = {
+      params: { heroId: 10 },
+      body: { name: 'pipo' }
+    };
+
+    const res = {
+      json: jest.fn(),
+      status: jest.fn()
+    };
+
+    // act
+    const { updateById } = heroesController([{ id: 10, name: 'hedi' }, { id: 11, name: 'eude' }]);
+    updateById(req, res);
+
+    // assert
+    expect(res.json).toHaveBeenCalledWith([{ id: 10, name: 'pipo' }, { id: 11, name: 'eude' }]);
+  });
+});
+
+describe('deleteById', () => {
+  test('shoud delete a hero', () => {
+    // arrange
+    const req = {
+      params: { heroId: 10 }
+    };
+
+    const res = {
+      json: jest.fn(),
+      status: jest.fn()
+    };
+
+    // act
+    const { deleteById } = heroesController([{ id: 10, name: 'hedi' }, { id: 11, name: 'eude' }]);
+    deleteById(req, res);
+
+    // assert
+    expect(res.json).toHaveBeenCalledWith([{ id: 11, name: 'eude' }]);
+  });
+});
