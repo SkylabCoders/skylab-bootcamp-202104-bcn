60161539a6309b50c65b9ea22c3fee622121deba
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-05-10 14:39:18 +0200

    Create rest api

--- Santiago/lectures/toh-rest-api/src/index.js
+++ Santiago/lectures/toh-rest-api/src/index.js
@@ -1,8 +1,50 @@
-const http = require('http');
+const express = require('express');
+let heroes = require('./heroes');
 
-const server = http.createServer((req, res) => {
-  console.log(req);
-  res.end('Hola mundo');
+const server = express();
+server.use(express.json());
+
+server.get('/heroes', (req, res) => {
+  res.json(heroes);
+});
+
+server.get('/heroes/:heroId', (req, res) => {
+  const heroById = heroes.find((hero) => hero.id === +req.params.heroId);
+  if (heroById) {
+    res.status(302);
+    res.json(heroById);
+  } else {
+    res.status(404);
+    res.status(heroById);
+  }
+});
+
+server.post('/heroes', (req, res) => {
+  const newHero = req.body;
+  heroes.push(newHero);
+  res.json(heroes);
+});
+
+server.put('/heroes/:heroId', (req, res) => {
+  const { heroId } = req.params;
+  const updateData = req.body;
+  heroes = heroes.map((hero) => {
+    if (hero.id === +heroId) {
+      return {
+        ...hero,
+        ...updateData
+      };
+    }
+    return hero;
+  });
+  res.json(heroes);
+});
+
+server.delete('/heroes/:heroId', (req, res) => {
+  const { heroId } = req.params;
+  heroes = heroes.filter((hero) => hero.id !== +heroId);
+
+  res.json(heroes);
 });
 
 server.listen('2021');
