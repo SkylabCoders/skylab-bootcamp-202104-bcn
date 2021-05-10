2535cf1559b02ade46789612cb99d6d940c5b07e
cristian-bars <patxi.cristian@gmail.com>
2021-05-10 12:33:42 +0200

    Create server petition

--- cristian-bars/lectures/toh-rest-api/src/index.js
+++ cristian-bars/lectures/toh-rest-api/src/index.js
@@ -8,8 +8,18 @@ const express = require('express');
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send('Hola Cristian de nou!');
+const users = [{ name: 'Cristian' }, { name: 'Irina' }];
+
+server.get('/', (req, res) => { res.send('Hola Cristian de nou!'); });
+server.get('/heroes', (req, res) => { res.send('Herois!!'); });
+server.get('/users', (req, res) => { res.json(); users; });
+server.get('/users/:userName', (req, res) => {
+  const response = users.find((user) => user.name.toLowerCase() === req.params.userName.toLowerCase());
+  if (response) {
+    res.json(response);
+  } else {
+    res.send('There are no users with name: ${req.params.userName}');
+  }
 });
 
 server.listen('2021');
