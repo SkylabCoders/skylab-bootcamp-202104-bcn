a18f2e2170579f55cb83151888878fad671b67d7
Anna Sala <anna.sala.mercade@gmail.com>
2021-05-10 18:42:45 +0200

    updat header test

--- 00-TEAMS/03-TEAM/skylart/src/components/Common/Header/header.test.js
+++ 00-TEAMS/03-TEAM/skylart/src/components/Common/Header/header.test.js
@@ -1,16 +1,31 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { render, screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import { render, unmountComponentAtNode } from 'react-dom';
 import Header from './index';
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
 
 describe('Header Component', () => {
   test.only('should contain "home" text', () => {
-    render(
-      <BrowserRouter>
-        <Header />
-      </BrowserRouter>,
-    );
-    expect(screen.getByText(/Home/i)).toBeInTheDocument();
+    act(() => {
+      render(
+        <Header />,
+        container,
+      );
+    });
+    expect(container.querySelector('left-elements').textContent).toBe('Home');
   });
 });
