d9cc11b564df881561e8f0b09bffd2d67fdd4abe
Daniel Martorell <dani.martorell@gmail.com>
2021-05-10 17:59:32 +0200

    Implementation of get methods

--- dani-martorell/challenge/toh-rest-api/src/index.js
+++ dani-martorell/challenge/toh-rest-api/src/index.js
@@ -1,1 +1,21 @@
-console.log('hill');
+const express = require('express');
+const heroes = require('./heroes2');
+
+const server = express();
+server.use(express.json());
+
+server.get('/heroes', (req, res) => {
+  res.send(heroes);
+});
+
+server.get('/heroes/:heroId', (req, res) => {
+  const { heroId } = req.params;
+  const heroToGet = heroes.find((hero) => hero.id === +heroId);
+  if (heroToGet) {
+    res.json(heroToGet);
+  } else {
+    res.status(404).json('Invalid Id.');
+  }
+});
+
+server.listen('2021');
