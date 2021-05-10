f8e6f71bc84d1f3bd83710b392d4cb18a0c3a43d
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-05-10 20:00:06 +0200

    Create server folder

---
+++ Santiago/challenge/toh-api-connect/server/src/controllers/heroesController.js
@@ -0,0 +1,66 @@
+let heroes = require('../heroes');
+
+function heroesController() {
+  function getAll(req, res) {
+    res.json(heroes);
+  }
+
+  function getById(req, res) {
+    const heroById = heroes.find((hero) => hero.id === +req.params.heroId);
+    if (heroById) {
+      res.status(302);
+      res.json(heroById);
+    } else {
+      res.status(404);
+      res.status(heroById);
+    }
+  }
+
+  function createOne(req, res) {
+    const newHero = req.body;
+    heroes.push(newHero);
+    res.json(heroes);
+  }
+
+  function updateById(req, res) {
+    const { heroId } = req.params;
+    const updateData = req.body;
+    heroes = heroes.map((hero) => {
+      if (hero.id === +heroId) {
+        return {
+          ...hero,
+          ...updateData
+        };
+      }
+      return hero;
+    });
+    res.json(heroes);
+  }
+
+  function deleteById(req, res) {
+    const { heroId } = req.params;
+    const heroById = heroes.filter((hero) => hero.id === +heroId);
+
+    if (heroById) {
+      heroes = heroes.filter((hero) => hero.id !== +heroId);
+      res.status(204);
+      res.end();
+    } else {
+      res.status(404);
+      res.json();
+    }
+
+    res.status(204);
+    res.json(heroes);
+  }
+
+  return {
+    getAll,
+    createOne,
+    getById,
+    updateById,
+    deleteById
+  };
+}
+
+module.exports = heroesController(heroes);
