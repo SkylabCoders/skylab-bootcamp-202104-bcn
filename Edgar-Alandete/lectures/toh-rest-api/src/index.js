2aa94e1d9b97eff392257fbaa49e372516e0b707
Edgar Alandete <edgalandete@gmail.com>
2021-05-10 13:22:15 +0200

    Create new hero by post

--- Edgar-Alandete/lectures/toh-rest-api/src/index.js
+++ Edgar-Alandete/lectures/toh-rest-api/src/index.js
@@ -1,8 +1,22 @@
-const http = require('http');
+const express = require('express');
+const heroes = require('./heroes');
 
-const server = http.createServer((req, res) => {
-  console.log(req);
-  res.end('Hola Mundo!');
+const server = express();
+server.use(express.json());
+
+const routeCallback = (req, res) => { res.send('Skylab rules'); };
+const routeHeroesCallback = (req, res) => { res.json(heroes); };
+const routeOneHeroCallback = (req, res) => {
+  const heroById = heroes.find((hero) => hero.id === +req.params.heroId);
+  res.json(heroById);
+};
+
+server.get('/', routeCallback);
+server.get('/heroes', routeHeroesCallback);
+server.get('/heroes/:heroId', routeOneHeroCallback);
+server.post('/heroes', (req, res) => {
+  const newHero = req.body;
+  heroes.push(newHero);
+  res.json(heroes);
 });
-
 server.listen('2021');
