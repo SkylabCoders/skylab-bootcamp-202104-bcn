1cea80e5333ae4023298bd6a4031a6b2e0a142c4
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-05-10 19:10:11 +0200

    Modify loadHeroes actionCreator

---
+++ alfredo-zimperz/challenges/toh-fullstack/backend/src/controllers/heroesController.test.js
@@ -0,0 +1,197 @@
+const heroesController = require('./heroesController');
+const { getNextId } = require('../helpers/index');
+
+describe('getAll', () => {
+  test('shoud get all heroes', () => {
+    const res = {
+      json: jest.fn(),
+      status: jest.fn(),
+    };
+
+    const { getAll } = heroesController([{ name: 'Pepe' }]);
+    getAll(null, res);
+    expect(res.json).toHaveBeenCalledWith([{ name: 'Pepe' }]);
+  });
+
+  test('shoud return status 200', () => {
+    const res = {
+      json: jest.fn(),
+      status: jest.fn(),
+    };
+
+    const { getAll } = heroesController([{ name: 'Pepe' }]);
+    getAll(null, res);
+    expect(res.status).toHaveBeenCalledWith(200);
+  });
+});
+
+describe('createOne', () => {
+  test('shoud create one hero', () => {
+    const res = {
+      json: jest.fn(),
+      status: jest.fn(),
+    };
+
+    const req = {
+      body: {
+        name: 'Elvis',
+      },
+    };
+
+    const { createOne } = heroesController([{ name: 'Pepe' }]);
+    createOne(req, res);
+    expect(res.json).toHaveBeenCalledWith({ id: getNextId(), name: 'Elvis' });
+  });
+  test('shoud return status 201', () => {
+    const res = {
+      json: jest.fn(),
+      status: jest.fn(),
+    };
+
+    const req = {
+      body: {
+        name: 'Elvis',
+      },
+    };
+
+    const { createOne } = heroesController([{ name: 'Pepe' }]);
+    createOne(req, res);
+    expect(res.status).toHaveBeenCalledWith(201);
+  });
+});
+
+describe('getById', () => {
+  test('shoud return a Hero with the selected id', () => {
+    const res = {
+      json: jest.fn(),
+      status: jest.fn(),
+    };
+
+    const req = {
+      params: { heroId: 2 },
+    };
+
+    const { getById } = heroesController([{ id: 1, name: 'Pepe' }, { id: 2, name: 'Tino' }]);
+    getById(req, res);
+    expect(res.json).toHaveBeenCalledWith({ id: 2, name: 'Tino' });
+  });
+  test('shoud return status 302', () => {
+    const res = {
+      json: jest.fn(),
+      status: jest.fn(),
+    };
+
+    const req = {
+      params: { heroId: 2 },
+    };
+
+    const { getById } = heroesController([{ id: 1, name: 'Pepe' }, { id: 2, name: 'Tino' }]);
+    getById(req, res);
+    expect(res.status).toHaveBeenCalledWith(302);
+  });
+  test('shoud return a Hero without argument', () => {
+    const res = {
+      json: jest.fn(),
+      status: jest.fn(),
+    };
+
+    const req = {
+      params: { heroId: 7 },
+    };
+
+    const { getById } = heroesController([{ id: 1, name: 'Pepe' }, { id: 2, name: 'Tino' }]);
+    getById(req, res);
+    expect(res.json).toHaveBeenCalledWith();
+  });
+  test('shoud return status 404', () => {
+    const res = {
+      json: jest.fn(),
+      status: jest.fn(),
+    };
+
+    const req = {
+      params: { heroId: 4 },
+    };
+
+    const { getById } = heroesController([{ id: 1, name: 'Pepe' }, { id: 2, name: 'Tino' }]);
+    getById(req, res);
+    expect(res.status).toHaveBeenCalledWith(404);
+  });
+});
+
+describe('updateById', () => {
+  test('shoud return an array with the updated heroes', () => {
+    const res = {
+      json: jest.fn(),
+      status: jest.fn(),
+    };
+
+    const req = {
+      body: { name: 'Josep' },
+      params: { heroId: 2 },
+    };
+
+    const { updateById } = heroesController([{ id: 1, name: 'Pepe' }, { id: 2, name: 'Tino' }]);
+    updateById(req, res);
+    expect(res.json).toHaveBeenCalledWith([{ id: 1, name: 'Pepe' }, { id: 2, name: 'Josep' }]);
+  });
+  test('shoud return status 200', () => {
+    const res = {
+      json: jest.fn(),
+      status: jest.fn(),
+    };
+
+    const req = {
+      params: { heroId: 2 },
+    };
+
+    const { updateById } = heroesController([{ id: 1, name: 'Pepe' }, { id: 2, name: 'Tino' }]);
+    updateById(req, res);
+    expect(res.status).toHaveBeenCalledWith(200);
+  });
+});
+
+describe('deleteById', () => {
+  test('shoud return an array with the updated heroes', () => {
+    const res = {
+      json: jest.fn(),
+      status: jest.fn(),
+    };
+
+    const req = {
+      params: { heroId: 2 },
+    };
+
+    const { deleteById } = heroesController([{ id: 1, name: 'Pepe' }, { id: 2, name: 'Tino' }]);
+    deleteById(req, res);
+    expect(res.json).toHaveBeenCalledWith([{ id: 1, name: 'Pepe' }]);
+  });
+  test('shoud return status 200', () => {
+    const res = {
+      json: jest.fn(),
+      status: jest.fn(),
+    };
+
+    const req = {
+      params: { heroId: 2 },
+    };
+
+    const { deleteById } = heroesController([{ id: 1, name: 'Pepe' }, { id: 2, name: 'Tino' }]);
+    deleteById(req, res);
+    expect(res.status).toHaveBeenCalledWith(200);
+  });
+  test('shoud return status 404', () => {
+    const res = {
+      json: jest.fn(),
+      status: jest.fn(),
+    };
+
+    const req = {
+      params: { heroId: 90 },
+    };
+
+    const { deleteById } = heroesController([{ id: 1, name: 'Pepe' }, { id: 2, name: 'Tino' }]);
+    deleteById(req, res);
+    expect(res.status).toHaveBeenCalledWith(404);
+  });
+});
---
+++ alfredo-zimperz/challenges/toh-fullstack/backend/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+const heroesRouter = require('./routes/heroesRouter');
+
+server.use('/heroes', heroesRouter);
+
+server.listen('2021');
