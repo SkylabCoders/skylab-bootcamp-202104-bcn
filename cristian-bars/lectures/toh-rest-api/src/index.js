995c98c1e03a26fe2ad47bf26a23e1f6851c38d3
cristian-bars <patxi.cristian@gmail.com>
2021-05-10 12:18:02 +0200

    Create express server

--- cristian-bars/lectures/toh-rest-api/src/index.js
+++ cristian-bars/lectures/toh-rest-api/src/index.js
@@ -1,8 +1,15 @@
-const http = require('http');
+// const http = require('http');
+const express = require('express');
 
-const server = http.createServer((req, res) => {
+/* const server = http.createServer((req, res) => {
   console.log(req);
   res.end('Hola Cristian');
+}); */
+
+const server = express();
+
+server.get('/', (req, res) => {
+  res.send('Hola Cristian de nou!');
 });
 
 server.listen('2021');
