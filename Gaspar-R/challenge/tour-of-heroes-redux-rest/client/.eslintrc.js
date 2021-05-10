5c1b81acfea217a7ff2f9f287e1106cb78423a64
Gaspar Rodriguez <gaspar.unlp@gmail.com>
2021-05-10 19:16:24 +0200

    Created proyect files

--- Gaspar-R/challenge/tour-of-heroes-redux-rest/client/.eslintrc.js
+++ Gaspar-R/challenge/tour-of-heroes-redux-rest/client/.eslintrc.js
@@ -2,21 +2,26 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
-    jest: true
+    node: true
   },
   extends: [
-    'airbnb-base'
+    'plugin:react/recommended',
+    'airbnb'
   ],
   parserOptions: {
-    ecmaVersion: 12
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
   },
+  plugins: [
+    'react'
+  ],
   rules: {
-
     'comma-dangle': [2, 'never'],
     'linebreak-style': 0,
     'global-require': 0,
     'eslint linebreak-style': [0, 'error', 'windows']
-
   }
 };
