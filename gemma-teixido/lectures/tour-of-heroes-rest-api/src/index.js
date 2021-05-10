decec343fd6fe3228beb510d687c1100e5847b7b
gteixi <gemmateixido25@gmail.com>
2021-05-10 12:20:58 +0200

    Create new route of /heroes.

--- gemma-teixido/lectures/tour-of-heroes-rest-api/src/index.js
+++ gemma-teixido/lectures/tour-of-heroes-rest-api/src/index.js
@@ -1,8 +1,14 @@
-const http = require('http');
+// const http = require('http');
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-  console.log(req);
-  res.end('Hola mundo!');
-});
+// const server = http.createServer((req, res) => {
+//   console.log(req);
+//   res.end('Hola mundo!');
+// });
+
+const server = express();
+
+server.get('/', (req, res) => { res.send('Skylab mola!!!'); });
+server.get('/heroes', (req, res) => { res.send('Heroes mola'); });
 
 server.listen('2021');
