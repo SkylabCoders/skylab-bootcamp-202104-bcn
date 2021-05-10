6686dec10dc0c609b679116f6615e804909996a6
Anna Sala <anna.sala.mercade@gmail.com>
2021-05-10 18:09:46 +0200

    add ul and key to favorite list

--- 00-TEAMS/03-TEAM/skylart/src/components/Favorites/index.jsx
+++ 00-TEAMS/03-TEAM/skylart/src/components/Favorites/index.jsx
@@ -2,25 +2,27 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import './Favorites.scss';
 
 function Favorites({ favorites }) {
   return (
     <>
       <h2>Favorites</h2>
-
-      {favorites.length && favorites.map(
-        (favoriteArtwork) => (
-          <li className="category__item">
-            <Link to={`/detail/${favoriteArtwork.objectID}`}>
-              <p className="detail-name">{favoriteArtwork.title}</p>
-              <img
-                src={favoriteArtwork.primaryImageSmall}
-                alt={favoriteArtwork.primaryImageSmall}
-              />
-            </Link>
-          </li>
-        ),
-      )}
+      <ul className="category">
+        {favorites.length && favorites.map(
+          (favoriteArtwork) => (
+            <li className="category__item" key={favoriteArtwork.objectID}>
+              <Link to={`/detail/${favoriteArtwork.objectID}`}>
+                <p className="detail-name">{favoriteArtwork.title}</p>
+                <img
+                  src={favoriteArtwork.primaryImageSmall}
+                  alt={favoriteArtwork.primaryImageSmall}
+                />
+              </Link>
+            </li>
+          ),
+        )}
+      </ul>
     </>
   );
 }
