149f2bcbd48705e7cb93e0bf2d821c8b1cc8e6b3
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-05-10 19:53:43 +0200

    Create main folder and copy toh-redux into client folder

---
+++ Santiago/challenge/toh-api-connect/client/src/redux/actions/actionCreators.js
@@ -0,0 +1,58 @@
+import HEROES from '../../Constants/heroes-mock';
+import actionTypes from './actionTypes';
+
+export function loadHeroes() {
+  return {
+    type: actionTypes.LOAD_HEROES
+  };
+}
+
+export function addHero(hero) {
+  return {
+    type: actionTypes.ADD_HERO,
+    hero
+  };
+}
+
+export function deleteHero(heroId) {
+  return {
+    type: actionTypes.DELETE_HERO,
+    heroId
+  };
+}
+
+export function updateHero(hero) {
+  return {
+    type: actionTypes.UPDATE_HERO,
+    hero
+  };
+}
+
+export function loadHero(hero) {
+  return {
+    type: actionTypes.LOAD_HERO,
+    hero
+  };
+}
+
+export function getHeroById(heroId) {
+  const hero = HEROES.find((current) => current.id === +heroId);
+
+  return {
+    type: actionTypes.LOAD_HERO,
+    hero
+  };
+}
+
+export function login(user) {
+  return {
+    type: actionTypes.AUTH_LOGIN,
+    user
+  };
+}
+
+export function logout() {
+  return {
+    type: actionTypes.AUTH_LOGOUT
+  };
+}
