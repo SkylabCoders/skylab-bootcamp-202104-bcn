9881dfa38dfb1c03db2413655e64703f55d406db
Hosni <hosni.marco@gmail.com>
2021-05-10 18:53:40 +0200

    Feature createOne-test

--- hosni-marco/lecture/rest/toh-rest-api/src/controllers/heroesController.test.js
+++ hosni-marco/lecture/rest/toh-rest-api/src/controllers/heroesController.test.js
@@ -6,12 +6,24 @@ describe('getAll', () => {
     const res = {
       json: jest.fn(),
     };
-
     // act
     const { getAll } = heroesController([{ name: 'Pepe' }]);
     getAll(null, res);
-
     // assert
     expect(res.json).toHaveBeenCalledWith([{ name: 'Pepe' }]);
+  });
+});
+
+describe('createOne', () => {
+  test('shoud get all heroes', () => {
+    // arrange
+    const res = {
+      json: jest.fn(),
+    };
+    // act
+    const { createOne } = heroesController([{ id: 20, name: 'Pepe' }]);
+    createOne(null, res);
+    // assert
+    expect(res.json).toHaveBeenCalledWith({ id: 20, name: 'Pepe' });
   });
 });
