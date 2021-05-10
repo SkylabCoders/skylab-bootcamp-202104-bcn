edd72fee39b6d29d615b52aa654883aa2db069da
Laura <laura.sabater.musach@gmail.com>
2021-05-10 08:53:43 +0200

    Add detail component test

---
+++ 00-TEAMS/03-TEAM/skylart/src/components/Detail/detail.test.js
@@ -0,0 +1,32 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import Detail from './index';
+import { render, screen } from '../../utils/test-utils';
+import { loadDetail } from '../../redux/actions/actionCreators';
+
+describe('For a Detail component', () => {
+  test.only('should containt "rights" ', () => {
+    render(<Detail />, {
+      initialState: {
+        detail: {
+          title: 'hello world',
+        },
+      },
+      urlParam: ':artworkId',
+    });
+    expect(screen.getByText(/hello/i).toBeInTheDocument());
+  });
+
+  test.only('should call loadDetail', () => {
+    render(<Detail />, {
+      initialState: {
+        detail: {
+          title: 'hello world',
+          objectID: 10,
+        },
+      },
+      urlParam: ':artworkId',
+    });
+    expect(loadDetail).toHaveBeenCalled();
+  });
+});
