3592ccce61840007f2d1a6a83ba25e9cd1fe0822
Isabel Gomez <belasi87@gmail.com>
2021-05-11 18:10:05 +0200

    Install mongoose and create HeroModel

--- Isabel-Gomez/challenge/toh-redux-rest/server/src/index.js
+++ Isabel-Gomez/challenge/toh-redux-rest/server/src/index.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const cors = require('cors');
 const chalk = require('chalk');
-const debug = require('debug')('server');
+const debug = require('debug')('app');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+require('../.env').config();
 
 const server = express();
 
+mongoose.connect(
+  process.env.DDBB_URL,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  },
+);
+
 server.use(cors());
 server.use(express.json());
-
-server.use(morgan('tiny'));
-
+server.use(morgan('dev'));
 const heroesRouter = require('./routes/heroesRouter');
 
 server.use('/heroes', heroesRouter);
