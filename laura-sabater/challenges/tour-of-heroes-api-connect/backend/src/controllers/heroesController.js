6f78e0eb15066b21916352898eea43acb3ef5106
Laura <laura.sabater.musach@gmail.com>
2021-05-11 11:47:18 +0200

    Change  status 302 for 300 in getById

--- laura-sabater/challenges/tour-of-heroes-api-connect/backend/src/controllers/heroesController.js
+++ laura-sabater/challenges/tour-of-heroes-api-connect/backend/src/controllers/heroesController.js
@@ -8,7 +8,7 @@ function heroesController() {
   function getById(req, res) {
     const heroById = heroes.find((hero) => hero.id === +req.params.heroId);
     if (heroById) {
-      res.status(302);
+      res.status(300);
       res.json(heroById);
     } else {
       res.status(404);
