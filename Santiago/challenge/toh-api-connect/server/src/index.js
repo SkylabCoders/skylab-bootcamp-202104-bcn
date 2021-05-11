7d465368522d2535e2f61b9e2ceb53e5b54d9445
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-05-11 10:24:17 +0200

    Add cors to be able to show the heroes

--- Santiago/challenge/toh-api-connect/server/src/index.js
+++ Santiago/challenge/toh-api-connect/server/src/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const server = express();
 
 server.use(express.json());
-
+server.use(cors());
 const heroesRouter = require('./routes/heroesRouter');
 
 server.use('/heroes', heroesRouter);
