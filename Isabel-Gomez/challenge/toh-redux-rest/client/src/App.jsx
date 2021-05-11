8378273a29b545eb67c3122a857ccb4afb40c251
Isabel Gomez <belasi87@gmail.com>
2021-05-11 11:26:00 +0200

    Install cors

--- Isabel-Gomez/challenge/toh-redux-rest/client/src/App.jsx
+++ Isabel-Gomez/challenge/toh-redux-rest/client/src/App.jsx
@@ -13,7 +13,7 @@ import './App.css';
 
 function App() {
   return (
-    <Provider store={store}>
+    <Provider store={store()}>
       <BrowserRouter>
         <h1>Tour Of Heroes</h1>
         <nav>
