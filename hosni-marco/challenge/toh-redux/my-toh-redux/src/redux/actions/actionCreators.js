b7fcd767d1bb9b350a4b42180dbfd50e452713e9
Hosni <hosni.marco@gmail.com>
2021-05-11 09:48:54 +0200

    Canged actionCreators

--- hosni-marco/challenge/toh-redux/my-toh-redux/src/redux/actions/actionCreators.js
+++ hosni-marco/challenge/toh-redux/my-toh-redux/src/redux/actions/actionCreators.js
@@ -1,38 +1,82 @@
+import axios from 'axios';
 import HEROES from '../../constants/heroes.mock';
 import actionTypes from './actionTypes';
 
+const url = 'http://localhost:2021/heroes';
+
 export function loadHeroes() {
-  return {
-    type: actionTypes.LOAD_HEROES
+  return async (dispatch) => {
+    try {
+      const { data } = await axios(url);
+      dispatch({
+        type: actionTypes.LOAD_HEROES,
+        heroes: data
+      });
+    } catch (error) {
+      dispatch({
+        type: 'LOAD_HEROES_ERROR'
+      });
+    }
   };
+  // return {
+  //   type: actionTypes.LOAD_HEROES
+  // };
 }
 
 export function addHero(hero) {
-  return {
-    type: actionTypes.ADD_HERO,
-    hero
+  return async (dispatch) => {
+    const { data } = await axios.post(url, hero);
+    dispatch({
+      type: actionTypes.ADD_HERO,
+      hero: data
+    });
   };
+  // return {
+  //   type: actionTypes.ADD_HERO,
+  //   hero
+  // };
 }
 
 export function deleteHero(heroId) {
-  return {
-    type: actionTypes.DELETE_HERO,
-    heroId
+  return async (dispatch) => {
+    await axios.delete(`${url}/${heroId}`);
+    dispatch({
+      type: actionTypes.DELETE_HERO,
+      heroId
+    });
   };
+  // return {
+  //   type: actionTypes.DELETE_HERO,
+  //   heroId
+  // };
 }
 
 export function updateHero(hero) {
-  return {
-    type: actionTypes.UPDATE_HERO,
-    hero
+  return async (dispatch) => {
+    const { data } = await axios.put(`${url}/${hero.id}`, hero);
+    dispatch({
+      type: actionTypes.UPDATE_HERO,
+      hero: data
+    });
   };
+  // return {
+  //   type: actionTypes.UPDATE_HERO,
+  //   hero
+  // };
 }
 
 export function loadHero(hero) {
-  return {
-    type: actionTypes.LOAD_HERO,
-    hero
+  return async (dispatch) => {
+    const { data } = await axios(`${url}/${hero.id}`);
+    dispatch({
+      type: actionTypes.LOAD_HERO,
+      hero: data
+    });
   };
+  // return {
+  //   type: actionTypes.LOAD_HERO,
+  //   hero
+  // };
 }
 
 export function getHeroById(heroId) {
@@ -42,4 +86,10 @@ export function getHeroById(heroId) {
     type: actionTypes.LOAD_HERO,
     hero
   };
+  // const hero = HEROES.find((current) => current.id === +heroId);
+
+  // return {
+  //   type: actionTypes.LOAD_HERO,
+  //   hero
+  // };
 }
