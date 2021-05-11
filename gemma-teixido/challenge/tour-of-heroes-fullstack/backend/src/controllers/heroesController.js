b3571dfd2dd9682305c3d05332f6cb89c5e8c98a
gteixi <gemmateixido25@gmail.com>
2021-05-11 12:46:53 +0200

    Challenge of API with Post and installed: - Chalk - Debug - Morgan

---
+++ gemma-teixido/challenge/tour-of-heroes-fullstack/backend/src/controllers/heroesController.js
@@ -0,0 +1,69 @@
+let nextHeroId = 31;
+const chalk = require('chalk');
+const debug = require('debug')('server:heroesConntroller');
+
+function heroesController(heroes) {
+  function getAll(req, res) {
+    debug(`Dentro de la funcion ${chalk.yellowBright('getAll')}`);
+    res.status(200);
+    res.json(heroes);
+  }
+
+  function createOne(req, res) {
+    const newHero = { ...req.body, id: nextHeroId };
+    heroes.push(newHero);
+    res.status(201);
+    res.json(newHero);
+    nextHeroId += 1;
+  }
+
+  function getById(req, res) {
+    const heroById = heroes.find((hero) => hero.id === +req.params.heroId);
+    if (heroById) {
+      res.status(200);
+      res.json(heroById);
+    } else {
+      res.status(404);
+      res.json();
+    }
+  }
+
+  function updateById(req, res) {
+    const { heroId } = req.params;
+    const updateData = req.body;
+    const updatedHeroes = heroes.map((hero) => {
+      if (hero.id === +heroId) {
+        return {
+          ...hero,
+          ...updateData
+        };
+      }
+      return hero;
+    });
+    res.status(200);
+    res.json(updatedHeroes);
+  }
+
+  function deleteById(req, res) {
+    const { heroId } = req.params;
+    const heroById = heroes.find((hero) => hero.id === +heroId);
+    if (heroById) {
+      const updatedHeroes = heroes.filter((hero) => +heroId !== hero.id);
+      res.status(200);
+      res.json(updatedHeroes);
+    } else {
+      res.status(404);
+      res.json();
+    }
+  }
+
+  return {
+    getAll,
+    createOne,
+    getById,
+    updateById,
+    deleteById
+  };
+}
+
+module.exports = heroesController;
