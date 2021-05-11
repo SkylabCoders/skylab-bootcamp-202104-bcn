c21ef4974e5419e7742108b0f9ff424e0ab308eb
Anna Sala <anna.sala.mercade@gmail.com>
2021-05-11 10:04:52 +0200

    update heroesReducer to print heroes from backend

--- anna-sala/challenges/toh-full-stack/frontend/src/redux/reducers/heroesReducer.js
+++ anna-sala/challenges/toh-full-stack/frontend/src/redux/reducers/heroesReducer.js
@@ -1,10 +1,9 @@
 import actionTypes from '../actions/actionTypes';
-import HEROES from '../../assets/heroes';
 
 function heroesReducer(heroes = [], action) {
   switch (action.type) {
     case actionTypes.LOAD_HEROES:
-      return HEROES;
+      return action.heroes;
     case actionTypes.DELETE_HERO:
       return heroes.filter((hero) => hero.id !== action.heroId);
     case actionTypes.ADD_HERO:
