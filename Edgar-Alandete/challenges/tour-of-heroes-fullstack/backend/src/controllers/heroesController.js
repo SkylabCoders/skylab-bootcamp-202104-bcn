7a89286fed062dba7ace34956e757e3a826635eb
Edgar Alandete <edgalandete@gmail.com>
2021-05-11 17:57:43 +0200

    Create model and create hero action

--- Edgar-Alandete/challenges/tour-of-heroes-fullstack/backend/src/controllers/heroesController.js
+++ Edgar-Alandete/challenges/tour-of-heroes-fullstack/backend/src/controllers/heroesController.js
@@ -1,82 +1,71 @@
 const debug = require('debug')('server:heroesController');
 const chalk = require('chalk');
+const Hero = require('../model/heroModel');
 
-function heroesController(heroes) {
-  heroes.sort((firstHero, secondHero) => {
-    if (firstHero.id > secondHero.id) {
-      return 1;
-    }
-    if (firstHero.id < secondHero.id) {
-      return -1;
-    }
-    return 0;
-  });
-  const newId = heroes[heroes.length - 1].id + 1;
-  function getAll(req, res) {
+function heroesController() {
+  async function getAll(req, res) {
     debug(`dentro de la función ${chalk.magenta('getAll')}`);
+    const heroes = await Hero.find();
     res.json(heroes);
   }
 
-  function createOne(req, res) {
-    // eslint-disable-next-line no-debugger
-    debugger;
-    const newHero = {
-      ...req.body,
-      ...{ id: newId },
-    };
-    heroes.push(newHero);
-    res.json(heroes);
-  }
+  async function createOne(req, res) {
+    const newHero = new Hero(req.body);
 
-  function getById(req, res) {
-    const { heroId } = req.params;
-    const heroById = heroes.find((hero) => hero.id === +heroId);
-    if (heroById) {
-      res.status(200);
-      res.json(heroById);
-    } else {
-      res.status(404);
-      res.send(`The hero with the id ${heroId} doesn't exist`);
+    try {
+      await newHero.save();
+      res.json(newHero);
+    } catch (error) {
+      debug(error);
     }
   }
 
-  function deleteById(req, res) {
-    const { heroId } = req.params;
-    let newHeroes = [...heroes];
-    const heroById = heroes.find((hero) => +heroId === hero.id);
-    if (heroById) {
-      newHeroes = heroes.filter((hero) => +heroId !== hero.id);
-      res.status(200);
-    } else {
-      res.status(404);
-      res.send(`The hero with the id ${heroId} doesn't exist`);
-    }
-    res.json(newHeroes);
-  }
+  // function getById(req, res) {
+  //   const { heroId } = req.params;
+  //   const heroById = heroes.find((hero) => hero.id === +heroId);
+  //   if (heroById) {
+  //     res.status(200);
+  //     res.json(heroById);
+  //   } else {
+  //     res.status(404);
+  //     res.send(`The hero with the id ${heroId} doesn't exist`);
+  //   }
+  // }
 
-  function updateById(req, res) {
-    const { heroId } = req.params;
-    const updateData = req.body;
-    let newHeroes = [...heroes];
-    newHeroes = heroes.map((hero) => {
-      if (hero.id === +heroId) {
-        return {
-          ...hero,
-          ...updateData,
-        };
-      }
-      return hero;
-    });
+  // function deleteById(req, res) {
+  //   const { heroId } = req.params;
+  //   let newHeroes = [...heroes];
+  //   const heroById = heroes.find((hero) => +heroId === hero.id);
+  //   if (heroById) {
+  //     newHeroes = heroes.filter((hero) => +heroId !== hero.id);
+  //     res.status(200);
+  //   } else {
+  //     res.status(404);
+  //     res.send(`The hero with the id ${heroId} doesn't exist`);
+  //   }
+  //   res.json(newHeroes);
+  // }
 
-    res.json(newHeroes);
-  }
+  // function updateById(req, res) {
+  //   const { heroId } = req.params;
+  //   const updateData = req.body;
+  //   let newHeroes = [...heroes];
+  //   newHeroes = heroes.map((hero) => {
+  //     if (hero.id === +heroId) {
+  //       return {
+  //         ...hero,
+  //         ...updateData,
+  //       };
+  //     }
+  //     return hero;
+  //   });
+
+  //   res.json(newHeroes);
+  // }
 
   return {
     getAll,
     createOne,
-    getById,
-    deleteById,
-    updateById,
   };
 }
 
--- Edgar-Alandete/challenges/tour-of-heroes-fullstack/backend/src/index.js
+++ Edgar-Alandete/challenges/tour-of-heroes-fullstack/backend/src/index.js
@@ -3,9 +3,18 @@ const cors = require('cors');
 const chalk = require('chalk');
 const debug = require('debug')('server');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const server = express();
 
+mongoose.connect(
+  process.env.DDBB_URL,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  },
+);
 server.use(cors());
 server.use(express.json());
 
