3c36ab7ad91a91aa6dc7d1dc043912be2cb5bb25
Anna Sala <anna.sala.mercade@gmail.com>
2021-05-11 18:39:10 +0200

    installed dotenv and updated backend index

--- anna-sala/challenges/toh-full-stack-ddbb/backend/src/index.js
+++ anna-sala/challenges/toh-full-stack-ddbb/backend/src/index.js
@@ -1,16 +1,25 @@
 const express = require('express');
+require('dotenv').config();
 
 const server = express();
 
 const cors = require('cors');
+const morgan = require('morgan');
 const chalk = require('chalk');
 const debug = require('debug')('server');
-const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 server.use(express.json());
-
 server.use(cors());
 server.use(morgan('dev'));
+
+mongoose.connect(
+  process.env.DDBB_URL,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  },
+);
 
 const heroesRouter = require('./routes/heroesRouter');
 
