534dbb622c94583a091c76424d34865603574f9a
Anna Sala <anna.sala.mercade@gmail.com>
2021-05-11 07:32:15 +0200

    index in frontend update to call function store in provider

--- anna-sala/challenges/toh-full-stack/frontend/src/index.js
+++ anna-sala/challenges/toh-full-stack/frontend/src/index.js
@@ -9,7 +9,7 @@ import store from './redux/stores';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={store()}>
       <App />
     </Provider>
   </React.StrictMode>,
