1de8de9ad409c1ace72effa84a7f9821a5143576
CheCabeza <chemartinc@gmail.com>
2021-05-12 12:48:56 +0200

    Added some functions to actionCreators

--- Equipo099/client/099ecommerce/src/redux/actions/actionCreators.js
+++ Equipo099/client/099ecommerce/src/redux/actions/actionCreators.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import actionTypes from './actionTypes';
 
-const url = 'http://localhost:2021/ecommerce';
+const url = 'http://localhost:2022/';
 
 export function loadItems() {
   return async (dispatch) => {
@@ -16,5 +16,35 @@ export function loadItems() {
         type: 'LOAD_ITEMS_ERROR',
       });
     }
+  };
+}
+
+export function addItem(item) {
+  return async (dispatch) => {
+    const { data } = await axios.post(url, item);
+    dispatch({
+      type: actionTypes.ADD_ITEM,
+      hero: data,
+    });
+  };
+}
+
+export function deleteItem(itemId) {
+  return async (dispatch) => {
+    await axios.delete(`${url}/${itemId}`);
+    dispatch({
+      type: actionTypes.DELETE_ITEM,
+      itemId,
+    });
+  };
+}
+
+export function updateHero(item) {
+  return async (dispatch) => {
+    const { data } = await axios.put(`${url}/${item.id}`, item);
+    dispatch({
+      type: actionTypes.UPDATE_ITEM,
+      item: data,
+    });
   };
 }
