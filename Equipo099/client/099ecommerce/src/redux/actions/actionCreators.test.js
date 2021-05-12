936f9e86c8e9291e5582bfa79d2a0a78117552c0
Daniel Martorell <dani.martorell@gmail.com>
2021-05-12 19:09:06 +0200

    Add actionCreator test file

---
+++ Equipo099/client/099ecommerce/src/redux/actions/actionCreators.test.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { loadItems } from './actionCreators';
+import actionTypes from './actionTypes';
+
+jest.mock('axios');
+
+describe('loadItems', () => {
+    test('should dispatch LOAD_ITEMS', async () => {
+        axios.mockResolvedValue({ data: [{id: 1, name: 'crema', price: 200}] });
+        const dispatch = jest.fn();
+        await loadItems()(dispatch);
+
+        expect(dispatch).toHaveBeenCalledWith({
+            type: actionTypes.LOAD_HEROES,
+            heroes: [{ data: [{id: 1, name: 'crema', price: 200}] }],
+          });
+    });
+});
+
+
