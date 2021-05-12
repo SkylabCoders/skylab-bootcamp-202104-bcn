9a32ab8154ed8e103254da64a370c2abdc501428
Fernando Gomez <shadyfast8.services@gmail.com>
2021-05-12 12:42:30 +0200

    Add routes to backend index

--- bon-prufit/backend/src/index.js
+++ bon-prufit/backend/src/index.js
@@ -21,8 +21,10 @@ server.use(express.json());
 
 server.use(morgan('dev'));
 
-// const heroesRouter = require('./routes/heroesRouter');
+const productsRouter = require('./routes/productsRouter');
+const cartsRouter = require('./routes/cartsRouter');
 
-// server.use('/heroes', heroesRouter);
+server.use('/products', productsRouter);
+server.use('/carts', cartsRouter);
 
 server.listen('2021', () => debug(`Server is running in ${chalk.yellow('localhost:2021')}`));
