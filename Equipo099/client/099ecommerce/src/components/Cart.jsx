55252ee86afb65c60f88ca8dfc91cb6c3b26f559
Josep Suarez <djose.anton@gmail.com>
2021-05-12 12:34:00 +0200

    App running with 2 components

---
+++ Equipo099/client/099ecommerce/src/components/Cart.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const CartItems = () => {
+    return (
+        <h1>Cart items</h1>
+    )
+}
+export default CartItems
---
+++ Equipo099/client/099ecommerce/src/components/List.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const ListItems = () => {
+    return (
+        <h1>List items</h1>
+    )
+}
+export default ListItems
