395ce2cb9e330f7c864fd5f339b6c9d434a18cd1
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-05-12 21:06:33 +0200

    Implement Add to cart button

--- row1-promofarma/frontend/src/components/Product/index.jsx
+++ row1-promofarma/frontend/src/components/Product/index.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
+import { addProducts } from '../../redux/actions/actionCreators';
 
-function Product({ productItem: { productName, stock, price } }) {
+function Product({
+  productItem: {
+    _id, productName, stock, price
+  }, dispatch
+}) {
+  function handleAddItemToCart() {
+    const newProduct = {
+      _id, productName, stock, price
+    };
+    newProduct.stock -= 1;
+    dispatch(addProducts(newProduct));
+  }
+
   return (
     <>
       <li>
         {`Product name: ${productName}`}
-        <button type="button">ADD to cart</button>
+        <button type="button" onClick={handleAddItemToCart}>ADD to cart</button>
         <ul>
           <li>{`Stock: ${stock}`}</li>
           <li>{`Price ${price} €`}</li>
@@ -18,7 +31,9 @@ function Product({ productItem: { productName, stock, price } }) {
 }
 
 Product.propTypes = {
+  dispatch: PropTypes.func.isRequired,
   productItem: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
     productName: PropTypes.string.isRequired,
     stock: PropTypes.number.isRequired,
     price: PropTypes.number.isRequired
--- row1-promofarma/frontend/src/redux/reducers/productsReducer.js
+++ row1-promofarma/frontend/src/redux/reducers/productsReducer.js
@@ -1,8 +1,18 @@
+/* eslint-disable no-underscore-dangle */
 import actionTypes from '../actions/actionTypes';
 
 function productsReducer(products = [], action) {
+  const newProducts = [...products];
   if (action.type === actionTypes.LOAD_PRODUCTS) {
     return action.products;
+  }
+
+  if (action.type === actionTypes.ADD_PRODUCTS) {
+    return newProducts.map(
+      (product) => (product._id === action.product._id
+        ? { ...product, ...action.product }
+        : product)
+    );
   }
   return products;
 }
