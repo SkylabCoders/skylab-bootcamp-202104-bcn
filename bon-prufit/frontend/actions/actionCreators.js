9c45ec5b22a289a754085c15fbf846bfb3352d73
Laura <laura.sabater.musach@gmail.com>
2021-05-12 12:33:29 +0200

    Create url constants

--- bon-prufit/frontend/actions/actionCreators.js
+++ bon-prufit/frontend/actions/actionCreators.js
@@ -1,43 +1,44 @@
 import axios from 'axios';
 import actionTypes from './actionTypes';
 
-const url = 'http://localhost:2021/heroes';
+const productsUrl = 'http://localhost:2021/products';
+const cartsUrl = 'http://localhost:2021/carts';
 
 export function loadProducts() {
   return async (dispatch) => {
     try {
-      const { data } = await axios(url);
+      const { data } = await axios(productsUrl);
       dispatch({
         type: actionTypes.LOAD_PRODUCTS,
         prducts: data,
       });
     } catch (error) {
       dispatch({
-        type: actionTypes.ERROR
+        type: actionTypes.ERROR_PRODUCT
       });
     }
   };
 }
 
-export function addProduct(product) {
+export function addProductToCart(product) {
   return async (dispatch) => {
     try {
-      const { data } = await axios.post(url, product);
+      const { data } = await axios.post(productsUrl, product);
       dispatch({
         type: actionTypes.ADD_PRODUCT,
         product: data,
       });
     } catch (error) {
       dispatch({
-        type: actionTypes.ERROR,
+        type: actionTypes.ERROR_PRODUCT,
       });
     }
   };
 }
 
-export function deleteProduct(productId) {
+export function deleteProductFromCart(productId) {
   return async (dispatch) => {
-    await axios.delete(`${url}/${productId}`);
+    await axios.delete(`${cartsUrl}/${productId}`);
     dispatch({
       type: actionTypes.DELETE_PRODUCT,
       productId,
