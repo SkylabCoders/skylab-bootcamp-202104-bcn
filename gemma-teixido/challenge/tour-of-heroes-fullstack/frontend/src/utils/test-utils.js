561a12e28943c93b241461bc88ab2b2f10334bcd
gteixi <gemmateixido25@gmail.com>
2021-05-12 11:26:18 +0200

    Remove folders.

--- gemma-teixido/challenge/tour-of-heroes-fullstack/frontend/src/utils/test-utils.js
+++ gemma-teixido/challenge/tour-of-heroes-fullstack/frontend/src/utils/test-utils.js
@@ -1,10 +1,8 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-filename-extension */
-// test-utils.js
 import React from 'react';
 import { render as rtlRender } from '@testing-library/react';
 import { Provider } from 'react-redux';
-// Import your own reducer
 import { BrowserRouter } from 'react-router-dom';
 import configureStore from '../redux/stores/index';
 
@@ -13,7 +11,7 @@ function render(
   {
     initialState,
     ...renderOptions
-  } = {},
+  } = {}
 ) {
   function Wrapper({ children }) {
     return (
@@ -27,7 +25,5 @@ function render(
   return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
 }
 
-// re-export everything
 export * from '@testing-library/react';
-// override render method
 export { render };
