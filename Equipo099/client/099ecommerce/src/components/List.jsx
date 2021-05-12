7597d8440cff0754ea3d1cd865734fd340f84aef
Josep Suarez <djose.anton@gmail.com>
2021-05-12 12:56:58 +0200

    component list and cart OK

--- Equipo099/client/099ecommerce/src/components/List.jsx
+++ Equipo099/client/099ecommerce/src/components/List.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import itemsCart from '../constants/items'
 
 const ListItems = () => {
+    // function addToCart(itemId){
+    //     dispatch(addItem(itemId));
+    // }
     return (
-        <h1>List items</h1>
+        itemsCart.map(item =>{
+           return (
+               <>
+            <p>{item.name}</p>
+            <p>{"Precio: "+item.price}</p>
+            <p>{"Stock: "+item.stock}</p>
+            <button type="button" onClick={() => console.log((item.id))}>Add to Cart</button>
+            </>
+            ) 
+        })
     )
 }
 export default ListItems
