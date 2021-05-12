42aa774fceafcfb15b4bd613e5b48c3354c8e1f8
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-05-12 12:30:05 +0200

    Create products data mock file

---
+++ bon-prufit/backend/src/data/products.js
@@ -0,0 +1,40 @@
+const products = [
+  {
+    title: 'Maybena 30 Days Mask Sheet',
+    cost: 200,
+    url: 'https://picsum.photos/200'
+  },
+  {
+    title: 'Avene A-Oxitive Sérum Defensa Anti-Oxidante 30ml',
+    cost: 300,
+    url: 'https://picsum.photos/200'
+  },
+  {
+    title: 'Heliocare 360º SPF50+ gel oil-free 50ml',
+    cost: 200,
+    url: 'https://picsum.photos/200'
+  },
+  {
+    title: 'Maybena 30 Days Mask Sheet',
+    cost: 100,
+    url: 'https://picsum.photos/200'
+  },
+  {
+    title: 'Maybena 30 Days Mask Sheet',
+    cost: 50,
+    url: 'https://picsum.photos/200'
+  },
+  {
+    title: 'Maybena 30 Days Mask Sheet',
+    cost: 10,
+    url: 'https://picsum.photos/200'
+  },
+  {
+    title: 'Maybena 30 Days Mask Sheet',
+    cost: 200,
+    url: 'https://picsum.photos/200'
+  }
+
+];
+
+module.exports = products;
