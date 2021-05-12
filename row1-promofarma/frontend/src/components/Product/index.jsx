dcc2e8d7be202ef6d05e27fb0a837f95abce9f44
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-05-12 18:38:49 +0200

    Display product info

--- row1-promofarma/frontend/src/components/Product/index.jsx
+++ row1-promofarma/frontend/src/components/Product/index.jsx
@@ -2,15 +2,26 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
 
-function Product({ productItem: { productName } }) {
+function Product({ productItem: { productName, stock, price } }) {
   return (
-    <li>{productName}</li>
+    <>
+      <li>
+        {`Product name: ${productName}`}
+        <button type="button">ADD to cart</button>
+        <ul>
+          <li>{`Stock: ${stock}`}</li>
+          <li>{`Price ${price} €`}</li>
+        </ul>
+      </li>
+    </>
   );
 }
 
 Product.propTypes = {
   productItem: PropTypes.shape({
-    productName: PropTypes.string.isRequired
+    productName: PropTypes.string.isRequired,
+    stock: PropTypes.number.isRequired,
+    price: PropTypes.number.isRequired
   }).isRequired
 };
 
