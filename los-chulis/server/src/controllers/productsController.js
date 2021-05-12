fd6fd5792ce8c09b3274b669b36ed677f83fd83b
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-05-12 13:19:51 +0200

    CreateOne controller done

--- los-chulis/server/src/controllers/productsController.js
+++ los-chulis/server/src/controllers/productsController.js
@@ -1,3 +1,4 @@
+const debug = require('debug')('app:heroesController');
 const Product = require('../model/productModel');
 
 function productsController() {
@@ -5,7 +6,19 @@ function productsController() {
     const products = await Product.find();
     res.json(products);
   }
-  return { getAllProducts };
+
+  async function createOne(req, res) {
+    const newProduct = new Product(req.body);
+    debug(newProduct);
+    try {
+      await newProduct.save();
+      res.json(newProduct);
+    } catch (error) {
+      debug(error);
+      res.send(error);
+    }
+  }
+  return { getAllProducts, createOne };
 }
 
 module.exports = productsController;
