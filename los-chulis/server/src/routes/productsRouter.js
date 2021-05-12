55794746eed69f842677d6b4c5af28a01ef172ea
Anna Sala <anna.sala.mercade@gmail.com>
2021-05-12 13:01:57 +0200

    resolve conflicts

---
+++ los-chulis/server/src/routes/productsRouter.js
@@ -0,0 +1,21 @@
+const { Router } = require('express');
+const productsController = require('../controllers/productsControllers')();
+
+function productsRouter() {
+  const routes = Router();
+
+  routes
+    .route('/')
+    .get(productsController.getAllProducts)
+    .post(productsController.createOne);
+
+  routes
+    .route('/:productId')
+    .get(productsController.getById)
+    .put(productsController.updateById)
+    .delete(productsController.deleteById);
+
+  return routes;
+}
+
+module.exports = productsRouter();
