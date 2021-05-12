b2b12943a88afe8b2a3919f3155ba522c6e6b1e5
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-05-12 13:23:32 +0200

    Create products component

--- row1-promofarma/frontend/src/App.jsx
+++ row1-promofarma/frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Provider } from 'react-redux';
+import configureStore from './redux/store';
+import ProductList from './components/ProductsList';
 
 function App() {
   return (
-    <div>
-      <h1>Team frontend</h1>
-    </div>
+    <Provider store={configureStore}>
+      <div>
+        <h1>Team frontend</h1>
+        <ProductList />
+      </div>
+    </Provider>
+
   );
 }
 
---
+++ row1-promofarma/frontend/src/components/ProductsList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProductList() {
+  return (
+    <div>
+      <h3>PRODUCTS</h3>
+    </div>
+  );
+}
+
+export default ProductList;
