8bb059bce3d45d19de80472df180c44ddefa12e1
blopez-dev <pjgl.frontend@gmail.com>
2021-05-12 16:10:14 +0200

    Updated products

--- row1-promofarma/backend/src/model/productModel.js
+++ row1-promofarma/backend/src/model/productModel.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
-
   productName: String,
   stock: Number,
   price: Number,
+  product_poster: String
 
 });
 
--- row1-promofarma/backend/src/products.js
+++ row1-promofarma/backend/src/products.js
@@ -1,20 +1,37 @@
 const products = [
   {
     _id: 1,
-    productName: 'Patatas',
+    productName: 'Caudalie Vinoperfect Esencia Luminosidad 100ml',
     stock: 100,
-    price: 2
+    price: 17.01,
+    product_poster: 'https://statics.promofarma.com/static/promofarma/prod/product_images/mr/FYNA9_es_ES_0.jpg'
   },
   {
     _id: 2,
-    productName: 'Champu',
-    stock: 4,
-    price: 3
+    productName: 'Caudalie Vinoperfect esencia resplandor concentrado 150ml',
+    stock: 40,
+    price: 19.60,
+    product_poster: 'https://statics.promofarma.com/static/promofarma/prod/product_images/mr/FYNA9_es_ES_0.jpg'
   },
   {
     _id: 3,
-    productName: 'Leche',
-    stock: 15,
-    price: 1
+    productName: 'Caudalie Vinoperfect sérum resplandor antimanchas 30ml',
+    stock: 150,
+    price: 40.66,
+    product_poster: 'https://image.promofarma.com/product/115752/sd/3017382/large/coupon_image_1.jpg'
+  },
+  {
+    _id: 4,
+    productName: 'Caudalie Vinoperfect Crema Glycolic Night 50Ml',
+    stock: 120,
+    price: 27.29,
+    product_poster: 'https://image.promofarma.com/product/115752/sd/3017382/large/coupon_image_1.jpg'
+  },
+  {
+    _id: 5,
+    productName: 'Caudalie Vinoperfect fluido piel perfecta SPF20+ 40ml',
+    stock: 50,
+    price: 21.89,
+    product_poster: 'https://statics.promofarma.com/static/promofarma/prod/product_images/mr/Q805E_es_ES_0.jpg'
   },
 ]
