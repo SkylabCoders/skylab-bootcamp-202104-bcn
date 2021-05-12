866cb0a6f74c7fc54829a683a3528488f30af1ba
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-05-12 13:38:39 +0200

    Connect productList component with store

--- row1-promofarma/frontend/src/App.jsx
+++ row1-promofarma/frontend/src/App.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import './App.css';
-import { Provider } from 'react-redux';
-import configureStore from './redux/store';
-import ProductList from './components/ProductsList';
+import ProductList from './components/product-list/ProductsList';
 
 function App() {
   return (
-    <Provider store={configureStore}>
-      <div>
-        <h1>Team frontend</h1>
-        <ProductList />
-      </div>
-    </Provider>
-
+    <div>
+      <h1>Team frontend</h1>
+      <ProductList />
+    </div>
   );
 }
 
--- row1-promofarma/frontend/src/redux/reducers/index.js
+++ row1-promofarma/frontend/src/redux/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import products from './productsReducer';
 
 const rootReducer = combineReducers({
-
+  products
 });
 
 export default rootReducer;
