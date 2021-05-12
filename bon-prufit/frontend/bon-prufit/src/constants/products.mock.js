9e71561427c1499e6c62a4ed228c45e59883e43c
IrenePerezAlonso <irperal@hotmail.com>
2021-05-12 15:59:06 +0200

    change products.mock.js

--- bon-prufit/frontend/bon-prufit/src/constants/products.mock.js
+++ bon-prufit/frontend/bon-prufit/src/constants/products.mock.js
@@ -1,19 +1,22 @@
 const PRODUCTS = [
-    {
-      id: 1,
-      name: 'shampoo',
-      price: 10
-    },
-    {
-      id: 2,
-      name: 'perro',
-      price: 200
-    },
-    {
-      id: 3,
-      name: 'smartphone',
-      price: 300
-    }
+  {
+    title: 'Maybena 30 Days Mask Sheet',
+    cost: 200,
+    url: 'https://picsum.photos/200',
+    stock: 30
+  },
+  {
+    title: 'Avene A-Oxitive Sérum Defensa Anti-Oxidante 30ml',
+    cost: 300,
+    url: 'https://picsum.photos/200',
+    stock: 30
+  },
+  {
+    title: 'Heliocare 360º SPF50+ gel oil-free 50ml',
+    cost: 200,
+    url: 'https://picsum.photos/200',
+    stock: 20
+  }
   ];
 
 export default PRODUCTS;
