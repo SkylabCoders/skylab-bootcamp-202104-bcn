aab83a006ad4c74d437809200b42c37bf7cf6dfd
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-05-12 12:58:26 +0200

    Create constants folder and actionCreators.js

---
+++ row1-promofarma/frontend/src/redux/actions/actionCreators.js
@@ -0,0 +1,26 @@
+import actionTypes from './actionTypes';
+
+export function addProducts(newProduct) {
+  return {
+    type: actionTypes.ADD_TASK,
+    newProduct
+  };
+}
+
+export function loadProducts() {
+  return {
+    type: actionTypes.LOAD_PRODUCTS
+  };
+}
+
+export function deleteProduct() {
+  return {
+    type: actionTypes.DELETE_PRODUCT
+  };
+}
+
+export function deleteAll() {
+  return {
+    type: actionTypes.DELETE_ALL
+  };
+}
