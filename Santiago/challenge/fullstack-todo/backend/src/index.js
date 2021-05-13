d84c90e2d885be07087da193c1682b55e6ab68a9
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-05-13 11:57:50 +0200

    Create server without db connection and install nodemon

--- Santiago/challenge/fullstack-todo/backend/src/index.js
+++ Santiago/challenge/fullstack-todo/backend/src/index.js
@@ -3,26 +3,22 @@ const cors = require('cors');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 require('dotenv').config();
 
 const server = express();
 
-mongoose.connect(
-  process.env.DDBB_URL,
-  {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-  }
-);
+// mongoose.connect(
+//   process.env.DDBB_URL,
+//   {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true
+//   }
+// );
 
 server.use(cors());
 server.use(express.json());
 server.use(morgan('dev'));
 
-const heroesRouter = require('./routes/heroesRouter');
-
-server.use('/heroes', heroesRouter);
-
-server.listen('2021',
-  () => debug(`Server is running in ${chalk.yellow('localhost:2021')}`));
+server.listen('3103',
+  () => debug(`Server is running in ${chalk.yellow('localhost:3103')}`));
