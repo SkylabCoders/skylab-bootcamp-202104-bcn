333f7e815e5fcd36e62f2578fac5a2d3e93e0bf0
Josep Suarez <djose.anton@gmail.com>
2021-05-13 09:20:09 +0200

    merge dmartorell

--- Equipo099/client/099ecommerce/src/redux/actions/actionCreators.js
+++ Equipo099/client/099ecommerce/src/redux/actions/actionCreators.js
@@ -6,81 +6,56 @@ const url = 'http://localhost:2099/products';
 
 export function loadItems() {
   debugger;
-  return async (dispatch) => {
-    try {
+  try {
+    return async (dispatch) => {
       const { data } = await axios(url);
       dispatch({
         type: actionTypes.LOAD_ITEMS,
         items: data
       });
-    } catch(error){
-      dispatch({
-        type: 'LOAD_ITEMS_ERROR',
-      })
-    }
+    };
+  } catch(error){
+    console.error(error);
   }
 }
 
 export function addItem(item) {
   return async (dispatch) => {
-    try {
-      const { data } = await axios.post(url, item);
-      dispatch({
-        type: actionTypes.ADD_ITEM,
-        items: data
-      });
-    } catch(error){
-      dispatch({
-        type: 'ADD_ITEM_ERROR',
-      })
-    }
+    const { data } = await axios.post(url, item);
+    dispatch({
+      type: actionTypes.ADD_ITEM,
+      item: data
+    });
   };
 }
 
 export function deleteItem(itemId) {
-  return async (dispatch) => {
-    try{
-      const {data} = await axios.delete(`${url}/${itemId}`);
-      dispatch({
-        type: actionTypes.DELETE_ITEM,
-        items: data,
-      });
-    } catch(error) {
-      dispatch({
-        type: 'DELETE_ITEM_ERROR',
-      })
-    }
-  };
-}
+    return {
+      type: actionTypes.DELETE_ITEM,
+      itemId
+    };
+};
 
 export function updateItem(item) {
   return async (dispatch) => {
-    try {
-      const { data } = await axios.put(`${url}/${item.id}`, item);
-      dispatch({
-        type: actionTypes.UPDATE_ITEM,
-        items: data
-      });
-    } catch(error){
-      dispatch({
-        type: 'UPDATE_ITEM_ERROR'
-      })
-    }
+    const { data } = await axios.put(`${url}/${item.id}`, item);
+    dispatch({
+      type: actionTypes.UPDATE_ITEM,
+      item: data
+    });
   };
 }
 
-export function loadItemById(itemId) {
-  return async (dispatch) => {
-    try {
-      const { data } = await axios(`${url}/${itemId}`);
-      dispatch({
-        type: actionTypes.LOAD_ITEM,
-        items: data
-      });
-    } catch(error){
-      dispatch({
-        type: 'LOAD_ITEM_ERROR'
-      })
-    } 
+export function loadItemById(item) {
+  debugger
+    return {
+      type: actionTypes.ADD_ITEM,
+      item
+    };
+}
+
+export function loadCart() {
+  return {
+    type: actionTypes.LOAD_ITEM,
   };
 }
