bf060056b61cc6dc29bbdb0ac6a1298185485daa
Edgar Alandete <edgalandete@gmail.com>
2021-05-13 13:26:22 +0200

    Create test task creation

--- Edgar-Alandete/challenges/todo-list-fullstack/backend/src/controllers/tasksController.test.js
+++ Edgar-Alandete/challenges/todo-list-fullstack/backend/src/controllers/tasksController.test.js
@@ -1,4 +1,4 @@
-const { getAll } = require('./tasksController')();
+const { getAll, createTask } = require('./tasksController')();
 const Task = require('../model/taskModel');
 
 jest.mock('./../model/taskModel');
@@ -24,5 +24,49 @@ describe('Given a getAll function', () => {
     Task.find.mockRejectedValueOnce('Error al cargar la lista de tareas');
     await getAll(null, res);
     expect(res.send).toHaveBeenCalledWith('Error al cargar la lista de tareas');
+  });
+});
+
+describe('Given a createTask function', () => {
+  class MockTask {
+    constructor(name) {
+      this.name = name;
+      this.isFinished = false;
+    }
+
+    // eslint-disable-next-line class-methods-use-this
+    save() {}
+  }
+  test('A task is created', async () => {
+    const req = {
+      body: null,
+    };
+
+    const res = {
+      json: jest.fn(),
+      send: jest.fn(),
+    };
+
+    const newTask = new MockTask('Comprar pan');
+    Task.mockReturnValueOnce(newTask);
+    await createTask(req, res);
+    expect(res.json).toHaveBeenCalledWith({ isFinished: false, name: 'Comprar pan' });
+  });
+
+  test('An error is send', async () => {
+    const req = {
+      body: null,
+    };
+    const res = {
+      json: jest.fn(),
+      send: jest.fn(),
+    };
+
+    Task.mockReturnValueOnce({
+      save: jest.fn().mockRejectedValueOnce('error'),
+    });
+
+    await createTask(req, res);
+    expect(res.send).toHaveBeenCalledWith('error');
   });
 });
