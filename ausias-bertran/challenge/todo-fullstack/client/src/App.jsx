f44929209e8f613cde3d5c5d984b7718475572a8
ulldecorb <ausias.bertran@gmail.com>
2021-05-13 12:11:08 +0200

    Add chalk cors debug dotenv express jest mongoose morgan nodemon libraries to server

--- ausias-bertran/challenge/todo-fullstack/client/src/App.jsx
+++ ausias-bertran/challenge/todo-fullstack/client/src/App.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import Clicker from './components/Clicker';
 
 function App() {
   return (
-    <div className="App">
-      <Clicker />
-    </div>
+    <div className="App" />
   );
 }
 
