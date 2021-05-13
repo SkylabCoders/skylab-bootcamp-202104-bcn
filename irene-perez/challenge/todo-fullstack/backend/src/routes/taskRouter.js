8b1533f0720a5176e32f6f17d60566e9c794a97a
IrenePerezAlonso <irperal@hotmail.com>
2021-05-13 12:20:31 +0200

    Create a taskRouter file at routes folder

---
+++ irene-perez/challenge/todo-fullstack/backend/src/routes/taskRouter.js
@@ -0,0 +1,18 @@
+const { Router } = require('express');
+const taskController = require('../controllers/taskController');
+
+function taskRouter() {
+  const routes = Router();
+
+  routes
+    .route('/')
+    .post(taskController.createTask);
+
+  routes
+    .route('/:taskId')
+    .delete(taskController.deleteTaskById);
+
+  return routes;
+}
+
+module.exports = taskRouter();
