6e745f394ccbf9b1db152572f9aef760cebee4bc
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-05-13 11:25:30 +0200

    Install redux-thunk

---
+++ alfredo-zimperz/challenges/todo-fullstack/frontend/src/components/Dashboard/index.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import { loadTasks } from '../../redux/actions/actionCreators';
+import Task from '../Task';
+import './dashboard.css';
+
+const Dashboard = ({ tasks, dispatch }) => {
+  useEffect(() => {
+    if (!tasks.length) dispatch(loadTasks());
+  }, []);
+
+  const pendingTasks = tasks.filter((task) => !task.done);
+
+  return (
+    <>
+      <div className="dashboard">
+        <h2>Dashboard</h2>
+        <p>
+          Total tasks:
+          {' '}
+          {tasks.length}
+        </p>
+        <p>
+          Pending tasks:
+          {' '}
+          {pendingTasks.length}
+        </p>
+      </div>
+      <div className="pendingTasks">
+        <h3>Pending tasks</h3>
+        {pendingTasks.map((task) => <Task task={task} key={task.id} />)}
+      </div>
+    </>
+  );
+};
+
+Dashboard.propTypes = {
+  tasks: PropTypes.shape([]).isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+function mapStateToProps(store) {
+  return {
+    tasks: store.tasks,
+  };
+}
+
+export default connect(mapStateToProps)(Dashboard);
