8e3abae5bb42b2cd56b94594de66a6771f4ae85f
Daniel Martorell <dani.martorell@gmail.com>
2021-05-13 17:03:44 +0200

    Add propTypes to List component

--- dani-martorell/challenge/todo-list-redux-fullStack/client/src/components/List/index.jsx
+++ dani-martorell/challenge/todo-list-redux-fullStack/client/src/components/List/index.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+
 import './list.css';
 
-function List() {
+function List({ tasks, dispatch }) {
   return (
     <h3>List component</h3>
   );
 }
 
-export default List;
+List.propTypes = {
+  tasks: PropTypes.shape([{}]).isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+function mapStateToProps({ tasks }) {
+  return {
+    tasks,
+  };
+}
+
+export default connect(mapStateToProps)(List);
