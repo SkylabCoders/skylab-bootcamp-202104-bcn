8c7375d408dc45c2203c5acfb25f3c1c5e2d34c0
Laura <laura.sabater.musach@gmail.com>
2021-05-13 13:06:16 +0200

    Create taskRouter file

---
+++ laura-sabater/challenges/todo-list-fullstack/backend/src/routes/tasksRouter.js
@@ -0,0 +1,21 @@
+const { Router } = require('express');
+// const products = require('../data/products');
+const tasksController = require('../controllers/tasksController')();
+
+function tasksRouter() {
+  const routes = Router();
+
+  routes
+    .route('/')
+    .post(tasksController.addTask);
+
+  // routes
+  //   .route('/:productId/')
+  //   .put(productsController.addOneToStock)
+  //   .delete(productsController.removeOneFromStock);
+  // // .get(productsController.getById);
+
+  return routes;
+}
+
+module.exports = tasksRouter();
