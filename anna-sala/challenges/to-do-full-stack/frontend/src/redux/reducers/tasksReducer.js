fc94bf5033df10621e2463a99d4ea4dd5d2486fe
Anna Sala <anna.sala.mercade@gmail.com>
2021-05-13 15:30:30 +0200

    Adapt reducer to return data from Api

--- anna-sala/challenges/to-do-full-stack/frontend/src/redux/reducers/tasksReducer.js
+++ anna-sala/challenges/to-do-full-stack/frontend/src/redux/reducers/tasksReducer.js
@@ -1,10 +1,9 @@
 import actionTypes from '../actions/actionTypes';
-import TASKS from '../../constants/tasks.mock';
 
 function tasksReducer(tasks = [], action) {
   switch (action.type) {
     case actionTypes.LOAD_TASKS:
-      return TASKS;
+      return action.tasks;
     case actionTypes.DELETE_TASK:
       return tasks.filter((task) => task.id !== action.taskId);
     case actionTypes.ADD_TASK:
