c1849ca79aff68ca5b7eddf061cebcd6132c1ec1
Daniel Martorell <dani.martorell@gmail.com>
2021-05-13 11:34:41 +0200

    Add taskController

---
+++ dani-martorell/challenge/todo-list-redux-fullStack/server/src/controllers/tasksController.js
@@ -0,0 +1,63 @@
+const Task = require('../model/taskModel');
+
+function tasksController() {
+  async function getAll(req, res) {
+    const tasks = await Task.find();
+    res.json(tasks);
+  }
+
+  async function createOne(req, res) {
+    const newTask = new Task(req.body);
+    try {
+      await newTask.save();
+      res.json(newTask);
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  async function getById(req, res) {
+    try {
+      const taskById = await Task.findById(
+        req.params.heroId,
+      );
+      res.json(taskById);
+    } catch (error) {
+      res.status(404);
+      res.send(error);
+    }
+  }
+
+  async function updateById(req, res) {
+    try {
+      const updatedTask = await Task.findByIdAndUpdate(
+        req.params.taskId,
+        req.body,
+        { new: true },
+      );
+      res.json(updatedTask);
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  async function deleteById(req, res) {
+    try {
+      await Task.findByIdAndDelete(req.params.taskId);
+      res.status(204);
+      res.json();
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  return {
+    getAll,
+    createOne,
+    getById,
+    updateById,
+    deleteById,
+  };
+}
+
+module.exports = tasksController;
