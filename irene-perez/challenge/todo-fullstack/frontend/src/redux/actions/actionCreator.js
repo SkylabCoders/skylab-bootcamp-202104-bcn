fb1ca16faedf5fac4cdf2f1e968fff591ab08769
IrenePerezAlonso <irperal@hotmail.com>
2021-05-13 16:05:44 +0200

    Create functions at actionCreator file

--- irene-perez/challenge/todo-fullstack/frontend/src/redux/actions/actionCreator.js
+++ irene-perez/challenge/todo-fullstack/frontend/src/redux/actions/actionCreator.js
@@ -4,17 +4,23 @@ import actionTypes from './actionTypes';
 const tasksUrl = 'http://localhost:2021/todo';
 
 export function addTask(task) {
-  return ({
-    type: actionTypes.ADD_TASK,
-    task
-  });
+  return async (dispatch) => {
+    const { data } = await axios.post(tasksUrl, task);
+    dispatch({
+      type: actionTypes.ADD_TASK,
+      task: data
+    });
+  };
 }
 
 export function deleteTask(taskId) {
-  return ({
-    type: actionTypes.DELETE_TASK,
-    taskId
-  });
+  return async (dispatch) => {
+    await axios.delete(`${tasksUrl}/${taskId}`);
+    dispatch({
+      type: actionTypes.DELETE_TASK,
+      taskId
+    });
+  };
 }
 
 export function loadTasks() {
