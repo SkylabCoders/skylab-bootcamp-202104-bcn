03ff7e453b5dcbe2262f107819c9c10b6efe1e1e
Emerson Fernandes <emerson_feral@hotmail.com>
2021-05-13 15:58:19 +0200

    Adding action types and action creators

---
+++ emerson-fernandes/challenge-todolist-fullstack/frontend/src/redux/actions/actionCreator.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import actionTypes from './actionTypes';
+
+const productsUrl = 'http://localhost:2021/todo';
+
+export function addTask(task) {
+  return ({
+    type: actionTypes.ADD_TASK,
+    task
+  });
+}
+export function deleteTask(taskId) {
+  return ({
+    type: actionTypes.DELETE_TASK,
+    taskId
+  });
+}
+export function loadTasks() {
+  return async (dispatch) => {
+    const { data } = await axios(taskUrl);
+    dispatch({
+      type: actionTypes.LOAD_TASK,
+      products: data
+    });
+    dispatch({
+      type: actionTypes.ERROR_PRODUCT
+    });
+  };
+}
