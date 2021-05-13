b96eed00f8b547bf408a54c5c454303f9469adf7
cristian-bars <patxi.cristian@gmail.com>
2021-05-13 12:04:54 +0200

    Configure start strings

--- cristian-bars/challenge/todo-list-redux-server/server/.eslintrc.js
+++ cristian-bars/challenge/todo-list-redux-server/server/.eslintrc.js
@@ -3,18 +3,19 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
+    jest: true
   },
   extends: [
-    'airbnb-base',
+    'airbnb-base'
   ],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   rules: {
     'comma-dangle': [2, 'never'],
     'linebreak-style': 0,
     'global-require': 0,
-    'eslint linebreak-style': [0, 'error', 'windows'],
-  },
+    'eslint linebreak-style': [0, 'error', 'windows']
+  }
 };
