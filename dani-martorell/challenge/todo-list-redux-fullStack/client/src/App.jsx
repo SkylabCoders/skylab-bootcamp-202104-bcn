5b65e6f945555400f0a74615d59f2216618afb0f
Daniel Martorell <dani.martorell@gmail.com>
2021-05-13 16:35:16 +0200

    Finish Input component

--- dani-martorell/challenge/todo-list-redux-fullStack/client/src/App.jsx
+++ dani-martorell/challenge/todo-list-redux-fullStack/client/src/App.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from './redux/store';
-
 import './App.css';
+import List from './components/List';
+import Input from './components/Input';
 
 function App() {
   return (
     <Provider store={configureStore()}>
-
-      <h1>Tour of Heroes</h1>
-
+      <h1>Tasks Organizer</h1>
+      <Input />
+      <List />
     </Provider>
   );
 }
