93acac4603e4b3cb6a191472612015e3c0cb6522
Anna Sala <anna.sala.mercade@gmail.com>
2021-05-13 12:16:13 +0200

    Create get all and create one controller

---
+++ anna-sala/challenges/to-do-full-stack/backend/src/controllers/tasksController.js
@@ -0,0 +1,26 @@
+const debug = require('debug')('server:tasksController');
+const Task = require('../model/taskModel');
+
+function tasksController() {
+  debug('dentro de getAll');
+  async function getAll(req, res) {
+    const tasks = await Task.find();
+    res.json(tasks);
+  }
+
+  async function createTask(req, res){
+      const newTask = new Task(req.body);
+      try{
+        await newTask.save()
+        res.json(newTask)
+      }catch (error){
+        res.send(error);
+      }
+  }
+  return {
+    getAll,
+    createTask,
+  };
+}
+
+module.exports = tasksController;
