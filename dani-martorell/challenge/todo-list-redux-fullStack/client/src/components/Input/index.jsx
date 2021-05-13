9726a44d5d929ef5c365a4e7b332196caa00ff02
Daniel Martorell <dani.martorell@gmail.com>
2021-05-13 19:17:07 +0200

    Add functionality to input button

--- dani-martorell/challenge/todo-list-redux-fullStack/client/src/components/Input/index.jsx
+++ dani-martorell/challenge/todo-list-redux-fullStack/client/src/components/Input/index.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import { React, useState } from 'react';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import { addTask } from '../../redux/actions/actionCreators';
+
 import './input.css';
 
-function Input() {
+function Input({ dispatch }) {
+  const [taskName, setTaskName] = useState('');
+  function handleInputChange(e) {
+    setTaskName(e.target.value);
+  }
+  function handleClick() {
+    const newTask = { name: taskName };
+    dispatch(addTask(newTask));
+  }
+
   return (
     <form className="form-block">
-      <input className="form-block__input" type="text" id="fname" size="50" />
-      <button type="button" className="form-block__btn">Add Task</button>
+      <input className="form-block__input" onChange={(e) => handleInputChange(e)} type="text" id="fname" size="50" />
+      <button type="button" className="form-block__btn" onClick={handleClick}>Add Task</button>
     </form>
   );
 }
 
-export default Input;
+Input.propTypes = {
+//   tasks: PropTypes.shape([{}]).isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+function mapStateToProps({ tasks }) {
+  return {
+    tasks,
+  };
+}
+
+export default connect(mapStateToProps)(Input);
