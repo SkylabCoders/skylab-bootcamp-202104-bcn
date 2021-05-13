50b5afa06a83fc03c1d3faf681ffbf73ddd96dfc
Laura <laura.sabater.musach@gmail.com>
2021-05-13 17:34:58 +0200

    Update App.jsx

--- laura-sabater/challenges/todo-list-fullstack/frontend/src/App.jsx
+++ laura-sabater/challenges/todo-list-fullstack/frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from './redux/store';
-import List from './components/List';
+import List from './components/list';
 
 function App() {
   return (
