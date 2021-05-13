7acf622082035e677e3c915c27b9140bfd1aeeaa
Daniel Martorell <dani.martorell@gmail.com>
2021-05-13 16:05:40 +0200

    Add sass to app.jsx file

--- dani-martorell/challenge/todo-list-redux-fullStack/client/src/App.jsx
+++ dani-martorell/challenge/todo-list-redux-fullStack/client/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import store from './redux/stores';
+import configureStore from './redux/store';
 
 import './App.css';
 
 function App() {
   return (
-    <Provider store={store()}>
+    <Provider store={configureStore()}>
 
       <h1>Tour of Heroes</h1>
 
