d5145b147ccac5aa4f9170f21529434dd74d61b2
Anna Sala <anna.sala.mercade@gmail.com>
2021-05-13 11:26:23 +0200

    Add and set up tasksRouter

---
+++ anna-sala/challenges/to-do-full-stack/backend/src/router/tasksRouter.js
@@ -0,0 +1,21 @@
+const { Router } = require('express');
+const tasksController = require('../controllers/tasksController')();
+
+function tasksRouter() {
+  const routes = Router();
+
+  routes
+    .route('/')
+    .get(tasksController.getAll)
+    .post(tasksController.createOne);
+
+  routes
+    .route('/:taskId')
+    .get(tasksController.getById)
+    .put(tasksController.updateById)
+    .delete(tasksController.deleteById);
+
+  return routes;
+}
+
+module.exports = tasksRouter();
