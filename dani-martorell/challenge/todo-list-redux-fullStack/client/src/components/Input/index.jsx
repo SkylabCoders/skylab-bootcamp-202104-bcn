c02709e06431a396bdd89dc28b47b86cd989a3d3
Daniel Martorell <dani.martorell@gmail.com>
2021-05-13 16:35:16 +0200

    Finish Input component

--- dani-martorell/challenge/todo-list-redux-fullStack/client/src/components/Input/index.jsx
+++ dani-martorell/challenge/todo-list-redux-fullStack/client/src/components/Input/index.jsx
@@ -3,7 +3,10 @@ import './input.css';
 
 function Input() {
   return (
-    <h3>Input component</h3>
+    <form className="form-block">
+      <input className="form-block__input" type="text" id="fname" size="50" />
+      <button type="button" className="form-block__btn">Add Task</button>
+    </form>
   );
 }
 
