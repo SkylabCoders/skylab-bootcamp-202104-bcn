680c39029f788cbfb1ead3c52a6b7efd885c60be
Anna Sala <anna.sala.mercade@gmail.com>
2021-05-13 15:29:32 +0200

    Update task model

--- anna-sala/challenges/to-do-full-stack/backend/src/model/taskModel.js
+++ anna-sala/challenges/to-do-full-stack/backend/src/model/taskModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const taskSchema = mongoose.Schema({
   taskTitle: String,
-  taskStatus: Boolean,
+  done: Boolean,
 });
 
 module.exports = mongoose.model('Tasks', taskSchema);
