720364f1a77c28fdba07c8f16ac3faaa9aec26e4
Edgar Alandete <edgalandete@gmail.com>
2021-05-14 11:23:35 +0200

    Refactor reducer to have only one return

--- Edgar-Alandete/challenges/todo-list-fullstack/frontend/src/redux/reducers/tasksReducer.js
+++ Edgar-Alandete/challenges/todo-list-fullstack/frontend/src/redux/reducers/tasksReducer.js
@@ -2,26 +2,30 @@
 import actionTypes from '../actions/actionTypes';
 
 function tasksReducer(tasks = [], action) {
+  let newTasks = [...tasks];
   switch (action.type) {
     case actionTypes.LOAD_TASKS:
-      return action.tasks;
+      newTasks = action.tasks;
+      break;
     case actionTypes.DELETE_TASK:
-      return tasks.filter((task) => task._id !== action.taskId);
+      newTasks = newTasks.filter((task) => task._id !== action.taskId);
+      break;
     case actionTypes.ADD_TASK:
       return [
-        ...tasks,
+        ...newTasks,
         action.task,
       ];
     case actionTypes.UPDATE_TASK:
-      return tasks.map(
+      newTasks = newTasks.map(
         (task) => (task._id === action.task._id
           ? { ...task, ...action.task }
           : task),
       );
+      break;
     default:
       break;
   }
-  return tasks;
+  return newTasks;
 }
 
 export default tasksReducer;
