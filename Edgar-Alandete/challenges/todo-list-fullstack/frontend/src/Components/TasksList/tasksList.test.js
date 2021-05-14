892d3002f75565a95520c39e42601eddf093997c
Edgar Alandete <edgalandete@gmail.com>
2021-05-14 09:48:55 +0200

    Test fire event

--- Edgar-Alandete/challenges/todo-list-fullstack/frontend/src/Components/TasksList/tasksList.test.js
+++ Edgar-Alandete/challenges/todo-list-fullstack/frontend/src/Components/TasksList/tasksList.test.js
@@ -1,7 +1,10 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import TasksList from './TasksList';
-import { render, screen } from '../../assets/test-utils';
+import { fireEvent, render, screen } from '../../assets/test-utils';
+import { createTask } from '../../redux/actions/actionCreators';
+
+// jest.mock('../../redux/actions/actionCreators');
 
 describe('Given a TasksList component', () => {
   test('Should render All Tasks', () => {
@@ -10,7 +13,7 @@ describe('Given a TasksList component', () => {
     expect(screen.getByText(/All tasks/)).toBeInTheDocument();
   });
 
-  test('Should render All Tasks', () => {
+  test('Should render sacar', () => {
     render(<TasksList />, {
       initialState: {
         tasks: [{
@@ -24,7 +27,7 @@ describe('Given a TasksList component', () => {
     expect(screen.getByText(/Sacar/)).toBeInTheDocument();
   });
 
-  test('Should render All Tasks', () => {
+  test('Should render iaia', () => {
     render(<TasksList />, {
       initialState: {
         tasks: [{
@@ -36,5 +39,23 @@ describe('Given a TasksList component', () => {
     });
 
     expect(screen.getByText(/iaia/)).toBeInTheDocument();
+  });
+
+  test('Should call dispatch', () => {
+    const { getAllByRole } = render(<TasksList />, {
+      initialState: {
+        tasks: [{
+          _id: '123',
+          name: 'Llamar a iaia',
+          isFinished: false,
+        }],
+      },
+    });
+    const buttonCreate = getAllByRole('button')[0];
+    fireEvent.click(buttonCreate);
+    const dispatch = jest.fn();
+    dispatch(createTask());
+
+    expect(dispatch).toHaveBeenCalled();
   });
 });
