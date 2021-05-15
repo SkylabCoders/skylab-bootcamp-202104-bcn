bd6bf0f19c50ad34b4ee0cdc4609491387c71f08
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-05-15 14:39:08 +0200

    ShoppingCart component tested almost 100%

--- los-chulis/client/los-chulis/src/components/ShopingCart/index.jsx
+++ los-chulis/client/los-chulis/src/components/ShopingCart/index.jsx
@@ -31,21 +31,20 @@ function ShoppingCart({ cartList, dispatch }) {
     <div className="shopping-cart">
       <h3>MI CESTA</h3>
       <ul className="shopping-cart__product-list">
-        {
-        cartList.map((product) => (
-          <li key={product._id}>
-            {product.quantity}
-            {'x '}
-            {product.brand}
-            {': '}
-            <span className="shopping-cart__price">
-              {product.price}
-              {' €  '}
-            </span>
-            <button type="button" onClick={() => dispatch(deleteFromCart(product))}>-</button>
-          </li>
-        ))
-}
+        {cartList.length
+          ? cartList.map((product) => (
+            <li key={product._id}>
+              {product.quantity}
+              {'x '}
+              {product.brand}
+              {': '}
+              <span className="shopping-cart__price">
+                {product.price}
+                {' €  '}
+              </span>
+              <button type="button" className="button-remove" data-testid="button-remove" onClick={() => dispatch(deleteFromCart(product))}>-</button>
+            </li>
+          )) : <p>No products at cart</p>}
       </ul>
       <div className="shopping-cart__total-cost">
         Total:
@@ -53,7 +52,7 @@ function ShoppingCart({ cartList, dispatch }) {
         {cartList.length ? cartListTotalCost() : 0}
         {' €  '}
       </div>
-      <button type="button" onClick={buyCartList}>COMPRAR</button>
+      <button type="button" data-testid="button-buy" onClick={() => buyCartList()}>COMPRAR</button>
     </div>
   );
 }
