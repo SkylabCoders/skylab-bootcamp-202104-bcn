4002a8b95aa271e0877839e8c3b311546b0fb2c2
gteixi <gemmateixido25@gmail.com>
2021-05-15 17:29:19 +0200

    Server runnin in localhost:1505

---
+++ gemma-teixido/challenge/cv-fullstack/backend/src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const server = express();
+const port = process.env.PORT || 1505;
+
+mongoose.connect(
+  process.env.DDBB_URL,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  }
+);
+
+server.use(cors());
+server.use(express.json());
+server.use(morgan('dev'));
+
+const cvRouter = require('./routes/cvRouter');
+
+server.use('/heroes', cvRouter);
+
+server.listen(port,
+  () => debug(`Server is running in ${chalk.magentaBright(`localhost:${port}`)}`));
