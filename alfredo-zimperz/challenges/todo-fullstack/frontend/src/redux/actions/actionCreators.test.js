d38c10ab8126d840462e40d6b88bf17f8e235da5
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-05-15 11:58:17 +0200

    Create addTask actionCreator test

--- alfredo-zimperz/challenges/todo-fullstack/frontend/src/redux/actions/actionCreators.test.js
+++ alfredo-zimperz/challenges/todo-fullstack/frontend/src/redux/actions/actionCreators.test.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { loadTasks } from './actionCreators';
+import { loadTasks, addTask } from './actionCreators';
 
 jest.mock('axios');
 
@@ -9,6 +9,14 @@ describe('Given a loadTasks function', () => {
   test('should call dispatch with a task list', async () => {
     axios.get.mockResolvedValueOnce({ data: [{}] });
     await loadTasks()(dispatch);
+    expect(dispatch).toHaveBeenCalled();
+  });
+});
+
+describe('Given a addTask function', () => {
+  test('should call dispatch with a task list', async () => {
+    axios.post.mockResolvedValueOnce({ data: {} });
+    await addTask()(dispatch);
     expect(dispatch).toHaveBeenCalled();
   });
 });
