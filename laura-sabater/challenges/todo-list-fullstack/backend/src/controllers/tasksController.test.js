7bca7f65c4fe57a76c7cc7726dd5b8f964029595
Laura <laura.sabater.musach@gmail.com>
2021-05-15 13:04:34 +0200

    Create addTask test and add more scenarios

--- laura-sabater/challenges/todo-list-fullstack/backend/src/controllers/tasksController.test.js
+++ laura-sabater/challenges/todo-list-fullstack/backend/src/controllers/tasksController.test.js
@@ -4,6 +4,17 @@ const Task = require('../model/taskModel');
 jest.mock('../model/taskModel');
 
 describe('Given getTasks function', () => {
+  test('when resolved, then return res.status(200)', async () => {
+    const res = {
+      status: jest.fn(),
+      json: jest.fn(),
+      send: jest.fn(),
+    };
+    Task.find.mockResolvedValueOnce([{}]);
+    await getTasks(null, res);
+    expect(res.status).toHaveBeenCalledWith(200);
+  });
+
   test('when resolved, then return res.json([{}])', async () => {
     const res = {
       status: jest.fn(),
@@ -13,6 +24,17 @@ describe('Given getTasks function', () => {
     Task.find.mockResolvedValueOnce([{}]);
     await getTasks(null, res);
     expect(res.json).toHaveBeenCalledWith([{}]);
+  });
+
+  test('when rejected, then return res.status(500)', async () => {
+    const res = {
+      status: jest.fn(),
+      json: jest.fn(),
+      send: jest.fn(),
+    };
+    Task.find.mockRejectedValueOnce('error');
+    await getTasks(null, res);
+    expect(res.status).toHaveBeenCalledWith(500);
   });
 
   test('when rejected, then return res.send(error)', async () => {
@@ -27,41 +49,97 @@ describe('Given getTasks function', () => {
   });
 });
 
-// describe('Given addTask function', () => {
-//   // test('when resolved, then return res.json({})', async () => {
-//   //   const req = {
-//   //     body: {
-//   //       name: 'name',
-//   //     },
-//   //   };
-//   //   const res = {
-//   //     status: jest.fn(),
-//   //     json: jest.fn(),
-//   //     send: jest.fn(),
-//   //   };
-//   //   Task.save.mockResolvedValueOnce({ body: { name: 'name' } });
-//   //   await addTask(req, res);
-//   //   expect(res.json).toHaveBeenCalledWith({ body: { name: 'name' } });
-//   // });
-//   test('when rejected, then return res.send(error)', async () => {
-//     const req = {
-//       body: {
-//         name: 'name',
-//       },
-//     };
-//     const res = {
-//       status: jest.fn(),
-//       json: jest.fn(),
-//       send: jest.fn(),
-//     };
-//     const newTask = { name: 'name' };
-//     newTask.save.mockRejectedValueOnce('error');
-//     await addTask(req, res);
-//     expect(res.send).toHaveBeenCalledWith('error');
-//   });
-// });
+describe('Given addTask function', () => {
+  test('when resolved, then return res.status(200)', async () => {
+    const req = {
+      body: {
+        name: 'name',
+      },
+    };
+    const res = {
+      status: jest.fn(),
+      json: jest.fn(),
+      send: jest.fn(),
+    };
+    Task.mockReturnValueOnce({
+      save: jest.fn().mockResolvedValueOnce({ body: { name: 'name' } }),
+    });
+    await addTask(req, res);
+    expect(res.status).toHaveBeenCalledWith(200);
+  });
+
+  test('when resolved, then return res.json({})', async () => {
+    const req = {
+      body: {
+        name: 'name',
+      },
+    };
+    const res = {
+      status: jest.fn(),
+      json: jest.fn(),
+      send: jest.fn(),
+    };
+    Task.mockReturnValueOnce({
+      save: jest.fn().mockResolvedValueOnce({ body: { name: 'name' } }),
+    });
+    await addTask(req, res);
+    expect(res.json).toHaveBeenCalledWith({ body: { name: 'name' } });
+  });
+
+  test('when rejected, then return res.status(500)', async () => {
+    const req = {
+      body: {
+        name: 'name',
+      },
+    };
+    const res = {
+      status: jest.fn(),
+      json: jest.fn(),
+      send: jest.fn(),
+    };
+    Task.mockReturnValueOnce({
+      save: jest.fn().mockRejectedValueOnce('error'),
+    });
+    await addTask(req, res);
+    expect(res.status).toHaveBeenCalledWith(500);
+  });
+
+  test('when rejected, then return res.send(error)', async () => {
+    const req = {
+      body: {
+        name: 'name',
+      },
+    };
+    const res = {
+      status: jest.fn(),
+      json: jest.fn(),
+      send: jest.fn(),
+    };
+    Task.mockReturnValueOnce({
+      save: jest.fn().mockRejectedValueOnce('error'),
+    });
+    await addTask(req, res);
+    expect(res.send).toHaveBeenCalledWith('error');
+  });
+});
 
 describe('Given deleteTask function', () => {
+  test('when resolved, then return res.status(204)', async () => {
+    const req = {
+      params: {
+        taskId: 1,
+      },
+    };
+    const res = {
+      status: jest.fn(),
+      json: jest.fn(),
+      send: jest.fn(),
+    };
+    Task.findByIdAndDelete.mockResolvedValueOnce({ params: { taskId: 1 } });
+    await deleteTask(req, res);
+    expect(res.status).toHaveBeenCalledWith(204);
+  });
+
   test('when resolved, then return res.json()', async () => {
     const req = {
       params: {
@@ -76,6 +154,22 @@ describe('Given deleteTask function', () => {
     Task.findByIdAndDelete.mockResolvedValueOnce({ params: { taskId: 1 } });
     await deleteTask(req, res);
     expect(res.json).toHaveBeenCalledWith();
+  });
+
+  test('when rejected, then return res.status(500)', async () => {
+    const req = {
+      params: {
+        taskId: 1,
+      },
+    };
+    const res = {
+      status: jest.fn(),
+      json: jest.fn(),
+      send: jest.fn(),
+    };
+    Task.findByIdAndDelete.mockRejectedValueOnce('error');
+    await deleteTask(req, res);
+    expect(res.status).toHaveBeenCalledWith(500);
   });
 
   test('when rejected, then return res.send(error)', async () => {
