366984e600a11833400e0f5618a2430ad4142a98
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-05-15 12:03:40 +0200

    Create deleteTask actionCreator test

--- alfredo-zimperz/challenges/todo-fullstack/frontend/src/redux/actions/actionCreators.test.js
+++ alfredo-zimperz/challenges/todo-fullstack/frontend/src/redux/actions/actionCreators.test.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-console */
 import axios from 'axios';
-import { loadTasks, addTask } from './actionCreators';
+import { loadTasks, addTask, deleteTask } from './actionCreators';
 
 jest.mock('axios');
 
 const dispatch = jest.fn();
+global.console = { log: jest.fn() };
 
 describe('Given a loadTasks function', () => {
   test('should call dispatch with a task list', async () => {
@@ -18,5 +20,18 @@ describe('Given a addTask function', () => {
     axios.post.mockResolvedValueOnce({ data: {} });
     await addTask()(dispatch);
     expect(dispatch).toHaveBeenCalled();
+  });
+});
+
+describe('Given a deleteTask function', () => {
+  test('should call dispatch with a task id', async () => {
+    axios.delete.mockResolvedValueOnce({ data: {} });
+    await deleteTask(10)(dispatch);
+    expect(dispatch).toHaveBeenCalled();
+  });
+  test('when it catches an erorr should call console.log', async () => {
+    axios.delete.mockRejectedValue('error');
+    await deleteTask(10)(dispatch);
+    expect(console.log).toHaveBeenCalledWith('error');
   });
 });
