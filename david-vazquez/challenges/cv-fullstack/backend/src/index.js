8a61ff93ae61eba51252254c83365706ddb39129
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-05-15 20:52:56 +0200

    Create port configuration and conections for database

---
+++ david-vazquez/challenges/cv-fullstack/backend/src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+require('dotenv').config();
+
+const server = express();
+const port = 2500;
+
+const cors = require('cors');
+const morgan = require('morgan');
+const chalk = require('chalk');
+const debug = require('debug')('server');
+const mongoose = require('mongoose');
+
+server.use(express.json());
+server.use(cors());
+server.use(morgan('dev'));
+
+mongoose.connect(
+  process.env.DDBB_URL,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  },
+);
+
+const productsRouter = require('./routes/elementsRouter');
+
+server.use('/products', productsRouter);
+
+server.listen(port, () => debug(`Server is running in ${chalk.bgCyan(port)}`));
