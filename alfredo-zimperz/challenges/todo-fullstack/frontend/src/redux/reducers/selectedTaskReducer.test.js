303a1e61e429eae12071e345cb5941f774268de8
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-05-15 12:43:17 +0200

    Create selectedTask reducer test

---
+++ alfredo-zimperz/challenges/todo-fullstack/frontend/src/redux/reducers/selectedTaskReducer.test.js
@@ -0,0 +1,21 @@
+import selectedTaskReducer from './selectedTaskReducer';
+import actionTypes from '../actions/actionTypes';
+
+const action = {
+  type: 'LOAD_TASK',
+  task: {
+    _id: 1,
+    title: 'Title',
+    description: 'Description',
+    done: false,
+  },
+};
+
+describe('Given a selectedTaskReducer', () => {
+  describe('and invoked with a LOAD_TASK action', () => {
+    test('should return an updated selectedTask', () => {
+      const result = selectedTaskReducer(null, action);
+      expect(result).toEqual(action.task);
+    });
+  });
+});
