31d9813b2858ddec061c7aa4e7054cdfe02047e2
IrenePerezAlonso <irperal@hotmail.com>
2021-05-15 13:48:01 +0200

    Create a test for each controller

--- irene-perez/challenge/todo-fullstack/backend/src/controllers/taskController.test.js
+++ irene-perez/challenge/todo-fullstack/backend/src/controllers/taskController.test.js
@@ -1,8 +1,8 @@
 /* eslint-disable class-methods-use-this */
 const {
-  createTask
-//   deleteTaskById,
-//   getAllTasks
+  createTask,
+  deleteTaskById,
+  getAllTasks
 } = require('./taskController.js')();
 
 const Task = require('../model/taskModel');
@@ -35,5 +35,121 @@ describe('createTask', () => {
 
     // assert
     expect(res.json).toHaveBeenCalledWith({ name: 'task name' });
+  });
+
+  test('Should can send', async () => {
+    // arrange
+    const req = {
+      body: null
+    };
+    const res = {
+      status: jest.fn(),
+      json: jest.fn(),
+      send: jest.fn()
+    };
+    Task.mockRejectedValueOnce('error');
+
+    // act
+    await createTask(req, res);
+
+    // assert
+    expect(res.send).toHaveBeenCalledWith('error');
+  });
+});
+
+describe('deleteTaskById', () => {
+  test('should call json', async () => {
+    // arrange
+    const req = {
+      params: {
+        taskId: null
+      }
+    };
+    const res = {
+      status: jest.fn(),
+      json: jest.fn(),
+      send: jest.fn()
+    };
+
+    // act
+    await deleteTaskById(req, res);
+
+    // assert
+    expect(res.json).toHaveBeenCalled();
+  });
+
+  test('Should call status with 204', async () => {
+    // arrange
+    const req = {
+      params: {
+        taskId: null
+      }
+    };
+    const res = {
+      status: jest.fn(),
+      json: jest.fn(),
+      send: jest.fn()
+    };
+
+    // act
+    await deleteTaskById(req, res);
+
+    // assert
+    expect(res.status).toHaveBeenCalledWith(204);
+  });
+
+  test('Should fail and send an error', async () => {
+    // arrange
+    const req = {
+      params: {
+        taskId: null
+      }
+    };
+    const res = {
+      status: jest.fn(),
+      json: jest.fn(),
+      send: jest.fn()
+    };
+    Task.findByIdAndDelete.mockRejectedValueOnce('error');
+
+    // act
+    await deleteTaskById(req, res);
+
+    // assert
+    describe(res.send).toHaveBeenCalledWith('error');
+  });
+});
+
+describe('getAllTasks', () => {
+  test('Should get all tasks', async () => {
+    // arrange
+    const res = {
+      json: jest.fn(),
+      status: jest.fn(),
+      send: jest.fn()
+    };
+    Task.find.mockResolvedValueOnce([{ name: 'Milk' }]);
+
+    // act
+    await getAllTasks(null, res);
+
+    // assert
+    expect(res.json).toHaveBeenCalledWith([{ name: 'Milk' }]);
+  });
+
+  test('Should fail and send an error', async () => {
+    // arrange
+    const res = {
+      json: jest.fn(),
+      status: jest.fn(),
+      send: jest.fn()
+    };
+    Task.mockRejectedValueOnce('error');
+
+    // act
+    await getAllTasks(null, res);
+
+    // assert
+    expect(res.send).toHaveBeenCalledWith('error');
   });
 });
