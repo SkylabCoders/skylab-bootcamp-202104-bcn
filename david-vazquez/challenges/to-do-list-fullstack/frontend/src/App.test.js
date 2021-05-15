ebd3f0d3a672fd021542a210d3b39625954f2a41
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-05-15 06:56:49 +0200

    App and index tested 100%

--- david-vazquez/challenges/to-do-list-fullstack/frontend/src/App.test.js
+++ david-vazquez/challenges/to-do-list-fullstack/frontend/src/App.test.js
@@ -5,6 +5,6 @@ import App from './App';
 test('renders learn react link', () => {
   // eslint-disable-next-line react/react-in-jsx-scope
   render(<App />);
-  const linkElement = screen.getByText(/List/i);
+  const linkElement = screen.getByText(/List of task/i);
   expect(linkElement).toBeInTheDocument();
 });
