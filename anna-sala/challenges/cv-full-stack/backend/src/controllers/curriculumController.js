a3a2a1f78d084d0ea40a6949ee15e265f1eb6003
Anna Sala <anna.sala.mercade@gmail.com>
2021-05-16 15:10:05 +0200

    Add create Cv in controller

--- anna-sala/challenges/cv-full-stack/backend/src/controllers/curriculumController.js
+++ anna-sala/challenges/cv-full-stack/backend/src/controllers/curriculumController.js
@@ -1,14 +1,27 @@
 const debug = require('debug')('server:curriculumController');
-const cv = require('../model/cvModel');
+const CV = require('../model/cvModel');
 
 function curriculumController() {
   debug('inside getCV');
   async function getCV(req, res) {
-    const curriculum = await cv.find();
+    const curriculum = await CV.find();
     res.json(curriculum);
   }
+  async function createCV(req, res) {
+    const newCV = new CV(req.body);
+    debug(newCV);
+    try {
+      await newCV.save();
+      res.json(newCV);
+    } catch (error) {
+      debug(error);
+      res.send(error);
+    }
+  }
+
   return {
     getCV,
+    createCV,
   };
 }
 
