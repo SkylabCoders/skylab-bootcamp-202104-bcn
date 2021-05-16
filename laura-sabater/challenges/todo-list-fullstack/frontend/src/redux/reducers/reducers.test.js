392425a52856a587c258aca0cb96e9a7d3ac545f
Laura <laura.sabater.musach@gmail.com>
2021-05-16 09:50:04 +0200

    Remove comments of reducers test

--- laura-sabater/challenges/todo-list-fullstack/frontend/src/redux/reducers/reducers.test.js
+++ laura-sabater/challenges/todo-list-fullstack/frontend/src/redux/reducers/reducers.test.js
@@ -16,15 +16,4 @@ describe('Given tasksReducer function ', () => {
     expect(tasksReducer([], { type: actionTypes.DELETE_TASK, taskId: 3 }))
       .toEqual([]);
   });
-
-  // test('sholud handle DELETE_TASK', () => {
-  //   const updatedTasks = [{ taskId: 1 }, { taskId: 3 }];
-  //   const filter = jest.fn();
-  //   filter.mockResolvedValue([{ taskId: 3 }]);
-  //   updatedTasks.filter();
-
-  //   // const filterById = (task) => task.taskId !== 3;
-  //   expect(tasksReducer([], { type: actionTypes.DELETE_TASK, taskId: 3 }))
-  //     .toEqual([]);
-  // });
 });
