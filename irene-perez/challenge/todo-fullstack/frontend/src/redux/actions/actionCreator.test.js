0c59ce4a667a17eb5c383357d3ac63bd72b3b360
IrenePerezAlonso <irperal@hotmail.com>
2021-05-16 16:28:57 +0200

    Pass the actionCreator.test

--- irene-perez/challenge/todo-fullstack/frontend/src/redux/actions/actionCreator.test.js
+++ irene-perez/challenge/todo-fullstack/frontend/src/redux/actions/actionCreator.test.js
@@ -1,8 +1,26 @@
 // import actionTypes from './actionTypes';
 import axios from 'axios';
-import { deleteTask, loadTasks } from './actionCreator';
+import { addTask, deleteTask, loadTasks } from './actionCreator';
 
 jest.mock('axios');
+
+describe('addTask', () => {
+  test('Should return ADD_TASK', async () => {
+    // arrange
+    const mockedTask = { data: 'Mockdata' };
+    const dispatch = jest.fn();
+    axios.post.mockResolvedValue(mockedTask);
+
+    // act
+    await addTask(1)(dispatch);
+
+    // assert
+    expect(dispatch).toHaveBeenCalledWith({
+      type: 'ADD_TASK',
+      task: 'Mockdata'
+    });
+  });
+});
 
 describe('deleteTask', () => {
   test('Should return DELETE_TASK', async () => {
