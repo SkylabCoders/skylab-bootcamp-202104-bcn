51f4dcefd5a1092dc1793823ebc0275ed99f5c0f
Anna Sala <anna.sala.mercade@gmail.com>
2021-05-16 13:38:36 +0200

    Create cvModel

---
+++ anna-sala/challenges/cv-full-stack/backend/src/model/cvModel.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const cvSchema = mongoose.Schema({
+  person: {
+    name: String,
+    position: String,
+    personalInfo: {
+      address: String,
+      phone: String,
+      email: String,
+      website: String,
+      linkedin: String,
+      instagram: String,
+    },
+    skills: {
+      graphicDesign: [String],
+      webDevelopment: [String],
+      socialMedia: [String],
+      otherSkills: [String],
+    },
+    languages: [String],
+    presentation: String,
+    education: [{
+      timeFrame: String,
+      place: String,
+      description: String,
+    }],
+    experience: [{
+      timeFrame: String,
+      place: String,
+      title: String,
+      description: String,
+    }],
+    awards: [{
+      timeFrame: String,
+      description: String,
+    }],
+    leadership: [{
+      timeFrame: String,
+      title: String,
+      place: String,
+      description: String,
+    }],
+  },
+});
+
+module.exports = mongoose.model('curriculum', cvSchema);
