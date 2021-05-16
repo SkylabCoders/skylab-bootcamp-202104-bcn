cb7ef165f24844eab93c345677500160b801952f
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-05-16 15:04:44 +0200

    Create loader

--- alfredo-zimperz/challenges/cv-fullstack/frontend/src/components/Resume/index.jsx
+++ alfredo-zimperz/challenges/cv-fullstack/frontend/src/components/Resume/index.jsx
@@ -1,31 +1,69 @@
-import React from 'react';
+/* eslint-disable no-console */
+import React, { useEffect } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Loader from '../common/Loader';
+import loadCv from '../../redux/actions/actionCreators';
 import './resume.css';
-// import PropTypes from 'prop-types';
 
-const Resume = () => (
-  <div className="window">
-    <div className="bar">
-      <div className="btn" />
-    </div>
-    <div className="body">
-      <pre>
-        <div className="comment"># run this command:</div>
-        <div className="prompt">
-          $
-          <span className="command">echo hi</span>
-        </div>
-        <div className="prompt">hi</div>
-        <div className="prompt">
-          $
-          <span className="pulse">_</span>
-        </div>
-      </pre>
-    </div>
-  </div>
-);
+const Resume = ({ cv, actions }) => {
+  useEffect(() => {
+    if (!cv.name) { actions.loadCv(); } else console.log('ahora hay cv');
+    if (cv.name) { console.log(cv); }
+  }, []);
 
-Resume.propTypes = {
+  return (
+    <>
+      {
+        cv.name ? (
+          <div className="window">
+            <div className="bar">
+              <div className="btn" />
+            </div>
+            <div className="body">
+              <pre>
+                <div className="comment"># run this command:</div>
+                <div className="prompt">
+                  $
+                  <span className="command">show cv</span>
+                </div>
+                <div className="prompt">hi</div>
+                <div className="prompt">
+                  $
+                  <span className="pulse">_</span>
+                </div>
+              </pre>
+            </div>
+          </div>
+        ) : <Loader />
+    }
 
+    </>
+
+  );
 };
 
-export default Resume;
+Resume.propTypes = {
+  cv: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+  actions: PropTypes.shape({
+    loadCv: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+function mapStateToProps({ cv }) {
+  return { cv };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(
+      { loadCv },
+      dispatch,
+    ),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Resume);
