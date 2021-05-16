64800ec3b20c5b90eae6e651eaacee754a69daf4
Fernando Gomez <shadyfast8.services@gmail.com>
2021-05-17 00:45:48 +0200

    Add Dashboard curriculum component

--- fernando-gomez/challenge/curriculum/frontend/src/App.jsx
+++ fernando-gomez/challenge/curriculum/frontend/src/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from './redux/store';
-import TasksList from './components/TasksList';
-import TaskAdder from './components/TaskAdder';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <>
       <Provider store={configureStore()}>
-        <TaskAdder />
-        <TasksList />
+        <Dashboard />
       </Provider>
     </>
   );
