bc6dac4a13895d8e01af222f3af4b75893396e2c
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-05-16 14:21:02 +0200

    Create Header and Resume component

---
+++ david-vazquez/challenges/cv-fullstack/frontend/src/components/Header/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <>
+      <h1>David</h1>
+      <ul>
+        <li>About me</li>
+        <li>Resume</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
+      </ul>
+      <ul>
+        <li>Linkedin</li>
+        <li>Instagram</li>
+        <li>Github</li>
+        <li>Vimeo</li>
+      </ul>
+    </>
+  );
+}
+
+export default Header;
---
+++ david-vazquez/challenges/cv-fullstack/frontend/src/components/Resume/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Resume() {
+  return (
+    <>
+      <h2>Resume</h2>
+      <h3>Education</h3>
+      <h3>Experience</h3>
+      <h3>Design Skills</h3>
+      <h3>Coding Skills</h3>
+    </>
+  );
+}
+
+export default Resume;
