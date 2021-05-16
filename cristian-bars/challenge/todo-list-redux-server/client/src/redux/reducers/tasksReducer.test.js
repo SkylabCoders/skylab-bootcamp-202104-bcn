11063aaf3c9cec840f53391923e73a2490125f81
cristian-bars <patxi.cristian@gmail.com>
2021-05-17 01:12:37 +0200

    Modify load_task reducer and create add_task reducer

--- cristian-bars/challenge/todo-list-redux-server/client/src/redux/reducers/tasksReducer.test.js
+++ cristian-bars/challenge/todo-list-redux-server/client/src/redux/reducers/tasksReducer.test.js
@@ -1,19 +1,46 @@
 import tasksReducer from './tasksReducer';
 
-const action = {
+const taskList = [{
+  task: 'Title',
+  completed: false
+},
+{
+  task: 'Title 2',
+  completed: true
+}];
+
+const loadAction = {
   type: 'LOAD_TASKS',
-  tasks: {
-    _id: 1,
-    task: 'Title',
-    completed: false
-  }
+  tasks: taskList[0]
+};
+
+const deleteAction = {
+  type: 'DELETE_TASK',
+  taskId: 1
+};
+
+const addAction = {
+  type: 'ADD_TASK',
+  task: { ...taskList[1] }
+};
+
+const updateAction = {
+  type: 'UPDATE_TASK',
+  task: { ...taskList[1], title: 'Updated title' }
 };
 
 describe('Given a taskReducer', () => {
   describe('and invoked with a LOAD_TASK action', () => {
     test('should return an updated selectedTask', () => {
-      const result = tasksReducer(null, action);
-      expect(result).toEqual(action.tasks);
+      const result = tasksReducer(null, loadAction);
+      expect(result).toEqual(taskList[0]);
     });
+  });
+});
+
+describe('and invoked with a ADD_TASK action and a task', () => {
+  test('should return an updated task list including the given task', () => {
+    const result = tasksReducer([{ ...taskList[0] }], addAction);
+    expect(result).toEqual(taskList);
   });
 });
