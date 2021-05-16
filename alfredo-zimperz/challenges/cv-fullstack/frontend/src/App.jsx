30ef6bc384b513325cdcfd4323d7f82813504865
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-05-16 13:10:44 +0200

    Modify App & Resume: - Modify HTML - Add styles

--- alfredo-zimperz/challenges/cv-fullstack/frontend/src/App.jsx
+++ alfredo-zimperz/challenges/cv-fullstack/frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Switch, Route, Link,
+  Switch, Link, Route,
 } from 'react-router-dom';
 import Resume from './components/Resume';
 import Portfolio from './components/Portfolio';
@@ -11,18 +11,24 @@ function App() {
   return (
     <main className="App">
       <header className="header">
-        <h1>App</h1>
+        <h1>Alfredo Zimperz CV</h1>
         <nav>
-          <Link to="/">Resume</Link>
-          <Link to="/portfolio">Portfolio</Link>
-          <Link to="/contact">Contact</Link>
+          <Link className="nav__link" to="/">Resume</Link>
+          <Link className="nav__link" to="/portfolio">Portfolio</Link>
+          <Link className="nav__link" to="/contact">Contact</Link>
         </nav>
       </header>
+      <p>
+        Fullstack developer
+      </p>
       <Switch>
         <Route path="/" exact component={Resume} />
         <Route path="/portfolio" exact component={Portfolio} />
         <Route path="/contact" exact component={Contact} />
       </Switch>
+      <footer>
+        <p>Footer content</p>
+      </footer>
     </main>
   );
 }
--- alfredo-zimperz/challenges/cv-fullstack/frontend/src/components/Resume/index.jsx
+++ alfredo-zimperz/challenges/cv-fullstack/frontend/src/components/Resume/index.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import './resume.css';
 // import PropTypes from 'prop-types';
 
-const Resume = () => {
-  const title = 'Resume';
-  return (
-    <div>
-      <h2>{title}</h2>
+const Resume = () => (
+  <div className="window">
+    <div className="bar">
+      <div className="btn" />
     </div>
-  );
-};
+    <div className="body">
+      <pre>
+        <div className="comment"># run this command:</div>
+        <div className="prompt">
+          $
+          <span className="command">echo hi</span>
+        </div>
+        <div className="prompt">hi</div>
+        <div className="prompt">
+          $
+          <span className="pulse">_</span>
+        </div>
+      </pre>
+    </div>
+  </div>
+);
 
 Resume.propTypes = {
 
