ca96ac9860d0723fd93ced3da084f89da9b96fe1
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-05-16 11:57:43 +0200

    Install dependencies: - react-router-dom - redux - react-redux - prop-types Create routes in App Create main structure from components: - Resume - Portfolio - Contact

--- alfredo-zimperz/challenges/cv-fullstack/frontend/src/App.jsx
+++ alfredo-zimperz/challenges/cv-fullstack/frontend/src/App.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import {
+  Switch, Route, Link,
+} from 'react-router-dom';
+import Resume from './components/Resume';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <main className="App">
+      <header className="header">
+        <h1>App</h1>
+        <nav>
+          <Link to="/">Resume</Link>
+          <Link to="/portfolio">Portfolio</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+      </header>
+      <Switch>
+        <Route path="/" exact component={Resume} />
+        <Route path="/portfolio" exact component={Portfolio} />
+        <Route path="/contact" exact component={Contact} />
+      </Switch>
+    </main>
   );
 }
 
---
+++ alfredo-zimperz/challenges/cv-fullstack/frontend/src/components/Resume/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+const Resume = () => {
+  const title = 'Resume';
+  return (
+    <div>
+      <h2>{title}</h2>
+    </div>
+  );
+};
+
+Resume.propTypes = {
+
+};
+
+export default Resume;
