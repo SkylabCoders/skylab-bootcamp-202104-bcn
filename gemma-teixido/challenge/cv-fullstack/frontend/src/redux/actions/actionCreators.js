15652228e99575ea80a456522728332be66727ae
gteixi <gemmateixido25@gmail.com>
2021-05-16 17:18:11 +0200

    Add new font and change border to 20px.

--- gemma-teixido/challenge/cv-fullstack/frontend/src/redux/actions/actionCreators.js
+++ gemma-teixido/challenge/cv-fullstack/frontend/src/redux/actions/actionCreators.js
@@ -1,14 +1,10 @@
-/* eslint-disable no-debugger */
 import axios from 'axios';
 import actionTypes from './actionTypes';
 
 const url = process.env.REACT_APP_URL;
 
-console.log(process.env.REACT_APP_URL);
-
 export default function loadInformation() {
   return async (dispatch) => {
-    debugger;
     try {
       const { data } = await axios(url);
       dispatch({
