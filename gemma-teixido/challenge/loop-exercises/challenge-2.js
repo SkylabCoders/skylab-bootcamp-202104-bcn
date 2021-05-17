dd2c12d4cdcf2b7d8f2735131dfc9c2c39cf0e4d
gteixi <gemmateixido25@gmail.com>
2021-05-17 19:07:14 +0200

    Done first challenge

---
+++ gemma-teixido/challenge/loop-exercises/challenge-2.js
@@ -0,0 +1,22 @@
+/*
+  Just like Challenge 1 but this time with repeated delay values.
+  Print Hello World 5 times with a delay of 100 ms.
+  Then Print it again 5 more times with a delay of 200 ms.
+  Then print it again 5 more times with a delay of 300 ms.
+  And so on until the program is killed.
+  Include the delay in the printed message:
+  Hello World. 100
+  Hello World. 100
+  Hello World. 100
+  Hello World. 100
+  Hello World. 100
+  Hello World. 200
+  Hello World. 200
+  Hello World. 200
+  ...
+
+  Constraints:
+   - Only use setInterval (not setTimeout)
+   - Use only ONE if statement
+   
+*/
