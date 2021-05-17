ed07f6b85e32a8a42ef27d9dfb3425d9868a99ff
Anna Sala <anna.sala.mercade@gmail.com>
2021-05-17 09:22:46 +0200

    Add headers and arrow with link

--- anna-sala/challenges/cv-full-stack/frontend/src/components/Dashboard/index.jsx
+++ anna-sala/challenges/cv-full-stack/frontend/src/components/Dashboard/index.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect } from 'react';
 import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
+import {
+  Link,
+} from 'react-router-dom';
 import loadCurriculum from '../../redux/actions/actionCreators';
+
+import('./dashboard.scss');
 
 function Dashboard({ dispatch, curriculum }) {
   useEffect(() => {
@@ -10,7 +15,24 @@ function Dashboard({ dispatch, curriculum }) {
   return (
     <>
       {
-      curriculum.length && <h1>{curriculum[0].person.name}</h1>
+      curriculum.length
+      && (
+        <>
+          <div className="headers">
+            <h1 className="headers__element">{curriculum[0].person.name}</h1>
+            <h2 className="headers__element">{curriculum[0].person.position}</h2>
+          </div>
+          <Link to="/info">
+            <figure className="arrow-container">
+              <img
+                src="https://www.freeiconspng.com/thumbs/white-arrow-png/white-down-arrow-png-2.png"
+                alt="arrow"
+                className="arrow"
+              />
+            </figure>
+          </Link>
+        </>
+      )
     }
     </>
   );
