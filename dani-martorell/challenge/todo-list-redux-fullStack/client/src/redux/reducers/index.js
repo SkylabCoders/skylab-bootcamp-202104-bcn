a7ebbe613958848dd58b4430e714c16ef76427cb
Daniel Martorell <dani.martorell@gmail.com>
2021-05-17 09:27:40 +0200

    Remove selectedTaskReducer

--- dani-martorell/challenge/todo-list-redux-fullStack/client/src/redux/reducers/index.js
+++ dani-martorell/challenge/todo-list-redux-fullStack/client/src/redux/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import tasksReducer from './tasksReducer';
-import selectedTaskReducer from './selectedTaskReducer';
 
 const rootReducer = combineReducers({
   tasks: tasksReducer,
-  selectedTask: selectedTaskReducer,
 });
 
 export default rootReducer;
