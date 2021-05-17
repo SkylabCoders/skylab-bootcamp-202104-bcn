4d95d7171e382503adfb04ae702e8d688e6e7507
cristian-bars <patxi.cristian@gmail.com>
2021-05-17 09:23:44 +0200

    Delete comments in code

--- cristian-bars/challenge/todo-list-redux-server/client/src/components/todoList/todoListComponent.test.js
+++ cristian-bars/challenge/todo-list-redux-server/client/src/components/todoList/todoListComponent.test.js
@@ -1,8 +1,8 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import { render, screen, fireEvent } from '../../../utils/test-utils';
+import { render, screen } from '../../../utils/test-utils';
 import TodoListComponent from './TodoListComponent';
-import { loadTasks, deleteTask, addTask } from '../../redux/actions/actionCreator';
+import { loadTasks } from '../../redux/actions/actionCreator';
 import actionTypes from '../../redux/actions/actionTypes';
 
 jest.mock('../../redux/actions/actionCreator');
@@ -18,34 +18,4 @@ describe('Given a TodoListComponent component', () => {
       expect(screen.getByText(/No Tasks/i)).toBeInTheDocument();
     });
   });
-
-  /* describe('When is redered with one task', () => {
-    let initialState;
-    beforeEach(() => {
-      initialState = { tasks: [{ task: 'pass test', completed: false }] };
-      render(<TodoListComponent />, { initialState });
-    });
-    describe('And delete button is clicked', () => {
-      test('Then clearTask should be invoked', () => {
-        deleteTask.mockImplementationOnce(() => ({ type: actionTypes.DELETE_TASK }));
-        fireEvent.click(screen.getByText(/delete/i));
-        expect(deleteTask).toHaveBeenCalledTimes(1);
-      });
-    });
-  }); */
-
-  /* describe('When is redered with one task', () => {
-    let initialState;
-    beforeEach(() => {
-      initialState = { tasks: [{ task: 'pass test', completed: false }] };
-      render(<TodoListComponent />, { initialState });
-    });
-    describe('And addTask button is clicked', () => {
-      test('Then newTask should be invoked', () => {
-        addTask.mockImplementationOnce(() => ({ type: actionTypes.ADD_TASK }));
-        fireEvent.click(screen.getByText(/Add task/i));
-        expect(addTask).toHaveBeenCalledTimes(1);
-      });
-    });
-  }); */
 });
