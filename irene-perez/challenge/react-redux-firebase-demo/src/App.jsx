4c0de8b68040d709c0ba5129beb65d3c4e56defd
IrenePerezAlonso <irperal@hotmail.com>
2021-05-18 18:24:49 +0200

    Add a Login button and a Lorem ipsum paragraph at App.jsx

--- irene-perez/challenge/react-redux-firebase-demo/src/App.jsx
+++ irene-perez/challenge/react-redux-firebase-demo/src/App.jsx
@@ -6,7 +6,16 @@ function App() {
     <>
       <div className="App">
         <header className="App-header">
-          <h1>Hola</h1>
+          <nav>
+            <button type="button">Login</button>
+          </nav>
+          <h1>Bienvenido</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisci elit,
+            sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrum exercitationem ullam
+            corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur.
+          </p>
         </header>
       </div>
     </>
