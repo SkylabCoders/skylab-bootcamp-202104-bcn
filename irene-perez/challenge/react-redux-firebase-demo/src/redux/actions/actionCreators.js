fa47fbe089cab96d0248923138f9a8585b5f8ea4
IrenePerezAlonso <irperal@hotmail.com>
2021-05-18 18:43:12 +0200

    Fix comma bugs

--- irene-perez/challenge/react-redux-firebase-demo/src/redux/actions/actionCreators.js
+++ irene-perez/challenge/react-redux-firebase-demo/src/redux/actions/actionCreators.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import firebase from 'firebase';
 import actionTypes from './actionTypes';
 
@@ -10,7 +11,7 @@ export function login() {
       const { user } = await firebase.auth().signInWithPopup(provider);
       dispatch({
         type: actionTypes.LOGIN,
-        userData: user,
+        userData: user
       });
     } catch (error) {
       console.log(error);
@@ -23,7 +24,7 @@ export function logout() {
     try {
       await firebase.auth().signOut();
       dispatch({
-        type: actionTypes.LOGOUT,
+        type: actionTypes.LOGOUT
       });
     } catch (error) {
       console.log(error);
