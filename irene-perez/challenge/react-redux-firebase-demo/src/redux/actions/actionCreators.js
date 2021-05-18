fa57a0d264bf43ce9d134c899753f8d2b4e170fe
IrenePerezAlonso <irperal@hotmail.com>
2021-05-18 18:36:24 +0200

    Create actionCreators file

---
+++ irene-perez/challenge/react-redux-firebase-demo/src/redux/actions/actionCreators.js
@@ -0,0 +1,32 @@
+import firebase from 'firebase';
+import actionTypes from './actionTypes';
+
+export function login() {
+  return async (dispatch) => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+
+    try {
+      const { user } = await firebase.auth().signInWithPopup(provider);
+      dispatch({
+        type: actionTypes.LOGIN,
+        userData: user,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+}
+
+export function logout() {
+  return async (dispatch) => {
+    try {
+      await firebase.auth().signOut();
+      dispatch({
+        type: actionTypes.LOGOUT,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+}
