acf1dd3c722293f2ff04cd782fd4dcb51e65c0e2
IrenePerezAlonso <irperal@hotmail.com>
2021-05-18 19:24:25 +0200

    Modify de App.jsx file

--- irene-perez/challenge/react-redux-firebase-demo/src/App.jsx
+++ irene-perez/challenge/react-redux-firebase-demo/src/App.jsx
@@ -1,25 +1,60 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+import { connect } from 'react-redux';
+import { login, logout } from './redux/actions/actionCreators';
 import './App.css';
 
-function App() {
+function App({ user, dispatch }) {
   return (
     <>
       <div className="App">
         <header className="App-header">
-          <nav>
-            <button type="button">Login</button>
+          <nav className="App-header__nav">
+            {
+              user.isLoggedIn
+                ? (
+                  <button
+                    type="button"
+                    className="nav-button button"
+                    onClick={() => dispatch(logout())}
+                  >
+                    Logout
+                  </button>
+                )
+                : (
+                  <button
+                    type="button"
+                    className="nav-button button"
+                    onClick={() => dispatch(login())}
+                  >
+                    Login
+                  </button>
+                )
+            }
           </nav>
-          <h1>Bienvenido</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisci elit,
-            sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrum exercitationem ullam
-            corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur.
-          </p>
         </header>
+        <h1>
+          Wellcome
+          {' '}
+          {user.displayName}
+          !!
+        </h1>
+        <img src={user.photoURL} alt={user.email} />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisci elit,
+          sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrum exercitationem ullam
+          corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur.
+        </p>
       </div>
     </>
   );
 }
 
-export default App;
+function mapStateToProps({ user }) {
+  return {
+    user
+  };
+}
+
+export default connect(mapStateToProps)(App);
