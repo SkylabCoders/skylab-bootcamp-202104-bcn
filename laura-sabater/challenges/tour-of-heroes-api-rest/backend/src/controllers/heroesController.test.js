2d262d100cccaf1f81a97411f5d278181a2e8c26
Laura <laura.sabater.musach@gmail.com>
2021-05-18 12:58:34 +0200

    Create copy of toh-api-connect

---
+++ laura-sabater/challenges/tour-of-heroes-api-rest/backend/src/controllers/heroesController.test.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+const heroesController = require('./heroesController');
+
+describe('getAll', () => {
+  test('shoud get all heroes', () => {
+    // arrange
+    const res = {
+      json: jest.fn(),
+    };
+
+    // act
+    const { getAll } = heroesController([{ name: 'Pepe' }]);
+    getAll(null, res);
+
+    // assert
+    expect(res.json).toHaveBeenCalledWith([{ name: 'Pepe' }]);
+  });
+});
