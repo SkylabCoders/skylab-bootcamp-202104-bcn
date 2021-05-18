a37550b6c195f591d7d90de397c83a9e629686a5
Isabel Gomez <belasi87@gmail.com>
2021-05-18 19:43:42 +0200

    Create actions and reducers

---
+++ Isabel-Gomez/challenge/firebase-demo/src/actions/actionCreators.js
@@ -0,0 +1,23 @@
+import '../firebase';
+import firebase from 'firebase';
+import actionTypes from './actionTypes';
+
+export default function login() {
+  return async (dispatch) => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+
+    try {
+      const { user } = await firebase.auth().signInWithPopup(provider);
+      dispatch({
+        type: actionTypes.LOGIN,
+        userData: user,
+      });
+    } catch (error) {
+      dispatch({
+        type: actionTypes.LOGIN_ERROR,
+        error,
+      });
+    }
+  };
+}
