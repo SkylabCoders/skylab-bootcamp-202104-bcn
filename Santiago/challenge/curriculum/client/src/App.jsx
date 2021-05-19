e67659bd6e821f8cc90373aab5f7ada9e1af74f5
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-05-19 16:44:45 +0200

    Change provider from App.jsx to index.jsx

--- Santiago/challenge/curriculum/client/src/App.jsx
+++ Santiago/challenge/curriculum/client/src/App.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import DashBoard from './components/AboutMe';
-import configureStore from './redux/store';
 
 function App() {
   return (
-    <Provider store={configureStore()}>
-      <DashBoard />
-    </Provider>
+    <DashBoard />
   );
 }
 
