1b829fc4ccb9af24e371ebbd0fb41d21285c6250
Isabel Gomez <belasi87@gmail.com>
2021-05-19 19:07:28 +0200

    Create reducer

--- Isabel-Gomez/challenge/curriculum-fullstack/client/src/actions/actionCreators.js
+++ Isabel-Gomez/challenge/curriculum-fullstack/client/src/actions/actionCreators.js
@@ -3,19 +3,19 @@ import actionTypes from './actionTypes';
 
 const { URL } = process.env;
 
-export default function loadCurriculum() {
-  return async (dispatch) => {
-    try {
-      const { data } = await axios(URL);
-      dispatch({
-        type: actionTypes.LOAD_CURRICULUM,
-        cv: data,
-      });
-    } catch (error) {
-      dispatch({
-        type: actionTypes.LOAD_ERROR,
-        error: 'ERROR',
-      });
-    }
-  };
-}
+const loadCurriculum = () => async (dispatch) => {
+  try {
+    const { data } = await axios(URL);
+    dispatch({
+      type: actionTypes.LOAD_CURRICULUM,
+      cvs: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: actionTypes.LOAD_ERROR,
+      error: 'ERROR',
+    });
+  }
+};
+
+export default loadCurriculum;
