65ab5bc2d0f1c2d4701c6cdb76e51919581b671b
gteixi <gemmateixido25@gmail.com>
2021-05-19 15:49:35 +0200

    Create Sync creation and write of files

--- gemma-teixido/lectures/node/2-fs/createFile.js
+++ gemma-teixido/lectures/node/2-fs/createFile.js
@@ -2,46 +2,33 @@ const fs = require('fs');
 const path1 = 'test/test.txt'
 const content1 = 'Skylab mola!!'
 
-const path2 = 'bcnBootcamps/ironHack.txt'
-const content2 = ''
-
 const path3 = 'bcnBootcamps/skylab.txt'
 const content3 = 'Skylab mola más!!'
 
 const path = require('path');
 
 
-fs.writeFile(path1, content1, err => {
+fs.writeFileSync(path1, content1, err => {
     if (err) {
         console.error(err);
     }
     console.log("OK");
 })
 
-fs.mkdir(path.join(__dirname, 'bcnBootcamps'), (err) => {
+fs.mkdirSync(path.join(__dirname, 'bcnBootcamps'), (err) => {
     if(err) {
         return console.error(err)
     }
     console.log('Directory created succesfully!')
 })
 
-fs.writeFile(path2,content2, err => {
+fs.writeFileSync(path3, content3, err => {
     if (err) {
         console.error(err);
     }
     console.log("OK");
 })
 
-fs.writeFile(path3, content3, err => {
-    if (err) {
-        console.error(err);
-    }
-    console.log("OK");
-})
+const value = fs.readFileSync("C:\\Users\\gemma\\Documents\\skylab-bootcamp-202104-bcn\\gemma-teixido\\lectures\\node\\2-fs\\bcnBootcamps\\skylab.txt")
+fs.writeFileSync("C:\\Users\\gemma\\Documents\\skylab-bootcamp-202104-bcn\\gemma-teixido\\lectures\\node\\2-fs\\bcnBootcamps\\ironHack.txt", value);
 
-process.stdin.on('readable', () => {
-    const chunk = process.stdin.read();
-    if (chunk !== null) {
-        process.stdout.write(chunk);
-    }
-})
