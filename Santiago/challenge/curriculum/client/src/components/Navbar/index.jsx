83bcb9b6ba953bfc5277b93edf7cd7eacb3cb87f
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-05-19 19:48:15 +0200

    Cnange a Link url and change some styles

--- Santiago/challenge/curriculum/client/src/components/Navbar/index.jsx
+++ Santiago/challenge/curriculum/client/src/components/Navbar/index.jsx
@@ -8,7 +8,7 @@ function Navbar() {
       <ul>
         <Link to="/"><li>About me</li></Link>
         <Link to="/education"><li>Education</li></Link>
-        <Link to="/Experience"><li>Experience</li></Link>
+        <Link to="/experience"><li>Experience</li></Link>
         <Link to="/more"><li>More information</li></Link>
       </ul>
     </nav>
