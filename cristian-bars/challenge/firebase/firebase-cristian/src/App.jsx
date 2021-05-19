243a7fdb2625e108eb3372b66c02f3a6a9a4ddd0
cristian-bars <patxi.cristian@gmail.com>
2021-05-19 08:58:59 +0200

    Last details of login

--- cristian-bars/challenge/firebase/firebase-cristian/src/App.jsx
+++ cristian-bars/challenge/firebase/firebase-cristian/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi Cristian</h1>
+      <Dashboard />
     </div>
   );
 }
