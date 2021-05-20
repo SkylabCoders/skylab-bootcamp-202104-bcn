ebd8d19c4376a944de9ba7deec01047dd55caa75
Edgar Alandete <edgar.sanchez.alandete@gmail.com>
2021-05-20 11:08:42 +0200

    Update model backend

--- Edgar-Alandete/challenges/cv-fullstack/server/src/model/curriculumModel.js
+++ Edgar-Alandete/challenges/cv-fullstack/server/src/model/curriculumModel.js
@@ -1,7 +1,48 @@
 const mongoose = require('mongoose');
 
 const curriculumSchema = mongoose.Schema({
-  name: String,
+
+  personalData: {
+    name: String,
+    birthDate: Date,
+    jobTitle: String,
+    phoneNumber: Number,
+    email: String,
+    description: String,
+    socialMedia: [{
+      name: String,
+      image: String,
+      url: String,
+    }],
+  },
+
+  workExperience: [{
+    companyName: String,
+    startDate: Date,
+    finishDate: Date,
+    isActual: Boolean,
+    jobDescription: String,
+  }],
+
+  studies: {
+    oficial: [{
+      school: String,
+      startDate: Date,
+      finishDate: Date,
+    }],
+    languages: [{
+      name: String,
+      level: String,
+      certification: String,
+    }],
+  },
+
+  stack: [String],
+
+  certifications: [{
+    name: String,
+  }],
+
 });
 
 module.exports = mongoose.model('Curriculums', curriculumSchema);
