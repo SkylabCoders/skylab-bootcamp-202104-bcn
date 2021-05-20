e258ea019b73601ff440970475985151d2cbbcf6
Emerson Fernandes <emerson_feral@hotmail.com>
2021-05-20 21:39:51 +0200

    Updating mongoose schema

--- emerson-fernandes/challenge-curriculum/server/src/model/curriculumModel.js
+++ emerson-fernandes/challenge-curriculum/server/src/model/curriculumModel.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose');
 
 const curriculumSchema = mongoose.Schema({
-  name: String
+  name: String,
+  avatarPicture: String,
+  contact: {
+    email: String,
+    cellphone: Number,
+    address: {
+      street: String,
+      postal: String,
+      province: String
+    }
+  }
+
 });
 
 module.exports = mongoose.model('Curriculum', curriculumSchema);
