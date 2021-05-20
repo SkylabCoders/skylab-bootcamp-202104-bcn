a9312693e225b944d04c6f4ed31843caaa66f332
Emerson Fernandes <emerson_feral@hotmail.com>
2021-05-20 10:00:16 +0200

    Redux files added and component created

--- emerson-fernandes/challenge-curriculum/client/src/App.jsx
+++ emerson-fernandes/challenge-curriculum/client/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import Main from './components/Main';
 
 function App() {
   return (
-    <div className="App">
-      HOla
+    <div>
+      <Main />
     </div>
   );
 }
