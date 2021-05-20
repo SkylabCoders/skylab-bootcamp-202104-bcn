8b940e1802fb51fe782b3ccd843a3991d0d27ae9
Fernando Gomez <shadyfast8.services@gmail.com>
2021-05-20 16:15:35 +0200

    Create custom node package

---
+++ fernando-gomez/lectures/node-practice/modules/test.js
@@ -0,0 +1,7 @@
+// const moduleApi = require('./wrapper');
+const moduleApi = require('./array');
+
+global.answer = 42;
+
+console.log(moduleApi);
+console.log(global);
---
+++ fernando-gomez/lectures/node-practice/npm/index.js
@@ -0,0 +1,6 @@
+module.exports = function print(msg) {
+    console.log('************');
+    console.log('shady printed:');
+    console.log(msg);
+    console.log('************');
+};
