e1de6e4fb023ee3e0fc7bff05cc42a0ec2fb462d
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-05-21 15:43:42 +0200

    Printing a hero detail

--- david-vazquez/challenges/angular-toh/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ david-vazquez/challenges/angular-toh/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  constructor() {
+   }
 
   ngOnInit(): void {
   }
-
 }
