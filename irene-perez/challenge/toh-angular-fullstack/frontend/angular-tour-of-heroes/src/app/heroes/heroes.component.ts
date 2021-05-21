5e08e12e59cc92aee000589d64cf42d4371c51d9
IrenePerezAlonso <irperal@hotmail.com>
2021-05-21 18:57:47 +0200

    Change the content in the heroes.component.ts file

--- irene-perez/challenge/toh-angular-fullstack/frontend/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ irene-perez/challenge/toh-angular-fullstack/frontend/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,24 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-
 export class HeroesComponent implements OnInit {
 
-  heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  getHeroes(): void {
-    this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
-  }
+  heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit() {
     this.getHeroes();
@@ -26,5 +23,11 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 }
