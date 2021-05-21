05ce04ce2c44b76466998afecacfcbb8fdbffdee
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-05-21 16:24:03 +0200

    Create hero.service

--- alfredo-zimperz/challenges/toh-angular/src/app/heroes/heroes.component.ts
+++ alfredo-zimperz/challenges/toh-angular/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,13 +9,18 @@ import { HEROES } from '../mock-heroes';
 })
 
 export class HeroesComponent implements OnInit {
+  constructor(private heroService: HeroService) {}
 
-  heroes = HEROES;
+ heroes: Hero[] = [];;
   selectedHero?: Hero;
 
-  constructor() { }
+getHeroes(): void {
+  this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+}
 
   ngOnInit() {
+this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
