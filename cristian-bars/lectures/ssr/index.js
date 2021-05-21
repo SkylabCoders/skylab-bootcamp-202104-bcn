d154d7674d46929c13d0e3651f6ae9bafef61f4b
cristian-bars <patxi.cristian@gmail.com>
2021-05-21 09:46:32 +0200

    Create a form and pass data to home

--- cristian-bars/lectures/ssr/index.js
+++ cristian-bars/lectures/ssr/index.js
@@ -3,12 +3,31 @@ const server = express();
 
 server.set('view engine', 'ejs');
 
+server.use(express.urlencoded({
+    extended: false
+}));
+
+server.use(express.json());
+
 server.get('/', (req,res)=> {
     res.render('index');
 })
 
 server.get('/about', (req,res)=> {
     res.render('about');
+})
+
+server.get('/form', (req,res)=> {
+    res.render('form');
+})
+
+
+server.post('/home', (req,res)=> {
+    const {name, surename} = req.body;
+    res.render('home', {
+        name,
+        surename
+    });
 })
 
 server.listen("4242", () => {
