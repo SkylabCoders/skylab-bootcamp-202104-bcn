9e9740c47bd6dd84bf057e0dbd62cb7e3c23cbcf
Isabel Gomez <belasi87@gmail.com>
2021-05-21 15:00:21 +0200

    Login with firebase and ssr (on going)

---
+++ Isabel-Gomez/lectures/ssr/firebase/firebase.config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: process.env.EJS_APP_apiKey,
+  authDomain: process.env.EJS_APP_authDomain,
+  projectId: process.env.EJS_APP_projectId,
+  storageBucket: process.env.EJS_APP_storageBucket,
+  messagingSenderId: process.env.EJS_APP_messagingSenderId,
+  appId: process.env.EJS_APP_appId,
+};
