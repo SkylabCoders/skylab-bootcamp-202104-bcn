7c35d78696094b4dece62b5cff2db5fda3fd8512
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-05-21 09:55:08 +0200

    Add form and home page

--- Santiago/lectures/server-sr/index.js
+++ Santiago/lectures/server-sr/index.js
@@ -5,12 +5,28 @@ const port = 3103;
 
 server.set('view engine', 'ejs');
 
+//Para parsear el body
+server.use(express.urlencoded({extended: false}));
+server.use(express.json());
+
 server.get('/', (req, res) => {
     res.render('index');
 })
 
 server.get('/about', (req, res) => {
     res.render('about');
+})
+
+server.get('/form', (req, res) => {
+    res.render('form');
+})
+
+server.post('/home', (req, res) => {
+    const { name, surname } = req.body
+    res.render('home', {
+        name,
+        surname 
+    });
 })
 
 server.listen(`${port}`, () => {
