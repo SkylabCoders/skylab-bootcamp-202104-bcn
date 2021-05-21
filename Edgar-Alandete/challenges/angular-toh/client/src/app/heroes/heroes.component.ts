5016a40fed32698ae1d19914db708bcde422e538
Edgar Alandete <edgar.sanchez.alandete@gmail.com>
2021-05-21 16:36:20 +0200

    Finish second section of tutorial

--- Edgar-Alandete/challenges/angular-toh/client/src/app/heroes/heroes.component.ts
+++ Edgar-Alandete/challenges/angular-toh/client/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from '../hero'
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -12,9 +13,11 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  hero : Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
 
 }
