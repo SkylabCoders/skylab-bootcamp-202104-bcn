14f386132a2a9ca3573ade3734e83bc7b3892a63
IrenePerezAlonso <irperal@hotmail.com>
2021-05-21 18:49:46 +0200

    Fix the hero.service.ts file

--- irene-perez/challenge/toh-angular-fullstack/frontend/angular-tour-of-heroes/src/app/hero.service.ts
+++ irene-perez/challenge/toh-angular-fullstack/frontend/angular-tour-of-heroes/src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,9 @@ export class HeroService {
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 }
