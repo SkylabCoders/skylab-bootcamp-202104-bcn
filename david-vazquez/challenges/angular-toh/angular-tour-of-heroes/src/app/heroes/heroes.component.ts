40eb32862523c7efaa2742c1115a4587aaf960d0
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-05-21 18:04:02 +0200

    Detail linked to dashboard and list, and go back button

--- david-vazquez/challenges/angular-toh/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ david-vazquez/challenges/angular-toh/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,24 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
-
-  selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-  this.messageService.add(`HeroesComponent: Selected hero id=${hero.id} and name=${hero.name}`);
   
-}
-
-  constructor(private heroService: HeroService, private messageService: MessageService) {
+  constructor(private heroService: HeroService) {
    }
    getHeroes(): void {
     this.heroService.getHeroes()
