fd8a5f88bdae0fdbf154d800a80183e39b3ad7a2
Laura <laura.sabater.musach@gmail.com>
2021-05-21 16:57:35 +0200

    Import mock heroes in heroes compoentn

--- laura-sabater/challenges/tour-of-heroes-angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ laura-sabater/challenges/tour-of-heroes-angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes'
 
 @Component({
   selector: 'app-heroes',
@@ -7,10 +7,7 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  }
+  heroes = HEROES;
   constructor() { }
 
   ngOnInit(): void {
