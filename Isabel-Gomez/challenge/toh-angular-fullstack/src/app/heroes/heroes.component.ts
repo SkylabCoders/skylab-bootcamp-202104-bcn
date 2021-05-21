199f60da24c2cb45a3277a1e334a4bd1d56206ff
Isabel Gomez <belasi87@gmail.com>
2021-05-21 17:33:27 +0200

    Print heroes list and detail with input

---
+++ Isabel-Gomez/challenge/toh-angular-fullstack/src/app/heroes/heroes.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  constructor() { }
+  
+  ngOnInit(): void {
+    
+  }
+
+}
