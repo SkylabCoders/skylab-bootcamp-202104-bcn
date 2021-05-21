00d24f6c0e2559f0f5a8218d4fbc1a485a6bca71
cristian-bars <patxi.cristian@gmail.com>
2021-05-21 15:35:47 +0200

    Add a hero and show in the page

--- cristian-bars/challenge/toh-angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ cristian-bars/challenge/toh-angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -3,10 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.sass']
+  styleUrls: ['./heroes.component.sass'],
+  
 })
 export class HeroesComponent implements OnInit {
 
+  hero = 'Windstorm';
   constructor() { }
 
   ngOnInit(): void {
