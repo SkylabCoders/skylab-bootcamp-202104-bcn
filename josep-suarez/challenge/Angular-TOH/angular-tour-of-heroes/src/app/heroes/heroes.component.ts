9f65103f67e767cdb264c46b994349a140c6f2f2
Josep Suarez <djose.anton@gmail.com>
2021-05-21 16:36:07 +0200

    message component created and running

--- josep-suarez/challenge/Angular-TOH/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ josep-suarez/challenge/Angular-TOH/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,24 +11,23 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
 
+  selectedHero?: Hero;
+
   heroes: Hero[] = [];
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  selectedHero?: Hero;
-onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-}
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
-  constructor(private heroService: HeroService) {}
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
   }
-  ngOnInit(): void {
-    this.getHeroes()
-  }
-
 }
