765051170d0ca3ae02d3a3ba7257b35029f90bb6
Laura <laura.sabater.musach@gmail.com>
2021-05-21 16:43:01 +0200

    Remove AppRoutingModule from app module

--- laura-sabater/challenges/tour-of-heroes-angular/angular-tour-of-heroes/src/app/app.module.ts
+++ laura-sabater/challenges/tour-of-heroes-angular/angular-tour-of-heroes/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
@@ -12,8 +11,7 @@ import { HeroesComponent } from './heroes/heroes.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
