9f83f937b642f0d3ccf6b66a58c633ce853821f7
Hosni <hosni.marco@gmail.com>
2021-05-21 16:28:36 +0200

    Ssr without firebase

---
+++ hosni-marco/challenge/ssr/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const server = express();
+
+server.set('view engine', 'ejs');
+
+server.use('/public', express.static('public'))
+
+server.use(express.urlencoded({extended: false}));
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.render('index');
+});
+
+server.get('/about', (req, res) => {
+    res.render('about');
+});
+
+server.get('/form', (req, res) => {
+    res.render('form');
+});
+
+server.post('/home', (req, res) => {
+    const { name, surname } = req.body;
+    res.render('home', {
+        name,
+        surname
+    });
+});
+
+server.listen(4242, () => {
+    console.log('Express server is runing... ')
+});
