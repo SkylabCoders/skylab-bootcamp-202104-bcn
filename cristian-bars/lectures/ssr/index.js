572578bcb062442754bf0c1b336ce3ded5a7522f
cristian-bars <patxi.cristian@gmail.com>
2021-05-21 09:26:36 +0200

    Create ssr

---
+++ cristian-bars/lectures/ssr/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const server = express();
+
+server.set('view engine', 'ejs');
+
+server.get('/', (req,res)=> {
+    res.render('index');
+})
+
+server.get('/about', (req,res)=> {
+    res.render('about');
+})
+
+server.listen("4242", () => {
+    console.log('Express Server is running...')
+})
