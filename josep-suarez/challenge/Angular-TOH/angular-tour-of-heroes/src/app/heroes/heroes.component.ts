f8aeeb129c3b2966a5736810b532b267ab1b14ec
Josep Suarez <djose.anton@gmail.com>
2021-05-21 16:29:14 +0200

    observable and subscribes created on hero component

--- josep-suarez/challenge/Angular-TOH/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ josep-suarez/challenge/Angular-TOH/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock.heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,7 +9,7 @@ import { HEROES } from '../mock.heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[] = [];
 
   hero: Hero = {
     id: 1,
@@ -20,8 +20,13 @@ onSelect(hero: Hero): void {
   this.selectedHero = hero;
 }
 
-  constructor() { }
+  constructor(private heroService: HeroService) {}
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
   ngOnInit(): void {
+    this.getHeroes()
   }
 
 }
