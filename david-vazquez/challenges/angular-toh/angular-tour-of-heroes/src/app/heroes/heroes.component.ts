76d80c3c20c52510f8bbc9164d7fc13e1bc50fd7
David Vazquez <76447293+davzlum@users.noreply.github.com>
2021-05-21 16:17:05 +0200

    Can click and see details of each hero

--- david-vazquez/challenges/angular-toh/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ david-vazquez/challenges/angular-toh/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -9,7 +9,12 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
-  
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
+
   constructor() {
    }
 
