e6dca152ad000385f05cd9128374e25f9290030c
IrenePerezAlonso <irperal@hotmail.com>
2021-05-21 16:41:13 +0200

    Delete AppRoutingModule in the app.module.ts

--- irene-perez/challenge/toh-angular-fullstack/frontend/angular-tour-of-heroes/src/app/app.module.ts
+++ irene-perez/challenge/toh-angular-fullstack/frontend/angular-tour-of-heroes/src/app/app.module.ts
@@ -1,11 +1,9 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +12,6 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule
   ],
   providers: [],
