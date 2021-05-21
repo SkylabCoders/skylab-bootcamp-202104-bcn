a4d8e4cb62fc56c85e93e7145132754a280b33ac
Laura <laura.sabater.musach@gmail.com>
2021-05-21 19:13:48 +0200

    Add additional messages to hero service

--- laura-sabater/challenges/tour-of-heroes-angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ laura-sabater/challenges/tour-of-heroes-angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,10 +12,11 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
