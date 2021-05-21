23c7b93f94f8b1ec1aabe19e54f5d8aac67a31b7
Edgar Alandete <edgar.sanchez.alandete@gmail.com>
2021-05-21 17:44:28 +0200

    Finish add service section·

--- Edgar-Alandete/challenges/angular-toh/client/src/app/heroes/heroes.component.ts
+++ Edgar-Alandete/challenges/angular-toh/client/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,14 +10,22 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
-  heroes = HEROES;
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+  
+  heroes: Hero[] = [];  
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
 
