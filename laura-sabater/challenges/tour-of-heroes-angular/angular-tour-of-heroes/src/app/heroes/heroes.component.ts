914e6aecf1c69c64b4deed40b71f4a3f6a7ab002
Laura <laura.sabater.musach@gmail.com>
2021-05-21 18:48:16 +0200

    Create observable HeroService

--- laura-sabater/challenges/tour-of-heroes-angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ laura-sabater/challenges/tour-of-heroes-angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
 
 @Component({
@@ -8,15 +8,21 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor() { }
-  
-  ngOnInit(): void {
-  }
+  constructor(private heroService: HeroService) {}
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+  
+  ngOnInit() {
+    this.getHeroes();
   }
 }
