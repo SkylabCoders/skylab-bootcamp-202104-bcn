cb3688440d2da5722d31afd7e380008a2b1644e4
Laura <laura.sabater.musach@gmail.com>
2021-05-21 16:34:44 +0200

    Import FormsModule in app.module

--- laura-sabater/challenges/tour-of-heroes-angular/angular-tour-of-heroes/src/app/app.module.ts
+++ laura-sabater/challenges/tour-of-heroes-angular/angular-tour-of-heroes/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -12,6 +12,7 @@ import { HeroesComponent } from './heroes/heroes.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
