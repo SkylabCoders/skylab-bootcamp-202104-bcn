a963917943fb8a50a211c2c431051375f05ec974
gteixi <gemmateixido25@gmail.com>
2021-05-21 17:50:03 +0200

    Display list and selected hero

--- gemma-teixido/challenge/tour-of-heroes-angular/src/app/app.module.ts
+++ gemma-teixido/challenge/tour-of-heroes-angular/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { HeroesComponent } from './heroes/heroes.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    HeroesComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
