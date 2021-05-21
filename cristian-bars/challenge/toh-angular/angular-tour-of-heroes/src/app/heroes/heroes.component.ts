5392fb2266df6992f00f75769ca42e15685ac934
cristian-bars <patxi.cristian@gmail.com>
2021-05-21 15:58:42 +0200

    Create a heroes list and apply styles

--- cristian-bars/challenge/toh-angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ cristian-bars/challenge/toh-angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Hero } from './../hero';
 import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -13,6 +14,7 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Windstorm'
   };
+  heroes = HEROES;
   
   constructor() { }
 
