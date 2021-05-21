df739c3f9ef36391f62975e6fde1fa87b848e17a
gteixi <gemmateixido25@gmail.com>
2021-05-21 09:59:33 +0200

    Create a form with post and get

--- gemma-teixido/lectures/ssr/index.js
+++ gemma-teixido/lectures/ssr/index.js
@@ -3,8 +3,28 @@ const server = express();
 
 server.set('view engine', 'ejs');
 
+server.use(express.urlencoded({extended:false}))
+
+server.use(express.json());
+
 server.get('/', (req,res) => {
     res.render('index');
+})
+
+server.get('/about', (req,res) => {
+    res.render('about');
+})
+
+server.get('/form', (req,res) => {
+    res.render('form');
+})
+
+server.post('/home', (req,res) => {
+    const {name, surname} = req.body
+    res.render('home', {
+        name,
+        surname
+    });
 })
 
 server.listen(2105, () => {
