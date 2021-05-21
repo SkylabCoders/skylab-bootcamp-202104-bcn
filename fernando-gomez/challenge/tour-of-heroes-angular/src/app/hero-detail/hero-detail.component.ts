23738de685fd95153efe5d29471b4bd969e92475
Fernando Gomez <shadyfast8.services@gmail.com>
2021-05-21 17:15:48 +0200

    Add messaging to app

--- fernando-gomez/challenge/tour-of-heroes-angular/src/app/hero-detail/hero-detail.component.ts
+++ fernando-gomez/challenge/tour-of-heroes-angular/src/app/hero-detail/hero-detail.component.ts
@@ -4,7 +4,7 @@ import { Hero } from '../hero';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css'],
+  styleUrls: ['./hero-detail.component.scss'],
 })
 export default class HeroDetailComponent implements OnInit {
   @Input() hero?: Hero;
--- fernando-gomez/challenge/tour-of-heroes-angular/src/app/mock-heroes.ts
+++ fernando-gomez/challenge/tour-of-heroes-angular/src/app/mock-heroes.ts
@@ -11,6 +11,7 @@ const HEROES: Hero[] = [
   { id: 18, name: 'Dr IQ' },
   { id: 19, name: 'Magma' },
   { id: 20, name: 'Tornado' },
+  { id: 21, name: 'Lefty' },
 ];
 
 export default HEROES;
