7a107f9a138ea7dade2e01b6248cc828f74ebeae
gteixi <gemmateixido25@gmail.com>
2021-05-22 11:26:04 +0200

    Create a new component for hero-detail

--- gemma-teixido/challenge/tour-of-heroes-angular/src/app/app.module.ts
+++ gemma-teixido/challenge/tour-of-heroes-angular/src/app/app.module.ts
@@ -1,21 +1,22 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HeroesComponent } from './heroes/heroes.component';
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent
+  ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  declarations: [
-    AppComponent,
-    HeroesComponent
-  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-
 export class AppModule { }
