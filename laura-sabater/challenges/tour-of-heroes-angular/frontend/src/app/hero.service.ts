c45ecf8f51b8bf16f869adbd86e0f099a72b406d
Laura <laura.sabater.musach@gmail.com>
2021-05-22 15:15:40 +0200

    Get heroes with HttpClient

--- laura-sabater/challenges/tour-of-heroes-angular/frontend/src/app/hero.service.ts
+++ laura-sabater/challenges/tour-of-heroes-angular/frontend/src/app/hero.service.ts
@@ -3,17 +3,21 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class HeroService {
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'http://localhost:2021/heroes'
 
-  getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
-  }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService,
+  ) { }
   
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
+  }
+
   getHero(id: number): Observable<Hero> {
     // For now, assume that a hero with the specified `id` always exists.
     // Error handling will be added in the next step of the tutorial.
