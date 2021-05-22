d0519b89fe0d0423a2e7dfc86b505728a9620820
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-05-22 20:40:45 +0200

    Add navigation

--- Santiago/challenge/angular-toh/src/app/hero-detail/hero-detail.component.ts
+++ Santiago/challenge/angular-toh/src/app/hero-detail/hero-detail.component.ts
@@ -1,17 +1,37 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./styles/hero-detail.component.scss']
 })
+
 export class HeroDetailComponent implements OnInit {
+  hero: Hero | undefined;
 
-  @Input() hero?: Hero;
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {
+    this.getHero();
+  }
 
-  ngOnInit() {
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 }
+
