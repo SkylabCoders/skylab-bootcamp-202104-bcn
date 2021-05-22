53a48893a86eb6870f0e4bbb6bba60f550fc3c4e
IrenePerezAlonso <irperal@hotmail.com>
2021-05-22 11:24:26 +0200

    Redirect to dashboard into a empty rute

--- irene-perez/challenge/toh-angular-fullstack/frontend/angular-tour-of-heroes/src/app/app-routing.module.ts
+++ irene-perez/challenge/toh-angular-fullstack/frontend/angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent },
-  { path: 'dashboard', component: DashboardComponent }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
 
 @NgModule({
