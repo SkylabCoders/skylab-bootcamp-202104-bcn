c3087868a1453bcb7d3a8730316b704e974494d6
CheCabeza <chemartinc@gmail.com>
2021-05-22 17:49:53 +0200

    finish client side app

--- chema-cabeza/challenge/toh-angular/client/src/app/app.module.ts
+++ chema-cabeza/challenge/toh-angular/client/src/app/app.module.ts
@@ -4,16 +4,22 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
