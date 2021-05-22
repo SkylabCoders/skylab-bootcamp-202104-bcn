8200ac85dae71fd5b49b6d618dd8702a8b989953
Josep Suarez <djose.anton@gmail.com>
2021-05-22 17:10:59 +0200

    Structure with typescript ok

---
+++ josep-suarez/challenge/react-native/Cryptomonedas/App.tsx
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return <Text>Fredy mercury</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
