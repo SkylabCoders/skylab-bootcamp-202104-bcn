901722d425c41b72917612441be1e3e599c69827
Laura <laura.sabater.musach@gmail.com>
2021-05-22 11:43:33 +0200

    Add a hero detail route

--- laura-sabater/challenges/tour-of-heroes-angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ laura-sabater/challenges/tour-of-heroes-angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -10,21 +10,15 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
-  constructor(private heroService: HeroService, private messageService: MessageService) {}
+  constructor(private heroService: HeroService) {}
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  ngOnInit() {
+    this.getHeroes();
   }
 
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
-  }
-  
-  ngOnInit() {
-    this.getHeroes();
-  }
+  } 
 }
