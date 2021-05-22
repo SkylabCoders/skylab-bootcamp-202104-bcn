3be61df6fba10edd1780485eca3c44347b4592e7
Isabel Gomez <belasi87@gmail.com>
2021-05-22 12:43:09 +0200

    Create a service

--- Isabel-Gomez/challenge/toh-angular-fullstack/src/app/heroes/heroes.component.ts
+++ Isabel-Gomez/challenge/toh-angular-fullstack/src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
-
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -10,13 +9,18 @@ import { HEROES } from '../mock-heroes';
 
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) {}
 
   ngOnInit() {
-  }
+  this.getHeroes();
+}
+
+  getHeroes(): void {
+  this.heroes = this.heroService.getHeroes();
+}
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
