9f4d28412d4ccbd7a025364cf8b641be0883a9ba
Isabel Gomez <belasi87@gmail.com>
2021-05-22 12:45:24 +0200

    Create a message component

--- Isabel-Gomez/challenge/toh-angular-fullstack/src/app/hero.service.ts
+++ Isabel-Gomez/challenge/toh-angular-fullstack/src/app/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
@@ -7,8 +8,9 @@ import { HEROES } from './mock-heroes';
 })
 export class HeroService {
 
-  getHeroes(): Hero[] {
-  return HEROES;
+  getHeroes(): Observable<Hero[]> {
+  const heroes = of(HEROES);
+  return heroes;
 }
   constructor() { }
 }
--- Isabel-Gomez/challenge/toh-angular-fullstack/src/app/heroes/heroes.component.ts
+++ Isabel-Gomez/challenge/toh-angular-fullstack/src/app/heroes/heroes.component.ts
@@ -19,7 +19,8 @@ export class HeroesComponent implements OnInit {
 }
 
   getHeroes(): void {
-  this.heroes = this.heroService.getHeroes();
+  this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
 }
 
   onSelect(hero: Hero): void {
