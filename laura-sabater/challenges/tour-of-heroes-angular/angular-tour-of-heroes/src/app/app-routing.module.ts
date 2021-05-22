037dfb24374719157c4585e0de7815ef99fabe14
Laura <laura.sabater.musach@gmail.com>
2021-05-22 11:28:03 +0200

    Add a default route

--- laura-sabater/challenges/tour-of-heroes-angular/angular-tour-of-heroes/src/app/app-routing.module.ts
+++ laura-sabater/challenges/tour-of-heroes-angular/angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent }
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
 ];
 
