39f6c2fc155a38fb5b1533fb15992a5326897ed8
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2021-05-22 11:55:16 +0200

    Modify service to fecth from API

--- alfredo-zimperz/challenges/toh-angular/client/src/app/app.module.ts
+++ alfredo-zimperz/challenges/toh-angular/client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -14,7 +15,8 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   declarations: [
     AppComponent,
