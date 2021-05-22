0f98526b0e2c4ee0bd6493233c574426f5fe6155
IrenePerezAlonso <irperal@hotmail.com>
2021-05-22 16:00:31 +0200

    Add addHero() into the hero.service.ts

--- irene-perez/challenge/toh-angular-fullstack/frontend/angular-tour-of-heroes/src/app/hero.service.ts
+++ irene-perez/challenge/toh-angular-fullstack/frontend/angular-tour-of-heroes/src/app/hero.service.ts
@@ -44,6 +44,13 @@ export class HeroService {
     );
   }
 
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
