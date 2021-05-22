28bcf952120dfa72bec8270aeae991620605405e
IrenePerezAlonso <irperal@hotmail.com>
2021-05-22 15:44:47 +0200

    Fix the hero.service.ts file

--- irene-perez/challenge/toh-angular-fullstack/frontend/angular-tour-of-heroes/src/app/hero.service.ts
+++ irene-perez/challenge/toh-angular-fullstack/frontend/angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Observable, of } from 'rxjs';
 
@@ -18,15 +19,32 @@ export class HeroService {
 
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_ => this.log('fetched heroes')),
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
   }
 
   getHero(id: number): Observable<Hero> {
-    const hero = HEROES.find(h => h.id === id)!;
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(hero);
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      console.error(error); 
+  
+      this.log(`${operation} failed: ${error.message}`);
+  
+      return of(result as T);
+    };
   }
 }
