e6aeaf65bdb1c9f8bdf61a6165088f3f9a08399e
Laura <laura.sabater.musach@gmail.com>
2021-05-22 12:02:30 +0200

    Reorganize hero service

--- laura-sabater/challenges/tour-of-heroes-angular/angular-tour-of-heroes/src/app/hero.service.ts
+++ laura-sabater/challenges/tour-of-heroes-angular/angular-tour-of-heroes/src/app/hero.service.ts
@@ -4,15 +4,16 @@ import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class HeroService {
+  constructor(private messageService: MessageService) { }
+
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
+  
   getHero(id: number): Observable<Hero> {
     // For now, assume that a hero with the specified `id` always exists.
     // Error handling will be added in the next step of the tutorial.
@@ -20,5 +21,4 @@ export class HeroService {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(hero);
   }
-  constructor(private messageService: MessageService) { }
 }
