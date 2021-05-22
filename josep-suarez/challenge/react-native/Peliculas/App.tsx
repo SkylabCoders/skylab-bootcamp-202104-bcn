330bbcf8be446afd3fb23ddffac66cba9193191c
Josep Suarez <djose.anton@gmail.com>
2021-05-22 17:46:35 +0200

    project with typescript

---
+++ josep-suarez/challenge/react-native/Peliculas/App.tsx
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return <Text>Hola</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
