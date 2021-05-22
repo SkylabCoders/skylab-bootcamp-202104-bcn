091fc89f601e649d5907c899dfdcf7790d988295
Fernando Gomez <shadyfast8.services@gmail.com>
2021-05-22 13:38:26 +0200

    Create Vue project

--- fernando-gomez/challenge/tour-of-heroes-fullStack/frontend-vue/README.md
+++ fernando-gomez/challenge/tour-of-heroes-fullStack/frontend-vue/README.md
@@ -1,4 +1,4 @@
-# frontend-vue
+# pfrontend-vue
 
 ## Project setup
 ```
--- fernando-gomez/challenge/tour-of-heroes-fullStack/frontend-vue/src/App.vue
+++ fernando-gomez/challenge/tour-of-heroes-fullStack/frontend-vue/src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld/index.vue'
 
 export default {
   name: 'App',
