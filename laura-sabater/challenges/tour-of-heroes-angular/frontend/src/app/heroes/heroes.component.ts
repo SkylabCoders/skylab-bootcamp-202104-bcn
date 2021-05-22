bdf3b0ad27919e1346ae5a46ed77f17d259a020a
Laura <laura.sabater.musach@gmail.com>
2021-05-22 17:54:25 +0200

    Add add and delete functions in hero component

--- laura-sabater/challenges/tour-of-heroes-angular/frontend/src/app/heroes/heroes.component.ts
+++ laura-sabater/challenges/tour-of-heroes-angular/frontend/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HeroService } from '../hero.service';
+
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,7 +11,7 @@ import { Hero } from '../hero';
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
@@ -18,6 +19,21 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
-  } 
+    .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero._id).subscribe();
+  }
+
 }
