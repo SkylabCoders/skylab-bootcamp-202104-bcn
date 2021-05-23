f3f1b83e5ee3ab398aa09c28e00ff8ffe84a1768
Isabel Gomez <belasi87@gmail.com>
2021-05-23 12:28:02 +0200

    Create backend arquitecture

---
+++ Isabel-Gomez/challenge/toh-angular-fullstack/server/src/controllers/heroesController.js
@@ -0,0 +1,59 @@
+const Hero = require('../model/heroModel');
+
+const heroesController = () => {
+  async function getAll(req, res) {
+    const heroes = await Hero.find();
+    res.json(heroes);
+  }
+
+  async function createOne(req, res) {
+    const newHero = Hero(req.body);
+    try {
+      await newHero.save();
+      res.json(newHero);
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  async function getById(req, res) {
+    try {
+      const heroById = await Hero.findById(req.params.heroId);
+      res.json(heroById);
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  async function updateById(req, res) {
+    try {
+      const updatedHeroById = await Hero.findByIdAndUpdate(
+        req.params.heroId,
+        req.body,
+        { new: true },
+      );
+      res.json(updatedHeroById);
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  async function deleteById(req, res) {
+    try {
+      await Hero.findOneAndDelete(req.params.heroId);
+      res.json();
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  return {
+    getAll,
+    createOne,
+    getById,
+    updateById,
+    deleteById,
+  };
+};
+
+module.exports = heroesController;
