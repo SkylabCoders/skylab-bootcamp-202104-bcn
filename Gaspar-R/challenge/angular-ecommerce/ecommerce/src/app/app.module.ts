4ad115311b546f9f3b507ef92a9f42846932a7eb
Gaspar Rodriguez <gaspar.unlp@gmail.com>
2021-05-23 21:33:06 +0200

    Delete comments Function refactored in product-list.component.html

--- Gaspar-R/challenge/angular-ecommerce/ecommerce/src/app/app.module.ts
+++ Gaspar-R/challenge/angular-ecommerce/ecommerce/src/app/app.module.ts
@@ -38,9 +38,3 @@ import { ShippingComponent } from './shipping/shipping.component';
   providers: [CartService]
 })
 export class AppModule {}
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
