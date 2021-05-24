70005e0fbc4dd915e027ce50e6d6c9769b91301c
Gilberto Cao <gilbe.cao@gmail.com>
2021-05-24 15:23:58 +0200

    Define router and outlet

---
+++ gilbe-cao/tour-of-heroes/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { HeroDetailComponent } from "./components/hero-detail/hero-detail.component";
+import { HeroesComponent } from "./components/heroes/heroes.component";
+
+const routes: Routes = [
+    { path: '', component: DashboardComponent},
+    { path: 'heroes', component: HeroesComponent},
+    { path: 'detail', component: HeroDetailComponent},
+    { path: '**', redirectTo: '/', pathMatch: 'full'}
+]
+
+@NgModule({
+    imports:[RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- gilbe-cao/tour-of-heroes/src/app/app.module.ts
+++ gilbe-cao/tour-of-heroes/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
