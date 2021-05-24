43663b1aa9e33ba0cdaba225fff755a143919d00
cristian-bars <patxi.cristian@gmail.com>
2021-05-24 17:13:48 +0200

    Create a angular project

--- cristian-bars/lectures/tour-of-heroes/src/app/app.module.ts
+++ cristian-bars/lectures/tour-of-heroes/src/app/app.module.ts
@@ -1,11 +1,15 @@
+import { HeroesComponent } from './heroes/heroes.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule
