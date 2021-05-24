c5a3624dee1cd356f266cde191f54f11b2ebdff8
Josep Suarez <djose.anton@gmail.com>
2021-05-24 15:30:09 +0200

    dashboard component created and running with anchors

--- josep-suarez/lectures/angular/tour-of-heroes-angular/src/app/app-routing.module.ts
+++ josep-suarez/lectures/angular/tour-of-heroes-angular/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from '@angular/router'
 import { HeroesComponent } from "./components/heroes/heroes.component";
 
 const routes: Routes = [
-    { path: '', component:HeroesComponent},
+    { path: '', component:DashboardComponent},
+    { path: 'heroes', component: HeroesComponent},
     { path: 'detail', component: HeroDetailComponent},
-    { path: '**', redirectTo: '/', pathMatch: ''}
+    { path: '**', redirectTo: '/', pathMatch: 'full'}
 ]
 
 @NgModule({
--- josep-suarez/lectures/angular/tour-of-heroes-angular/src/app/app.module.ts
+++ josep-suarez/lectures/angular/tour-of-heroes-angular/src/app/app.module.ts
@@ -5,12 +5,14 @@ import {FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
