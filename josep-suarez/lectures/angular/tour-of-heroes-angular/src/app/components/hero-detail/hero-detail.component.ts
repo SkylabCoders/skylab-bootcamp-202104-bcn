562b25fe58f59a1cf4ccee07917f0b72a8029a70
Josep Suarez <djose.anton@gmail.com>
2021-05-24 17:05:10 +0200

    angular lecture finished

--- josep-suarez/lectures/angular/tour-of-heroes-angular/src/app/components/hero-detail/hero-detail.component.ts
+++ josep-suarez/lectures/angular/tour-of-heroes-angular/src/app/components/hero-detail/hero-detail.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import {Hero} from '../../hero'
+import {HEROES} from '../../assets/hero-mock'
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero!: Hero;
-  constructor() { }
+  hero?: Hero;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const {heroId} = this.route.snapshot.params;
+    this.hero = HEROES.find(hero => hero.id === +heroId)
   }
 
 }
--- josep-suarez/lectures/angular/tour-of-heroes-angular/src/app/components/heroes/heroes.component.ts
+++ josep-suarez/lectures/angular/tour-of-heroes-angular/src/app/components/heroes/heroes.component.ts
@@ -1,24 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../../hero'
-
+import {HEROES} from '../../assets/hero-mock'
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[] = [
-    { id: 11, name: 'Dr Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-    ]
+  heroes = HEROES
     selectedHero!: Hero;
   constructor() { }
 
