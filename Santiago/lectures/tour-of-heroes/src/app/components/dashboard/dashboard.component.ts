0cb221bb27711bc884fcbcc4ab15c36040a319ce
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-05-24 16:11:39 +0200

    Add dashboard

--- Santiago/lectures/tour-of-heroes/src/app/components/dashboard/dashboard.component.ts
+++ Santiago/lectures/tour-of-heroes/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HEROES } from 'src/app/constants/heroes-mock';
+import { Hero } from './../../hero';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  heroes?: Hero[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.heroes = HEROES.slice(1,5);
   }
 
 }
