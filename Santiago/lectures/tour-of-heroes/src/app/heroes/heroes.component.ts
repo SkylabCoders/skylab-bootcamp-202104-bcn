3559879c1bcb63911aaab8274d5b39e47605fb35
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-05-24 14:42:50 +0200

    Create heroe list and desplay its details

---
+++ Santiago/lectures/tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from './../constants/heroes-mock'
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent {
+
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+}
