d9693b11eff9dd0914afb81635be03d7870ac1f0
Isabel Gomez <belasi87@gmail.com>
2021-05-24 19:59:18 +0200

    Add css style

--- Isabel-Gomez/challenge/holapisos-angular/client/src/app/core/models/flat.ts
+++ Isabel-Gomez/challenge/holapisos-angular/client/src/app/core/models/flat.ts
@@ -3,6 +3,6 @@ export interface Flat {
     attributes: {field_inmu_imag_arra: [string],
     field_inmu_tipo_via: [string],
     field_inmu_nomb_call: [string],
-    field_inmu_prec: [string]},
-    type: string,
+    field_inmu_prec: [string]
+    field_inmu_tipo_sin_agru: [string]},
 }
