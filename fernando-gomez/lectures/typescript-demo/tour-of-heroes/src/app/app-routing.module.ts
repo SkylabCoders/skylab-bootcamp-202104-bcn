7c81cb76d8b68b8091cc78c8d43031c469029d5d
Fernando Gomez <shadyfast8.services@gmail.com>
2021-05-24 15:30:34 +0200

    Add routing between dashboard and heroes list

--- fernando-gomez/lectures/typescript-demo/tour-of-heroes/src/app/app-routing.module.ts
+++ fernando-gomez/lectures/typescript-demo/tour-of-heroes/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',  component: DashboardComponent},
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'detail', component: HeroDetailComponent },
+  { path: '**', redirectTo: '/', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- fernando-gomez/lectures/typescript-demo/tour-of-heroes/src/app/app.module.ts
+++ fernando-gomez/lectures/typescript-demo/tour-of-heroes/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
