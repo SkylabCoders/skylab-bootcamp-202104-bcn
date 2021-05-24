2f1511afb8d9f92298363fe1bf3402708c4601d2
Isabel Gomez <belasi87@gmail.com>
2021-05-24 21:01:58 +0200

    Add roms and meters number and sfinish css styles

--- Isabel-Gomez/challenge/holapisos-angular/client/src/app/core/models/flat.ts
+++ Isabel-Gomez/challenge/holapisos-angular/client/src/app/core/models/flat.ts
@@ -4,5 +4,8 @@ export interface Flat {
     field_inmu_tipo_via: [string],
     field_inmu_nomb_call: [string],
     field_inmu_prec: [string]
-    field_inmu_tipo_sin_agru: [string]},
+    field_inmu_tipo_sin_agru: [string],
+    field_inmu_area_cons: [string],
+    field_inmu_nume_habi: [number]
+},
 }
