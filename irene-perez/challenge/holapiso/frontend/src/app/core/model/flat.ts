714d76f535131ccef113fa01ae7f4cc4a728aae3
IrenePerezAlonso <irperal@hotmail.com>
2021-05-24 19:32:17 +0200

    Print img of the flats

--- irene-perez/challenge/holapiso/frontend/src/app/core/model/flat.ts
+++ irene-perez/challenge/holapiso/frontend/src/app/core/model/flat.ts
@@ -1,13 +1,20 @@
+export interface Flat {
+    id: string,
+    attributes: {
+        field_inmu_imag_arra: [string],
+        field_inmu_prec: string,
+        field_inmu_tipo_via: string,
+        field_inmu_nomb_call: string,
+        field_inmu_tipo_sin_agru: string
+    }
+    type: string,
+    price: string,
+    address: string
+}
+
 export interface ApiData {
     jsonapi: object,
     data: [],
     meta: object,
     links: object
-}
-export interface Flat {
-    id: string,
-    img: string,
-    type: string,
-    price: string,
-    address: string
 }
