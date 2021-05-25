8451c5e56858d4a71a5de52cefc158eeb2177091
Laura <laura.sabater.musach@gmail.com>
2021-05-25 15:28:01 +0200

    Register the reactive forms module

--- laura-sabater/challenges/tour-of-heroes-angular-forms/toh-pt6/src/app/app.module.ts
+++ laura-sabater/challenges/tour-of-heroes-angular-forms/toh-pt6/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
 import { MessagesComponent } from './messages/messages.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
@@ -21,6 +22,7 @@ import { MessagesComponent } from './messages/messages.component';
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
+    ReactiveFormsModule,
 
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
     // and returns simulated server responses.
