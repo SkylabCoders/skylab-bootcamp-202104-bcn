ad0bbe901c8e83870f2366fc792af03add6cdf66
IrenePerezAlonso <irperal@hotmail.com>
2021-05-25 15:48:22 +0200

    Import ReactiveFormsModule into the app.module.ts

--- irene-perez/challenge/toh-angular-mock/frontend/src/app/app.module.ts
+++ irene-perez/challenge/toh-angular-mock/frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
@@ -22,6 +23,7 @@ import { NameEditorComponent } from './name-editor/name-editor.component';
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
+    ReactiveFormsModule,
 
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
     // and returns simulated server responses.
