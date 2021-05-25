9d105ce4cbc41ced72a21a04a022e49886f97afb
Anna Sala Mercade <anna@Annas-MacBook-Pro.local>
2021-05-25 11:49:27 +0200

    Add httpClient to app.module.ts

--- anna-sala/challenges/anticipa-angular/src/app/app.module.ts
+++ anna-sala/challenges/anticipa-angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
@@ -9,7 +10,8 @@ import { ListComponent } from './list/list.component';
     AppComponent,
     ListComponent
   ],
-  imports: [
+  imports: [ 
+    HttpClientModule,
     BrowserModule
   ],
   providers: [],
