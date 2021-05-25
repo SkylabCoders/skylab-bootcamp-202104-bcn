8037a2c329c392484f412d7eabb012fa60534d33
cristian-bars <patxi.cristian@gmail.com>
2021-05-25 17:05:28 +0200

    Add alias input and accion to create new alias

--- cristian-bars/challenge/toh-angular-tutorial/src/app/components/profile-editor/profile-editor.component.ts
+++ cristian-bars/challenge/toh-angular-tutorial/src/app/components/profile-editor/profile-editor.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-editor',
@@ -8,14 +10,45 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class ProfileEditorComponent implements OnInit {
 
-  profileForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
+  profileForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: [''],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: ['']
+    }),
+    aliases: this.fb.array([
+      this.fb.control('')
+    ])
   });
   
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+  
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.profileForm.value);
+  }
+  
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Cristian',
+      address: {
+        street: 'Casa meva'
+      }
+    });
+  }
+  
+  get aliases() {
+    return this.profileForm.get('aliases') as FormArray;
+  }
+  
+  addAlias() {
+    this.aliases.push(this.fb.control(''));
   }
 
 }
