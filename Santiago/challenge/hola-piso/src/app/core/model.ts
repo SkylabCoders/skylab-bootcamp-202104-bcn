658e0e12f7a402b9be0d77e37346b3bffe6abff5
Santiago.Martinez <41674380j@institutmontilivi.cat>
2021-05-25 11:51:00 +0200

    Add property in model and display it in the web

--- Santiago/challenge/hola-piso/src/app/core/model.ts
+++ Santiago/challenge/hola-piso/src/app/core/model.ts
@@ -5,7 +5,8 @@ export interface Flat {
         field_inmu_prec: string,
         field_inmu_tipo_via: string,
         field_inmu_nomb_call: string,
-        field_inmu_tipo_sin_agru: string
+        field_inmu_tipo_sin_agru: string,
+        field_inmu_pobl: string
     }
 }
 
