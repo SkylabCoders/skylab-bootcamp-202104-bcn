a31bab7878021b3401b01a1b98a2ad6edbfabafd
IrenePerezAlonso <irperal@hotmail.com>
2021-05-25 16:20:06 +0200

    Add an address nested element into the form group into the profile-editor.component.ts

--- irene-perez/challenge/toh-angular-mock/frontend/src/app/profile-editor/profile-editor.component.ts
+++ irene-perez/challenge/toh-angular-mock/frontend/src/app/profile-editor/profile-editor.component.ts
@@ -10,6 +10,12 @@ export class ProfileEditorComponent implements OnInit {
   profileForm = new FormGroup({
     firstName: new FormControl(''),
     lastName: new FormControl(''),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl(''),
+      state: new FormControl(''),
+      zip: new FormControl('')
+    })
   });
 
   constructor() { }
