7b371ddd5256165bd19eeee2366071cb9b217aec
ulldecorb <ausias.bertran@gmail.com>
2021-05-25 18:00:44 +0200

    Add validating form input

--- ausias-bertran/lecture/toh-pt6/src/app/profile-editor/profile-editor.component.ts
+++ ausias-bertran/lecture/toh-pt6/src/app/profile-editor/profile-editor.component.ts
@@ -1,21 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-profile-editor',
   templateUrl: './profile-editor.component.html',
   styleUrls: ['./profile-editor.component.css']
 })
-export class ProfileEditorComponent implements OnInit {
-
-  constructor() { }
-
-  profileForm = new FormGroup({
-  firstName: new FormControl(''),
-  lastName: new FormControl(''),
+export class ProfileEditorComponent {
+    profileForm = this.fb.group({
+    firstName: ['',Validators.required],
+    lastName: [''],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: ['']
+    }),
   });
 
-  ngOnInit(): void {
-  }
+constructor(private fb: FormBuilder) { }
+
+onSubmit() {
+  // TODO: Use EventEmitter with form value
+  console.warn(this.profileForm.value);
+}
 
 }
