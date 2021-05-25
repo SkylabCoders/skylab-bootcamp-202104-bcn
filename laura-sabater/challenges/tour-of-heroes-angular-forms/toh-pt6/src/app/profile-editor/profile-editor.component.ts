53e491e552af11cb7275f240bec7309e2d4e920c
Laura <laura.sabater.musach@gmail.com>
2021-05-25 16:59:59 +0200

    Add FormBuilder service to generate controls

--- laura-sabater/challenges/tour-of-heroes-angular-forms/toh-pt6/src/app/profile-editor/profile-editor.component.ts
+++ laura-sabater/challenges/tour-of-heroes-angular-forms/toh-pt6/src/app/profile-editor/profile-editor.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-editor',
@@ -7,18 +8,18 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./profile-editor.component.css']
 })
 export class ProfileEditorComponent implements OnInit {
-  profileForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    address: new FormGroup({
-      street: new FormControl(''),
-      city: new FormControl(''),
-      state: new FormControl(''),
-      zip: new FormControl('')
-    })
+  profileForm = this.fb.group({
+    firstName: [''],
+    lastName: [''],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: ['']
+    }),
   });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   onSubmit() {
     // TODO: Use EventEmitter with form value
