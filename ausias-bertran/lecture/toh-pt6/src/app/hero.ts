7c52fe955d9d92f1549fa7fef7186031845b55d2
ulldecorb <ausias.bertran@gmail.com>
2021-05-25 21:00:54 +0200

    Add update form functionality

--- ausias-bertran/lecture/toh-pt6/src/app/hero.ts
+++ ausias-bertran/lecture/toh-pt6/src/app/hero.ts
@@ -1,4 +1,12 @@
 export interface Hero {
   id: number;
   name: string;
+  appearance: {
+    gender: string;
+    race: string;
+    height: [string];
+    weight: [string];
+    eyeColor: string;
+    hairColor: string;
+  }
 }
--- ausias-bertran/lecture/toh-pt6/src/app/profile-editor/profile-editor.component.ts
+++ ausias-bertran/lecture/toh-pt6/src/app/profile-editor/profile-editor.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Validators } from '@angular/forms';
+import { FormArray } from '@angular/forms';
+import { Hero } from '../hero';
+
+import { HeroService } from '../hero.service';
 
 
 @Component({
@@ -8,19 +12,44 @@ import { Validators } from '@angular/forms';
   templateUrl: './profile-editor.component.html',
   styleUrls: ['./profile-editor.component.css']
 })
+  
 export class ProfileEditorComponent {
-    profileForm = this.fb.group({
-    firstName: ['',Validators.required],
-    lastName: [''],
-    address: this.fb.group({
-      street: [''],
-      city: [''],
-      state: [''],
-      zip: ['']
-    }),
-  });
 
-constructor(private fb: FormBuilder) { }
+  @Input() hero!: Hero;
+
+      profileForm = this.fb.group({
+    id: ['',Validators.required],
+    name: [''],
+    address: this.fb.group({
+      gender: [''],
+      race: [''],
+      height: [''],
+      weight: [''],
+      eyeColor: [''],
+      hairColor: ['']
+    }),
+
+  aliases: this.fb.array([
+    this.fb.control('')
+  ])
+    });
+
+  constructor(
+    private fb: FormBuilder,
+    private heroService: HeroService
+  ) { }
+
+  
+
+ 
+  
+  
+  get aliases() {
+  return this.profileForm.get('aliases') as FormArray;
+  }
+  addAlias() {
+  this.aliases.push(this.fb.control(''));
+}
 
 onSubmit() {
   // TODO: Use EventEmitter with form value
