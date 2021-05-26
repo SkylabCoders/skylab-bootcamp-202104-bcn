0ed14aa18464d3503a58be3e812c5df3f547b673
Fernando Gomez <shadyfast8.services@gmail.com>
2021-05-26 14:42:27 +0200

    Add backend to project

---
+++ fernando-gomez/lectures/toh-react-passport/backend/src/controllers/heroesController.js
@@ -0,0 +1,69 @@
+const debug = require('debug')('app:heroesController');
+const Hero = require('../model/heroModel');
+
+function heroesController() {
+  async function getAll(req, res) {
+    const heroes = await Hero.find({});
+    res.json(heroes);
+  }
+
+  async function createOne(req, res) {
+    const newHero = new Hero(req.body);
+    debug(newHero);
+    try {
+      await newHero.save();
+      res.json(newHero);
+    } catch (error) {
+      debug(error);
+      res.send(error);
+    }
+  }
+
+  async function getById(req, res) {
+    try {
+      const heroById = await Hero.findById(
+        req.params.heroId,
+      );
+      res.json(heroById);
+    } catch (error) {
+      debug(error);
+      res.status(404);
+      res.send(error);
+    }
+  }
+
+  async function updateById(req, res) {
+    try {
+      const updatedHero = await Hero.findByIdAndUpdate(
+        req.params.heroId,
+        req.body,
+        { new: true },
+      );
+      res.json(updatedHero);
+    } catch (error) {
+      debug(error);
+      res.send(error);
+    }
+  }
+
+  async function deleteById(req, res) {
+    try {
+      await Hero.findByIdAndDelete(req.params.heroId);
+      res.status(204);
+      res.json();
+    } catch (error) {
+      debug(error);
+      res.send(error);
+    }
+  }
+
+  return {
+    getAll,
+    createOne,
+    getById,
+    updateById,
+    deleteById,
+  };
+}
+
+module.exports = heroesController;
