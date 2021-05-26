b4bd9d6213af66ec39b8b0ccc532b8828503aa4b
Gaspar Rodriguez <gaspar.unlp@gmail.com>
2021-05-26 23:31:28 +0200

    Passport connected with login and working

---
+++ Gaspar-R/challenge/toh-mongoDB/server/src/controllers/usersController.js
@@ -0,0 +1,73 @@
+const debug = require('debug')('server:heroesController');
+const chalk = require('chalk');
+const User = require('../model/userModel');
+
+function heroesController() {
+  async function getAll(req, res) {
+    debug(`dentro de la función ${chalk.magenta('getAll')}`);
+    const heroes = await User.find();
+    res.json(heroes);
+  }
+
+  async function createOne(req, res) {
+    const newUser = new User(req.body);
+
+    try {
+      await newUser.save();
+      res.json(newUser);
+    } catch (error) {
+      res.send(error);
+      debug(error);
+    }
+  }
+
+  async function getById(req, res) {
+    const { userId } = req.params;
+    try {
+      const userById = await User.findById(userId);
+      res.status(200);
+      res.json(userById);
+    } catch (error) {
+      res.status(404);
+      res.send(`The hero with the id ${userId} doesn't exist`);
+      debug(error);
+    }
+  }
+
+  async function deleteById(req, res) {
+    const { userId } = req.params;
+    try {
+      await User.findByIdAndDelete(userId);
+      res.status(204);
+      res.json();
+    } catch (error) {
+      res.status(404);
+      debug(error);
+    }
+  }
+
+  async function updateById(req, res) {
+    const { userId } = req.params;
+    const updateData = req.body;
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        userId,
+        updateData,
+        { new: true }
+      );
+      res.json(updatedUser);
+    } catch (error) {
+      debug(error);
+    }
+  }
+
+  return {
+    getAll,
+    createOne,
+    getById,
+    deleteById,
+    updateById
+  };
+}
+
+module.exports = heroesController;
--- Gaspar-R/challenge/toh-mongoDB/server/src/index.js
+++ Gaspar-R/challenge/toh-mongoDB/server/src/index.js
@@ -1,21 +1,45 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable max-len */
 const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
 const cors = require('cors');
+const morgan = require('morgan');
 const chalk = require('chalk');
 const debug = require('debug')('server');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
+
 require('dotenv').config();
-const heroesRouter = require('./routes/heroesRouter');
+require('./passport/local.strategy')(passport);
+
+const auth = require('connect-ensure-login');
+require('dotenv').config();
 
 const server = express();
 
-server.set('view engine', 'ejs');
-server.use(express.urlencoded({ extended: false })); // para parsear el body
-server.use('/public', express.static('public')); // para que utilice el css que esta en public.
+const port = 2025;
+
+/** *************esto lo copie de internet, y escuche que habia que usarlo, pero no se donde ni como ni para que.*********** */
+
+//   server.use(session({
+//     secret: 'caca',
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: { maxAge: 60 * 60 * 1000 } // 1 hour
+//   }));
+
+// // Passport Local Strategy
+// passport.use(User.createStrategy());
+
+// // To use with sessions
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+// server.use('/heroes', heroesRouter);
+
+//* ****************************** */
 
 try {
-  // Connect to the MongoDB cluster
+  // Connect to the MongoDB cluster w/ try catch and debug message//
   mongoose.connect(
     process.env.DDBB_URL,
     { useNewUrlParser: true, useUnifiedTopology: true },
@@ -25,32 +49,42 @@ try {
   debug('could not connect');
 }
 
-server.get('/', (req, res) => { // for rendering index
-  res.render('index.ejs');
-});
+server.use(cors());
+server.set('view engine', 'ejs');
+server.use(morgan('tiny'));
+server.use(express.json());
 
-server.get('/about', (req, res) => { // for rendering about
-  res.render('about.ejs');
-});
+server.use(express.urlencoded({ extended: false })); // para parsear el body
+server.use(require('body-parser').urlencoded({ extended: true })); // parser middleware
+server.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false })); // session middleware
 
-server.get('/form', (req, res) => {
-  res.render('form.ejs');
-});
+server.use(passport.initialize());
+server.use(passport.session());
+server.use('/views', express.static('views')); // para que utilice el css que esta en views(si tuviera).
+
+server.get('/',
+  auth.ensureLoggedIn({ redirectTo: '/loginForm' }),
+  (req, res) => {
+    res.render('index', { user: req.user });
+  });
+
+server.post('/loginForm',
+  passport.authenticate('local', {
+    successRedirect: '/home',
+    failureRedirect: '/loginForm'
+  }));
 
 server.post('/home', (req, res) => {
-  const { name, surname } = req.body; // renderiza home con los datos que se le pasan del formulario (req.body)
+  const { username, password } = req.body; // renderiza home con los datos que se le pasan del formulario (req.body)
   res.render('home.ejs', {
-    name,
-    surname
+    username,
+    password
   });
 });
 
-server.use(cors());
+server.get('/', (req, res) => res.render('index')); // for rendering index
+server.get('/loginForm', (req, res) => res.render('loginForm'));
+server.get('/home', (req, res) => res.render('home'));
 
-server.use(express.json());
-server.use(morgan('tiny'));
-
-server.use('/heroes', heroesRouter);
-
-server.listen('2022',
-  () => debug(`Server is running in ${chalk.magentaBright('2022')}`));
+server.listen(port,
+  () => debug(`Server is running in ${chalk.magentaBright(port)}`));
