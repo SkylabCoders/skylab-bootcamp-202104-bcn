ac10a1090c5ecf45c815fb5158d5f66feed26998
Josep Suarez <djose.anton@gmail.com>
2021-05-26 16:30:04 +0200

    navegacion ok

--- josep-suarez/lectures/passport/backend/src/index.js
+++ josep-suarez/lectures/passport/backend/src/index.js
@@ -5,8 +5,13 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 require('dotenv').config();
-
 const server = express();
+let passport = require('passport');
+let LocalStrategy = require('passport-local').Strategy;
+
+server.use(express.urlencoded({extended: false}))
+server.use(express.json())
+server.set('view engine', 'ejs');
 
 mongoose.connect(
   process.env.DDBB_URL,
@@ -20,10 +25,45 @@ server.use(cors());
 server.use(express.json());
 server.use(morgan('dev'));
 
-const todosRouter = require('./routes/todosRouter.js');
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
 
-server.use('/todos', todosRouter);
-server.post('/login',(req,res) => {
-  res.send('login works!')
+passport.serializeUser(function (user,cb){
+  cb(null,user)
+})
+
+passport.deserializeUser(function(id, cb){
+  User.findById(id, function(err,user){
+    if(err) {return cb(err)}
+    cb(null,user)
+  })
+})
+
+server.use(passport.initialize());
+server.use(passport.session())
+server.use('views', express.static('views'))
+
+server.post('/login',
+  passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login', failureFlash: true })
+);
+
+server.get('/',(req, res)=>{
+  res.render('index');
 });
+server.get('/login',(req, res)=>{
+  res.render('login');
+});
+
 server.listen('2021',() => debug(`Server is running in ${chalk.yellow('DDBB_URL')}`));
