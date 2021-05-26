bb7118b57ea7f7adea5fdc017c54f574a3a23826
Fernando Gomez <shadyfast8.services@gmail.com>
2021-05-26 18:45:37 +0200

    Add user model and methods to interact with passport

--- fernando-gomez/lectures/toh-react-passport/backend/index.js
+++ fernando-gomez/lectures/toh-react-passport/backend/index.js
@@ -4,9 +4,20 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-require('dotenv').config();
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const auth = require('connect-ensure-login');
+const User = require('./src/model/userModel');
 
 const server = express();
+
+require('dotenv').config();
+
+server.use(express.urlencoded({ extended: false }));
+server.set('view engine', 'ejs');
+server.use(cors());
+server.use(express.json());
+server.use(morgan('dev'));
 
 mongoose.connect(
   process.env.DDBB_URL,
@@ -16,16 +27,65 @@ mongoose.connect(
   },
 );
 
-server.use(cors());
-server.use(express.json());
-server.use(morgan('dev'));
-
 const heroesRouter = require('./src/routes/heroesRouter');
+
+server.get('/', (req, res) => {
+  res.redirect('/login');
+});
+
+server.get('/login', (req, res) => {
+  res.render('index');
+});
+
+server.post('/logout', (req, res) => {
+  res.redirect('/login');
+});
 
 server.use('/heroes', heroesRouter);
 
-server.post('/login', (req, res) => {
-  res.send('Login works!');
+passport.use(new LocalStrategy(
+  (username, password, done) => {
+    User.findOne({ username }, (err, user) => {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (!user.verifyPassword(password)) { return done(null, false); }
+      return done(null, user);
+    });
+  },
+));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+server.use(passport.initialize());
+server.use(passport.session());
+server.use('views', express.static('views'));
+
+server.post('/login',
+  passport.authenticate('local', {
+    successRedirect: '/user',
+    failureRedirect: '/login',
+    failureFlash: true,
+  }));
+
+server.get('/',
+  auth.ensureLoggedIn({ redirectTo: '/login' }),
+  (req, res) => {
+    res.render('index', { user: req.user });
+  });
+server.get('/login', (req, res) => {
+  res.render('login');
+});
+server.get('/user', (req, res) => {
+  res.render('loggedIn');
 });
 
 server.listen('2021',
---
+++ fernando-gomez/lectures/toh-react-passport/backend/src/controllers/usersController.js
@@ -0,0 +1,35 @@
+const User = require('../model/userModel');
+
+function usersController() {
+  async function getAll(req, res) {
+    const users = await User.find();
+    res.json(users);
+  }
+  async function createOne(req, res) {
+    const newTask = new User(req.body);
+    try {
+      await newTask.save();
+      res.json(newTask);
+    } catch (error) {
+      res.send(error);
+    }
+  }
+  async function getById(req, res) {
+    try {
+      const userById = await User.findById(
+        req.params.userById,
+      );
+      res.json(userById);
+    } catch (error) {
+      res.send(error);
+    }
+  }
+
+  return {
+    getAll,
+    createOne,
+    getById,
+  };
+}
+
+module.exports = usersController;
