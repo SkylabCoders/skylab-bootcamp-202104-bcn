83afcfbda79c63ece2b929147b1509de91f3041c
Gaspar Rodriguez <gaspar.unlp@gmail.com>
2021-05-26 16:43:57 +0200

    Added ejs files

--- Gaspar-R/challenge/toh-mongoDB/server/src/index.js
+++ Gaspar-R/challenge/toh-mongoDB/server/src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 const express = require('express');
 const cors = require('cors');
 const chalk = require('chalk');
@@ -5,8 +6,13 @@ const debug = require('debug')('server');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const heroesRouter = require('./routes/heroesRouter');
 
 const server = express();
+
+server.set('view engine', 'ejs');
+server.use(express.urlencoded({ extended: false })); // para parsear el body
+server.use('/public', express.static('public')); // para que utilice el css que esta en public.
 
 try {
   // Connect to the MongoDB cluster
@@ -19,12 +25,30 @@ try {
   debug('could not connect');
 }
 
+server.get('/', (req, res) => { // for rendering index
+  res.render('index');
+});
+
+server.get('/about', (req, res) => { // for rendering about
+  res.render('about');
+});
+
+server.get('/form', (req, res) => {
+  res.render('form');
+});
+
+server.post('/home', (req, res) => {
+  const { name, surname } = req.body; // renderiza home con los datos que se le pasan del formulario (req.body)
+  res.render('home', {
+    name,
+    surname
+  });
+});
+
 server.use(cors());
 
 server.use(express.json());
 server.use(morgan('tiny'));
-
-const heroesRouter = require('./routes/heroesRouter');
 
 server.use('/heroes', heroesRouter);
 
