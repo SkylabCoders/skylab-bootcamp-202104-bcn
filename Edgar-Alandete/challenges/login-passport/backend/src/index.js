747efaebf08f485c2a04de2bcfac82b62ff8371f
Edgar Alandete <edgar.sanchez.alandete@gmail.com>
2021-05-26 18:13:05 +0200

    Login with passport

--- Edgar-Alandete/challenges/login-passport/backend/src/index.js
+++ Edgar-Alandete/challenges/login-passport/backend/src/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const cors = require('cors');
-const chalk = require('chalk');
-const debug = require('debug')('server');
-const morgan = require('morgan');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const auth = require('connect-ensure-login');
+const passportLocal = require('passport-local');
+const User = require('./model/userModel');
 require('dotenv').config();
 
+const LocalStrategy = passportLocal.Strategy;
 const server = express();
 
 mongoose.connect(
@@ -15,12 +16,55 @@ mongoose.connect(
     useNewUrlParser: true,
   },
 );
-server.use(cors());
+
+server.set('view engine', 'ejs');
+
+passport.use(new LocalStrategy(
+  (username, password, done) => {
+    User.findOne({ username }, (err, user) => {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (!user.verifyPassword(password)) { return done(null, false); }
+      return done(null, user);
+    });
+  },
+));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+server.use(express.urlencoded({ extended: false }));
 server.use(express.json());
+server.use(require('morgan')('combined'));
+server.use(require('body-parser').urlencoded({ extended: true }));
+server.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 
-server.use(morgan('tiny'));
-const heroesRouter = require('./routes/heroesRouter');
+server.use(passport.initialize());
+server.use(passport.session());
+server.use('/views', express.static('views'));
 
-server.use('/heroes', heroesRouter);
+server.get('/',
+  auth.ensureLoggedIn({ redirectTo: '/login' }),
+  (req, res) => {
+    res.render('index', { user: req.user });
+  });
 
-server.listen('2021', () => debug(`Server is running in ${chalk.blue('2021')}`));
+server.post('/login',
+  passport.authenticate('local', {
+    successRedirect: '/home',
+    failureRedirect: '/login',
+  }));
+
+server.get('/', (req, res) => res.render('index'));
+server.get('/login', (req, res) => res.render('login'));
+server.get('/home', (req, res) => res.render('home'));
+
+server.listen('2021', () => console.log('Server is running in localhost:2021'));
---
+++ Edgar-Alandete/challenges/login-passport/backend/src/model/userModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  username: String,
+  password: String,
+
+});
+
+userSchema.methods.verifyPassword = function (password) {
+  return password === this.password;
+};
+
+module.exports = mongoose.model('Users', userSchema);
