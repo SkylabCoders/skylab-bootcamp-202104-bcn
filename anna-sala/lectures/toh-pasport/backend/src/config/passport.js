5eeb52a3e25765b55c7f1e07d76f504716aacb89
Anna Sala Mercade <anna@Annas-MacBook-Pro.local>
2021-05-27 07:25:35 +0200

    Push to code review errors

---
+++ anna-sala/lectures/toh-pasport/backend/src/config/passport.js
@@ -0,0 +1,16 @@
+// const passport = require('passport')();
+// require('./local.strategy');
+// const User = require('../model/userModel');
+
+// module.exports = function passportConfig(server) {
+//   server.use(passport.initialize());
+//   server.use(passport.session());
+//   passport.serializeUser((user, done) => {
+//     done(null, user.id);
+//   });
+//   passport.deserializeUser((id, done) => {
+//     User.findById(id, (err, user) => {
+//       done(err, user);
+//     });
+//   });
+// };
