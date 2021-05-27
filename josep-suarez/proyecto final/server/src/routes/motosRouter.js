b9eab988d4ee42524e9fcc626d43fe02234728e9
Josep Suarez <djose.anton@gmail.com>
2021-05-27 17:57:33 +0200

    routes created

---
+++ josep-suarez/proyecto final/server/src/routes/motosRouter.js
@@ -0,0 +1,36 @@
+const { Router } = require('express');
+const motosController = require('../controllers/motosController')();
+
+function motosRouter() {
+  const routes = Router();
+  routes
+    .route('/')
+    .get(motosController.getAll);
+
+  routes
+    .route('/racing')
+    .get(motosController.getByType);
+  routes
+    .route('/custom')
+    .get(motosController.getByType);
+  routes
+    .route('/scooter')
+    .get(motosController.getByType);
+  routes
+    .route('/vespa')
+    .get(motosController.getByType);
+  routes
+    .route('/accesorios')
+    .get(motosController.getByType);
+  routes
+    .route('/ofertas')
+    .get(motosController.getByType);
+
+  routes
+    .route('/:motoId')
+    .get(motosController.deleteById);
+
+  return routes;
+}
+
+module.exports = motosRouter();
