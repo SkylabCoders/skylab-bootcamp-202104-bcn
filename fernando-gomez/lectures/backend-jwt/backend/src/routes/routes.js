b060fd2c9adc46874686a55d0e7fb52e50cd3c83
Fernando Gomez <shadyfast8.services@gmail.com>
2021-05-27 17:34:12 +0200

    Add passport jwt backend

---
+++ fernando-gomez/lectures/backend-jwt/backend/src/routes/routes.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-underscore-dangle */
+const express = require('express');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const router = express.Router();
+
+router.post(
+  '/login',
+  async (req, res, next) => {
+    passport.authenticate(
+      'login',
+      async (err, user, info) => {
+        try {
+          if (err || !user) {
+            const error = new Error('An error occurred.');
+
+            return next(error);
+          }
+
+          req.login(
+            user,
+            { session: false },
+            async (error) => {
+              if (error) return next(error);
+
+              const body = { _id: user._id, email: user.email };
+              const token = jwt.sign({ user: body }, 'TOP_SECRET');
+
+              return res.json({ token });
+            },
+          );
+        } catch (error) {
+          return next(error);
+        }
+        return err; // by default returns an error WARNING
+      },
+    )(req, res, next);
+  },
+);
+
+module.exports = router;
