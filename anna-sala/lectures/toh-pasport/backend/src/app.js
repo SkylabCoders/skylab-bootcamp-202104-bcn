3284ab9b024ed88d3c01fe6324f067007bf3af23
Anna Sala Mercade <anna@Annas-MacBook-Pro.local>
2021-05-27 22:59:01 +0200

    Delete old files toh-passport

--- anna-sala/lectures/toh-pasport/backend/src/app.js
+++
@@ -1,9 +0,0 @@
-app.configure(() => {
-  app.use(express.static('public'));
-  app.use(express.cookieParser());
-  app.use(express.bodyParser());
-  app.use(express.session({ secret: 'keyboard cat' }));
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(app.router);
-});
--- anna-sala/lectures/toh-pasport/backend/src/config/user.mock.js
+++
@@ -1,6 +0,0 @@
-const USERS = {
-  email: 'annasm@mail.com',
-  password: '1234',
-};
-
-module.exports = USERS;
