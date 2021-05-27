ed22b1b285d29df86a26aa1a0c10f0dda69767d3
Isabel Gomez <belasi87@gmail.com>
2021-05-27 16:03:32 +0200

    Create new project and insatl all dependencies

---
+++ Isabel-Gomez/lectures/toh-passport-jwt/src/models/model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const UserModel = mongoose.model('user', UserSchema);
+
+module.exports = UserModel;
---
+++ Isabel-Gomez/lectures/toh-passport-jwt/src/passport/strategies/local.strategy.js
@@ -0,0 +1,50 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const UserModel = require('../../models/model');
+
+passport.use(
+  'signup',
+  new LocalStrategy.Stratergy(
+    {
+      usernameField: 'email',
+      passwordField: 'password',
+    },
+    async (email, password, done) => {
+      try {
+        const user = await UserModel.create({ email, password });
+        return done(null, user);
+      } catch (error) {
+        done(error);
+      }
+    },
+  ),
+);
+
+passport.use(
+  'login',
+  new LocalStrategy.Stratergy(
+    {
+      usernameField: 'email',
+      passwordField: 'password',
+    },
+    async (email, password, done) => {
+      try {
+        const user = await UserModel.findOne({ email });
+
+        if (!user) {
+          return done(null, false, { message: 'User not found' });
+        }
+
+        const validate = await user.isValidPassword(password);
+
+        if (!validate) {
+          return done(null, false, { message: 'Wrong Password' });
+        }
+
+        return done(null, user, { message: 'Logged in Successfully' });
+      } catch (error) {
+        return done(error);
+      }
+    },
+  ),
+);
