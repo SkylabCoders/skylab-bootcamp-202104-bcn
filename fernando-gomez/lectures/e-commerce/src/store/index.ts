8189815b8c675aa86c419841f41a75d58a989d53
Fernando Gomez <shadyfast8.services@gmail.com>
2021-05-27 22:05:31 +0200

    Create Vuex store

--- fernando-gomez/lectures/e-commerce/src/store/index.ts
+++ fernando-gomez/lectures/e-commerce/src/store/index.ts
@@ -2,8 +2,20 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    count: 0,
+  },
+  getters: {
+    getCount(state) {
+      return state.count;
+    },
   },
   mutations: {
+    increaseOne(state) {
+      state.count += 1;
+    },
+    decreaseOne(state) {
+      state.count -= 1;
+    },
   },
   actions: {
   },
