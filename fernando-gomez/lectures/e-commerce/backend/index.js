21122206328da2b8c8a2ebf1c9869fb844926632
Fernando Gomez <shadyfast8.services@gmail.com>
2021-05-28 13:00:19 +0200

    Add backend jwt routes

---
+++ fernando-gomez/lectures/e-commerce/backend/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const passport = require('passport');
+const authRoutes = require('./src/routes/auth.routes');
+const userRoutes = require('./src/routes/user.routes');
+
+require('dotenv').config();
+
+require('./src/passport/passport.config');
+
+require('./src/database/mongoose.config');
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', authRoutes);
+app.use(
+  '/user',
+  passport.authenticate('jwt', { session: false }),
+  userRoutes,
+);
+
+app.listen(port, debug(`server is running on port ${port}`));
