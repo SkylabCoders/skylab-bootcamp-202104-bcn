ce55eaf26f192594ea16371199973c66bab464f8
Fernando Gomez <shadyfast8.services@gmail.com>
2021-05-28 10:30:02 +0200

    Add asyncronous charge of data from API

--- fernando-gomez/lectures/e-commerce/frontend/src/components/Counter.vue
+++ fernando-gomez/lectures/e-commerce/frontend/src/components/Counter.vue
@@ -3,16 +3,24 @@
     <p>{{ getCount }}</p>
     <button type="button" @click="increaseOne">+</button>
     <button type="button" @click="decreaseOne">-</button>
+    <p>Pokemons:</p>
+    <ul v-for='pokemon in getPokemons' :key="pokemon">
+        <li>{{pokemon}}</li>
+    </ul>
 </template>
 
 <script lang='ts'>
 import { defineComponent } from 'vue';
-import { mapGetters, mapMutations } from 'vuex';
+import {
+  mapActions,
+  mapGetters, mapMutations, useStore,
+} from 'vuex';
 
 export default defineComponent({
   computed: {
     ...mapGetters([
       'getCount',
+      'getPokemons',
     ]),
   },
   methods: {
@@ -20,10 +28,16 @@ export default defineComponent({
       'increaseOne',
       'decreaseOne',
     ]),
+    ...mapActions([
+      'getPokemonsFromApi',
+    ]),
   },
   setup() {
-    return { };
+    const store = useStore();
+    store.dispatch('getPokemonsFromApi');
+    return {};
   },
+
 });
 </script>
 
--- fernando-gomez/lectures/e-commerce/frontend/src/store/index.ts
+++ fernando-gomez/lectures/e-commerce/frontend/src/store/index.ts
@@ -1,12 +1,17 @@
 import { createStore } from 'vuex';
+import axios from 'axios';
 
 export default createStore({
   state: {
     count: 0,
+    pokemons: [{ 1: 'hola' }],
   },
   getters: {
     getCount(state) {
       return state.count;
+    },
+    getPokemons(state) {
+      return state.pokemons;
     },
   },
   mutations: {
@@ -16,8 +21,17 @@ export default createStore({
     decreaseOne(state) {
       state.count -= 1;
     },
+    updatePokemons(state: any, payload) {
+      state.pokemons = [...payload];
+    },
   },
   actions: {
+    getPokemonsFromApi({ commit }) {
+      axios.get('https://pokeapi.co/api/v2/pokemon/').then((response) => {
+        console.log(response.data);
+        commit('updatePokemons', response.data.results);
+      });
+    },
   },
   modules: {
   },
