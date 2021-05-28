c51add64d77f48a3535b5944bc50ff36d0aaa0a9
Josep Suarez <djose.anton@gmail.com>
2021-05-28 12:29:29 +0200

    all data comes ok from mongoose, withmodel

--- josep-suarez/proyecto final/server/src/models/motosModel.js
+++ josep-suarez/proyecto final/server/src/models/motosModel.js
@@ -1,18 +1,7 @@
 const mongoose = require('mongoose');
 
 const motosSchema = mongoose.Schema({
-  motos: [{
-    type: String,
-    data: [
-      {
-        marca: String,
-        modelo: String,
-        cc: String,
-        descripcion: String,
-        precio: Number,
-        stock: Number
-      }]
-  }]
+  motos: [Object]
 });
 
 module.exports = mongoose.model('Motos', motosSchema);
