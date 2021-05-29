460d9454bac36d1cc852af727519917853fd8a7a
Fernando Gomez <shadyfast8.services@gmail.com>
2021-05-29 21:23:58 +0200

    Add user and product model

--- fernando-gomez/lectures/e-commerce/backend/src/models/user.model.js
+++ fernando-gomez/lectures/e-commerce/backend/src/models/user.model.js
@@ -3,6 +3,9 @@ const { model, Schema } = require('mongoose');
 const userSchema = Schema({
   email: String,
   password: String,
+  address: String,
+  whislist: [Schema.Types.ObjectId],
+  cart: [Schema.Types.ObjectId],
 });
 
 userSchema.methods.isValidPassword = function isValidPassword(password) {
