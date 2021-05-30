95027106864fadbe4dd73864f8ff5cb0fa52fbae
gteixi <gemmateixido25@gmail.com>
2021-05-30 09:13:52 +0200

    Delete file

--- gemma-teixido/lectures/ssr/firebase/firebase.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    apiKey: "AIzaSyCHHCcg1Z02aXn_F0oiyW-PbRRPeMLKdf0",
-    authDomain: "gemma-ssr.firebaseapp.com",
-    projectId: "gemma-ssr",
-    storageBucket: "gemma-ssr.appspot.com",
-    messagingSenderId: "394564084464",
-    appId: "1:394564084464:web:93bb162043b20f522f9835"
-  };
