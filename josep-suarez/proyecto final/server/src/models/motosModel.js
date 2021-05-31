74e81a235ed7a893d493189a52164630297fb1da
Josep Suarez <djose.anton@gmail.com>
2021-05-31 09:02:13 +0200

    data in postman ok

--- josep-suarez/proyecto final/server/src/models/motosModel.js
+++ josep-suarez/proyecto final/server/src/models/motosModel.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 
 const motosSchema = mongoose.Schema({
-  motos: [Object]
+  tipo: String,
+  marca: String,
+  modelo: String,
+  cc: String,
+  descripcion: String,
+  precio: Number,
+  stock: Number
 });
 
 module.exports = mongoose.model('Motos', motosSchema);
